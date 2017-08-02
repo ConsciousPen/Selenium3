811ddaf20bd1abac2eae60c7e63a8219a7eb8025
Anton Perapecha <aperapecha@eisgroup.com>
2017-08-01 19:00:56 -0700

    Update

--- aaa-automation-tests/src/test/java/aaa/modules/example/TestSingleTest1.java
+++ aaa-automation-tests/src/test/java/aaa/modules/example/TestSingleTest1.java
@@ -1,14 +1,18 @@
 package aaa.modules.example;
 
+import java.time.LocalDateTime;
+
 import org.testng.annotations.Test;
 
 import aaa.helpers.Groups;
 import aaa.modules.BaseTest;
+import toolkit.utils.datetime.DateTimeUtils;
 
 public class TestSingleTest1 extends BaseTest {
 
 	@Test(groups = {Groups.SMOKE, Groups.AUTO_SS})
 	public void test1() {
 		log.info("TestSingleGroups1");
+		LocalDateTime.parse("08/02/2017", DateTimeUtils.MM_DD_YYYY);
 	}
 }
