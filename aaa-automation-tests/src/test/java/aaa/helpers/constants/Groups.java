9db88eccd0004fbd59609836c920facdf0a34833
dchubkov <dchubkov@eisgroup.com>
2017-08-10 14:20:28 +0300

    added MEDIUM severity

--- aaa-automation-tests/src/test/java/aaa/helpers/constants/Groups.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/constants/Groups.java
@@ -9,6 +9,7 @@ public class Groups {
 	public static final String BLOCKER = "Blocker";
 	public static final String CRITICAL = "Critical";
 	public static final String HIGH = "High";
+	public static final String MEDIUM = "Medium";
 	public static final String LOW = "Low";
 
 
