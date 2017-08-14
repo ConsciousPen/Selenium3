1c41f4ade6f3dd84b0d563f2e277743a99cb3005
gmdreva <oreva@eisgroup.com>
2017-08-15 00:06:04 +0300

    CT HSS Delta ho6, dp3 added

---
+++ aaa-automation-tests/src/test/java/aaa/modules/delta/home_ss/dp3/TestCTDeltaScenario1.java
@@ -0,0 +1,45 @@
+package aaa.modules.delta.home_ss.dp3;
+
+import org.testng.annotations.Test;
+
+import aaa.main.modules.policy.PolicyType;
+import aaa.modules.delta.templates.CTDeltaScenario1;
+import toolkit.datax.TestData;
+
+public class TestCTDeltaScenario1 extends CTDeltaScenario1 {
+public String scenarioPolicyType = "DP3";
+	
+	@Override
+	protected PolicyType getPolicyType() {
+		return PolicyType.HOME_SS_DP3;
+	}
+	
+	@Test
+	public void TC01_createQuote() {		
+		TestData td_sc1 = getTestSpecificTD("TestData");		
+		super.TC01_createQuote(td_sc1, scenarioPolicyType);
+	}
+
+	@Test
+	public void TC02_verifyWindstormMitigationDiscount() {
+		super.TC02_verifyWindstormMitigationDiscount();
+	}
+	
+	@Test
+	public void TC03_verifyELC() {
+		super.TC03_verifyELC();
+	}
+	
+	@Test
+	public void TC04_purchasePolicy() {
+		TestData td_sc1 = getTestSpecificTD("TestData");
+		super.TC04_purchasePolicy(td_sc1, scenarioPolicyType);
+	}
+	
+	@Test
+	public void TC05_verifyODDPolicy() {} 
+	
+	@Test
+	public void TC06_verifyCancelNoticeAction() {}
+
+}
