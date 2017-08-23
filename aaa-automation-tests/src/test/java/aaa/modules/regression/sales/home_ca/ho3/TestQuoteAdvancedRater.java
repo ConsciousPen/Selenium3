a3c4ab3fdcb231a95d4a262fcb7f5db0481ea339
jkuznecov <jkuznecov@eisgroup.com>
2017-08-23 15:30:34 +0300

    Fixed TestQuoteAdvancedRater

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/TestQuoteAdvancedRater.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/TestQuoteAdvancedRater.java
@@ -2,6 +2,8 @@ package aaa.modules.regression.sales.home_ca.ho3;
 
 import org.testng.annotations.Test;
 import toolkit.utils.TestInfo;
+import toolkit.webdriver.controls.ComboBox;
+import toolkit.webdriver.controls.composite.assets.metadata.AssetDescriptor;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.main.metadata.policy.HomeCaMetaData;
@@ -14,19 +16,21 @@ import com.exigen.ipb.etcsa.utils.Dollar;
 
 public class TestQuoteAdvancedRater extends HomeCaHO3BaseTest {
 
+    PremiumsAndCoveragesQuoteTab premiumsAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
+
     /**
      * @author Jurij Kuznecov
      * @name Test CAH Quote Advanced Rater
-     * @scenario 
-     * 1. Create new or open existent Customer
-     * 2. Initiate HO3 quote creation
-     * 3. Fill General, Applicant, Property Info tabs. Order reports. Navigate to P&C screen
-     * 4. Click Calculate Premium button
-     * 5. Check values sent to Rating Services has premium change of (+$0.00)
-     * 6. Select other Deductible value
-     * 7. Check System resets the premium to $0
-     * 8. Click Calculate Premium button
-     * 9. Check Premium changed according to selected value
+     * @scenario
+     * 1.  Create new or open existent Customer
+     * 2.  Initiate HO3 quote creation
+     * 3.  Fill General, Applicant, Property Info tabs. Order reports. Navigate to P&C screen
+     * 4.  Click Calculate Premium button
+     * 5.  Check values sent to Rating Services has premium change of (+$0.00)
+     * 6.  Select other Deductible value
+     * 7.  Check System resets the premium to $0
+     * 8.  Click Calculate Premium button
+     * 9.  Check Premium changed according to selected value
      * 10. Select other E Coverage value
      * 11. Check System resets the premium to $0
      * 12. Click Calculate Premium button
@@ -34,43 +38,57 @@ public class TestQuoteAdvancedRater extends HomeCaHO3BaseTest {
      * 14. Issue quote. Check Total Premium Summary
      */
 
-	@Test(groups = { Groups.REGRESSION, Groups.HIGH })
-	@TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3)
+    @Test(groups = {Groups.REGRESSION, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3)
     public void testQuoteAdvancedRater() {
-        PremiumsAndCoveragesQuoteTab premiumsAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
-        String expectedCoverageEValue = "$500,000 (+$0.00)";
-        String expectedDeductibleValue = "$1,000 (+$0.00)";
-        Dollar newDeductiblePremiumValue = new Dollar(423);
-        Dollar newCoverageEPremiumValue = new Dollar(396);
-
         mainApp().open();
         createCustomerIndividual();
 
         policy.initiate();
         policy.getDefaultView().fillUpTo(getPolicyTD(), PremiumsAndCoveragesQuoteTab.class);
         premiumsAndCoveragesQuoteTab.calculatePremium();
+        Dollar origPremiumValue = new Dollar(PremiumsAndCoveragesQuoteTab.getPolicyTermPremium());
 
-        premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_E).verify.value(expectedCoverageEValue);
-        premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.DEDUCTIBLE).verify.value(expectedDeductibleValue);
+        verifyPremiumChangeOf(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.DEDUCTIBLE);
+        verifyPremiumChangeOf(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_E);
 
-        String newDeductibleValue = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.DEDUCTIBLE).getAllValues().get(0);
-        premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.DEDUCTIBLE).setValue(newDeductibleValue);
+        Dollar premiuimChangeOf = changeCoverage(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.DEDUCTIBLE);
+        Dollar newPremiumValue = origPremiumValue.add(premiuimChangeOf);
+        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(newPremiumValue);
 
-        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(new Dollar(0));
-        premiumsAndCoveragesQuoteTab.calculatePremium();
-        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(newDeductiblePremiumValue);
-
-        String newCoveragEValue = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_E).getAllValues().get(0);
-        premiumsAndCoveragesQuoteTab.getAssetList().getAsset(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_E).setValue(newCoveragEValue);
-
-        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(new Dollar(0));
-        premiumsAndCoveragesQuoteTab.calculatePremium();
-        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(newCoverageEPremiumValue);
+        premiuimChangeOf = changeCoverage(HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_E);
+        newPremiumValue = newPremiumValue.add(premiuimChangeOf);
+        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(newPremiumValue);
 
         PremiumsAndCoveragesQuoteTab.btnContinue.click();
         policy.getDefaultView().fillFromTo(getPolicyTD(), MortgageesTab.class, PurchaseTab.class, true);
         new PurchaseTab().submitTab();
 
-        PolicySummaryPage.getTotalPremiumSummaryForProperty().verify.equals(newCoverageEPremiumValue);
+        PolicySummaryPage.getTotalPremiumSummaryForProperty().verify.equals(newPremiumValue);
+    }
+
+    private void verifyPremiumChangeOf(AssetDescriptor<ComboBox> field) {
+        String origValue = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(field).getValue();
+        Dollar premiumChangeOf = new Dollar((origValue.substring(origValue.indexOf('+') + 1, origValue.indexOf(')'))).replace("$", ""));
+        premiumChangeOf.verify.equals(new Dollar(0));
+    }
+
+    private Dollar changeCoverage(AssetDescriptor<ComboBox> field) {
+        int index = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(field).getSelectedIndex();
+        if (index == 0)
+            index++;
+        else
+            index--;
+        String fieldValue = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(field).getAllValues().get(index);
+        Dollar premiumChangeOf = new Dollar((fieldValue.substring(fieldValue.indexOf('+') + 1, fieldValue.indexOf(')'))).replace("$", ""));
+        premiumsAndCoveragesQuoteTab.getAssetList().getAsset(field).setValue(fieldValue);
+
+        PremiumsAndCoveragesQuoteTab.getPolicyTermPremium().verify.equals(new Dollar(0));
+        premiumsAndCoveragesQuoteTab.calculatePremium();
+
+        fieldValue = premiumsAndCoveragesQuoteTab.getAssetList().getAsset(field).getAllValues().get(index);
+        Dollar premiumNewChangeOf = new Dollar((fieldValue.substring(fieldValue.indexOf('+') + 1, fieldValue.indexOf(')'))).replace("$", ""));
+
+        return premiumChangeOf.subtract(premiumNewChangeOf);
     }
 }
