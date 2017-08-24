ea5ebcc6bc7d04c1655c697c3d1e7d2a1f321e86
Yonggang.Sun <Yonggang.Sun@eisgroup.com>
2017-08-24 20:33:59 +0800

    Created PupQuoteVoiceBind test

--- aaa-automation-ui/src/main/java/aaa/main/metadata/policy/PurchaseMetaData.java
+++ aaa-automation-ui/src/main/java/aaa/main/metadata/policy/PurchaseMetaData.java
@@ -1,6 +1,7 @@
 package aaa.main.metadata.policy;
 
 import org.openqa.selenium.By;
+
 import aaa.main.metadata.BillingAccountMetaData;
 import aaa.toolkit.webdriver.customcontrols.AddPaymentMethodsMultiAssetList;
 import aaa.toolkit.webdriver.customcontrols.PaymentMethodAllocationControl;
@@ -26,6 +27,14 @@ public class PurchaseMetaData {
 		public static final AssetDescriptor<TextBox> MINIMUM_REQUIRED_DOWNPAYMENT = declare("Minimum Required Down Payment", TextBox.class, Waiters.AJAX);
 		public static final AssetDescriptor<ComboBox> REASON_FOR_CHANGING = declare("Reason for Changing", ComboBox.class, Waiters.AJAX);
 		public static final AssetDescriptor<TextBox> ADDITIONAL_INFORMATION = declare("Additional Information", TextBox.class, Waiters.AJAX);
+		public static final AssetDescriptor<TextBox> PAYMENT_METHOD_CASH = declare("Cash", TextBox.class, Waiters.AJAX, false, By.xpath(".//input[@id='purchaseForm:amount_0']"));
+		public static final AssetDescriptor<TextBox> PAYMENT_METHOD_CHECK = declare("Check", TextBox.class, Waiters.AJAX, false, By.xpath(".//input[@id='purchaseForm:amount_1']"));
+		public static final AssetDescriptor<TextBox> PAYMENT_METHOD_CHECK_REFERENCE = declare("Check Reference", TextBox.class, Waiters.AJAX, false, By.xpath(".//input[@id='purchaseForm:reference_1']"));
+		
+		public static final AssetDescriptor<TextBox> PAYMENT_METHOD_VISA = declare("Visa", TextBox.class, Waiters.AJAX, false, By.xpath(".//input[@id='purchaseForm:amount_2']"));
+		public static final AssetDescriptor<TextBox> PAYMENT_METHOD_ACH = declare("CheckAch", TextBox.class, Waiters.AJAX, false, By.xpath(".//input[@id='purchaseForm:amount_3']"));
+		public static final AssetDescriptor<RadioGroup> VOICE_SIGNATURE = declare("Bind by Voice Signature", RadioGroup.class, Waiters.AJAX, false, By.xpath(".//table[@id='purchaseForm:voiceBindingSelection']"));
+		public static final AssetDescriptor<Button> ADD_CARD_BANK_BTN = declare("Add Credit Card/Bank Account", Button.class, Waiters.AJAX, false, By.id("purchaseForm:addPaymentMethodBtn"));
 
 		public static final AssetDescriptor<PaymentMethodAllocationControl> PAYMENT_ALLOCATION = declare("PaymentAllocation", PaymentMethodAllocationControl.class, MetaData.class, By.id("purchaseForm:PaymentDetailsTable"));
 
@@ -36,6 +45,20 @@ public class PurchaseMetaData {
 			public static final AssetDescriptor<Button> OK = declare("Ok", Button.class, Waiters.AJAX, false, By.id("purchaseForm:okBtn"));
 
 		}
+	}
+	
+	public static final class PurchasePaymentMethod extends MetaData {
+		public static final AssetDescriptor<ComboBox> PAYMENT_METHOD = declare("Payment Method", ComboBox.class, Waiters.AJAX);
+		public static final AssetDescriptor<ComboBox> GENERAL_TYPE = declare("Type", ComboBox.class, Waiters.AJAX, By.xpath(".//select[@id='paymentMethodForm:generalPaymentMethod_displayType']"));
+		public static final AssetDescriptor<TextBox> NUMBER = declare("Number", TextBox.class, Waiters.AJAX, By.xpath(".//input[@id='paymentMethodForm:generalPaymentMethod_fullNumber']"));
+		public static final AssetDescriptor<Button> ADD_CARD_BANK_BTN = declare("AddCard", Button.class, Waiters.AJAX, false, By.id("paymentMethodForm:pciSaveBtn"));
+		public static final AssetDescriptor<Button> BACK_BUTTON = declare("Back", Button.class, Waiters.AJAX, false, By.id("primaryButtonsForm:backButton_footer"));
+		
+		public static final AssetDescriptor<TextBox> TRANSIT_NUMBER = declare("Transit", TextBox.class, Waiters.AJAX, false, By.id("paymentMethodEFTForm:paymentEFT_transitNumber"));
+		public static final AssetDescriptor<TextBox> BANK_NAME = declare("Bank Name", TextBox.class, Waiters.AJAX);
+		public static final AssetDescriptor<TextBox> ACCOUNT_NUMBER = declare("Account", TextBox.class, Waiters.AJAX, false, By.id("paymentMethodEFTForm:paymentEFT_accountNumber"));
+		
+		public static final AssetDescriptor<Button> ADD_ACH_SAVE_BTN = declare("AddACH", Button.class, Waiters.AJAX, false, By.id("paymentMethodEFTForm:eftSaveBtn"));
 	}
 
 }
--- aaa-automation-ui/src/main/java/aaa/main/modules/policy/pup/defaulttabs/PurchaseTab.java
+++ aaa-automation-ui/src/main/java/aaa/main/modules/policy/pup/defaulttabs/PurchaseTab.java
@@ -1,11 +1,48 @@
 package aaa.main.modules.policy.pup.defaulttabs;
 
+import org.openqa.selenium.By;
+
+import toolkit.datax.TestData;
+import toolkit.webdriver.controls.Button;
+import toolkit.webdriver.controls.TextBox;
+import aaa.common.Tab;
+import aaa.common.components.Dialog;
 import aaa.main.metadata.policy.PurchaseMetaData;
 import aaa.main.modules.policy.abstract_tabs.Purchase;
 
 public class PurchaseTab extends Purchase {
-
+	
+	public TextBox cash = new  TextBox(By.xpath(".//input[@id='purchaseForm:amount_0']"));
+	public TextBox check = new  TextBox(By.xpath(".//input[@id='purchaseForm:amount_1']"));
+	public TextBox checkReference = new  TextBox(By.xpath(".//input[@id='purchaseForm:reference_1']"));
+	public TextBox visaAndACH = new  TextBox(By.xpath(".//input[@id='purchaseForm:amount_2']"));
+	
+	public Dialog acceptcontinueDialog = new Dialog("//div[@id='purchaseForm:VoiceSignatureDialog_container']");
+	public Button acceptcontinueButton = new Button(By.id("purchaseForm:acceptBtn"));
+	
 	public PurchaseTab() {
 		super(PurchaseMetaData.PurchaseTab.class);
 	}
+	
+    @Override
+    public Tab fillTab(TestData td) {
+        super.fillTab(td);
+        return this;
+    }
+
+    public Tab superfillTab(TestData td) {
+        assetList.fill(td);
+        return this;
+    }
+
+    @Override
+    public Tab submitTab() {
+        btnApplyPayment.click();
+        confirmPurchase.confirm();
+        if (acceptcontinueDialog.isVisible() && acceptcontinueDialog.isPresent()) {
+            acceptcontinueButton.click();
+        }
+        return this;
+    }
+	
 }
