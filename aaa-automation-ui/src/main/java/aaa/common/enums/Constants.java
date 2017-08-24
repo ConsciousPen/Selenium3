599f6606e996acd66289262fb64bb62211cf8bd3
jkuznecov <jkuznecov@eisgroup.com>
2017-08-24 19:26:37 +0300

    updated States Constants

--- aaa-automation-ui/src/main/java/aaa/common/enums/Constants.java
+++ aaa-automation-ui/src/main/java/aaa/common/enums/Constants.java
@@ -2,19 +2,20 @@ package aaa.common.enums;
 
 public class Constants {
 
-	public final class States {
-		public static final String UT = "UT";
-		public static final String AZ = "AZ";
-		public static final String CA = "CA";
-		public static final String CO = "CO";
-		public static final String IN = "IN";
-		public static final String PA = "PA";
-		public static final String NJ = "NJ";
-		public static final String VA = "VA";
-		public static final String MD = "MD";
-		public static final String KY = "KY";
-		public static final String WV = "WV";
-		public static final String MT = "MT";
-		public static final String NY = "NY";
-	}
+    public final class States {
+        public static final String UT = "UT";
+        public static final String AZ = "AZ";
+        public static final String CA = "CA";
+        public static final String CO = "CO";
+        public static final String IN = "IN";
+        public static final String PA = "PA";
+        public static final String NJ = "NJ";
+        public static final String VA = "VA";
+        public static final String MD = "MD";
+        public static final String KY = "KY";
+        public static final String WV = "WV";
+        public static final String MT = "MT";
+        public static final String NY = "NY";
+        public static final String OK = "OK";
+    }
 }
