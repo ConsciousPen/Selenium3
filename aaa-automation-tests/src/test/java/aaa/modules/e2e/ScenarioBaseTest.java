bcdfa6bbbe0dfbe75a84167533a4cec66153e96d
Kanstantsin Maiko <kmaiko@eisgroup.com>
2017-08-23 19:39:34 -0700

    e2e scenarios refactoring and fixes

--- aaa-automation-tests/src/test/java/aaa/modules/e2e/ScenarioBaseTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/e2e/ScenarioBaseTest.java
@@ -1,18 +1,23 @@
 package aaa.modules.e2e;
 
+import aaa.common.enums.Constants;
 import aaa.common.pages.SearchPage;
 import aaa.helpers.billing.BillingBillsAndStatementsVerifier;
 import aaa.helpers.billing.BillingHelper;
+import aaa.helpers.billing.BillingInstallmentsScheduleVerifier;
 import aaa.helpers.billing.BillingPaymentsAndTransactionsVerifier;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.main.enums.BillingConstants;
+import aaa.main.pages.summary.BillingSummaryPage;
 import aaa.modules.BaseTest;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import toolkit.utils.datetime.DateTimeUtils;
+import toolkit.verification.CustomAssert;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ScenarioBaseTest extends BaseTest {
 
@@ -37,5 +42,64 @@ public class ScenarioBaseTest extends BaseTest {
 		SearchPage.openBilling(policyNum);
 		Dollar minDue = new Dollar(BillingHelper.getBillCellValue(installmentDueDate, BillingConstants.BillingBillsAndStatmentsTable.MINIMUM_DUE));
 		new BillingPaymentsAndTransactionsVerifier().verifyAutoPaymentGenerated(DateTimeUtils.getCurrentDateTime(), minDue.negate());
+	}
+
+	protected void verifyRenewOfferGenerated(LocalDateTime policyExpDate, LocalDateTime renewOfferGenDate, List<LocalDateTime> installmentDates) {
+		BillingSummaryPage.showPriorTerms();
+
+		CustomAssert.enableSoftMode();
+		for (int i = 1; i < installmentDates.size(); i++) { //Do not include Deposit bill
+			new BillingInstallmentsScheduleVerifier().setDescription(BillingConstants.InstallmentDescription.INSTALLMENT)
+					.setInstallmentDueDate(installmentDates.get(i).plusYears(1)).verifyPresent();
+		}
+		if (!getState().equals(Constants.States.CA)) {
+			new BillingBillsAndStatementsVerifier().setType(BillingConstants.BillsAndStatementsType.OFFER).verifyPresent(false);
+		}
+		CustomAssert.disableSoftMode();
+		CustomAssert.assertAll();
+	}
+
+	/**
+	 * @param expirationDate - original policy expiration date
+	 * @param renewOfferDate - Renew generate offer date
+	 * @param billGenDate - Bill generation date
+	 * @param installmentsCount:
+	 * MONTHLY_STANDARD or ELEVEN_PAY: 11 installments
+	 * QUARTERLY: 4 installments
+	 * SEMI_ANNUAL: 2 installments
+	 * PAY_IN_FULL or ANNUAL: 1 installment
+	 */
+	protected void verifyRenewalOfferPaymentAmount(LocalDateTime expirationDate, LocalDateTime renewOfferDate, LocalDateTime billGenDate, Integer installmentsCount) {
+		BillingSummaryPage.showPriorTerms();
+		Dollar fullAmount = BillingHelper.getPolicyRenewalProposalSum(renewOfferDate);
+		Dollar fee = BillingHelper.getFeesValue(billGenDate);
+
+		Dollar expOffer = BillingHelper.calculateFirstInstallmentAmount(fullAmount, installmentsCount).add(fee);
+		new BillingBillsAndStatementsVerifier().setType(BillingConstants.BillsAndStatementsType.BILL).setDueDate(expirationDate).setMinDue(expOffer).verifyPresent();
+	}
+
+	/**
+	 * Same as {@link #verifyRenewalOfferPaymentAmount(java.time.LocalDateTime, java.time.LocalDateTime, java.time.LocalDateTime, java.lang.Integer)}
+	 */
+	protected void verifyCaRenewalOfferPaymentAmount(LocalDateTime expirationDate, LocalDateTime renewOfferDate, Integer installmentsCount) {
+		BillingSummaryPage.showPriorTerms();
+		new BillingPaymentsAndTransactionsVerifier().setTransactionDate(renewOfferDate)
+				.setType(BillingConstants.PaymentsAndOtherTransactionType.FEE).verifyPresent();
+
+		Dollar fullAmount = BillingHelper.getPolicyRenewalProposalSum(renewOfferDate);
+		Dollar fee = BillingHelper.getFeesValue(renewOfferDate);
+
+		Dollar expOffer = BillingHelper.calculateFirstInstallmentAmount(fullAmount, installmentsCount).add(fee);
+		new BillingBillsAndStatementsVerifier().setType(BillingConstants.BillsAndStatementsType.OFFER).setDueDate(expirationDate).setMinDue(expOffer).verifyPresent();
+	}
+
+	protected void verifyRenewPremiumNotice(LocalDateTime renewDate, LocalDateTime billGenerationDate) {
+		BillingSummaryPage.showPriorTerms();
+		Dollar billAmount = BillingHelper.getInstallmentDueByDueDate(renewDate).add(BillingHelper.getFeesValue(billGenerationDate));
+		new BillingBillsAndStatementsVerifier().setType(BillingConstants.BillsAndStatementsType.BILL).verifyRowWithDueDate(renewDate);
+		//TODO Check whu there are no verifications for KY and WV
+//		if (!BaseTest.getState().equals(Constants.States.KY) && !BaseTest.getState().equals(Constants.States.WV)) {
+		new BillingBillsAndStatementsVerifier().setMinDue(billAmount).verifyRowWithDueDate(renewDate);
+//		}
 	}
 }
--- aaa-automation-tests/src/test/java/aaa/modules/e2e/auto_ss/TestScenario4.java
+++ aaa-automation-tests/src/test/java/aaa/modules/e2e/auto_ss/TestScenario4.java
@@ -32,86 +32,86 @@ public class TestScenario4 extends Scenario4 {
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC02_Overpayment() {
-		super.TC02_Overpayment();
+		super.overpayment();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC03_Automatic_refund() {
-		super.TC03_Automatic_refund();
+		super.automaticRefund();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC04_Overpayment_High() {
-		super.TC04_Overpayment_High();
+		super.overpaymentHigh();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC05_Automatic_refund_High() {
-		super.TC05_Automatic_refund_High();
+		super.automaticRefundHigh();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC06_Endorse_Policy() {
-		super.TC06_Endorse_Policy();
+		super.endorsePolicy();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC07_Generate_Off_Cycle_Bill() {
-		super.TC07_Generate_Off_Cycle_Bill();
+		super.generateOffCycleBill();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC08_Generate_CancellNotice() {
-		super.TC08_Generate_CancellNotice();
+		super.generateCancellNotice();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC09_Payment_In_Full_Cancell_Notice_Amount() {
-		super.TC09_Payment_In_Full_Cancell_Notice_Amount();
+		super.paymentInFullCancellNoticeAmount();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC10_Verify_Form_AHCWXX() {
-		super.TC10_Verify_Form_AHCWXX();
+		super.verifyFormAHCWXX();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC11_Renewal_Image_Generation() {
-		super.TC11_Renewal_Image_Generation();
+		super.renewalImageGeneration();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC12_Renewal_Preview_Generation() {
-		super.TC12_Renewal_Preview_Generation();
+		super.renewalPreviewGeneration();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC13_Renewal_Offer_Generation() {
-		super.TC13_Renewal_Offer_Generation();
+		super.renewalOfferGeneration();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC14_Renewal_Premium_Notice() {
-		super.TC14_Renewal_Premium_Notice();
+		super.renewalPremiumNotice();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC15_Expire_Policy() {
-		super.TC15_Expire_Policy();
+		super.expirePolicy();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC16_Customer_Decline_Renewal() {
-		super.TC16_Customer_Decline_Renewal();
+		super.customerDeclineRenewal();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC17_Pay_Renew_Offer() {
-		super.TC17_Pay_Renew_Offer();
+		super.payRenewOffer();
 	}
 
 	@Test(dependsOnMethods = "TC01_createPolicy")
 	public void TC18_Bind_Renew() {
-		super.TC18_Bind_Renew();
+		super.bindRenew();
 	}
 }
