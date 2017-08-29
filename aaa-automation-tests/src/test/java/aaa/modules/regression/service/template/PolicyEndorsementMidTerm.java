6ec63cfd0ad19a2207e0cea64b05ff56625d3d5d
Anton Perapecha <aperapecha@eisgroup.com>
2017-08-28 17:14:18 -0700

    Backdated Policy method creation

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyEndorsementMidTerm.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyEndorsementMidTerm.java
@@ -13,9 +13,8 @@ import com.exigen.ipb.etcsa.utils.Dollar;
 /**
  * @author Lina Li
  * @name Test Midterm Endorsement for Policy
- * @scenario
- * 1. Create Customer
- * 2. Create a Backdated Policy 
+ * @scenario 1. Create Customer
+ * 2. Create a Backdated Policy
  * 3. Create endorsement
  * 4. Verify 'Pended Endorsement' button is disabled
  * 5. Verify Policy status is 'Policy Active'
@@ -28,22 +27,18 @@ public abstract class PolicyEndorsementMidTerm extends PolicyBaseTest {
 	protected void testPolicyEndorsementMidTerm() {
 		mainApp().open();
 
-		if (getPolicyType().equals(PolicyType.PUP)) {
-			new TestPolicyBackdated().testPolicyBackdated();
-		} else {
-			createCustomerIndividual();
-			createPolicy(getBackDatedPolicyTD());
-		}
-		
+		createCustomerIndividual();
+		createPolicy(getBackDatedPolicyTD());
+
 		Dollar policyPremium = PolicySummaryPage.TransactionHistory.getEndingPremium();
 
 		log.info("TEST: MidTerm Endorsement for Policy #" + PolicySummaryPage.labelPolicyNumber.getValue());
 
 		TestData endorsementTd = getTestSpecificTD("TestData");
 		policy.createEndorsement(endorsementTd.adjust(getPolicyTD("Endorsement", "TestData")));
-		
+
 		PolicySummaryPage.buttonPendedEndorsement.verify.enabled(false);
-	    PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
-	    CustomAssert.assertFalse(policyPremium.equals(PolicySummaryPage.TransactionHistory.getEndingPremium()));
+		PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
+		CustomAssert.assertFalse(policyPremium.equals(PolicySummaryPage.TransactionHistory.getEndingPremium()));
 	}
 }
--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyRenewFlatCancellation.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyRenewFlatCancellation.java
@@ -8,11 +8,11 @@ import aaa.main.modules.policy.PolicyType;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
 import aaa.modules.regression.sales.pup.TestPolicyBackdated;
+
 /**
  * @author Xiaolan Ge
  * @name Test renew flat cancellation for Umbrella Policy
- * @scenario
- * 1. Create Customer
+ * @scenario 1. Create Customer
  * 2. Create Policy
  * 3. Manual Renew for Policy
  * 4. Cancellation Policy Renewal
@@ -20,29 +20,25 @@ import aaa.modules.regression.sales.pup.TestPolicyBackdated;
  * @details
  */
 public class PolicyRenewFlatCancellation extends PolicyBaseTest {
-    public void testPolicyRenewFlatCancellation() {
-    	mainApp().open();
-		if (getPolicyType().equals(PolicyType.PUP)) {
-			new TestPolicyBackdated().testPolicyBackdated();
+	public void testPolicyRenewFlatCancellation() {
+		mainApp().open();
+
+		createCustomerIndividual();
+		createPolicy(getBackDatedPolicyTD());
+		String policyNumber = PolicySummaryPage.labelPolicyNumber.getValue();
+
+		log.info("Manual Renew for Policy #" + policyNumber);
+		if (getPolicyType().equals(PolicyType.AUTO_SS) || getPolicyType().equals(PolicyType.AUTO_CA_SELECT) || getPolicyType().equals(PolicyType.AUTO_CA_CHOICE)) {
+			policy.renew().perform(new SimpleDataProvider());
 		} else {
-			createCustomerIndividual();
-			createPolicy(getBackDatedPolicyTD());
+			policy.renew().performAndExit(new SimpleDataProvider());
 		}
-        String policyNumber = PolicySummaryPage.labelPolicyNumber.getValue();
+		log.info("TEST: Cancellation Policy Renewal #" + policyNumber);
+		policy.cancel().perform(getPolicyTD("Cancellation", "TestData_Plus3Days"));
 
-        log.info("Manual Renew for Policy #" + policyNumber);
-        if (getPolicyType().equals(PolicyType.AUTO_SS) || getPolicyType().equals(PolicyType.AUTO_CA_SELECT) || getPolicyType().equals(PolicyType.AUTO_CA_CHOICE)){
-	    	 policy.renew().perform(new SimpleDataProvider());
-	     }	     
-	     else {
-	    	  policy.renew().performAndExit(new SimpleDataProvider());
-	     }
-        log.info("TEST: Cancellation Policy Renewal #" + policyNumber);
-        policy.cancel().perform(getPolicyTD("Cancellation", "TestData_Plus3Days"));
-
-        PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.CANCELLATION_PENDING);
-        PolicySummaryPage.buttonTransactionHistory.click();
-        PolicySummaryPage.tableTransactionHistory.getRow(1).getCell(2).verify.value(ProductConstants.TransactionHistoryType.CANCELLATION);
-        PolicySummaryPage.tableTransactionHistory.getRow(2).getCell(2).verify.value(ProductConstants.TransactionHistoryType.ISSUE);
-    }
+		PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.CANCELLATION_PENDING);
+		PolicySummaryPage.buttonTransactionHistory.click();
+		PolicySummaryPage.tableTransactionHistory.getRow(1).getCell(2).verify.value(ProductConstants.TransactionHistoryType.CANCELLATION);
+		PolicySummaryPage.tableTransactionHistory.getRow(2).getCell(2).verify.value(ProductConstants.TransactionHistoryType.ISSUE);
+	}
 }
