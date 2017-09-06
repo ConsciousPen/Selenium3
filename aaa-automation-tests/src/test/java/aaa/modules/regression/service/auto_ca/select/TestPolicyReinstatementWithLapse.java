5d8b99ec0185e4b4f697457fb771cf3d24aa04d2
Jelena Dembovska <jdembovska@eisgroup.com>
2017-09-06 15:09:57 +0300

    Regression tests fix

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ca/select/TestPolicyReinstatementWithLapse.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ca/select/TestPolicyReinstatementWithLapse.java
@@ -2,13 +2,8 @@
  * CONFIDENTIAL AND TRADE SECRET INFORMATION. No portion of this work may be copied, distributed, modified, or incorporated into any other media without EIS Group prior written consent. */
 package aaa.modules.regression.service.auto_ca.select;
 
-import org.testng.annotations.Test;
-
-
-
 
 import aaa.helpers.constants.ComponentConstant;
-import aaa.helpers.constants.Groups;
 import aaa.main.modules.policy.PolicyType;
 import aaa.modules.regression.service.template.PolicyReinstatementWithLapse;
 import toolkit.utils.TestInfo;
@@ -36,7 +31,7 @@ public class TestPolicyReinstatementWithLapse extends PolicyReinstatementWithLap
     
     //currently removed from suite - not sure if applicable? field "Reinstate Date:" is disabled
     @Override
-    @Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
+    //@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
 	@TestInfo(component = ComponentConstant.Service.AUTO_CA_SELECT )
     public void testPolicyReinstatementWithLapse() {
 
