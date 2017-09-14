8770d0298d43f492a1a61421ce9479f6dfc085a1
qyu <qing.yu@eisgroup.com>
2017-09-14 17:14:44 +0800

    Implement TestDocgenScenarios - testMortgagePolicyDocuments for home_ss_dp3

--- aaa-automation-tests/src/test/java/aaa/modules/docgen/home_ss/dp3/TestDocgenScenarios.java
+++ aaa-automation-tests/src/test/java/aaa/modules/docgen/home_ss/dp3/TestDocgenScenarios.java
@@ -247,6 +247,224 @@ public class TestDocgenScenarios extends HomeSSDP3BaseTest{
     /**
      * <pre>
      * TC Steps:
+     * Create HO3 quote:
+     *
+     * Set   Payment Plan : Mortgagee Bill
+     * Add one morgagee                                        // for HSES and 438BFUNS forms
+     *
+     * Set  Fire department type = 'S - Subscription based'
+     * Subscription to fire department/station = 'Yes'
+     * Public protection class = 5                             // for HSRFIXX form generation
+     * Construction type = "Frame"
+     * Proof of subscription to fire department = "No"
+     *
+     * Rate the quote.
+     * Save and exit quote, Go to On-Demand Documents tab,
+     * Verify state of forms on the quote:
+     * AHAUXX - enabled
+     * AHFMXX - enabled
+     * HSILXX - enabled
+     * DS11   - enabled
+     * DSIQXX - enabled
+     * HSRFIXX - disabled
+     * HSU01XX - disabled
+     * HSU02XX - disabled
+     * HSU03XX - enabled
+     * HSU04XX - enabled
+     * HSU05XX - enabled
+     * HSU06XX - enabled
+     * HSU07XX - disabled
+     * HSU08XX - enabled
+     * HSU09XX - disabled
+     * Verify that below forms aren't present:
+     * 438 BFUNS
+     * AHRCTXX
+     * AHPNXX
+     * DS02
+     * AHNBXX
+     * HSEIXX
+     * HSES
+     * Select forms on On-Demand Documents Tab :
+     * HSU03XX,HSU04XX,HSU05XX,HSU06XX,HSU08XX
+     * Press "Generate" button
+     * Verify that below forms are generated in xml batch:
+     * AHAUXX
+     * HSU03XX
+     * HSU04XX
+     * HSU05XX
+     * HSU06XX
+     * HSU08XX
+     * Bind the quote.
+     * Verify that below forms are generated in xml batch after bind:
+     * DS02
+     * AHNBXX
+     * 438BFUNS
+     * Go To On-Demand Document Tab
+     * Verify state of forms on the policy:
+     * AHFMXX - enabled
+     * AHRCTXX - disabled
+     * HS11 - enabled
+     * HSEIXX - enabled
+     * HSES - enabled
+     * HSILXX - enabled
+     * HSRFIXX - enabled
+     * HSU01XX - enabled
+     * HSU02XX - enabled
+     * HSU03XX - disabled
+     * HSU04XX - enabled
+     * HSU05XX - enabled
+     * HSU06XX - enabled
+     * HSU07XX - enabled
+     * HSU08XX - enabled
+     * HSU09XX - enabled
+     *
+     * Verify that below forms aren't present:
+     * AHNBXX
+     * HSIQXX
+     * AHPNXX
+     * 438 BFUNS
+     * DS02
+     *
+     * Select forms on On-Demand Documents Tab :
+     * HSES
+     * HSRFIXX
+     * HSU01XX
+     * HSU09XX
+     * Press "Generate" button
+     * Verify that below forms are generated in xml batch:
+     * HSES
+     * HSRFIXX
+     * HSU01XX
+     * HSU09XX
+     * <pre>
+     * Req:
+     *  AHAUXX - 15377: US CL GD-94 Consumer Information Notice
+     *  AHFMXX - 15379: US CL GD-96 Generate Fax Memorandum Document
+     * DS11   - 15209: US CL GD-77 Generate Application Document All Products
+     * DSIQXX - 15207: US CL GD-76 Generate Quote Document All Products
+     * HSRFIXX - 16187: US CL GD-124 Generate HSRFIXX Request For Information
+     * HSU01XX - 15272: US CL GD-53 Generate Underwriting Letter HSU01 Advisory
+     * HSU02XX - 15274: US CL GD-55 Generate Underwriting Letter HSU02 Cancellation
+     * HSU03XX - 15275: US CL GD-56 Generate Underwriting Letter HSU03 Customer Decline
+     * HSU04XX - 15276: US CL GD-57 Generate Underwriting Letter HSU04 Free Form to Insured
+     * HSU05XX - 15277: US CL GD-58 Generate Undewriting Letter HSU05 Free Form to Other
+     * HSU06XX - 15278: US CL GD-59 Generate Underwriting Letter HSU06 Free Form to Producer
+     * HSU07XX - 16238:US CL GD-60 Generate Underwriting letter HSU07 Non Renewal
+     * HSU08XX - 15282: US CL GD-62 Generate Underwriting Letter HSU08 Request Add'l Info
+     * HSU09XX - 15283: US CL GD-63 Generate Underwriting Letter HSU09 Uprate
+     * 438 BFUNS - 15210: US CL GD-01 Generate 438BFUNS Endorsement
+     * AHRCTXX - 15384: US CL GD-101 Generate Insured Receipt for Funds Received by Agent
+     * AHPNXX - 15382: US CL GD-99 Generate Privacy Information Notice
+     * DS02 - 16881: US CL GD-78 Generate  Declaration Documents All Products;
+     * AHNBXX - 15381: US CL GD-98 Generate New Business Welcome Letter
+     * HSEIXX - 16184: US CL GD-121 Generate HSEIXX Evidence of Insurance
+     * HSES - 16200: US CL GD-119 Generate HSESXX Property Insurance Invoice
+     * HSILXX - 16185:US CL GD-122 Generate HSILXX Property Inventory List
+     */
+
+    @Test
+    public void testMortgagePolicyDocuments() {
+    	CustomAssert.enableSoftMode();
+		mainApp().open();
+		String currentHandle = WebDriverHelper.getWindowHandle();
+		createCustomerIndividual();
+		String quoteNum = createQuote(getPolicyTD().adjust(getTestSpecificTD("TestData_MortgagePolicy")));
+		
+		policy.quoteDocGen().start();
+		documentActionTab.verify.documentsEnabled(
+				Documents.AHAUXX,
+				Documents.AHFMXX,
+				Documents.HSILXX,
+				Documents.DS11.setState(getState()),
+				Documents.DSIQXX,
+				Documents.HSU03XX,
+				Documents.HSU04XX,
+				Documents.HSU05XX,
+				Documents.HSU06XX,
+				Documents.HSU08XX
+				);
+		documentActionTab.verify.documentsEnabled(false,
+				Documents.HSRFIXX,
+				Documents.HSU01XX,
+				Documents.HSU02XX,
+				Documents.HSU07XX,
+				Documents.HSU09XX
+				);
+		documentActionTab.verify.documentsPresent(false,
+				Documents._438BFUNS,
+				Documents.AHRCTXX,
+				Documents.AHPNXX,
+				Documents.DS02,
+				Documents.AHNBXX,
+//				Documents.HSEIXX, // TODO Present on the page, need to confirm the request
+				Documents.HSES);
+		documentActionTab.generateDocuments(getTestSpecificTD("QuoteGenerateHSU"), 
+				Documents.HSU03XX, 
+				Documents.HSU04XX, 
+				Documents.HSU05XX, 
+				Documents.HSU06XX, 
+				Documents.HSU08XX
+				);
+		WebDriverHelper.switchToWindow(currentHandle);
+		DocGenHelper.verifyDocumentsGenerated(quoteNum, 
+				Documents.AHAUXX,
+				Documents.HSU03XX, 
+				Documents.HSU04XX, 
+				Documents.HSU05XX, 
+				Documents.HSU06XX, 
+				Documents.HSU08XX
+				);
+		
+		policy.purchase(getPolicyTD());
+		String policyNum = PolicySummaryPage.labelPolicyNumber.getValue();
+		DocGenHelper.verifyDocumentsGenerated(policyNum, Documents.DS02, Documents.AHNBXX, Documents._438BFUNS);
+		
+		policy.policyDocGen().start();
+		documentActionTab.verify.documentsEnabled( 
+				Documents.AHRCTXX,
+				Documents.DS11.setState(getState()), 
+				Documents.HSEIXX,
+//				Documents.HSES, //TODO HSES is not present, need to check the request
+				Documents.HSILXX, 
+				Documents.HSRFIXX,
+				Documents.HSU01XX,
+				Documents.HSU04XX,
+				Documents.HSU05XX,
+				Documents.HSU06XX,
+				Documents.HSU07XX,
+				Documents.HSU08XX,
+				Documents.HSU09XX
+				);
+		documentActionTab.verify.documentsEnabled(false,
+				Documents.AHFMXX,
+				Documents.HSU02XX,
+				Documents.HSU03XX
+				);
+		documentActionTab.verify.documentsPresent(false,
+				Documents.AHNBXX,
+				Documents.HSIQXX,
+				Documents.AHPNXX,
+				Documents._438BFUNS,
+				Documents.DS02
+				);
+		
+		documentActionTab.generateDocuments(getTestSpecificTD("PolicyGenerateHSU"), 
+				Documents.HSRFIXX, 
+				Documents.HSU01XX, 
+				Documents.HSU09XX
+				);
+		DocGenHelper.verifyDocumentsGenerated(policyNum, 
+				Documents.HSRFIXX, 
+				Documents.HSU01XX, 
+				Documents.HSU09XX
+				);
+		CustomAssert.disableSoftMode();
+		CustomAssert.assertAll();
+    }
+    
+    /**
+     * <pre>
+     * TC Steps:
      * Create a DP3 quote:
      * Go to Endorsements Tab
      * Add endorsements:
