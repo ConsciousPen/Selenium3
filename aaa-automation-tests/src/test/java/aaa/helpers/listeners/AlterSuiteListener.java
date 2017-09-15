cc5fed9ffe09787a004c9d0e3635dd63656ceea7
Anton Perapecha <aperapecha@eisgroup.com>
2017-09-14 21:15:50 -0700

    Added 6 and 8 thread browser pool

--- aaa-automation-tests/src/test/java/aaa/helpers/listeners/AlterSuiteListener.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/listeners/AlterSuiteListener.java
@@ -17,35 +17,36 @@ public class AlterSuiteListener implements IAlterSuiteListener {
 	public void alter(List<XmlSuite> suites) {
 
 		for (XmlSuite suite : suites) {
+			XmlTest newCATest = new XmlTest();
 			CustomLogger.getInstance().info(suite.toXml());
 			List<XmlTest> newTests = new LinkedList<>();
 			List<XmlTest> tests = suite.getTests();
 			for (XmlTest test : tests) {
+				newCATest = createTest(test, Constants.States.CA);
 				List<String> states = getStates(test);
 
 				for (String state : states) {
-					XmlTest xmlTest = new XmlTest();
-					//xmlTest = test;
-					xmlTest.setName(state + " " + test.getName());
-					xmlTest.setVerbose(test.getVerbose());
-					xmlTest.setPreserveOrder(test.getPreserveOrder());
-					//xmlTest.setThreadCount(test.getThreadCount());
-					//xmlTest.setParallel(test.getParallel());
-					xmlTest.setIncludedGroups(test.getIncludedGroups());
-					xmlTest.setExcludedGroups(test.getExcludedGroups());
-					//xmlTest.setPackages(test.getPackages());
-					Map<String, String> parameters = Maps.newHashMap();
-					parameters.put("state", state);
-					xmlTest.setParameters(parameters);
+
+					XmlTest xmlTest = createTest(test, state);
 					List<XmlClass> classes = test.getClasses();
 					for (XmlClass xmlClass : classes) {
-						xmlTest.getClasses().add(xmlClass);
+						if ((isSSProduct(xmlClass) || isPUPProduct(xmlClass)) && !state.equalsIgnoreCase(Constants.States.CA)) {
+							xmlTest.getClasses().add(xmlClass);
+						} else if ((isCAProduct(xmlClass) || isPUPProduct(xmlClass)) && state.equalsIgnoreCase(Constants.States.CA)) {
+							xmlTest.getClasses().add(xmlClass);
+						} else if (isCAProduct(xmlClass) && !states.contains(Constants.States.CA)) {
+							newCATest.getClasses().add(xmlClass);
+						}
 					}
+					if(!xmlTest.getClasses().isEmpty())
 					newTests.add(xmlTest);
+				}
+				if (!newCATest.getClasses().isEmpty()) {
+					newTests.add(newCATest);
 				}
 			}
 			suite.setTests(newTests);
-			for(XmlTest newTest : newTests){
+			for (XmlTest newTest : newTests) {
 				newTest.setSuite(suite);
 			}
 			CustomLogger.getInstance().info(suite.toXml());
@@ -67,7 +68,32 @@ public class AlterSuiteListener implements IAlterSuiteListener {
 		return states;
 	}
 
-	private static List<String> parseStates(String input) {
-		return Arrays.asList(input.split(","));
+	private List<String> parseStates(String input) {
+		return Arrays.asList(input.toUpperCase().trim().split(","));
+	}
+
+	private Boolean isCAProduct(XmlClass xmlClass) {
+		return xmlClass != null && xmlClass.getName().contains("_ca.");
+	}
+
+	private Boolean isSSProduct(XmlClass xmlClass) {
+		return xmlClass != null && xmlClass.getName().contains("_ss.");
+	}
+
+	private Boolean isPUPProduct(XmlClass xmlClass) {
+		return xmlClass != null && xmlClass.getName().contains(".pup.");
+	}
+
+	private XmlTest createTest(XmlTest test, String state) {
+		XmlTest xmlTest = new XmlTest();
+		xmlTest.setName(state + " " + test.getName());
+		xmlTest.setVerbose(test.getVerbose());
+		xmlTest.setPreserveOrder(test.getPreserveOrder());
+		xmlTest.setIncludedGroups(test.getIncludedGroups());
+		xmlTest.setExcludedGroups(test.getExcludedGroups());
+		Map<String, String> parameters = Maps.newHashMap();
+		parameters.put("state", state);
+		xmlTest.setParameters(parameters);
+		return xmlTest;
 	}
 }
