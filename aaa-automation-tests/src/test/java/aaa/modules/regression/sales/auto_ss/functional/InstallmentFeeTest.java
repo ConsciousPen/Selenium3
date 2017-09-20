f23e528966310b3823167bab0f738c8ddc147da9
Oleg Stasyuk <ostasyuk@eisgroup.com>
2017-09-20 14:07:42 -0700

    megha

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/InstallmentFeeTest.java
@@ -0,0 +1,29 @@
+package aaa.modules.regression.sales.auto_ss.functional;
+
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.modules.policy.AutoSSBaseTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import toolkit.datax.TestData;
+import toolkit.utils.TestInfo;
+
+public class InstallmentFeeTest extends AutoSSBaseTest
+{
+
+    @Parameters({"state"})
+    @Test(groups = { Groups.REGRESSION, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.Sales.AUTO_SS)
+    public void installmentfee() {
+
+        mainApp().open();
+
+        createCustomerIndividual();
+
+        log.info("Policy Creation Started...");
+
+        TestData bigPolicy_td = getTestSpecificTD("TestData_UT");
+        getPolicyType().get().createPolicy(bigPolicy_td);
+
+
+    }}
