d152c8d69028eb9ea1fdcf9afb20bc39d3a2f646
Illia Guliam <iguliam@eisgroup.com>
2017-09-28 14:33:35 +0300

    CFT Scenario1. Updates.

--- aaa-automation-tests/src/test/java/aaa/modules/cft/ControlledFinancialBaseTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/cft/ControlledFinancialBaseTest.java
@@ -5,20 +5,23 @@ package aaa.modules.cft;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
 import aaa.common.pages.SearchPage;
+import aaa.helpers.billing.BillingBillsAndStatementsVerifier;
 import aaa.helpers.billing.BillingHelper;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.main.enums.BillingConstants;
 import aaa.main.enums.ProductConstants;
-import aaa.main.enums.SearchEnum;
+import aaa.main.modules.policy.PolicyType;
 import aaa.main.pages.summary.BillingSummaryPage;
 import aaa.main.pages.summary.NotesAndAlertsSummaryPage;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
+import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.BeforeSuite;
 import toolkit.datax.TestData;
+import toolkit.exceptions.IstfException;
 import toolkit.utils.datetime.DateTimeUtils;
 
 import java.time.LocalDateTime;
@@ -32,6 +35,7 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 	private LocalDateTime startTime;
 	private ThreadLocal<List<LocalDateTime>> installments = new ThreadLocal<>();
 	private ThreadLocal<String> policyNumber = ThreadLocal.withInitial(() -> StringUtils.EMPTY);
+	private ThreadLocal<BillingBillsAndStatementsVerifier> billingBillsAndStatementsVerifier = ThreadLocal.withInitial(BillingBillsAndStatementsVerifier::new);
 
 	@BeforeSuite(alwaysRun = true)
 	public void runCFTJob() {
@@ -43,12 +47,10 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 	 * Creating of the policy for test
 	 * DD0 - means today time - start time point
 	 */
-	protected void testCFTScenario1CreateQuote() {
+	protected void testCFTScenario1CreatePolicy() {
 		mainApp().open();
 		createCustomerIndividual();
-		TestData td = getStateTestData(testDataManager.policy.get(getPolicyType()), "DataGather", "TestData");
-		td.adjust("PremiumAndCoveragesTab", getTestSpecificTD("PremiumAndCoveragesTab_DataGather"));
-		td.adjust("PurchaseTab", getTestSpecificTD("PurchaseTab_DataGather"));
+		TestData td = getPolicyTestData(getPolicyType());
 		policyNumber.set(createPolicy(td.resolveLinks()));
 		NavigationPage.toMainTab(NavigationEnum.AppMainTabs.BILLING.get());
 		installments.set(BillingHelper.getInstallmentDueDates());
@@ -60,9 +62,8 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 	 */
 	protected void testCFTScenario1Endorsement() {
 		TimeSetterUtil.getInstance().nextPhase(startTime.plusDays(2));
-		JobUtils.executeJob(Jobs.cftDcsEodJob);
 		mainApp().reopen();
-		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber.get());
+		SearchPage.openPolicy(policyNumber.get());
 		policy.endorse().performAndFill(getTestSpecificTD(DEFAULT_TEST_DATA_KEY));
 		NotesAndAlertsSummaryPage.activitiesAndUserNotes.verify.descriptionExist(String.format("Bind Endorsement effective %1$s for Policy %2$s", TimeSetterUtil.getInstance().getCurrentTime().plusDays(2).format(DateTimeUtils.MM_DD_YYYY), policyNumber.get()));
 	}
@@ -76,17 +77,15 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 		TimeSetterUtil.getInstance().nextPhase(billDueDate);
 		JobUtils.executeJob(Jobs.cftDcsEodJob);
 		mainApp().reopen();
-		SearchPage.search(SearchEnum.SearchFor.BILLING, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber.get());
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.TYPE).verify
-				.value(BillingConstants.BillsAndStatementsType.BILL);
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.DUE_DATE).verify
-				.value(installments.get().get(0).format(DateTimeUtils.MM_DD_YYYY));
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.MINIMUM_DUE).verify
-				.value(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.MIN_DUE).getValue());
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.PAST_DUE).verify
-				.value(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.PAST_DUE).getValue());
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.TOTAL_DUE).verify
-				.value(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.TOTAL_DUE).getValue());
+		SearchPage.openBilling(policyNumber.get());
+		getBillingBillsAndStatementsVerifier().getValues().clear();
+		getBillingBillsAndStatementsVerifier()
+				.setType(BillingConstants.BillsAndStatementsType.BILL)
+				.setDueDate(installments.get().get(0))
+				.setMinDue(new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.MIN_DUE).getValue()))
+				.setPastDue(new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.PAST_DUE).getValue()))
+				.setTotalDue(new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1).getCell(BillingConstants.BillingAccountPoliciesTable.TOTAL_DUE).getValue()))
+				.verifyPresent();
 	}
 
 	/**
@@ -101,11 +100,12 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 		TimeSetterUtil.getInstance().nextPhase(cancellationNoticeDate);
 		JobUtils.executeJob(Jobs.cftDcsEodJob);
 		mainApp().reopen();
-		SearchPage.search(SearchEnum.SearchFor.BILLING, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber.get());
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.DUE_DATE)
-				.verify.value(cancellationDate.format(DateTimeUtils.MM_DD_YYYY));
-		BillingSummaryPage.tableBillsStatements.getRow(1).getCell(BillingConstants.BillingBillsAndStatmentsTable.TYPE)
-				.verify.value(BillingConstants.BillsAndStatementsType.CANCELLATION_NOTICE);
+		SearchPage.openBilling(policyNumber.get());
+		getBillingBillsAndStatementsVerifier().getValues().clear();
+		getBillingBillsAndStatementsVerifier()
+				.setDueDate(cancellationDate)
+				.setType(BillingConstants.BillsAndStatementsType.CANCELLATION_NOTICE)
+				.verifyPresent();
 	}
 
 	/**
@@ -120,8 +120,29 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 		TimeSetterUtil.getInstance().nextPhase(cancellationDate);
 		JobUtils.executeJob(Jobs.cftDcsEodJob);
 		mainApp().reopen();
-		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber.get());
+		SearchPage.openPolicy(policyNumber.get());
 		PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_CANCELLED);
+	}
+
+	/**
+	 * Generate 1st EP bill
+	 */
+	protected void testCFTScenario1GenerateFirstEPBill() {
+		generateAndCheckEarnedPremiumBill(getTimePoints().getEarnedPremiumBillFirst(installments.get().get(0)));
+	}
+
+	/**
+	 * Generate 2st EP bill
+	 */
+	protected void testCFTScenario1GenerateSecondEPBill() {
+		generateAndCheckEarnedPremiumBill(getTimePoints().getEarnedPremiumBillSecond(installments.get().get(0)));
+	}
+
+	/**
+	 * Generate 3st EP bill
+	 */
+	protected void testCFTScenario1GenerateThirdEPBill() {
+		generateAndCheckEarnedPremiumBill(getTimePoints().getEarnedPremiumBillThird(installments.get().get(0)));
 	}
 
 	/**
@@ -132,9 +153,18 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 		TimeSetterUtil.getInstance().nextPhase(writeOffDate);
 		JobUtils.executeJob(Jobs.cftDcsEodJob);
 		mainApp().reopen();
-		SearchPage.search(SearchEnum.SearchFor.BILLING, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber.get());
+		SearchPage.openBilling(policyNumber.get());
 		// check write off was generated
 		log.info("");
+	}
+
+	protected void generateAndCheckEarnedPremiumBill(LocalDateTime date) {
+		TimeSetterUtil.getInstance().nextPhase(date);
+		JobUtils.executeJob(Jobs.cftDcsEodJob);
+		mainApp().open();
+		SearchPage.openBilling(policyNumber.get());
+		getBillingBillsAndStatementsVerifier().getValues().clear();
+		getBillingBillsAndStatementsVerifier().setType(BillingConstants.BillsAndStatementsType.BILL).verifyRowWithDueDate(date);
 	}
 
 	private void runCFTJobs() {
@@ -143,4 +173,27 @@ public class ControlledFinancialBaseTest extends PolicyBaseTest {
 		JobUtils.executeJob(Jobs.policyTransactionLedgerJob);
 	}
 
+	private TestData getPolicyTestData(PolicyType policyType) {
+		switch (policyType.getShortName()) {
+			case "AutoSS": {
+				TestData td = getStateTestData(testDataManager.policy.get(getPolicyType()), "DataGather", "TestData");
+				td.adjust("PremiumAndCoveragesTab", getTestSpecificTD("PremiumAndCoveragesTab_DataGather"));
+				td.adjust("PurchaseTab", getTestSpecificTD("PurchaseTab_DataGather"));
+				return td;
+			}
+			case "HomeSS": {
+				TestData td = getStateTestData(testDataManager.policy.get(getPolicyType()), "DataGather", "TestData");
+				td.adjust("PremiumAndCoveragesTab", getTestSpecificTD("PremiumAndCoveragesTab_DataGather"));
+				td.adjust("PurchaseTab", getTestSpecificTD("PurchaseTab_DataGather"));
+				return td;
+			}
+			default: {
+				throw new IstfException("You have to provide existing product short name");
+			}
+		}
+	}
+
+	private BillingBillsAndStatementsVerifier getBillingBillsAndStatementsVerifier() {
+		return billingBillsAndStatementsVerifier.get();
+	}
 }
--- aaa-automation-tests/src/test/java/aaa/modules/cft/home_ss/ho3/TestCFTScenario1.java
+++ aaa-automation-tests/src/test/java/aaa/modules/cft/home_ss/ho3/TestCFTScenario1.java
@@ -1,4 +1,5 @@
-package aaa.modules.cft.auto_ss;
+package aaa.modules.cft.home_ss.ho3;
+
 
 import aaa.helpers.constants.Groups;
 import aaa.main.modules.policy.PolicyType;
@@ -9,20 +10,11 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import toolkit.utils.TestInfo;
 
-/**
- * Controlled Financial Testing Scenario 1
- * For any product and any defined state from params
- * NB W/O Emp Ben
- * Down pay_Cash
- * 1st installment
- * Cancel with future date
- * Earned Premium Write off
- */
 public class TestCFTScenario1 extends ControlledFinancialBaseTest {
 
 	@Override
 	protected PolicyType getPolicyType() {
-		return PolicyType.AUTO_SS;
+		return PolicyType.HOME_SS_HO3;
 	}
 
 	@Test(groups = {Groups.CFT})
