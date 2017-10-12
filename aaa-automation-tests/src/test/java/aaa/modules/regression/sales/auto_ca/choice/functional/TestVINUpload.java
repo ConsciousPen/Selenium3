4d3883d13b74958808a317d6e4d0933eadc74b03
gu1xkaz <Lev.Kazarnovskiy@csaa.com>
2017-10-12 11:15:54 -0700

    Added links to features in Jira for Select and Choice tests

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestVINUpload.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestVINUpload.java
@@ -17,8 +17,11 @@ public class TestVINUpload extends TestVINUploadTemplate {
     }
 
     /**
+     * * @author Lev Kazarnovskiy
      *
-     *See detailed steps in template file
+     * PAS-1406 - Data Refresh - PAS-533 -Quote Refresh -Add New VIN
+     *
+     * See detailed steps in template file
      */
     @Parameters({"state"})
     @Test(groups = { Groups.FUNCTIONAL, Groups.MEDIUM })
@@ -27,6 +30,13 @@ public class TestVINUpload extends TestVINUploadTemplate {
         super.testVINUpload_NewVINAdded("VINconfig_CA_CHOICE.xlsx", "VINupload_CA_CHOICE.xlsx", "BBBKN3DD0E0344466");
     }
 
+    /**
+     * * @author Lev Kazarnovskiy
+     *
+     * PAS-1406 - Data Refresh - PAS-527 -Renewal Refresh -Add New VIN & Update Existing
+     *
+     * See detailed steps in template file
+     */
     @Parameters({"state"})
     @Test(groups = { Groups.FUNCTIONAL, Groups.MEDIUM })
     @TestInfo(component = ComponentConstant.Sales.AUTO_CA_CHOICE)
@@ -34,6 +44,13 @@ public class TestVINUpload extends TestVINUploadTemplate {
         super.testVINUpload_NewVINAdded_Renewal("VINconfig_CA_CHOICE.xlsx", "VINupload_CA_CHOICE.xlsx", "BBBKN3DD0E0344466");
     }
 
+    /**
+     * * @author Lev Kazarnovskiy
+     *
+     * PAS-1406 - Data Refresh - PAS-527 -Renewal Refresh -Add New VIN & Update Existing
+     *
+     * See detailed steps in template file
+     */
     @Parameters({"state"})
     @Test(groups = { Groups.FUNCTIONAL, Groups.MEDIUM })
     @TestInfo(component = ComponentConstant.Sales.AUTO_CA_CHOICE)
