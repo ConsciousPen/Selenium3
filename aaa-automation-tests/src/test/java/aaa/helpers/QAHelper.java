22998f0dcbbb7adb3e33950d5196654a923d7ea5
Jemison <g6fjemi@ent.rt.csaa.com>
2017-10-23 15:11:59 -0700

    Updated AutoSSMetaData to Include DDD labels. Included TestData files.

--- aaa-automation-tests/src/test/java/aaa/helpers/QAHelper.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/QAHelper.java
@@ -1,7 +1,13 @@
 package aaa.helpers;
 
+import aaa.common.Tab;
+import aaa.common.pages.SearchPage;
+import aaa.main.modules.policy.IPolicy;
+import aaa.main.modules.policy.auto_ca.defaulttabs.GeneralTab;
 import aaa.modules.BaseTest;
 import toolkit.datax.TestData;
+
+import static java.security.Policy.getPolicy;
 
 /**
  * @author Tyrone Jemison
@@ -13,6 +19,7 @@ public class QAHelper extends BaseTest
     public String _customerFirstName;
     public String _customerLastName;
     public TestData _td;
+    public IPolicy _policy;
 
 
     // High Traffic Methods
@@ -39,4 +46,28 @@ public class QAHelper extends BaseTest
         }
     }
 
+    public void quoteBeginFillUntilTab(TestData in_td, Class<? extends Tab> in_tabClass, boolean in_bIsFillTab)
+    {
+        try {
+            _policy.initiate();
+            _policy.getDefaultView().fillUpTo(in_td, in_tabClass, in_bIsFillTab);
+        }catch(Exception ex)
+        {
+            System.out.println("--DEBUG--");
+            System.out.println("--DEBUG--");
+            System.out.println("Failed quoteBeginFillUntilTab() with parameters: ");
+            System.out.println(String.format("TestData:{0} -- TabClass:{1} -- bIsFillTab:{2}",
+                    in_td.getValue(), in_tabClass.getClass(), in_bIsFillTab));
+        }
+    }
+
+    public void getCreatedPolicy(String in_policyNumber)
+    {
+        SearchPage.openPolicy(in_policyNumber);
+    }
+
+    public void endorsePolicy(TestData endorsementTD)
+    {
+        //_policy.createEndorsement(endorsementTD.adjust(getPolicyTD("Endorsement", "TestData")));
+    }
 }
--- aaa-automation-tests/src/test/java/aaa/modules/functionalTests/TestCLUENoUpdateOnFail.java
+++ aaa-automation-tests/src/test/java/aaa/modules/functionalTests/TestCLUENoUpdateOnFail.java
@@ -1,4 +1,52 @@
 package aaa.modules.functionalTests;
 
-public class TestCLUENoUpdateOnFail {
+import aaa.helpers.QAHelper;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.modules.policy.AutoSSBaseTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import toolkit.datax.TestData;
+import toolkit.utils.TestInfo;
+
+public class TestCLUENoUpdateOnFail extends AutoSSBaseTest
+{
+    @Parameters({"state"})
+    @BeforeClass
+    public void testSetup(@Optional("") String state)
+    {
+
+    }
+
+    @Parameters({"state"})
+    @Test(groups= {Groups.SMOKE})
+    @TestInfo(component= ComponentConstant.Sales.AUTO_CA_SELECT)
+    public void createPreconditions(@Optional("AZ") String state)
+    {
+        // Must create a helper class object. Cannot be static as it uses non-static methods and objects.
+        QAHelper _myHelper = new QAHelper();
+
+        // Create and Define Test Data To Use in Run
+        TestData _myTD = getTestSpecificTD("TestData_" + state);
+
+        // Launching App
+        mainApp().open();
+
+        // Create Customer with helper class obj.
+        _myHelper.createCustomer(true, _myTD);
+
+        // Creating a Bound Policy
+        createPolicy(_myTD);
+    }
+
+    @Parameters({"state"})
+    @Test(groups= {Groups.SMOKE})
+    @TestInfo(component= ComponentConstant.Sales.AUTO_CA_SELECT)
+    public void moveJVMTimeForward(@Optional("MT") String state)
+    {
+
+    }
+
 }
