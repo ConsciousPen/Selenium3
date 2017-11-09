6d44ed4da51bd9861b56568c963bde8670597c2a
g6fjemi <tyrone.jemison@csaa.com>
2017-11-09 09:32:30 -0700

    Deleted invalid Test Scenario. Created New Test scenario in Regression>Home_SS>H03>functional

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ss/ho3/functional/ValidateMembershipErrorMsg.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ss/ho3/functional/ValidateMembershipErrorMsg.java
@@ -1,18 +1,24 @@
-package aaa.modules.functionalTests;
+package aaa.modules.regression.sales.home_ss.ho3.functional;
 
-import aaa.helpers.QAHelper;
 import aaa.helpers.constants.ComponentConstant;
-import aaa.helpers.constants.Groups;
-import aaa.main.modules.policy.IPolicy;
 import aaa.main.modules.policy.PolicyType;
-import aaa.main.modules.policy.auto_ss.defaulttabs.DocumentsAndBindTab;
-import aaa.main.modules.policy.auto_ss.defaulttabs.PurchaseTab;
-import aaa.modules.policy.AutoSSBaseTest;
+import aaa.main.modules.policy.home_ss.defaulttabs.PurchaseTab;
+import aaa.modules.policy.HomeSSHO3BaseTest;
 import org.testng.annotations.*;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
 
-public class Membership_NBValidation extends AutoSSBaseTest
+/**
+ * @author Tyrone Jemison
+ * @name Validate Membership Error Message
+ * @scenario
+ * 1. GIVEN an agent binding a transaction of NB, Endorsement, Manual Renewal...
+ * 2. GIVEN member number is 'Active'.
+ * 3. WHEN there is NO match in FIRST or LAST or DOB between RMS and Member/Driver on Quote/Policy.
+ * 4. THEN UW Eligibility rules fire and display Error Message:
+ * 'Membership Validation Failed. Please review the Membership Report and confirm Member details. Rule can be overridden with Level 4 Authority'
+ */
+public class ValidateMembershipErrorMsg extends HomeSSHO3BaseTest
 {
     // Scenario-Level variables to carry data throughout test.
     String _customerNumber;
@@ -37,9 +43,10 @@ public class Membership_NBValidation extends AutoSSBaseTest
     {
         log.info(">Running Test TC01_createCustomer()...<");
         // Get Test Data and create customer.
-        TestData _td = getTestSpecificTD("TestDataBlock");
-        _customerNumber = createCustomerIndividual(_td);
+        TestData _tdCustomer = getTestSpecificTD("TestData_Customer");
+        _customerNumber = createCustomerIndividual(_tdCustomer);
         log.info(">Completed Test TC01_createCustomer()<");
+        log.info("Customer Number = " + _customerNumber);
     }
 
     @Parameters({"state"})
@@ -48,7 +55,7 @@ public class Membership_NBValidation extends AutoSSBaseTest
     public void TC02_initiatePolicy(@Optional("") String state)
     {
         log.info(">Running Test TC02_initiatePolicy()...<");
-        TestData _td = getTestSpecificTD("TestDataBlock");
+        TestData _td = getTestSpecificTD("TestData");
         _policyType.get().initiate();
         _policyType.get().getDefaultView().fillUpTo(_td, PurchaseTab.class, true);
         log.info(">Completed Test TC02_initiatePolicy()<");
@@ -60,7 +67,7 @@ public class Membership_NBValidation extends AutoSSBaseTest
     public void TC03_verifyMembershipErrorMsg(@Optional("") String state)
     {
         log.info(">Running Test TC03_verifyMembershipErrorMsg()...<");
-        TestData _td = getTestSpecificTD("TestDataBlock");
+        TestData _td = getTestSpecificTD("TestData");
         // Call Custom Method to Verify Presence of Error Message.
         log.info(">Completed Test TC03_verifyMembershipErrorMsg()<");
     }
@@ -77,5 +84,4 @@ public class Membership_NBValidation extends AutoSSBaseTest
         }
         log.info(">Completed @AfterClass testClose()<");
     }
-
 }
