83249c127e50dd49342b82193d198925bd5d5785
dchubkov <dchubkov@eisgroup.com>
2017-11-13 20:30:37 +0200

    applied patch fixes

--- aaa-automation-tests/src/test/java/aaa/modules/e2e/templates/Scenario10.java
+++ aaa-automation-tests/src/test/java/aaa/modules/e2e/templates/Scenario10.java
@@ -139,7 +139,7 @@ public class Scenario10 extends ScenarioBaseTest {
 		BillingSummaryPage.showPriorTerms();
 		new BillingAccountPoliciesVerifier().setPolicyStatus(PolicyStatus.POLICY_ACTIVE).verifyRowWithEffectiveDate(policyEffectiveDate);
 		new BillingAccountPoliciesVerifier().setPolicyStatus(PolicyStatus.PROPOSED).verifyRowWithEffectiveDate(policyExpirationDate);
-		verifyRenewOfferGenerated(policyExpirationDate, installmentDueDates);
+		verifyRenewOfferGenerated(installmentDueDates);
 		new BillingPaymentsAndTransactionsVerifier().setTransactionDate(renewOfferGenDate)
 			.setSubtypeReason(PaymentsAndOtherTransactionSubtypeReason.RENEWAL_POLICY_RENEWAL_PROPOSAL).verifyPresent();
 
