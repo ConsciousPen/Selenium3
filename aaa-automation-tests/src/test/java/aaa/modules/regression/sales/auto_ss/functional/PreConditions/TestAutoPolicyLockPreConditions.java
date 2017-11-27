bf873b755c6f22c39e7509d49d14a803f53a0034
Caprara, Steve <steve.caprara@csaa.com>
2017-11-27 08:31:40 -0700

    Testing a change in git using comments and new line

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/PreConditions/TestAutoPolicyLockPreConditions.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/PreConditions/TestAutoPolicyLockPreConditions.java
@@ -1,10 +1,10 @@
 package aaa.modules.regression.sales.auto_ss.functional.PreConditions;
 
 public interface TestAutoPolicyLockPreConditions {
-    String INSERT_QUERY = "INSERT INTO lookupValue "
-            + "(lookUpList_id,dType,code,displayValue,effective,expiration,productCD,riskStateCD) "
-            + "VALUES (%s,'AAAFactorsLockLookupValue','%s','TRUE',%s,null,'AAA_SS','%s')";
+	String INSERT_QUERY = "INSERT INTO lookupValue "
+			+ "(lookUpList_id,dType,code,displayValue,effective,expiration,productCD,riskStateCD) "
+			+ "VALUES (%s,'AAAFactorsLockLookupValue','%s','TRUE',%s,null,'AAA_SS','%s')";
 
-    String DELETE_QUERY = "DELETE FROM lookupValue lv "
-            + "WHERE lv.lookupList_id IN %s AND CODE = '%s' AND DISPLAYVALUE='TRUE' AND EFFECTIVE=%s AND RISKSTATECD='%s'";
+	String DELETE_QUERY = "DELETE FROM lookupValue lv "
+			+ "WHERE lv.lookupList_id IN %s AND CODE = '%s' AND DISPLAYVALUE='TRUE' AND EFFECTIVE=%s AND RISKSTATECD='%s'";
 }
