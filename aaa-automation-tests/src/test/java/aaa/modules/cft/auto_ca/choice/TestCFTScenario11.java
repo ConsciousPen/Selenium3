c9f14937f28ab6ff1964ca6ca505ac55c084754a
jkuznecov <jkuznecov@eisgroup.com>
2017-12-11 17:57:43 +0200

    Test CFT Scenario11 implementation

---
+++ aaa-automation-tests/src/test/java/aaa/modules/cft/auto_ca/choice/TestCFTScenario11.java
@@ -0,0 +1,54 @@
+package aaa.modules.cft.auto_ca.choice;
+
+import org.apache.commons.lang3.StringUtils;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import toolkit.datax.TestData;
+import toolkit.utils.TestInfo;
+import aaa.helpers.constants.Groups;
+import aaa.main.metadata.policy.AutoCaMetaData;
+import aaa.main.modules.policy.PolicyType;
+import aaa.main.modules.policy.auto_ca.defaulttabs.PremiumAndCoveragesTab;
+import aaa.main.modules.policy.auto_ca.defaulttabs.PurchaseTab;
+import aaa.modules.cft.ControlledFinancialBaseTest;
+
+/**
+ * Controlled Financial Testing Scenario 11
+ * Automatic Cancel
+ * Collections
+ * Standard Monthly
+ * Cash $ 10 as down payment
+ */
+public class TestCFTScenario11 extends ControlledFinancialBaseTest {
+
+	@Test(groups = {Groups.CFT})
+	@TestInfo(component = Groups.CFT)
+	@Parameters({STATE_PARAM})
+	public void cftTestScenario11(@Optional(StringUtils.EMPTY) String state) {
+		createPolicyForTest();
+		endorsePolicyOnStartDatePlus2();
+		generateInstallmentBill(1);
+		automaticCancellationNotice(1);
+		automaticCancellation(1);
+		generateFirstEarnedPremiumBill(1);
+		generateSecondEarnedPremiumBill(1);
+		generateThirdEarnedPremiumBill(1);
+		generateCollection(1);
+	}
+
+	@Override
+	protected PolicyType getPolicyType() {
+		return PolicyType.AUTO_CA_CHOICE;
+	}
+
+	@Override
+	protected TestData getPolicyTestData() {
+		TestData td = getStateTestData(testDataManager.policy.get(getPolicyType()), "DataGather", DEFAULT_TEST_DATA_KEY);
+		td.adjust(TestData.makeKeyPath(PremiumAndCoveragesTab.class.getSimpleName(), AutoCaMetaData.PremiumAndCoveragesTab.PAYMENT_PLAN.getLabel()), getTestSpecificTD(
+			"PremiumAndCoveragesTab_DataGather").getValue(AutoCaMetaData.PremiumAndCoveragesTab.PAYMENT_PLAN.getLabel()));
+		td.adjust(PurchaseTab.class.getSimpleName(), getTestSpecificTD("PurchaseTab_DataGather"));
+		return td.resolveLinks();
+	}
+}
