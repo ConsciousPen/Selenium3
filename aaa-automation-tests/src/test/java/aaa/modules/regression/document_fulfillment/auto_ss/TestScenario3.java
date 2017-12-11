5e843d68e86328073f3ea2c8651518a1790d1cb9
qyu <qing.yu@eisgroup.com>
2017-12-11 16:53:08 +0800

    Docgen tests - Add AA02DC, AHRBXX

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/auto_ss/TestScenario3.java
@@ -0,0 +1,61 @@
+package aaa.modules.regression.document_fulfillment.auto_ss;
+
+import java.time.LocalDateTime;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.docgen.DocGenHelper;
+import aaa.helpers.http.HttpStub;
+import aaa.helpers.jobs.JobUtils;
+import aaa.helpers.jobs.Jobs;
+import aaa.main.enums.DocGenEnum.Documents;
+import aaa.main.pages.summary.PolicySummaryPage;
+import aaa.modules.policy.AutoSSBaseTest;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+
+/**
+ * Check AA02DC, AHRBXX for DC
+ * @author qyu
+ *
+ */
+public class TestScenario3 extends AutoSSBaseTest {
+	private String policyNumber;
+	private LocalDateTime policyExpirationDate;
+	
+	@Parameters({ "state" })
+	@Test(groups = { Groups.DOCGEN, Groups.CRITICAL })
+	public void TC01_CreatePolicy(@Optional("") String state) {
+		mainApp().open();
+		policyNumber = getCopiedPolicy();
+		policyExpirationDate = PolicySummaryPage.getExpirationDate();
+	}
+	
+	@Parameters({ "state" })
+	@Test(groups = { Groups.DOCGEN, Groups.CRITICAL }, dependsOnMethods = "TC01_CreatePolicy")
+	public void TC02_RenewImageGeneration(@Optional("") String state) {
+		TimeSetterUtil.getInstance().nextPhase(getTimePoints().getRenewImageGenerationDate(policyExpirationDate));
+		JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
+		HttpStub.executeAllBatches();
+		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+	}	
+	
+	@Parameters({ "state" })
+	@Test(groups = { Groups.DOCGEN, Groups.CRITICAL }, dependsOnMethods = "TC01_CreatePolicy")
+	public void TC03_RenewOfferGeneration(@Optional("") String state) {
+		TimeSetterUtil.getInstance().nextPhase(getTimePoints().getRenewOfferGenerationDate(policyExpirationDate));
+		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+		JobUtils.executeJob(Jobs.aaaDocGenBatchJob);
+		DocGenHelper.verifyDocumentsGenerated(true, true, policyNumber, Documents.AA02DC);
+	}
+	
+	@Parameters({ "state" })
+	@Test(groups = { Groups.DOCGEN, Groups.CRITICAL })
+	public void TC04_RenewaBillGeneration(@Optional("") String state) {
+		TimeSetterUtil.getInstance().nextPhase(getTimePoints().getBillGenerationDate(policyExpirationDate));
+		JobUtils.executeJob(Jobs.aaaRenewalNoticeBillAsyncJob);
+		JobUtils.executeJob(Jobs.aaaDocGenBatchJob);
+		DocGenHelper.verifyDocumentsGenerated(true, true, policyNumber, Documents.AHRBXX);
+	}
+
+}
