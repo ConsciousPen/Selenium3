059f2373a60a7986d1dc756b79a1d722d84bed2e
dchubkov <dchubkov@eisgroup.com>
2017-12-20 20:08:45 +0200

    added constructor with state argument in TestDataGenerator, small refactoring

--- aaa-automation-ui/src/main/java/aaa/utils/excel/io/entity/TableCell.java
+++ aaa-automation-ui/src/main/java/aaa/utils/excel/io/entity/TableCell.java
@@ -1,38 +1,30 @@
 package aaa.utils.excel.io.entity;
 
-import java.util.Arrays;
 import org.apache.poi.ss.usermodel.Cell;
-import aaa.utils.excel.io.celltype.CellType;
 
 public class TableCell extends ExcelCell {
-	private TableRow tableRow;
 
-	public TableCell(Cell cell, TableRow tableRow, CellType<?>... allowableCellTypes) {
-		super(cell, tableRow.getSheet(), allowableCellTypes);
-		this.tableRow = tableRow;
-	}
-
-	public TableRow getTableRow() {
-		return tableRow;
-	}
-
-	public String getHeaderColumnName() {
-		return getTableRow().getTable().getHeader().getColumnName(getColumnNumber());
+	public TableCell(Cell cell, TableRow tableRow) {
+		super(cell, tableRow);
 	}
 
 	@Override
-	public int getRowNumber() {
-		return getTableRow().getRowNumber();
+	public TableRow getRow() {
+		return (TableRow) super.getRow();
+	}
+
+	public String getHeaderColumnName() {
+		return getRow().getTable().getHeader().getColumnName(getColumnNumber());
 	}
 
 	@Override
 	public String toString() {
 		return "ExcelCell{" +
-				"Sheet name=" + getPoiCell().getSheet().getSheetName() +
+				"Sheet name=" + getRow().getSheet().getSheetName() +
 				", Row number=" + getRowNumber() +
 				", Column number=" + getColumnNumber() +
 				", Header column name=" + getHeaderColumnName() +
-				", Cell Types=" + Arrays.toString(getCellTypes()) +
+				", Cell Types=" + getCellTypes() +
 				", Cell value=" + getStringValue() +
 				'}';
 	}
