4577fa3d95f8ccc36b648ab1dccc44f9821b6600
eaj7sta <ostasyuk@eisgroup.com>
2017-12-21 19:45:42 -0700

    fix after merge from PAS18.1 + suite

--- aaa-automation-ui/src/main/java/aaa/main/modules/policy/abstract_tabs/Purchase.java
+++ aaa-automation-ui/src/main/java/aaa/main/modules/policy/abstract_tabs/Purchase.java
@@ -12,18 +12,24 @@ import aaa.toolkit.webdriver.customcontrols.PaymentMethodAllocationControl;
 import toolkit.datax.DataProviderFactory;
 import toolkit.datax.TestData;
 import toolkit.webdriver.controls.Button;
+import toolkit.webdriver.controls.Link;
 import toolkit.webdriver.controls.StaticElement;
 import toolkit.webdriver.controls.composite.assets.metadata.MetaData;
 import toolkit.webdriver.controls.composite.table.Table;
 import toolkit.webdriver.controls.waiters.Waiters;
 
 public abstract class Purchase extends Tab {
-    public Table tablePaymentPlan = new Table(By.id("purchaseForm:PaymentPlanTable"));
-    public Button btnApplyPayment = new Button(By.id("purchaseForm:finishBtn_footer"), Waiters.AJAX);
-    public Dialog confirmPurchase = new Dialog("//div[@id='purchaseForm:FinishConfirmationDialog_container']");
-    public Dialog confirmVoiceSignature= new Dialog("//div[@id='purchaseForm:VoiceSignatureDialog_container']");
-    public StaticElement totalRemainingTermPremium = new StaticElement(By.id("purchaseForm:downpaymentComponent_totalRemainingDueValue"));
-    public StaticElement remainingBalanceDueToday = new StaticElement(By.id("purchaseForm:downpaymentComponent_remainingBalanceValue"));
+    public static Table tablePaymentPlan = new Table(By.id("purchaseForm:PaymentPlanTable"));
+    public static Button btnApplyPayment = new Button(By.id("purchaseForm:finishBtn_footer"), Waiters.AJAX);
+    public static Dialog confirmPurchase = new Dialog("//div[@id='purchaseForm:FinishConfirmationDialog_container']");
+    public static Dialog confirmVoiceSignature= new Dialog("//div[@id='purchaseForm:VoiceSignatureDialog_container']");
+    public static StaticElement totalRemainingTermPremium = new StaticElement(By.id("purchaseForm:downpaymentComponent_totalRemainingDueValue"));
+    public static StaticElement remainingBalanceDueToday = new StaticElement(By.id("purchaseForm:downpaymentComponent_remainingBalanceValue"));
+
+    public static Table autoPaySetupSavingMessage = new Table (By.id("purchaseForm:installmentFeeAmountSavedPanel"));
+	public static Link linkViewApplicableFeeSchedule = new Link(By.id("purchaseForm:installmentFeeDetails"), Waiters.AJAX);
+	public static Table tableInstallmentFeeDetails = new Table(By.id("policyDataGatherForm:installmentFeeDetailsTable"));
+
 
     protected Purchase(Class<? extends MetaData> mdClass) {
         super(mdClass);
@@ -54,7 +60,7 @@ public abstract class Purchase extends Tab {
     @Override
     public Tab fillTab(TestData td) {
         ErrorTab errorTab = new ErrorTab();
-        if(errorTab.getErrorCodesList().contains(ErrorEnum.Errors.ERROR_AAA_AUTO_SS_MEM_LASTNAME.getCode())){
+        if(errorTab.isVisible() && errorTab.getErrorCodesList().contains(ErrorEnum.Errors.ERROR_AAA_AUTO_SS_MEM_LASTNAME.getCode())){
             errorTab.overrideErrors(ErrorEnum.Errors.ERROR_AAA_AUTO_SS_MEM_LASTNAME);
             DocumentsAndBindTab.btnPurchase.click();
             DocumentsAndBindTab.confirmPurchase.buttonYes.click();
