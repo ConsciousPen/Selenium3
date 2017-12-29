0af9c48177916ec94a377d359ed966c7c0ec2181
Viktor Petrenko <vpetrenko@eisgroup.com>
2017-12-29 10:42:59 -0700

    small changes for partial match

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestVINUpload.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestVINUpload.java
@@ -1,5 +1,6 @@
 package aaa.modules.regression.sales.auto_ss.functional;
 
+import static aaa.modules.regression.postconditions.TestVinUploadPostConditions.SELECT_VIN_NUMBER_BY_DATA;
 import static toolkit.verification.CustomAssertions.assertThat;
 import static toolkit.verification.CustomSoftAssertions.assertSoftly;
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -23,6 +25,7 @@ import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
+import aaa.main.enums.ProductConstants;
 import aaa.main.enums.SearchEnum;
 import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.main.modules.billing.account.BillingAccount;
@@ -34,6 +37,7 @@ import aaa.modules.regression.sales.auto_ss.functional.helpers.TestVinUploadHelp
 import aaa.modules.regression.sales.common_helpers.VinUploadCommonMethods;
 import toolkit.datax.DataProviderFactory;
 import toolkit.datax.TestData;
+import toolkit.db.DBService;
 import toolkit.utils.TestInfo;
 import toolkit.webdriver.controls.TextBox;
 
@@ -553,24 +557,42 @@ public class TestVINUpload extends TestVinUploadHelper {
 	public void pas730_MSRPRefreshCompColl(@Optional("UT") String state) {
 		String vinTableFile = vinMethods.getSpecificUploadFile(VinUploadCommonMethods.UploadFilesTypes.ADDED_VIN.get());
 
-		TestData testData = getPolicyTD().adjust(getTestSpecificTD("TestData").resolveLinks())
-				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.VIN.getLabel()), "7MSRP17H0V1234567")
-				//.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.TYPE.getLabel()), "Passenger Van")
-				//.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), "Change Vehicle Confirmation"), "OK")
-				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.STAT_CODE.getLabel()), "Passenger Van");
+		String vehYear = "2015";
+		String vehMake = "VOLKSWAGEN";
+		String vehModel = "GOLF";
+		String vehSeries = "GOLF";
+		String vehBodyStyle = "HATCHBACK 4 DOOR";
+		String vin = String.format(SELECT_VIN_NUMBER_BY_DATA,vehYear,vehMake,vehModel,vehSeries,vehBodyStyle);
+		TestData testData = getPolicyTD()
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.VIN.getLabel()), "")
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.YEAR.getLabel()), vehYear)
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.MAKE.getLabel()), vehMake)
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.MODEL.getLabel()), vehModel)
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.SERIES.getLabel()), vehSeries)
+				.adjust(TestData.makeKeyPath(vehicleTab.getMetaKey(), AutoSSMetaData.VehicleTab.BODY_STYLE.getLabel()),vehBodyStyle);
 
+		//Uploading of VinUpload info, then uploading of the updates for VIN_Control table
 		precondsTestVINUpload(testData, PremiumAndCoveragesTab.class);
 
 		PremiumAndCoveragesTab.buttonViewRatingDetails.click();
+		// Values from VIN comp and coll symbol in excel sheet
+		assertSoftly(softly -> {
+			softly.assertThat(PremiumAndCoveragesTab.tableRatingDetailsVehicles.getRow(1, "Comp Symbol").getCell(2).getValue()).isNotEqualTo("44");
+			softly.assertThat(PremiumAndCoveragesTab.tableRatingDetailsVehicles.getRow(1, "Coll Symbol").getCell(2).getValue()).isNotEqualTo("35");
+
+		});
 		String compSymbol = PremiumAndCoveragesTab.tableRatingDetailsVehicles.getRow(1, "Comp Symbol").getCell(2).getValue();
 		String collSymbol = PremiumAndCoveragesTab.tableRatingDetailsVehicles.getRow(1, "Coll Symbol").getCell(2).getValue();
 		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 
 		VehicleTab.buttonSaveAndExit.click();
 
+		Map<String, String> vinInfo = DBService.get().getRow(String.format(SELECT_VIN_NUMBER_BY_DATA,vehYear,vehMake,vehModel,vehSeries,vehBodyStyle));
+
 		String quoteNumber = PolicySummaryPage.labelPolicyNumber.getValue();
 
 		//Uploading of VinUpload info, then uploading of the updates for VIN_Control table
+		// Vin control table has version which overrides VERSION_2000, it is needed and important to get symbols for next steps
 		vinMethods.uploadFiles(vinTableFile);
 
 		//Go back to MainApp, open quote, calculate premium and verify if VIN value is applied
@@ -583,6 +605,26 @@ public class TestVINUpload extends TestVinUploadHelper {
 
 		});
 		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
+	}
+
+	@Parameters({"state"})
+	@Test(groups = { Groups.SMOKE, Groups.REGRESSION, Groups.BLOCKER })
+	@TestInfo(component = ComponentConstant.Sales.AUTO_SS)
+	public void testPolicyCreation(@Optional("SD") String state) {
+		TestData testData = getTestDataWithMembershipSinceDate(getPolicyTD());
+		TestData testdataPolicyInformation = testData.getTestData("GeneralTab").getTestData("PolicyInformation")
+				.adjust("Effective Date","01/01/2018")
+				.adjust("Override ASD Level","Yes")
+				.adjust("Advance Shopping Discount Override","Level 1")
+				.adjust("ASD Overriden By","QA");
+		TestData testDataGeneralTab = testData.getTestData("GeneralTab")
+				.adjust("PolicyInformation",testdataPolicyInformation);
+
+		mainApp().open();
+		createCustomerIndividual();
+		createPolicy(testData.adjust("GeneralTab",testDataGeneralTab));
+
+		PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
 	}
 
 	public void killChromeDrivers() throws IOException {
