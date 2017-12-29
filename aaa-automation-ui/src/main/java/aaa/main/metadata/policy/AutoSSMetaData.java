bf2a9f4815942d24dac3feee90b88ce6aea2aedb
Anton <a.perepecha@yandex.ru>
2017-12-29 11:49:38 +0300

    fix after merge

--- aaa-automation-ui/src/main/java/aaa/main/metadata/policy/AutoSSMetaData.java
+++ aaa-automation-ui/src/main/java/aaa/main/metadata/policy/AutoSSMetaData.java
@@ -356,13 +356,14 @@ public final class AutoSSMetaData {
 			public static final AssetDescriptor<StaticElement> STATUS = declare("Status", StaticElement.class);
 			public static final AssetDescriptor<Link> ACTION = declare("Action", Link.class);
 
-			public static final AssetDescriptor<AssetList> ADD_MEMBER_SINCE_DIALOG = declare("AddMemberSinceDialog", AssetList.class, AddMemberSinceDialog.class,  By.xpath("//div[@id='memberSinceDatePopup_container']"));
+			public static final AssetDescriptor<AssetList> ADD_MEMBER_SINCE_DIALOG =
+					declare("AddMemberSinceDialog", AssetList.class, AddMemberSinceDialog.class, By.xpath(".//div[@id='memberSinceDatePopup_container']"));
 		}
 
 		public static final class AddMemberSinceDialog extends MetaData {
-			public static final AssetDescriptor<TextBox> MEMBER_SINCE = declare("Member Since", TextBox.class);
-			public static final AssetDescriptor<Button> BTN_OK = declare("OK", Button.class, By.xpath("//input[@id='memberSinceDateFrom:addMemberSinceDateButton']"));
-			public static final AssetDescriptor<Button> BTN_CANCEL = declare("Cancel", Button.class, By.xpath("//input[@id='memberSinceDateFrom:cancelMemberSinceDateButton']"));
+			public static final AssetDescriptor<TextBox> MEMBER_SINCE = declare("Member Since", TextBox.class, By.xpath(".//input[@id='memberSinceDateFrom:popupMemberSinceDateInputDate']"));
+			public static final AssetDescriptor<Button> BTN_OK = declare("OK", Button.class, By.xpath(".//input[@id='memberSinceDateFrom:addMemberSinceDateButton']"));
+			public static final AssetDescriptor<Button> BTN_CANCEL = declare("Cancel", Button.class, By.xpath(".//input[@id='memberSinceDateFrom:cancelMemberSinceDateButton']"));
 		}
 
 		public static final class OrderInsuranceScoreReportRow extends MetaData {
