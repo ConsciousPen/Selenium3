edaff215450931660305f62476749a6f569920bb
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-01-08 14:44:54 -0700

    PAS-7242 Created testing for ability to override rules that fire with violations in past 36 months:
    BaseTest
    *removed createCustomerIndividual from getPrimaryPoliciesForPup, a customer must already be created to use this method (as is stated in the comments) and it causes it to create 2 customers
    
    ConvPUPBaseTest and TestBindPupWithoutAuto
    *refactored verifyErrorsOverrideAndBind method
    
    TestOverrideBindWithPropertyLosses java and yaml files created
    *testing for NB and Conversion
    
    Corresponding updates to PersonalUmbrellaMetaData and ErrorEnum classes

--- aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/ConvPUPBaseTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/ConvPUPBaseTest.java
@@ -1,7 +1,13 @@
 package aaa.modules.conversion.manual;
 
+import aaa.main.enums.ErrorEnum;
+import aaa.main.enums.ProductConstants;
 import aaa.main.metadata.CustomerMetaData;
 import aaa.main.modules.customer.actiontabs.InitiateRenewalEntryActionTab;
+import aaa.main.modules.policy.pup.defaulttabs.BindTab;
+import aaa.main.modules.policy.pup.defaulttabs.ErrorTab;
+import aaa.main.modules.policy.pup.defaulttabs.PurchaseTab;
+import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PersonalUmbrellaBaseTest;
 import toolkit.datax.TestData;
 
@@ -10,6 +16,9 @@ import toolkit.datax.TestData;
  * This class houses methods specific to PUP conversion policies and their corresponding test data requirements.
  */
 public class ConvPUPBaseTest extends PersonalUmbrellaBaseTest implements ManualConversionHelper {
+
+    private ErrorTab errorTab = policy.getDefaultView().getTab(ErrorTab.class);
+    private PurchaseTab purchaseTab = policy.getDefaultView().getTab(PurchaseTab.class);
 
     @Override
     protected TestData getManualConversionInitiationTd() {
@@ -36,5 +45,19 @@ public class ConvPUPBaseTest extends PersonalUmbrellaBaseTest implements ManualC
 
     protected TestData getPupConversionTdNoPolicyCreation() {
         return modifyConversionPolicyDefaultTD(getPolicyTD(), getPolicyType());
+    }
+
+    /**
+     * @param errors the specific error(s) from ErrorEnum.Errors class to be verified
+     * @param td the test data to be used to finish binding the policy
+     * @return boolean value representing error verification results
+     */
+    protected void verifyErrorsOverrideAndBind(TestData td, ErrorEnum.Errors... errors) {
+        errorTab.verify.errorsPresent(errors);
+        errorTab.overrideAllErrors();
+        errorTab.override();
+        policy.getDefaultView().fillFromTo(td, BindTab.class, PurchaseTab.class, true);
+        purchaseTab.submitTab();
+        PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
     }
 }
--- aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/pup/TestBindPupWithoutAuto.java
+++ aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/pup/TestBindPupWithoutAuto.java
@@ -4,14 +4,11 @@ package aaa.modules.conversion.manual.pup;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.main.enums.ErrorEnum;
-import aaa.main.enums.ProductConstants;
 import aaa.main.metadata.policy.HomeSSMetaData;
 import aaa.main.metadata.policy.PersonalUmbrellaMetaData;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.home_ss.defaulttabs.ApplicantTab;
 import aaa.main.modules.policy.pup.defaulttabs.BindTab;
-import aaa.main.modules.policy.pup.defaulttabs.ErrorTab;
-import aaa.main.modules.policy.pup.defaulttabs.PurchaseTab;
 import aaa.main.modules.policy.pup.defaulttabs.PrefillTab;
 import aaa.main.modules.policy.pup.defaulttabs.UnderlyingRisksAutoTab;
 import aaa.main.pages.summary.PolicySummaryPage;
@@ -21,13 +18,10 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
-import static toolkit.verification.CustomAssertions.assertThat;
 
 public class TestBindPupWithoutAuto extends ConvPUPBaseTest {
 
     private BindTab bindTab = policy.getDefaultView().getTab(BindTab.class);
-    private ErrorTab errorTab = policy.getDefaultView().getTab(ErrorTab.class);
-    private PurchaseTab purchaseTab = policy.getDefaultView().getTab(PurchaseTab.class);
 
     /**
      * @author Josh Carpenter
@@ -66,9 +60,7 @@ public class TestBindPupWithoutAuto extends ConvPUPBaseTest {
         policy.getDefaultView().fillUpTo(tdPUP, BindTab.class, true);
         bindTab.submitTab();
 
-        verifyOverrideErrorAndBind(tdPUP);
-
-        PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
+        verifyErrorsOverrideAndBind(tdPUP, ErrorEnum.Errors.ERROR_AAA_PUP_SS5071440);
     }
 
     /**
@@ -109,9 +101,7 @@ public class TestBindPupWithoutAuto extends ConvPUPBaseTest {
         policy.getDefaultView().fillUpTo(tdPUP, BindTab.class, true);
         bindTab.submitTab();
 
-        verifyOverrideErrorAndBind(tdPUP);
-
-        PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
+        verifyErrorsOverrideAndBind(tdPUP, ErrorEnum.Errors.ERROR_AAA_PUP_SS5071440);
     }
 
     /**
@@ -120,17 +110,5 @@ public class TestBindPupWithoutAuto extends ConvPUPBaseTest {
     private TestData getTdHome() {
         return getStateTestData(testDataManager.policy.get(PolicyType.HOME_SS_HO3).getTestData("DataGather"), "TestData_NJ")
                 .mask(TestData.makeKeyPath(ApplicantTab.class.getSimpleName(), HomeSSMetaData.ApplicantTab.OTHER_ACTIVE_AAA_POLICIES.getLabel()));
-    }
-
-    /**
-     * Verifies the expected error message, overrides, and finishes binding the policy
-     * @param td Test Data to be used to finish binding the PUP policy
-     */
-    private void verifyOverrideErrorAndBind(TestData td) {
-        errorTab.verify.errorsPresent(ErrorEnum.Errors.ERROR_AAA_PUP_SS5071440);
-        errorTab.overrideAllErrors();
-        errorTab.override();
-        policy.getDefaultView().fillFromTo(td, BindTab.class, PurchaseTab.class, true);
-        purchaseTab.submitTab();
     }
 }
