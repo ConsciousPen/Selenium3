b08b20e7cc8d630a95e01ad4200b217038760638
Stasyuk, Oleg (C) <ostasyuk@eisgroup.com>
2018-01-09 12:18:47 -0700

    Dev smoke fix

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/PreconditionsSetup.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/PreconditionsSetup.java
@@ -15,19 +15,10 @@ public class PreconditionsSetup implements EvalueInsertSetupPreConditions {
             + "set indx = %s\n"
             + "where ENTITYCLASS = 'com.exigen.ipb.policy.domain.PolicySummary'";
 
-    private static final String GET_POLICY_NUMBER_RANGE = "select indx from NumberRange\n"
-            + "where ENTITYCLASS = 'com.exigen.ipb.policy.domain.PolicySummary'";
-
     private static final String GET_DAY_OF_WEEK = "select to_char(sysdate,'D') from dual";
 
-    @Test(description = "Delete old tasks")
-    public static void deleteOldTasksUpdate() {
-        DBService.get().executeUpdate(DELETE_OLD_TASKS1);
-        DBService.get().executeUpdate(DELETE_OLD_TASKS2);
-    }
-
     @Test(description = "updates number range for policies to start with")
-    public static void numberRangeUpdate() {
+    public void numberRangeUpdate() {
 
         String env = PropertyProvider.getProperty(CustomTestProperties.APP_HOST);
         String dayOfWeek = DBService.get().getValue(GET_DAY_OF_WEEK).get();
@@ -68,56 +59,48 @@ public class PreconditionsSetup implements EvalueInsertSetupPreConditions {
         }
     }
 
-    private static void setNumberRangeForPolicy(String s) {
+    private void setNumberRangeForPolicy(String s) {
         DBService.get().executeUpdate(String.format(SET_POLICY_NUMBER_RANGE, s));
     }
-}
 
-enum Envss {
+    enum Envss {
 
-    NVD7PASQATRG037("nvd7pasqatrg037", "1", "0"),
-    NVD7PASQATRG038("nvd7pasqatrg038", "1", "1"),
-    NVD7PASQATRG039("nvd7pasqatrg039", "1", "2"),
-    NVD7PASQATRG041("nvd7pasqatrg041", "1", "3"),
-    NVM7PAS546("nvm7pas546", "1", "4"),
-    VDD7S0PLYPAS044("vdd7s0plypas044", "1", "5"),
-    VDD7S0PLYPAS128("vdd7s0plypas128", "1", "6"),
-    NVM7PASQATRG010("NVM7PASQATRG010", "1", "7"),
-    NVM7PASQATRG029("nvm7pasqatrg029", "1", "8"),
-    NVM7PASQATRG030("nvm7pasqatrg030", "1", "9");
+        NVD7PASQATRG037("nvd7pasqatrg037", "1", "0"),
+        NVD7PASQATRG038("nvd7pasqatrg038", "1", "1"),
+        NVD7PASQATRG039("nvd7pasqatrg039", "1", "2"),
+        NVD7PASQATRG041("nvd7pasqatrg041", "1", "3"),
+        NVM7PAS546("nvm7pas546", "1", "4"),
+        VDD7S0PLYPAS044("vdd7s0plypas044", "1", "5"),
+        VDD7S0PLYPAS128("vdd7s0plypas128", "1", "6"),
+        NVM7PASQATRG010("NVM7PASQATRG010", "1", "7"),
+        NVM7PASQATRG029("nvm7pasqatrg029", "1", "8"),
+        NVM7PASQATRG030("nvm7pasqatrg030", "1", "9");
 
-    private final String envName;
-    private final String teamNum;
-    private final String envNum;
+        private final String envName;
+        private final String teamNum;
+        private final String envNum;
 
-    Envss(String envName, String teamNum, String envNum) {
-        this.envName = envName;
-        this.teamNum = teamNum;
-        this.envNum = envNum;
+        Envss(String envName, String teamNum, String envNum) {
+            this.envName = envName;
+            this.teamNum = teamNum;
+            this.envNum = envNum;
+        }
+
+        public String getEnvName() {
+            return envName;
+        }
+
+        public String getTeamNum() {
+            return teamNum;
+        }
+
+        public String getEnvNum() {
+            return envNum;
+        }
+
     }
-
-    public String getEnvName() {
-        return envName;
-    }
-
-
-
-    public String getTeamNum() {
-        return teamNum;
-    }
-
-
-
-    public String getEnvNum() {
-        return envNum;
-    }
-
-
 
 }
-
-
-
 
 
 
