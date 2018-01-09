6d2a57c07b5b964a80f0f7df5f819efc1b355ff6
ENT\gunuraz <dominykas.razgunas@csaa.com>
2018-01-09 14:33:38 -0700

    Added Organic Policy creation

--- aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/pup/TestOverrideIneligibleUnderlyingRisks.java
+++ aaa-automation-tests/src/test/java/aaa/modules/conversion/manual/pup/TestOverrideIneligibleUnderlyingRisks.java
@@ -9,10 +9,15 @@ import org.testng.annotations.Test;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.main.enums.ErrorEnum;
+import aaa.main.enums.ProductConstants;
+import aaa.main.metadata.CustomerMetaData;
 import aaa.main.metadata.policy.PersonalUmbrellaMetaData;
+import aaa.main.modules.customer.actiontabs.InitiateRenewalEntryActionTab;
+import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.pup.defaulttabs.BindTab;
 import aaa.main.modules.policy.pup.defaulttabs.ErrorTab;
 import aaa.main.modules.policy.pup.defaulttabs.PurchaseTab;
+import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.conversion.manual.ConvPUPBaseTest;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
@@ -20,23 +25,26 @@ import toolkit.utils.TestInfo;
 public class TestOverrideIneligibleUnderlyingRisks extends ConvPUPBaseTest {
 
 	private ErrorTab errorTab = policy.getDefaultView().getTab(ErrorTab.class);
+	private BindTab bindTab = policy.getDefaultView().getTab(BindTab.class);
+	private PurchaseTab purchaseTab = policy.getDefaultView().getTab(PurchaseTab.class);
 
 	/**
 	 * @author Dominykas Razgunas
 	 * @name
 	 * @scenario
 	 * 1. Create Conversion PUP Policy
-	 * 2. Propose Policy
+	 * 2. Select LLC and Trustee as 'Yes'
+	 * 3. Fill All tabs until Bind
+	 * 4. Override new error
+	 * 5. Bind The policy
 	 * @details
 	 */
 	@Parameters({"state"})
 	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.Sales.PUP, testCaseId = "")
-	public void pas6977_ConversionMAIGpup(@Optional("NJ") String state) {
-
+	@TestInfo(component = ComponentConstant.Sales.PUP, testCaseId = "PAS-6977")
+	public void pas6977_OverrideRulesForConversionMAIG(@Optional("NJ") String state) {
 
 		mainApp().open();
-
 
 		createCustomerIndividual();
 
@@ -47,16 +55,55 @@ public class TestOverrideIneligibleUnderlyingRisks extends ConvPUPBaseTest {
 				.adjust(TestData.makeKeyPath(PersonalUmbrellaMetaData.PrefillTab.class.getSimpleName(), PersonalUmbrellaMetaData.PrefillTab.NAMED_INSURED.getLabel() + "[0]",
 						PersonalUmbrellaMetaData.PrefillTab.NamedInsured.TRUSTEE.getLabel()), "Yes");
 
-		initiateManualConversion();
+		initiateManualConversion(getManualConversionInitiationTd().adjust(TestData.makeKeyPath(InitiateRenewalEntryActionTab.class.getSimpleName(), CustomerMetaData.InitiateRenewalEntryActionTab.RENEWAL_EFFECTIVE_DATE.getLabel()), "$<today+30d>"));
 		policy.getDefaultView().fillUpTo(testdata, BindTab.class);
-		new BindTab().saveAndExit();
+		overrideAndBind();
+	}
 
+	/**
+	 * @author Dominykas Razgunas
+	 * @name
+	 * @scenario
+	 * 1. Create PUP Policy
+	 * 2. Select LLC and Trustee as 'Yes'
+	 * 3. Fill All tabs until Bind
+	 * 4. Override new error
+	 * 5. Bind The policy
+	 * @details
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+	@TestInfo(component = ComponentConstant.Sales.PUP, testCaseId = "PAS-6977")
+	public void pas6977_OverrideRulesForPup(@Optional("NJ") String state) {
 
+		mainApp().open();
+
+		createCustomerIndividual();
+
+		TestData testdata = getPolicyDefaultTD();
+
+		testdata.adjust(TestData.makeKeyPath(PersonalUmbrellaMetaData.PrefillTab.class.getSimpleName(), PersonalUmbrellaMetaData.PrefillTab.NAMED_INSURED.getLabel() + "[0]",
+				PersonalUmbrellaMetaData.PrefillTab.NamedInsured.LLC.getLabel()), "Yes")
+				.adjust(TestData.makeKeyPath(PersonalUmbrellaMetaData.PrefillTab.class.getSimpleName(), PersonalUmbrellaMetaData.PrefillTab.NAMED_INSURED.getLabel() + "[0]",
+						PersonalUmbrellaMetaData.PrefillTab.NamedInsured.TRUSTEE.getLabel()), "Yes");
+
+		PolicyType.PUP.get().initiate();
+		policy.getDefaultView().fillUpTo(testdata, BindTab.class);
+		overrideAndBind();
+	}
+
+	private void overrideAndBind() {
+		bindTab.submitTab();
 		errorTab.verify.errorsPresent(ErrorEnum.Errors.ERROR_AAA_PUP_SS7160072);
 		errorTab.overrideAllErrors();
 		errorTab.override();
-		policy.getDefaultView().fillFromTo(testdata, BindTab.class, PurchaseTab.class, true);
-		new PurchaseTab().submitTab();
+		bindTab.submitTab();
+
+		if(!PolicySummaryPage.labelPolicyNumber.isPresent()){
+			purchaseTab.fillTab(getPolicyTD());
+			purchaseTab.submitTab();
+		}
+		PolicySummaryPage.labelPolicyStatus.verify.value(ProductConstants.PolicyStatus.POLICY_ACTIVE);
 	}
 }
 
