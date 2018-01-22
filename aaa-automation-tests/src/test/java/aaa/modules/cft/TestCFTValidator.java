415f1403e3aeed37d319f603236d54fe44fb7889
Kuznecov, Jurij (C) <jkuznecov@eisgroup.com>
2018-01-22 15:20:43 +0200

    fix CFT Validation

--- aaa-automation-tests/src/test/java/aaa/modules/cft/TestCFTValidator.java
+++ aaa-automation-tests/src/test/java/aaa/modules/cft/TestCFTValidator.java
@@ -81,6 +81,7 @@ public class TestCFTValidator extends ControlledFinancialBaseTest {
 		Waiters.SLEEP(15000).go(); // add agile wait till file occurs, awaitatility (IGarkusha added dependency, read in www)
 		// condition that download/remote download folder listfiles.size==2
 		// moving data from monitor to download dir
+		mainApp().reopen();
 		String remoteFileLocation = PropertyProvider.getProperty(REMOTE_DOWNLOAD_FOLDER_PROP);
 		if (StringUtils.isNotEmpty(remoteFileLocation)) {
 			String monitorInfo = TimeShiftTestUtil.getContext().getBrowser().toString();
