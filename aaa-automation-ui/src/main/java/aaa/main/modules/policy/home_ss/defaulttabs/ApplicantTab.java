2b6688186a3cd254afa3fafacdacbb95671fc8a0
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-01-31 09:29:51 -0700

    Revert "readded commit that was causing merge conflict"
    This reverts commit c5bf59f9666d8ed05196e3dc4b925bd16365d34d.

--- aaa-automation-ui/src/main/java/aaa/main/modules/policy/home_ss/defaulttabs/ApplicantTab.java
+++ aaa-automation-ui/src/main/java/aaa/main/modules/policy/home_ss/defaulttabs/ApplicantTab.java
@@ -7,7 +7,6 @@ package aaa.main.modules.policy.home_ss.defaulttabs;
 import org.openqa.selenium.By;
 import aaa.common.Tab;
 import aaa.main.metadata.policy.HomeSSMetaData;
-import aaa.toolkit.webdriver.customcontrols.MultiInstanceAfterAssetList;
 import toolkit.webdriver.controls.Button;
 import toolkit.webdriver.controls.composite.assets.AssetList;
 import toolkit.webdriver.controls.composite.table.Table;
@@ -33,8 +32,8 @@ public class ApplicantTab extends Tab {
         return this;
     }
     
-    public MultiInstanceAfterAssetList getNamedInsuredAssetList() {
-    	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.NAMED_INSURED.getLabel(), MultiInstanceAfterAssetList.class);
+    public AssetList getNamedInsuredAssetList() {
+    	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.NAMED_INSURED.getLabel(), AssetList.class);
 	}
     public AssetList getAAAMembershipAssetList() {
     	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.AAA_MEMBERSHIP.getLabel(), AssetList.class);
@@ -48,8 +47,8 @@ public class ApplicantTab extends Tab {
     public AssetList getNamedInsuredInfoAssetList() {
     	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.NAMED_INSURED_INFORMATION.getLabel(), AssetList.class);
 	}
-    public MultiInstanceAfterAssetList getOtherAAAPoliciesAssetList() {
-    	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.OTHER_ACTIVE_AAA_POLICIES.getLabel(), MultiInstanceAfterAssetList.class);
+    public AssetList getOtherAAAPoliciesAssetList() {
+    	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.OTHER_ACTIVE_AAA_POLICIES.getLabel(), AssetList.class);
 	}
     public AssetList getAgentInfoAssetList() {
     	return getAssetList().getAsset(HomeSSMetaData.ApplicantTab.AGENT_INFORMATION.getLabel(), AssetList.class);
