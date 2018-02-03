b17ae6b933dafcdf57a72016c4356fd1319aa1ec
Stasyuk, Oleg (C) <ostasyuk@eisgroup.com>
2018-02-02 18:48:08 -0700

    JDI_nightly_20180202 fixes

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/EvalueInsertSetup.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/EvalueInsertSetup.java
@@ -25,7 +25,7 @@ public class EvalueInsertSetup implements EvalueInsertSetupPreConditions {
 
 	@Test(description = "Precondition updating Payperless Preferences Endpoint to a Stub")
 	public static void paperlessPreferencesStubEndpointUpdate() {
-		DBService.get().executeUpdate(String.format(PAPERLESS_PREFERENCE_API_SERVICE_UPDATE, APP_HOST));
+		DBService.get().executeUpdate(String.format(PAPERLESS_PREFERENCE_API_SERVICE_UPDATE, APP_HOST, APP_STUB_URL));
 	}
 
 	@Test(description = "setting Agent/Agency check against Zip to stub")
