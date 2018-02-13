ff4d6ed59c7aeb9f1e2fdb906ff4af52e0918bf9
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-02-13 14:29:46 -0700

    Created Branch Created Test Added TestData for cancellation endorsement reinstatement

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMidTermReinstatementPointLock.java
@@ -0,0 +1,99 @@
+package aaa.modules.regression.sales.auto_ss.functional;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.time.LocalDateTime;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+import aaa.common.enums.NavigationEnum;
+import aaa.common.pages.NavigationPage;
+import aaa.common.pages.SearchPage;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.main.enums.SearchEnum;
+import aaa.main.modules.policy.auto_ss.defaulttabs.DriverActivityReportsTab;
+import aaa.main.modules.policy.auto_ss.defaulttabs.PremiumAndCoveragesTab;
+import aaa.main.modules.policy.auto_ss.defaulttabs.PurchaseTab;
+import aaa.main.pages.summary.PolicySummaryPage;
+import aaa.modules.policy.AutoSSBaseTest;
+import toolkit.datax.TestData;
+import toolkit.utils.TestInfo;
+
+public class TestMidTermReinstatementPointLock extends AutoSSBaseTest {
+	/**
+	* @author Dominykas Razgunas
+	* @name Check if Mid Term Endorsement does not include reinstatement point change
+	* @scenario
+	 * 1. Create customer
+	 * 2. Create Auto SS Policy and Save Reinstatement Points Score value
+	 * 3. Cancel Policy
+	 * 4. Change Time TO today+2Months
+	 * 5. Reinstate Policy
+	 * 6. Endorse Policy
+	 * 7. Navigate To P&C page
+	 * 8. Calculate Premium
+	 * 9. View Rating Details
+	 * 10. Assert That the Reinstatement Points are the same
+	 * 11. Change Local Time to CSD
+	* @details
+	*/
+	private PremiumAndCoveragesTab premiumAndCoveragesTab = new PremiumAndCoveragesTab();
+	private PurchaseTab purchaseTab = new PurchaseTab();
+
+	@Parameters({"state"})
+	@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
+	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-9687")
+	public void pas9687_MidTermReinstatementPointsLocked(@Optional("") String state) {
+
+		LocalDateTime reinstatementDate = TimeSetterUtil.getInstance().getCurrentTime().plusMonths(2);
+		LocalDateTime resetTime = TimeSetterUtil.getInstance().getCurrentTime().plusMinutes(1);
+
+		TestData testData = getPolicyTD();
+
+		mainApp().open();
+		createCustomerIndividual();
+		policy.initiate();
+
+		//Calculate premium save Reinstatement point score
+
+		policy.getDefaultView().fillUpTo(testData, PremiumAndCoveragesTab.class, true);
+		PremiumAndCoveragesTab.buttonViewRatingDetails.click();
+		String reinstatementHistory = PremiumAndCoveragesTab.tableRatingDetailsUnderwriting.getRow(6).getCell("Score").getValue();
+
+		// Issue Policy and cancel it
+
+		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
+		premiumAndCoveragesTab.submitTab();
+		policy.getDefaultView().fillFromTo(testData, DriverActivityReportsTab.class, PurchaseTab.class, true);
+		purchaseTab.submitTab();
+		String policyNumber = PolicySummaryPage.labelPolicyNumber.getValue();
+		policy.cancel().perform(getTestSpecificTD("TestData_Cancellation"));
+
+		//Change system date to get policy reinstated with lapse
+
+		TimeSetterUtil.getInstance().nextPhase(reinstatementDate);
+		mainApp().open();
+		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
+
+		//Reinstate policy
+
+		policy.reinstate().perform(getTestSpecificTD("TestData_Reinstatement"));
+
+		//Endorse policy and check reinstatementHistory score
+
+		policy.endorse().perform(getTestSpecificTD("TestData_Endorse"));
+
+		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.PREMIUM_AND_COVERAGES.get());
+		PremiumAndCoveragesTab.calculatePremium();
+		PremiumAndCoveragesTab.buttonViewRatingDetails.click();
+		assertThat(PremiumAndCoveragesTab.tableRatingDetailsUnderwriting.getRow(6).getCell("Score").getValue()).isEqualTo(reinstatementHistory);
+		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
+
+		//Reset Time to CSD
+
+		TimeSetterUtil.getInstance().nextPhase(resetTime);
+
+	}
+}
