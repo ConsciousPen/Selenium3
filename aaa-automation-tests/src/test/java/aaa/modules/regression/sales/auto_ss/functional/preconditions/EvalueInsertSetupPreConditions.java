aa22bc8c3207fb78d8cffd29949702e612c6be80
Stasyuk, Oleg (C) <ostasyuk@eisgroup.com>
2018-02-14 12:25:24 -0700

    default endpoint updated

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/preconditions/EvalueInsertSetupPreConditions.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/preconditions/EvalueInsertSetupPreConditions.java
@@ -96,7 +96,7 @@ public interface EvalueInsertSetupPreConditions {
 			+ "where propertyname in('aaaBillingAccountUpdateActionBean.ccStorateEndpointURL','aaaPurchaseScreenActionBean.ccStorateEndpointURL','aaaBillingActionBean.ccStorateEndpointURL')\n";
 
 	String PAPERLESS_PREFERENCE_API_SERVICE_UPDATE = "update propertyconfigurerentity\n"
-			+ "set value = 'http://%s%sws/policy/preferences'\n"
+			+ "set value = 'https://master.apps.prod.pdc.digital.csaa-insurance.aaa.com/policy/preferences'\n"
 			+ "where propertyname = 'policyPreferenceApiService.policyPreferenceApiUri'";
 
 	String AHDRXX_CONFIG_CHECK = "SELECT dtype, code, displayValue, productCd, riskStateCd, effective, expiration \n"
