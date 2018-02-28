cc2dff10ac2b18037cd5c91c70961193d47b26d1
Petrenko, Viktor (C) <vpetrenko@eisgroup.com>
2018-02-28 16:57:30 -0700

    PAS-9816: line seprators

--- aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/home_ss/dp3/functional/TestMaigConversionHomeDP3.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/home_ss/dp3/functional/TestMaigConversionHomeDP3.java
@@ -13,193 +13,193 @@ import toolkit.utils.TestInfo;
 
 public class TestMaigConversionHomeDP3 extends TestMaigConversionHomeAbstract {
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data for Home
-	 * 4. Check that HSPRNXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2305"})
-	public void pas2305_preRenewalLetterHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2305_preRenewalLetterHSPRNXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data for Home
+     * 4. Check that HSPRNXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2305"})
+    public void pas2305_preRenewalLetterHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2305_preRenewalLetterHSPRNXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data for Home
-	 * 4. Initiate PUP conversion policy
-	 * 5. Check that HSPRNXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2305"})
-	public void pas2305_preRenewalLetterPupConvHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2305_preRenewalLetterPupConvHSPRNXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data for Home
+     * 4. Initiate PUP conversion policy
+     * 5. Check that HSPRNXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2305"})
+    public void pas2305_preRenewalLetterPupConvHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2305_preRenewalLetterPupConvHSPRNXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with PUP added to OtherActiveAAAPolicies for Home
-	 * 4. Check that HSPRNXX document is getting generated with PUP section
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-9170"})
-	public void pas9170_preRenewalLetterPupHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas9170_preRenewalLetterPupHSPRNXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with PUP added to OtherActiveAAAPolicies for Home
+     * 4. Check that HSPRNXX document is getting generated with PUP section
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-9170"})
+    public void pas9170_preRenewalLetterPupHSPRNXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas9170_preRenewalLetterPupHSPRNXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
-	 * 4. Check that HSPRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-7342"})
-	public void pas7342_preRenewalLetterHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas7342_preRenewalLetterHSPRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
+     * 4. Check that HSPRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-7342"})
+    public void pas7342_preRenewalLetterHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas7342_preRenewalLetterHSPRNMXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan
-	 * 4. Initiate PUP conversion policy
-	 * 5. Check that HSPRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-7342"})
-	public void pas7342_preRenewalLetterPupConvHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas7342_preRenewalLetterPupConvHSPRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan
+     * 4. Initiate PUP conversion policy
+     * 5. Check that HSPRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-7342"})
+    public void pas7342_preRenewalLetterPupConvHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas7342_preRenewalLetterPupConvHSPRNMXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Pre-renewal package)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan and PUP added to OtherActiveAAAPolicies  for Home
-	 * 4. Check that HSPRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-9170"})
-	public void pas9170_preRenewalLetterPupHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas9170_preRenewalLetterPupHSPRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Pre-renewal package)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan and PUP added to OtherActiveAAAPolicies  for Home
+     * 4. Check that HSPRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-9170"})
+    public void pas9170_preRenewalLetterPupHSPRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas9170_preRenewalLetterPupHSPRNMXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data for Home
-	 * 4. Check that HSRNHODPXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
-	public void pas2309_renewalCoverLetterHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2309_renewalCoverLetterHSRNHODPXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data for Home
+     * 4. Check that HSRNHODPXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
+    public void pas2309_renewalCoverLetterHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2309_renewalCoverLetterHSRNHODPXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data for home
-	 * 4. Initiate PUP conversion policy
-	 * 5. Check that HSRNHODPXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
-	public void pas2309_renewalCoverLetterPupConvHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2309_renewalCoverLetterPupConvHSRNHODPXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data for home
+     * 4. Initiate PUP conversion policy
+     * 5. Check that HSRNHODPXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
+    public void pas2309_renewalCoverLetterPupConvHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2309_renewalCoverLetterPupConvHSRNHODPXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with PUP added to OtherActiveAAAPolicies for Home
-	 * 4. Check that HSRNHODPXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
-	public void pas2309_renewalCoverLetterPupHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2309_renewalCoverLetterPupHSRNHODPXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with PUP added to OtherActiveAAAPolicies for Home
+     * 4. Check that HSRNHODPXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2309"})
+    public void pas2309_renewalCoverLetterPupHSRNHODPXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2309_renewalCoverLetterPupHSRNHODPXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
-	 * 4. Check that HSRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
-	public void pas2570_renewalCoverLetterHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2570_renewalCoverLetterHSRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
+     * 4. Check that HSRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
+    public void pas2570_renewalCoverLetterHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2570_renewalCoverLetterHSRNMXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
-	 * 4. Initiate PUP conversion policy.
-	 * 5. Check that HSRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
-	public void pas2570_renewalCoverLetterPupConvHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2570_renewalCoverLetterPupConvHSRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan for Home
+     * 4. Initiate PUP conversion policy.
+     * 5. Check that HSRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
+    public void pas2570_renewalCoverLetterPupConvHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2570_renewalCoverLetterPupConvHSRNMXX(state);
+    }
 
-	/**
-	 * @name Test Conversion Document generation (Renewal cover letter)
-	 * @scenario 1. Create Customer
-	 * 2. Initiate Renewal Entry
-	 * 3. Fill Conversion Policy data with Mortgagee payment plan and PUP added to OtherActiveAAAPolicies for Home
-	 * 4. Check that HSRNMXX document is getting generated
-	 * @details
-	 */
-	@Parameters({STATE_PARAM})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
-	public void pas2570_renewalCoverLetterPupHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
-		super.pas2570_renewalCoverLetterPupHSRNMXX(state);
-	}
+    /**
+     * @name Test Conversion Document generation (Renewal cover letter)
+     * @scenario 1. Create Customer
+     * 2. Initiate Renewal Entry
+     * 3. Fill Conversion Policy data with Mortgagee payment plan and PUP added to OtherActiveAAAPolicies for Home
+     * 4. Check that HSRNMXX document is getting generated
+     * @details
+     */
+    @Parameters({STATE_PARAM})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+    @TestInfo(component = ComponentConstant.DocumentFulfillment.HOME_SS_DP3, testCaseId = {"PAS-2570"})
+    public void pas2570_renewalCoverLetterPupHSRNMXX(@Optional("VA") String state) throws NoSuchFieldException {
+        super.pas2570_renewalCoverLetterPupHSRNMXX(state);
+    }
 
-	@Override
-	protected PolicyType getPolicyType() {
-		return PolicyType.HOME_SS_DP3;
-	}
+    @Override
+    protected PolicyType getPolicyType() {
+        return PolicyType.HOME_SS_DP3;
+    }
 
 }
