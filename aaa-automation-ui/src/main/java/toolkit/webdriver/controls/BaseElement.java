5f0cfb6e64f0a7a07840370da347eaa4a4263979
Perapecha, Anton (C) <aperapecha@eisgroup.com>
2018-03-13 20:18:53 +0300

    Chrome fix

--- aaa-automation-ui/src/main/java/toolkit/webdriver/controls/BaseElement.java
+++ aaa-automation-ui/src/main/java/toolkit/webdriver/controls/BaseElement.java
@@ -351,7 +351,7 @@ public abstract class BaseElement<I, O> implements Named {
 	protected void click() {
 		log.debug("Clicking control " + this);
 		ensureVisible();
-		Waiters.SLEEP(300).go();
+		Waiters.SLEEP(500).go();
 		ElementHighlighter.highlight(this);
 		getWebElement().click();
 		WaitMeters.capture(WaitMeters.PAGE_LOAD);
