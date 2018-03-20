766fa1cf86d50275b0b91681096a5b5c568bb3b3
Perapecha, Anton (C) <aperapecha@eisgroup.com>
2018-03-20 17:25:46 +0300

    Update according to 9.1 changes

--- aaa-automation-ui/src/main/java/aaa/main/pages/summary/QuoteSummaryPage.java
+++ aaa-automation-ui/src/main/java/aaa/main/pages/summary/QuoteSummaryPage.java
@@ -6,36 +6,28 @@ import static toolkit.verification.CustomAssertions.assertThat;
 import org.openqa.selenium.By;
 import aaa.common.components.Dialog;
 import aaa.common.pages.MainPage;
-import aaa.main.metadata.QuoteMetaData;
 import aaa.main.modules.policy.PolicyType;
-import toolkit.datax.DataProviderFactory;
-import toolkit.datax.TestData;
 import toolkit.webdriver.controls.Button;
-import toolkit.webdriver.controls.composite.assets.AbstractContainer;
-import toolkit.webdriver.controls.composite.assets.AssetList;
+import toolkit.webdriver.controls.ComboBox;
 import toolkit.webdriver.controls.composite.table.Table;
 
 public class QuoteSummaryPage extends MainPage {
 	public static final String DIALOG_META = "ProductInitiateDialog";
 	public static final String PERSONAL_LINES = "Personal Lines";
-	public static Dialog dialogSelectProduct = new Dialog("//div[@id='quoteForm:quotePreCreationPopup_container']");
 	public static Table tablePremiumSummary = new Table(By.xpath("//table[contains(@id,'productConsolidatedViewForm')]"));
 	public static Table tableQuoteList = new Table(By.id("quotePageContents:body_quote_list_table"));
 	public Button buttonAddNewQuote = new Button(By.xpath("//input[@id='quoteForm:newQuoteButton']"));
-	public AbstractContainer<?, ?> initiateQuote = new AssetList(By.xpath("//*[@id='quoteForm:quoteCreationPopup_container']"), QuoteMetaData.InitiateQuote.class);
+	public ComboBox broadLineOfBusiness = new ComboBox(By.xpath("//*[@id='quoteForm:quoteCreationPopup_container']//select[@id='quoteForm:quoteCreationPopupMultiEdit_blob']"));
+	public ComboBox product = new ComboBox(By.xpath("//*[@id='quoteForm:quoteCreationPopup_container']//select[@id='quoteForm:quoteCreationPopupMultiEdit_productCd']"));
+	public Button nextBtn = new Button(By.xpath("//*[@id='quoteForm:quoteCreationPopup_container']//input[@id='quoteForm:createQuoteButton']"));
+	public Button cancelBtn = new Button(By.xpath("//*[@id='quoteForm:quoteCreationPopup_container']//input[@id='quoteForm:cancelButton']"));
+	public Dialog dialogSelectProduct = new Dialog("//div[@id='quoteForm:quotePreCreationPopup_container']");
 
-	public synchronized void initiateQuote(PolicyType policyType) {
+	public void initiateQuote(PolicyType policyType) {
 		buttonAddNewQuote.click();
-
-		TestData td = DataProviderFactory.dataOf(
-				QuoteMetaData.InitiateQuote.BROAD_LINE_OF_BUSINESS.getLabel(), PERSONAL_LINES,
-				QuoteMetaData.InitiateQuote.PRODUCT.getLabel(), policyType.getName(),
-				QuoteMetaData.InitiateQuote.NEXT_BTN.getLabel(), "click"
-		);
-
-		TestData tdInitiateQuote = DataProviderFactory.dataOf(QuoteMetaData.InitiateQuote.class.getSimpleName(), td);
-
-		initiateQuote.fill(tdInitiateQuote);
+		broadLineOfBusiness.setValue(PERSONAL_LINES);
+		product.setValue(policyType.getName());
+		nextBtn.click();
 		assertThat(buttonAddNewQuote.isPresent()).isFalse();
 	}
 }
