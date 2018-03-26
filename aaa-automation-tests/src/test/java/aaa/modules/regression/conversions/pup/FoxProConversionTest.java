29f055686eedf4d3865c772e4631813d18a2397a
Maiko, Kanstantsin (C) <karlson.kgbnet@gmail.com>
2018-03-26 15:33:32 -0700

    Conversion tests fix

--- aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/pup/FoxProConversionTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/pup/FoxProConversionTest.java
@@ -1,6 +1,5 @@
 package aaa.modules.regression.conversions.pup;
 
-import aaa.common.Tab;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
 import aaa.common.pages.SearchPage;
@@ -16,10 +15,12 @@ import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.helpers.product.ProductRenewalsVerifier;
 import aaa.main.enums.BillingConstants;
+import aaa.main.enums.ErrorEnum;
 import aaa.main.enums.ProductConstants;
 import aaa.main.metadata.policy.PersonalUmbrellaMetaData;
 import aaa.main.modules.billing.account.BillingAccount;
 import aaa.main.modules.policy.pup.defaulttabs.BindTab;
+import aaa.main.modules.policy.pup.defaulttabs.ErrorTab;
 import aaa.main.modules.policy.pup.defaulttabs.PrefillTab;
 import aaa.main.pages.summary.BillingSummaryPage;
 import aaa.main.pages.summary.PolicySummaryPage;
@@ -119,7 +120,7 @@ public class FoxProConversionTest extends PersonalUmbrellaBaseTest {
 
 		mainApp().open();
 		SearchPage.openPolicy(policyNum);
-		fillPolicy(effDate);
+		fillPolicy();
 		new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
 
 		//TODO Verify coverages
@@ -158,7 +159,7 @@ public class FoxProConversionTest extends PersonalUmbrellaBaseTest {
 
 		mainApp().open();
 		SearchPage.openPolicy(policyNum);
-		fillPolicy(effDate);
+		fillPolicy();
 		new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
 
 		TimeSetterUtil.getInstance().nextPhase(getTimePoints().getRenewOfferGenerationDate(effDate));
@@ -200,7 +201,7 @@ public class FoxProConversionTest extends PersonalUmbrellaBaseTest {
 				.setType(BillingConstants.PaymentsAndOtherTransactionType.FEE).verifyPresent();
 	}
 
-	private void fillPolicy(LocalDateTime effDate) {
+	private void fillPolicy() {
 		policy.dataGather().start();
 		PrefillTab prefillTab = new PrefillTab();
 		prefillTab.getNamedInsuredListChangeLink(2).click();
@@ -214,7 +215,10 @@ public class FoxProConversionTest extends PersonalUmbrellaBaseTest {
 				.mask(TestData.makeKeyPath(PersonalUmbrellaMetaData.PrefillTab.class.getSimpleName(), "ActiveUnderlyingPolicies"))
 				.adjust(TestData.makeKeyPath(PersonalUmbrellaMetaData.GeneralTab.class.getSimpleName(), "PolicyInfo"), getTestSpecificTD("PolicyInfo"))
 				.adjust(PersonalUmbrellaMetaData.UnderlyingRisksAllResidentsTab.class.getSimpleName(), getTestSpecificTD("UnderlyingRisksAllResidentsTab"))
-				, BindTab.class);
-		Tab.buttonSaveAndExit.click();
+				, BindTab.class, true);
+		new BindTab().submitTab();
+		ErrorTab errorTab = new ErrorTab();
+		errorTab.overrideErrors(ErrorEnum.Errors.ERROR_AAA_PUP_SS7050000);
+		errorTab.submitTab();
 	}
 }
