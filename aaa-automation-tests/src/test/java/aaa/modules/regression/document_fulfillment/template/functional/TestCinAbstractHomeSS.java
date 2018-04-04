1faece1b3c4b2bf079b3ea8690ef4ffb2c36d32f
Mikhnevich, Pavel (C) <pavel.mikhnevich@csaa.com>
2018-04-04 16:15:34 -0700

    PAS-11798 Added Home SS Tests

--- aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/template/functional/TestCinAbstractHomeSS.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/template/functional/TestCinAbstractHomeSS.java
@@ -23,7 +23,38 @@ public class TestCinAbstractHomeSS extends TestCinAbstract {
     public static final String REPORTS_TAB = TestData.makeKeyPath(
             HomeSSMetaData.ReportsTab.class.getSimpleName());
 
+    public static final String NAME_INSURED_FIRST_NAME = TestData.makeKeyPath(
+            HomeSSMetaData.ApplicantTab.class.getSimpleName(),
+            HomeSSMetaData.ApplicantTab.NamedInsured.class.getSimpleName(),
+            HomeSSMetaData.ApplicantTab.NamedInsured.FIRST_NAME.getLabel());
+
+    public static final String NAME_INSURED_LAST_NAME = TestData.makeKeyPath(
+            HomeSSMetaData.ApplicantTab.class.getSimpleName(),
+            HomeSSMetaData.ApplicantTab.NamedInsured.class.getSimpleName(),
+            HomeSSMetaData.ApplicantTab.NamedInsured.FIRST_NAME.getLabel());
+
+    public static final String INSURANCE_SCORE_OVERRIDE = TestData.makeKeyPath(
+            HomeSSMetaData.ReportsTab.class.getSimpleName(),
+            HomeSSMetaData.ReportsTab.INSURANCE_SCORE_OVERRIDE.getLabel());
+
+
     /**********************************************************************************************************/
+
+    /**
+     * Adjust Named Insured {@link TestData} with First&Last name. The reason why this method is here is that most of mappings in
+     * the stub service are done using exactly those fields.
+     *
+     * @param testData {@link TestData}
+     */
+    public TestData adjustNameInsured(TestData testData, String nameInsuredTestData) {
+        TestData nameInsuredTD = getTestSpecificTD(nameInsuredTestData);
+
+        String firstName = nameInsuredTD.getValue("First Name");
+        String lastName = nameInsuredTD.getValue("Last Name");
+
+        return testData.adjust(NAME_INSURED_FIRST_NAME, firstName)
+                .adjust(NAME_INSURED_LAST_NAME, lastName);
+    }
 
     @Override
     protected void performRenewal(TestData renewalTD) {
