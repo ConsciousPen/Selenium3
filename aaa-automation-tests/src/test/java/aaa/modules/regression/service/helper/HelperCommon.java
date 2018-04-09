f5729bf63afb511ca6d2d96627c98b9f68deaa5d
Gubbala, Megha <megha.gubbala@csaa.com>
2018-04-09 16:48:26 -0700

    Merge branches 'PAS-7082-add-vehicle' and 'master' of https://csaa-insurance.aaa.com/bb/scm/pas/pas-cvqaautomation into PAS-7082-add-vehicle
    # Conflicts:
    #	aaa-automation-tests/src/test/java/aaa/modules/regression/service/helper/HelperCommon.java

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/helper/HelperCommon.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/helper/HelperCommon.java
@@ -1,26 +1,28 @@
 package aaa.modules.regression.service.helper;
 
-import static aaa.admin.modules.IAdmin.log;
-import java.util.HashMap;
-import java.util.Map;
-import javax.ws.rs.client.*;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import aaa.helpers.config.CustomTestProperties;
+import aaa.modules.regression.service.helper.dtoAdmin.RfiDocumentResponse;
+import aaa.modules.regression.service.helper.dtoDxp.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.sun.jna.platform.win32.Guid;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.entity.ContentType;
 import org.apache.xerces.impl.dv.util.Base64;
 import org.glassfish.jersey.client.HttpUrlConnectorProvider;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.sun.jna.platform.win32.Guid;
-import aaa.helpers.config.CustomTestProperties;
-import aaa.modules.regression.service.helper.dtoAdmin.RfiDocumentResponse;
-import aaa.modules.regression.service.helper.dtoDxp.*;
 import toolkit.config.PropertyProvider;
 import toolkit.exceptions.IstfException;
+
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+import static aaa.admin.modules.IAdmin.log;
 
 public class HelperCommon {
 	private static final String ADMIN_DOCUMENTS_RFI_DOCUMENTS_ENDPOINT = "/aaa-admin/services/aaa-policy-rs/v1/documents/rfi-documents/";
@@ -139,8 +141,8 @@ public class HelperCommon {
 		Vehicle request = new Vehicle();
 		request.purchaseDate = purchaseDate;
 		request.vehIdentificationNo = vin;
-		Vehicle vehicle = runJsonRequestPostDxp(requestUrl, request, Vehicle.class, 201);
-		return vehicle;
+		return runJsonRequestPostDxp(requestUrl, request, Vehicle.class, 201);
+
 	}
 
 	public static Vehicle[] pendedEndorsementValidateVehicleInfo(String policyNumber) {
