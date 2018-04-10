cc01d77f12a12661cfa0ccace6032c9fd7101867
Maiko, Kanstantsin (C) <kmaiko@eisgroup.com>
2018-04-10 11:41:15 -0700

    Conversion files added

--- aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ca/ho4/HdesConversionTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ca/ho4/HdesConversionTest.java
@@ -11,66 +11,24 @@ import toolkit.utils.TestInfo;
 
 public class HdesConversionTest extends HdesConversionTestTemplate {
 
-	@Parameters({"state"})
+	@Parameters({"state", "file"})
 	@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest1(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion("HO4/1.xml", context);
+	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO3)
+	public void hdesCAHO4ConversionTest1(@Optional("CA") String state, String file, ITestContext context) {
+		hdesCAConversion(file, context);
 	}
 
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest2(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion("HO4/2.xml", context);
-	}
-
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest3(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion("HO4/3.xml", context);
-	}
-
-	@Parameters({"state"})
+	@Parameters({"state", "file"})
 	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewWithLapse1(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewWithLapse("HO4/1.xml", context);
+	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO3)
+	public void hdesCAHO4ConversionTest_renewWithLapse1(@Optional("CA") String state, String file, ITestContext context) {
+		hdesCAConversion_renewWithLapse(file, context);
 	}
 
-	@Parameters({"state"})
+	@Parameters({"state", "file"})
 	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewWithLapse2(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewWithLapse("HO4/2.xml", context);
-	}
-
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewWithLapse3(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewWithLapse("HO4/3.xml", context);
-	}
-
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewAfterPayment1(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewAfterPayment("HO4/1.xml", context);
-	}
-
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewAfterPayment2(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewAfterPayment("HO4/2.xml", context);
-	}
-
-	@Parameters({"state"})
-	@Test(groups = {Groups.REGRESSION, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO4)
-	public void hdesCAHO4ConversionTest_renewAfterPayment3(@Optional("CA") String state, ITestContext context) {
-		hdesCAConversion_renewAfterPayment("HO4/3.xml", context);
+	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO3)
+	public void hdesCAHO4ConversionTest_renewAfterPayment1(@Optional("CA") String state, String file, ITestContext context) {
+		hdesCAConversion_renewAfterPayment(file, context);
 	}
 }
