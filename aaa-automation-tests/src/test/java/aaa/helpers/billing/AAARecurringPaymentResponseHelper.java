51c5fd507e2b9a30363c428f3fb0456ff8c55ea1
Stasyuk, Oleg (C) <ostasyuk@eisgroup.com>
2018-04-11 11:44:44 -0700

    removed unnecessary dependson tests

---
+++ aaa-automation-tests/src/test/java/aaa/helpers/billing/AAARecurringPaymentResponseHelper.java
@@ -0,0 +1,105 @@
+package aaa.helpers.billing;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import org.apache.commons.lang.RandomStringUtils;
+import aaa.helpers.config.CustomTestProperties;
+import aaa.helpers.ssh.RemoteHelper;
+import toolkit.config.PropertyProvider;
+import toolkit.exceptions.IstfException;
+import toolkit.utils.datetime.DateTimeUtils;
+import toolkit.utils.logging.CustomLogger;
+
+public class AAARecurringPaymentResponseHelper {
+
+	private static final String FILE_LAST_PART = "PMT_E_PMTCTRL_PASSYS_7002_D";
+	private static final String PAYMENT_CENTRAL_FILE_PATH = PropertyProvider.getProperty(CustomTestProperties.JOB_FOLDER) + FILE_LAST_PART + "/inbound/%1$s";
+	private static final DateTimeFormatter DATE_TIME_PATTERN = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+	private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("MMddyyyy");
+	private static final DateTimeFormatter TIME_PATTERN = DateTimeFormatter.ofPattern("HHmmss");
+
+	/**
+	 * This method is used for prepare disbursement engine file with data specified by input parameters.
+	 *
+	 * transactionNumber - transaction number
+	 * refundMethod  - 'R' for automated refund , 'M' for manual refund
+	 * paymentType - 'CHCK' - check, 'EFT' - eft, 'CRDC' - credit/debit card
+	 * policyNumber - policy number
+	 * productType - product type Auto = PA, Home = HO, PUP = PU
+	 * refundAmount - refund amount
+	 * accountLast4 - last 4 numbers
+	 * checkNumber - check number
+	 * accountType - MASTR, VISA, AMEX, DISC, CHKG, SAVG
+	 * cardSubType - Debit, Credit
+	 * refundStatus - SUCC - success, ERR - for error
+	 */
+	public synchronized File createFile(String policyNumber, String agmtAmount, String paymentId, String paymentStatus) {
+
+		File file;
+		String fileName;
+		LocalDateTime date = DateTimeUtils.getCurrentDateTime();
+
+		do {
+			fileName = date.format(DATE_PATTERN) + "_" + date.format(TIME_PATTERN) + "_" + FILE_LAST_PART + ".dat";
+			file = new File(CustomLogger.getLogDirectory().concat("/DisbursementEngine_Files/"), fileName);
+			date = date.plusSeconds(1);
+		}
+		while (file.exists());
+		file.getParentFile().mkdir();
+
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+			String header = MessageFormat.format("HDR|DEV|PMTCTRL|" + FILE_LAST_PART + "|ETLNONPROD|71DCF95E-C|{1}|{2}|C48192E5-E|1\n", FILE_LAST_PART, fileName, date.format(DATE_TIME_PATTERN));
+			bw.write(header);
+			AAARecurringPaymentResponseFileBuilder paymentCentralRejectionsFileBuilder = new AAARecurringPaymentResponseFileBuilder();
+			bw.write(paymentCentralRejectionsFileBuilder.buildData(policyNumber, agmtAmount, paymentId, date, paymentStatus));
+			bw.write(paymentCentralRejectionsFileBuilder.buildTrail(fileName, date.format(DATE_TIME_PATTERN), agmtAmount));
+			bw.flush();
+		} catch (IOException e) {
+			throw new IstfException(e);
+		}
+		return file;
+	}
+
+	public static synchronized void copyFileToServer(File file) {
+		if (file == null) {
+			throw new IstfException("Disbursement engine file is NULL");
+		}
+		RemoteHelper.uploadFile(file.getAbsolutePath(), String.format(PAYMENT_CENTRAL_FILE_PATH, file.getName()));
+	}
+
+	public class AAARecurringPaymentResponseFileBuilder {
+
+		String referenceId = "";    //1
+		String dateTime = "";   //2
+		String pcTransactionStatus = "";//3
+		String paymentId = "";  //6
+		String agmtAmount1 = "";    //7
+		String rejectionReason = "";    //9
+		String policyNumber = "";   //17
+		String bunchOfRandomFields = "";
+
+		String buildData(String policyNumber, String agmtAmount, String paymentId, LocalDateTime date, String paymentStatus) {
+			if (!"SUCC".equals(paymentStatus)) {
+				rejectionReason = "P:502-LOST STOLEN";
+			}
+			return new StringBuilder("DTL|")
+					.append(referenceId).append(RandomStringUtils.randomNumeric(7) + "|")
+					.append(date.format(DATE_TIME_PATTERN)).append("|")
+					.append(paymentStatus).append("|||")
+					.append(paymentId).append("|")
+					.append(agmtAmount).append("||")
+					.append(rejectionReason).append("||||||||")
+					.append(policyNumber).append("||||||||||||||||||\n").toString();
+		}
+
+		String buildTrail(String fileName, String fileDateTime, String agmtAmount) {
+			return "TRL|" + fileName + "|" + fileDateTime + "|whoknowwhat|1|0001|" + agmtAmount + "\n";
+		}
+	}
+
+}
