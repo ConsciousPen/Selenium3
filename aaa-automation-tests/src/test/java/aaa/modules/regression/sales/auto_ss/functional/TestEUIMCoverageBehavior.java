bdf8f3a7b2b4db0b99252df76f51ea569cd2883d
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-04-17 15:18:25 -0700

    PAS-11209 updated testing for 11209

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestEUIMCoverageBehavior.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestEUIMCoverageBehavior.java
@@ -234,18 +234,19 @@ public class TestEUIMCoverageBehavior extends AutoSSBaseTest {
         List<TestData> totalTermPremiumTD = premiumAndCoveragesTab.getTermPremiumByVehicleData();
         assertThat(totalTermPremiumTD.get(0).getKeys()).contains("Enhanced UIM Selected");
 
-        // AC1 PAS-11209. Display EUIM UIPD/UIMBI in VRD page.
+        // AC1 PAS-11209. Display EUIM UIMPD/UIMBI in VRD page.
         PremiumAndCoveragesTab.buttonViewRatingDetails.click();
-        List<TestData> vehicleVRDTestData = premiumAndCoveragesTab.getRatingDetailsVehiclesData();
-        assertThat(vehicleVRDTestData.get(0).getKeys()).contains("Enhanced Uninsured/Underinsured Motorist Bodily Injury");
-        assertThat(vehicleVRDTestData.get(0).getKeys()).contains("Enhanced Uninsured Motorist Property Damage");
+        assertThat(premiumAndCoveragesTab.getRatingDetailsVehiclesData().get(0).getValue("Enhanced UIM")).isEqualTo("Yes");
+        PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
+        enhancedUIM.setValue(false);
+        premiumAndCoveragesTab.calculatePremium();
+        PremiumAndCoveragesTab.buttonViewRatingDetails.click();
+        assertThat(premiumAndCoveragesTab.getRatingDetailsVehiclesData().get(0).getValue("Enhanced UIM")).isEqualTo("No");
         PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
         PremiumAndCoveragesTab.buttonSaveAndExit.click();
 
         // AC2 PAS-11209. Display EUIM UIPD/UIMBI in Policy Consolidated view Coverages section.
-        assertThat(PolicySummaryPage.getAutoCoveragesSummaryTextAt(2,2)).isEqualTo("Bodily Injury Liability");
-        assertThat(PolicySummaryPage.getAutoCoveragesSummaryTextAt(3,2)).isEqualTo("Property Damage Liability");
-        assertThat(PolicySummaryPage.getAutoCoveragesSummaryTextAt(4,2)).isEqualTo("Enhanced Uninsured/Underinsured Motorist Bodily Injury");
-        assertThat(PolicySummaryPage.getAutoCoveragesSummaryTextAt(5,2)).isEqualTo("Enhanced Uninsured Motorist Property Damage");
+        assertThat(PolicySummaryPage.getAutoCoveragesSummaryTestData().getTestData(PolicySummaryPage.getAutoCoveragesSummaryTextAt(1, 1)).getTestData("Enhanced UIM")
+                .getValue("Limit")).isEqualTo("Yes");
     }
 }
