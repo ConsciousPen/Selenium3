7ca4556e5756dd6a9a9d8987d034dbb8537f8b17
Petrenko, Viktor (C) <vpetrenko@eisgroup.com>
2018-04-16 22:44:18 -0700

    PAS-12465: moving back adn forward classes and methods

--- aaa-automation-ui/src/main/java/aaa/soap/aaaCSPolicyRate/exigeninsurance/eis/package-info.java
+++
@@ -1,3 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.exigeninsurance.com/data/EIS/1.0",
-		elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package aaa.soap.aaaCSPolicyRate.exigeninsurance.eis;
