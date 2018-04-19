b3a9186727379ab26b9052ae21af114b64a55da7
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-04-19 08:17:56 -0700

    Merged with master and fixed scenarios.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestLockedUWPoints.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestLockedUWPoints.java
@@ -62,7 +62,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 	 * 7. Change system date +2months.
 	 * 8. Reinstate Policy.
 	 * 9. Endorse Policy and Navigate to P&C View Rating Details.
-	 * 10. Check the sum of UW Points. Check that all of the UW components are not blank.
+	 * 10. Check the sum of UW Points. Check that all of the UW components are blank.
 	 * 11. Bind Endorsement.
 	 * 12. Change system date to Have upcoming renewal active.
 	 * 13. Initiate Renewal.
@@ -273,7 +273,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 	 * 11. Initiate Renewal
 	 * 12. Navigate to P&C
 	 * 13. Calculate Premium
-	 * 14. Check that Total UW score is the same as in Conversion policy
+	 * 14. Check that Total UW score is the same as in Conversion policies Endorsement
 	 * 15. Check that all UW components scores are hidden
 	 * 16. Issue Renewal
 	 * 17. Pay for the renewal
