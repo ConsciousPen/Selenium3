c79225b8473bf4861b6793ec289878fb6aa64090
Jaraminas, Sarunas (C) <Sarunas.Jaraminas@csaa.com>
2018-04-23 12:53:32 -0700

    PAS-9064: MAP Comp and Coll Symbols for DCS to put on Docs (CA - application);

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/auto_ca/choice/TestSymbolsPresenceApplicationFormForPremCalcQuote.java
@@ -0,0 +1,34 @@
+package aaa.modules.regression.document_fulfillment.auto_ca.choice;
+
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.main.modules.policy.PolicyType;
+import aaa.modules.regression.sales.template.functional.TestSymbolsPresenceTemplate;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import toolkit.utils.TestInfo;
+
+public class TestSymbolsPresenceApplicationFormForPremCalcQuote extends TestSymbolsPresenceTemplate {
+
+	@Override
+	protected PolicyType getPolicyType() {
+		return PolicyType.AUTO_CA_CHOICE;
+	}
+
+	/**
+	 * @author Sarunas Jaraminas
+	 * @name doc gen check for AA11CA
+	 * @scenario
+	 * 1. Calculate Premium CA policy
+	 * 2. Get DeclarationPage from db
+	 * 3. Check symbols presence CompDmgSymbl, CollDmgSymbl and VehSym
+	 * 4. Check stat code
+	 * @details
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH}, description = "PAS-9064")
+	@TestInfo(component = ComponentConstant.Sales.AUTO_CA_CHOICE, testCaseId = "PAS-9064")
+	public void pas2713_ApplicationFormStatCodeNotNA(@Optional("CA") String state) {
+		verifySymbolsPresenceInDocsAfterPremCalc(); }
+}
