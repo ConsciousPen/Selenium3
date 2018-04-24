a636d10ecfc3db84729e17bfb6229e4ff98b7928
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-04-24 09:25:43 -0700

    added code to close VRD page at end of verifyLockedLimitsRenewalAndEndorsement

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestLockedUWPoints.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestLockedUWPoints.java
@@ -71,7 +71,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 	*/
 
 	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM}, priority = 1)
+	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-9063, PAS-12443")
 	public void pas9063_verifyLockedUWPoints(@Optional("PA") String state) {
 
@@ -123,7 +123,6 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 		verifyLockedLimitsRenewalAndEndorsement();
 
 		// Bind Endorsement. Renew Policy and Navigate to P&C Page.
-		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 		documentsAndBindTab.fillTab(getTestSpecificTD("TestData_Authorized"));
 		documentsAndBindTab.submitTab();
@@ -143,7 +142,6 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 		verifyLockedLimitsRenewalAndEndorsement();
 
 		// Override errors Issue Renewal
-		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 		documentsAndBindTab.submitTab();
 		errorTab.overrideErrors(ErrorEnum.Errors.ERROR_AAA_200005);
@@ -179,7 +177,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 	 */
 
 	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM}, priority = 1)
+	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-9063, PAS-12443")
 	public void pas9063_verifyLockedUWPointsEndorsement(@Optional("PA") String state) {
 
@@ -228,7 +226,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 	 */
 
 	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM}, priority = 1)
+	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-9063")
 	public void pas9063_verifyLockedUWPointsRenewal(@Optional("PA") String state) {
 
@@ -332,7 +330,6 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 		verifyLockedLimitsRenewalAndEndorsement();
 
 		// Bind Endorsement. Renew Policy and Navigate to P&C Page.
-		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 		documentsAndBindTab.submitTab();
 
@@ -351,7 +348,6 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 		verifyLockedLimitsRenewalAndEndorsement();
 
 		// Issue Renewal
-		PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 		documentsAndBindTab.submitTab();
 
@@ -428,5 +424,7 @@ public class TestLockedUWPoints extends AutoSSBaseTest {
 				PremiumAndCoveragesTab.tableRatingDetailsUnderwriting.getRow(4, f)).isPresent());
 		pas9063FieldsRow2.forEach(f -> assertThat(
 				PremiumAndCoveragesTab.tableRatingDetailsUnderwriting.getRow(4, f).getCell(6).getValue()).isEmpty());
+
+        PremiumAndCoveragesTab.buttonRatingDetailsOk.click();
 	}
 }
