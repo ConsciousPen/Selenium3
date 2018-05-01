94c6490e2af3d31e3de5b76d7b0f48d917818ac6
Stasyuk, Oleg (C) <ostasyuk@eisgroup.com>
2018-05-01 16:34:06 -0700

    PAS-9331-JDI-CleanUpEmemberService-MASTER

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/preconditions/TestEValueMembershipProcessPreConditions.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/preconditions/TestEValueMembershipProcessPreConditions.java
@@ -4,13 +4,13 @@ public interface TestEValueMembershipProcessPreConditions {
 
 	String RETRIEVE_MEMBERSHIP_SUMMARY_STUB_POINT_CHECK = "select  value\n"
 			+ "from PROPERTYCONFIGURERENTITY\n"
-			+ "where propertyname ='retrieveMembershipSummaryServiceImpl.endpointRetrieveMembershipSummaryUri'";
+			+ "where propertyname ='retrieveValueshipSummaryServiceImpl.endpointRetrieveValueshipSummaryUri'";
 
 	String MEMBERSHIP_ELIGIBILITY_CHECK_FOR_VA_EXISTS = "SELECT dtype, code, displayValue, productCd, riskStateCd, effective, expiration \n"
 			+ "FROM LOOKUPVALUE WHERE LOOKUPLIST_ID IN \n"
 			+ "    (SELECT ID \n"
 			+ "    FROM LOOKUPLIST \n"
-			+ "    WHERE LOOKUPNAME='AAAeMemberQualifications')\n"
+			+ "    WHERE LOOKUPNAME='AAAeValueQualifications')\n"
 			+ "and riskstatecd = 'VA'\n"
 			+ "and productCD = 'AAA_SS'\n"
 			+ "and code = 'membershipEligibility'\n"
@@ -21,7 +21,7 @@ public interface TestEValueMembershipProcessPreConditions {
 			+ "FROM LOOKUPVALUE WHERE LOOKUPLIST_ID IN \n"
 			+ "    (SELECT ID \n"
 			+ "    FROM LOOKUPLIST \n"
-			+ "    WHERE LOOKUPNAME='AAAeMemberQualifications')\n"
+			+ "    WHERE LOOKUPNAME='AAAeValueQualifications')\n"
 			+ "and riskstatecd = 'VA'\n"
 			+ "and productCD = 'AAA_SS'\n"
 			+ "and code = 'membershipEligibility'\n"
@@ -30,14 +30,14 @@ public interface TestEValueMembershipProcessPreConditions {
 	String EVALUE_MEMBERSHIP_ELIGIBILITY_CONFIG_INSERT = "INSERT INTO LOOKUPVALUE\n" +
 			"(dtype, code, displayValue, productCd, riskStateCd, EFFECTIVE, EXPIRATION, lookuplist_id)\n" +
 			"values\n" +
-			"('BaseProductLookupValue', 'membershipEligibility', 'TRUE', 'AAA_SS', 'VA', null, null,(SELECT ID FROM LOOKUPLIST WHERE LOOKUPNAME='AAAeMemberQualifications'))\n";
+			"('BaseProductLookupValue', 'membershipEligibility', 'TRUE', 'AAA_SS', 'VA', null, null,(SELECT ID FROM LOOKUPLIST WHERE LOOKUPNAME='AAAeValueQualifications'))\n";
 
 	String MEMBERSHIP_ELIGIBILITY_SWITCH_FOR_VA_UPDATE = "update LOOKUPVALUE\n"
 			+ "set displayValue = '%s'\n"
 			+ "WHERE LOOKUPLIST_ID IN \n"
 			+ "    (SELECT ID \n"
 			+ "    FROM LOOKUPLIST \n"
-			+ "    WHERE LOOKUPNAME='AAAeMemberQualifications')\n"
+			+ "    WHERE LOOKUPNAME='AAAeValueQualifications')\n"
 			+ "and riskstatecd = 'VA'\n"
 			+ "and productCD = 'AAA_SS'\n"
 			+ "and code = 'membershipEligibility'\n"
