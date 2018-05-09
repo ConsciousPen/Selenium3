49e96e681509d93c701dc1e3e5a2d5807e177651
Jemison, Tyrone (C) <tyrone.jemison@csaa.com>
2018-05-09 14:00:42 -0700

    Added preliminary code to tests.
    Ready for feature delivery.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/TestCAFairPlanCompanion.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/TestCAFairPlanCompanion.java
@@ -1,17 +1,29 @@
 package aaa.modules.regression.sales.home_ca.ho3;
 
+import aaa.common.pages.SearchPage;
+import aaa.helpers.jobs.JobUtils;
+import aaa.helpers.jobs.Jobs;
+import aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab;
 import aaa.modules.policy.HomeCaHO3BaseTest;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
+import aaa.modules.regression.sales.home_ca.helper.HelperCommon;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
+import java.time.LocalDateTime;
 
 /**
  * @author Tyrone C Jemison
  */
 public class TestCAFairPlanCompanion extends HomeCaHO3BaseTest {
+    // Class Variables
+    TestData defaultPolicyData = getPolicyTD();
+    HelperCommon myHelper = new HelperCommon();
+
     /**
      * @scenario
      * 1. Initiate a HO3 Quote.
@@ -24,6 +36,18 @@ public class TestCAFairPlanCompanion extends HomeCaHO3BaseTest {
     @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
     @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3)
     public void AC1AC4_Quote_VisibleFPCECA(@Optional("") String state) {
+        // Test Variables
+
+        // Open App, Create Customer and Initiate Quote
+        mainApp().open();
+        createCustomerIndividual();
+        policy.initiate();
+        policy.getDefaultView().fillUpTo(defaultPolicyData, PremiumsAndCoveragesQuoteTab.class, false);
+
+        // TODO: Validate FPCECA is visible.
+        // TODO: Add FPCECA Endorsement
+        // TODO: Verify FPCECA no longer in Optional Endorsements
+        // TODO: Verify FPCECA now present on Documents Tab & Quote Tab
     }
 
     /**
@@ -40,6 +64,19 @@ public class TestCAFairPlanCompanion extends HomeCaHO3BaseTest {
     @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
     @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3)
     public void AC2AC5_Endorsement_VisibleFPCECA(@Optional("") String state) {
+
+        TestData endorsementTestData = getTestSpecificTD("Endorsement_AC2AC5").resolveLinks();;
+
+        // Open App, Create customer and Policy.
+        mainApp().open();
+        createCustomerIndividual();
+        createPolicy(defaultPolicyData);
+        policy.endorse().perform(endorsementTestData.adjust(getPolicyTD("Endorsement", "TestData")));
+        policy.getDefaultView().fillUpTo(endorsementTestData, PremiumsAndCoveragesQuoteTab.class, false);
+
+        // TODO: Validate FPCECA is visible
+        // TODO: Add FPCECA Endorsement
+        // TODO: Verify Message appears and it matches Mock-Up.
     }
 
     /**
@@ -56,5 +93,48 @@ public class TestCAFairPlanCompanion extends HomeCaHO3BaseTest {
     @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
     @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3)
     public void AC3_Renewal_VisibleFPCECA(@Optional("") String state) {
+
+        // Open App, Create Customer and Policy.
+        mainApp().open();
+        createCustomerIndividual();
+        String policyNumber = myHelper.createCustomerPolicyReturnPN(getCustomerIndividualTD("DataGather","TestData"), defaultPolicyData);
+        mainApp().close();
+
+        // Determine Time Values
+        LocalDateTime policyCreationDate = TimeSetterUtil.getInstance().getCurrentTime();
+        LocalDateTime policyExpirationDate  = policyCreationDate.plusYears(1);
+        LocalDateTime timePoint1 = policyExpirationDate.minusDays(73);
+        LocalDateTime timePoint2 = policyExpirationDate.minusDays(59);
+
+        // Move JVM to TP1 (R-73) & run Renewal jobs
+        moveJVMToDateAndRunRenewalJobs(timePoint1);
+        // Move JVM to TP2 (R-59) & run Renewal jobs
+        moveJVMToDateAndRunRenewalJobs(timePoint2);
+
+        // Open App, get renewal image.
+        mainApp().open();
+        SearchPage.openPolicy(policyNumber);
+
+        policy.renew().start().submit();
+        policy.getDefaultView().fillUpTo(getTestSpecificTD("Renewal_AC3"), PremiumsAndCoveragesQuoteTab.class, false);
+
+        // TODO: Validate FPCECA endorsement is visible.
+    }
+
+    public static void moveJVMToDateAndRunRenewalJobs(LocalDateTime desiredJVMLocalDateTime)
+    {
+        LocalDateTime policyCreationDate = TimeSetterUtil.getInstance().getCurrentTime();
+        printToDebugLog(" -- Current Date = " + policyCreationDate + ". Moving JVM to input time = "
+                + desiredJVMLocalDateTime.toString() + " -- ");
+
+        // Advance JVM to Generate Renewal Image.
+        TimeSetterUtil.getInstance().nextPhase(desiredJVMLocalDateTime);
+        printToDebugLog("Current Date is now = " + TimeSetterUtil.getInstance().getCurrentTime());
+
+        JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+
+        printToDebugLog(" -- Renewal Offer Generation Jobs Completed -- ");
     }
 }
