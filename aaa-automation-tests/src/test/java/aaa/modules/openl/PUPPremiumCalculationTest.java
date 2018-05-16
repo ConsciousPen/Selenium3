30b4192234e4065e0ca533e8152fcf06321123e1
Laptsionak, Mikhail (C) <Mikhail.Laptsionak@csaa.com>
2018-05-16 18:51:28 +0300

    added PUP rating tests

--- aaa-automation-tests/src/test/java/aaa/modules/openl/PUPPremiumCalculationTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/openl/PUPPremiumCalculationTest.java
@@ -1,9 +1,5 @@
 package aaa.modules.openl;
 
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-import com.exigen.ipb.etcsa.utils.Dollar;
 import aaa.helpers.constants.Groups;
 import aaa.helpers.openl.model.pup.PUPOpenLFile;
 import aaa.helpers.openl.model.pup.PUPOpenLPolicy;
@@ -13,6 +9,10 @@ import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.pup.defaulttabs.PrefillTab;
 import aaa.main.modules.policy.pup.defaulttabs.PremiumAndCoveragesQuoteTab;
 import aaa.main.modules.policy.pup.defaulttabs.UnderlyingRisksAutoTab;
+import com.exigen.ipb.etcsa.utils.Dollar;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 import toolkit.datax.TestData;
 
 public class PUPPremiumCalculationTest extends OpenLRatingBaseTest<PUPOpenLPolicy, PUPOpenLFile> {
@@ -33,7 +33,7 @@ public class PUPPremiumCalculationTest extends OpenLRatingBaseTest<PUPOpenLPolic
 		policy.initiate();
 		policy.getDefaultView().fillUpTo(quoteRatingData, PremiumAndCoveragesQuoteTab.class, false);
 		new PremiumAndCoveragesQuoteTab().fillTab(quoteRatingData);
-		return PremiumAndCoveragesQuoteTab.getPolicyTermPremium();
+		return "KY".equals(getState()) ? PremiumAndCoveragesQuoteTab.getPUPCoveragePremium() : PremiumAndCoveragesQuoteTab.getPolicyTermPremium();
 	}
 
 	@Parameters({"state", "fileName", "policyNumbers"})
