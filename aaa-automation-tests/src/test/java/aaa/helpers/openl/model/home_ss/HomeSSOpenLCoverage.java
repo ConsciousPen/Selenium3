f03b14b893d1c94db1bbb7b0492ff57306e82099
Chubkov, Dmytro (C) <dchubkov@eisgroup.com>
2018-05-17 12:20:47 +0300

    masked ID field from unmarshalling

--- aaa-automation-tests/src/test/java/aaa/helpers/openl/model/home_ss/HomeSSOpenLCoverage.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/openl/model/home_ss/HomeSSOpenLCoverage.java
@@ -4,10 +4,12 @@ import aaa.helpers.openl.model.OpenLCoverage;
 import aaa.helpers.openl.model.OpenLFile;
 import aaa.utils.excel.bind.annotation.ExcelColumnElement;
 import aaa.utils.excel.bind.annotation.ExcelTableElement;
+import aaa.utils.excel.bind.annotation.ExcelTransient;
 
 @ExcelTableElement(sheetName = OpenLFile.COVERAGE_SHEET_NAME, headerRowIndex = HomeSSOpenLFile.COVERAGE_HEADER_ROW_NUMBER)
 public class HomeSSOpenLCoverage extends OpenLCoverage {
-	private Integer id;
+	@ExcelTransient
+	private Integer id; // IDs in some files are too big for Integer type (e.g. see "ORTests-20171023.xls" file) therefore it's skipped
 
 	@SuppressWarnings({"FieldNameHidesFieldInSuperclass"})
 	@ExcelColumnElement(name = "code")
