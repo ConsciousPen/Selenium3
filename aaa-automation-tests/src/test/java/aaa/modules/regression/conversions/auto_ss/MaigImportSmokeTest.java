91359d8573972f95a767eb2b46f25f2c2735c18e
Maiko, Kanstantsin (C) <kmaiko@eisgroup.com>
2018-05-17 16:14:21 +0300

    Conversion tests added to Smoke suite

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/auto_ss/MaigImportSmokeTest.java
@@ -0,0 +1,33 @@
+package aaa.modules.regression.conversions.auto_ss;
+
+import java.time.LocalDateTime;
+import org.testng.ITestContext;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import aaa.common.pages.SearchPage;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.conversion.ConversionPolicyData;
+import aaa.helpers.conversion.ConversionUtils;
+import aaa.helpers.conversion.MaigConversionData;
+import aaa.helpers.product.ProductRenewalsVerifier;
+import aaa.main.enums.ProductConstants;
+import toolkit.utils.TestInfo;
+
+public class MaigImportSmokeTest extends MaigConversionTest {
+
+	@Parameters({"state"})
+	@Test(groups = {Groups.REGRESSION, Groups.CRITICAL})
+	@TestInfo(component = ComponentConstant.Conversions.AUTO_SS)
+	public void maigImportTest(@Optional("NJ") String state, ITestContext context) {
+		LocalDateTime effDate = getTimePoints().getConversionEffectiveDate();
+		ConversionPolicyData data = new MaigConversionData("5.xml", effDate);
+		String policyNum = ConversionUtils.importPolicy(data, context);
+
+		mainApp().open();
+		SearchPage.openPolicy(policyNum);
+		fillPolicy();
+		new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
+	}
+}
---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ca/ho3/HdesImportSmokeTest.java
@@ -0,0 +1,33 @@
+package aaa.modules.regression.conversions.home_ca.ho3;
+
+import java.time.LocalDateTime;
+import org.testng.ITestContext;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import aaa.common.pages.SearchPage;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.conversion.ConversionPolicyData;
+import aaa.helpers.conversion.ConversionUtils;
+import aaa.helpers.conversion.HdesConversionData;
+import aaa.helpers.product.ProductRenewalsVerifier;
+import aaa.main.enums.ProductConstants;
+import aaa.modules.policy.HomeCaHO3BaseTest;
+import toolkit.utils.TestInfo;
+
+public class HdesImportSmokeTest extends HomeCaHO3BaseTest {
+
+	@Parameters({"state"})
+	@Test(groups = {Groups.SMOKE, Groups.CRITICAL})
+	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO3)
+	public void hdesCAHO3ImportTest(@Optional("CA") String state, ITestContext context) {
+		LocalDateTime effDate = getTimePoints().getConversionEffectiveDate();
+		ConversionPolicyData data = new HdesConversionData("HO3/1.xml", effDate);
+		String policyNum = ConversionUtils.importPolicy(data, context);
+
+		mainApp().open();
+		SearchPage.openPolicy(policyNum);
+		new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
+	}
+}
