fcfd58723aee18a67e622d1e3c6e872c6b796335
Pukenaite, Jovita (C) <Jovita.Pukenaite@csaa.com>
2018-05-18 10:00:48 -0700

    part1 dto

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/helper/dtoDxp/UpdateDriverAssignmentRequest.java
@@ -0,0 +1,13 @@
+package aaa.modules.regression.service.helper.dtoDxp;
+
+import java.util.List;
+
+
+/**
+ * Created by gszdome on 5/16/2018.
+ */
+public class UpdateDriverAssignmentRequest {
+
+	public List<DriverAssignmentDto> assignmentRequests;
+}
+
