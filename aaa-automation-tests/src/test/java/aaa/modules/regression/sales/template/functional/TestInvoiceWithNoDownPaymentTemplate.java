0e436bbfde1d7d771b4722feb0d05167bb3b4102
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-05-29 10:55:33 -0700

    pas-9001 started endorsement testing implementation

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestInvoiceWithNoDownPaymentTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestInvoiceWithNoDownPaymentTemplate.java
@@ -1,20 +1,26 @@
 package aaa.modules.regression.sales.template.functional;
 
-import aaa.helpers.jobs.JobUtils;
-import aaa.helpers.jobs.Jobs;
+import aaa.common.Tab;
+import aaa.common.enums.NavigationEnum;
+import aaa.common.pages.NavigationPage;
+import aaa.main.metadata.policy.HomeCaMetaData;
+import aaa.main.metadata.policy.HomeSSMetaData;
 import aaa.main.metadata.policy.PurchaseMetaData;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.abstract_tabs.Purchase;
+import aaa.main.modules.policy.home_ca.defaulttabs.BindTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab;
 import aaa.main.modules.policy.home_ca.defaulttabs.PurchaseTab;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
-import toolkit.datax.TestData;
+import aaa.toolkit.webdriver.customcontrols.JavaScriptButton;
+import toolkit.webdriver.controls.ComboBox;
+import toolkit.webdriver.controls.composite.assets.metadata.AssetDescriptor;
 
 public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
 
     protected void pas9001_testInvoiceWithNoDownPaymentNB(PolicyType policyType) {
 
-        TestData td = getPolicyDefaultTD(policyType);
         Purchase purchaseTab;
         if (policyType.isCaProduct()) {
             purchaseTab = new PurchaseTab();
@@ -28,7 +34,7 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
 
         // Create policy with zero deposit on Purchase tab
         policyType.get().initiate();
-        policyType.get().getDefaultView().fillUpTo(td, purchaseTab.getClass());
+        policyType.get().getDefaultView().fillUpTo(getPolicyDefaultTD(policyType), purchaseTab.getClass());
         purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.CHANGE_MINIMUM_DOWNPAYMENT).setValue(true);
         purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.MINIMUM_REQUIRED_DOWNPAYMENT).setValue("0.00");
         purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.REASON_FOR_CHANGING).setValue("index=1");
@@ -44,7 +50,62 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
 
     protected void pas9001_testInvoiceWithNoDownPaymentEndorsement(PolicyType policyType) {
 
-        // TODO implement this
+        Tab premiumAndCoveragesQuoteTab;
+        Tab bindTab;
+        if (policyType.isCaProduct()) {
+            premiumAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
+            bindTab = new BindTab();
+        } else {
+            premiumAndCoveragesQuoteTab = new aaa.main.modules.policy.home_ss.defaulttabs.PremiumsAndCoveragesQuoteTab();
+            bindTab = new aaa.main.modules.policy.home_ss.defaulttabs.BindTab();
+        }
+
+        // Create customer and policy
+        mainApp().open();
+        createCustomerIndividual();
+        policyType.get().createPolicy(getPolicyDefaultTD(policyType));
+        String policyNumber = PolicySummaryPage.getPolicyNumber();
+
+        // Create a premium-bearing endorsement (increase)
+        policyType.get().endorse().perform(getStateTestData(testDataManager.policy.get(policyType).getTestData("Endorsement"), "TestData"));
+        navigateToPremiumAndCoveragesQuoteTab(policyType);
+        premiumAndCoveragesQuoteTab.getAssetList().getAsset(getCoverageB(policyType)).setValueContains("30%");
+        premiumAndCoveragesQuoteTab.getAssetList().getAsset(getCalculatePremiumButton(policyType)).click();
+        navigateToBindTab(policyType);
+        bindTab.submitTab();
+
+    }
+
+    private void navigateToPremiumAndCoveragesQuoteTab(PolicyType policyType) {
+        if (policyType.isCaProduct()) {
+            NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES.get());
+            NavigationPage.toViewSubTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
+        } else {
+            NavigationPage.toViewTab(NavigationEnum.HomeSSTab.PREMIUMS_AND_COVERAGES.get());
+            NavigationPage.toViewSubTab(NavigationEnum.HomeSSTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
+        }
+    }
+
+    private void navigateToBindTab(PolicyType policyType) {
+        if (policyType.isCaProduct()) {
+            NavigationPage.toViewTab(NavigationEnum.HomeCaTab.BIND.get());
+        } else {
+            NavigationPage.toViewTab(NavigationEnum.HomeSSTab.BIND.get());
+        }
+    }
+
+    private AssetDescriptor<ComboBox> getCoverageB(PolicyType policyType) {
+        if (policyType.isCaProduct()) {
+            return HomeCaMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_B;
+        }
+        return HomeSSMetaData.PremiumsAndCoveragesQuoteTab.COVERAGE_B;
+    }
+
+    private AssetDescriptor<JavaScriptButton> getCalculatePremiumButton(PolicyType policyType) {
+        if (policyType.isCaProduct()) {
+            return HomeCaMetaData.PremiumsAndCoveragesQuoteTab.CALCULATE_PREMIUM_BUTTON;
+        }
+        return HomeSSMetaData.PremiumsAndCoveragesQuoteTab.CALCULATE_PREMIUM;
     }
 
 }
