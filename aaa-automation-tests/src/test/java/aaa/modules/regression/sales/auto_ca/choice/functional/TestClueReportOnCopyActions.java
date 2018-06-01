fa0d69d94024bd1e85266aa3798af8a1f06b87f3
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-06-01 10:38:51 -0700

    added StateList annotation to Choice test

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestClueReportOnCopyActions.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestClueReportOnCopyActions.java
@@ -4,15 +4,18 @@ import java.util.List;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import aaa.common.enums.Constants;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.auto_ca.defaulttabs.DriverTab;
 import aaa.modules.regression.sales.auto_ca.choice.TestPolicyCreationBig;
 import aaa.modules.regression.sales.template.functional.TestClueReportOnCopyActionsTemplate;
+import aaa.utils.StateList;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
 
+@StateList(states = Constants.States.CA)
 public class TestClueReportOnCopyActions extends TestClueReportOnCopyActionsTemplate {
 
 	/**
