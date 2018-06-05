a52ff8b5f5dcd5a0fd31c302d86e43c15b83a44f
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-06-05 11:06:34 -0700

    PAS-9001 finished test template for HO products and added java docs for testing in DP3

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestInvoiceWithNoDownPaymentTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestInvoiceWithNoDownPaymentTemplate.java
@@ -1,6 +1,8 @@
 package aaa.modules.regression.sales.template.functional;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import aaa.common.Tab;
 import aaa.common.enums.NavigationEnum;
@@ -22,8 +24,13 @@ import aaa.modules.policy.PolicyBaseTest;
 import aaa.toolkit.webdriver.customcontrols.JavaScriptButton;
 import toolkit.webdriver.controls.ComboBox;
 import toolkit.webdriver.controls.composite.assets.metadata.AssetDescriptor;
+import static toolkit.verification.CustomAssertions.assertThat;
 
 public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
+
+	private final Dollar zeroDollars = new Dollar(0.00);
+	private String policyNumber;
+	private LocalDateTime dueDate;
 
     protected void pas9001_testInvoiceWithNoDownPaymentNB() {
 
@@ -38,19 +45,18 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
         mainApp().open();
         createCustomerIndividual();
 
-        // Create policy with zero deposit on Purchase tab
+        // Create policy with updated min deposit on Purchase tab
         getPolicyType().get().initiate();
         getPolicyType().get().getDefaultView().fillUpTo(getPolicyDefaultTD(), purchaseTab.getClass());
         purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.CHANGE_MINIMUM_DOWNPAYMENT).setValue(true);
-        purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.MINIMUM_REQUIRED_DOWNPAYMENT).setValue("0.00");
+        purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.MINIMUM_REQUIRED_DOWNPAYMENT).setValue("10.00");
         purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.REASON_FOR_CHANGING).setValue("index=1");
+		purchaseTab.getAssetList().getAsset(PurchaseMetaData.PurchaseTab.PAYMENT_METHOD_CASH).setValue("10.00");
         purchaseTab.submitTab();
+		setPolicyInfo();
 
-        // Capture policy number from summary page
-        String policyNumber = PolicySummaryPage.getPolicyNumber();
-
-        //JobUtils.executeJob(Jobs.aaaBillingInvoiceAsyncTaskJob);
-        //JobUtils.executeJob(Jobs.offCycleBillingInvoiceAsyncJob);
+		// Min Due and off cycle billing validation
+		validateMinDueAndOffCycleBillingInvoice();
 
     }
 
@@ -70,11 +76,10 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
         mainApp().open();
         createCustomerIndividual();
         getPolicyType().get().createPolicy(getPolicyDefaultTD());
-        String policyNumber = PolicySummaryPage.getPolicyNumber();
-        LocalDateTime effectiveDate = PolicySummaryPage.getEffectiveDate();
+		setPolicyInfo();
 
-        // Create a premium-bearing endorsement (increase) at effective date plus 30 days
-        TimeSetterUtil.getInstance().confirmDateIsAfter(effectiveDate.plusDays(30));
+        // Create a premium-bearing endorsement (increase) at effective date plus 5 days
+        TimeSetterUtil.getInstance().confirmDateIsAfter(PolicySummaryPage.getEffectiveDate().plusDays(5));
         reopenPolicy(policyNumber);
         getPolicyType().get().endorse().perform(getStateTestData(testDataManager.policy.get(getPolicyType()).getTestData("Endorsement"), "TestData"));
         navigateToPremiumAndCoveragesQuoteTab();
@@ -83,12 +88,8 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
         navigateToBindTab();
         bindTab.submitTab();
 
-        JobUtils.executeJob(Jobs.aaaBillingInvoiceAsyncTaskJob);
-        JobUtils.executeJob(Jobs.offCycleBillingInvoiceAsyncJob);
-
-        reopenPolicy(policyNumber);
-        NavigationPage.toMainTab(NavigationEnum.AppMainTabs.BILLING.get());
-        BillingSummaryPage.getInstallmentDueDate(1);
+        // Min Due and off cycle billing validation
+		validateMinDueAndOffCycleBillingInvoice();
 
     }
 
@@ -125,8 +126,33 @@ public class TestInvoiceWithNoDownPaymentTemplate extends PolicyBaseTest {
     }
 
     private void reopenPolicy(String policyNumber) {
-        mainApp().open();
+        mainApp().reopen();
         SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
     }
+
+    private void setPolicyInfo() {
+		policyNumber = PolicySummaryPage.getPolicyNumber();
+		dueDate = PolicySummaryPage.getEffectiveDate().plusMonths(1);
+	}
+
+    private void validateMinDueAndOffCycleBillingInvoice() {
+		// Verify min due
+		NavigationPage.toMainTab(NavigationEnum.AppMainTabs.BILLING.get());
+		assertThat(BillingSummaryPage.getMinimumDue()).isEqualTo(zeroDollars);
+
+		// Move to DD-20, run off cycle billing job, and refresh policy
+		TimeSetterUtil.getInstance().nextPhase(dueDate.minusDays(20));
+		JobUtils.executeJob(Jobs.offCycleBillingInvoiceAsyncJob);
+		reopenPolicy(policyNumber);
+		NavigationPage.toMainTab(NavigationEnum.AppMainTabs.BILLING.get());
+
+		// Verify off cycle invoice is generated
+		Dollar minDue = new Dollar(BillingSummaryPage.tableBillsStatements.getRow(1).getCell("Minimum Due").getValue());
+		assertThat(BillingSummaryPage.tableBillsStatements.getRows().size()).isEqualTo(1);
+		assertThat(BillingSummaryPage.tableBillsStatements.getRow(1).getCell("Due Date").getValue()).isEqualTo(dueDate.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+		assertThat(minDue).isNotEqualTo(zeroDollars);
+		assertThat(new Dollar(BillingSummaryPage.tableBillsStatements.getRow(1).getCell("Total Due").getValue())).isEqualTo(minDue);
+
+	}
 
 }
