f63c578f60efd054f7aa4be422fde563ecdf80ff
Chubkov, Dmytro (C) <dchubkov@eisgroup.com>
2018-06-13 12:48:03 +0300

    added DataProvider to OpenL tests

--- aaa-automation-tests/src/test/java/aaa/modules/openl/HomeCaDP3PremiumCalculationTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/openl/HomeCaDP3PremiumCalculationTest.java
@@ -1,23 +1,12 @@
 package aaa.modules.openl;
 
-import org.testng.ITestContext;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-import aaa.helpers.constants.Groups;
 import aaa.helpers.openl.model.home_ca.dp3.HomeCaDP3OpenLPolicy;
 import aaa.main.modules.policy.PolicyType;
 
 public class HomeCaDP3PremiumCalculationTest extends HomeCaPremiumCalculationTest<HomeCaDP3OpenLPolicy> {
-
+	
 	@Override
 	protected PolicyType getPolicyType() {
 		return PolicyType.HOME_CA_DP3;
-	}
-	
-	@Parameters({"state"})
-	@Test(groups = {Groups.OPENL, Groups.HIGH})
-	public void premiumCalculationTest(@Optional("") String state, ITestContext context) {
-		verifyPremiums(context);
 	}
 }
--- aaa-automation-tests/src/test/java/aaa/modules/openl/PUPPremiumCalculationTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/openl/PUPPremiumCalculationTest.java
@@ -1,11 +1,7 @@
 package aaa.modules.openl;
 
-import org.testng.ITestContext;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 import com.exigen.ipb.etcsa.utils.Dollar;
-import aaa.helpers.constants.Groups;
+import aaa.common.enums.Constants;
 import aaa.helpers.openl.model.pup.PUPOpenLPolicy;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.pup.defaulttabs.PrefillTab;
@@ -14,29 +10,23 @@ import aaa.main.modules.policy.pup.defaulttabs.UnderlyingRisksAutoTab;
 import toolkit.datax.TestData;
 
 public class PUPPremiumCalculationTest extends OpenLRatingBaseTest<PUPOpenLPolicy> {
-
+	
 	@Override
 	protected PolicyType getPolicyType() {
 		return PolicyType.PUP;
 	}
-
+	
 	@Override
 	protected TestData getRatingDataPattern() {
 		return super.getRatingDataPattern().mask(new PrefillTab().getMetaKey(), new UnderlyingRisksAutoTab().getMetaKey());
 	}
-
+	
 	@Override
 	protected Dollar createAndRateQuote(PUPOpenLPolicy openLPolicy) {
 		TestData quoteRatingData = openLPolicy.getTestDataGenerator(getState(), getRatingDataPattern()).getRatingData(openLPolicy);
 		policy.initiate();
 		policy.getDefaultView().fillUpTo(quoteRatingData, PremiumAndCoveragesQuoteTab.class, false);
 		new PremiumAndCoveragesQuoteTab().fillTab(quoteRatingData);
-		return "KY".equals(getState()) ? PremiumAndCoveragesQuoteTab.getPUPCoveragePremium() : PremiumAndCoveragesQuoteTab.getPolicyTermPremium();
-	}
-	
-	@Parameters({"state"})
-	@Test(groups = {Groups.OPENL, Groups.HIGH})
-	public void premiumCalculationTest(@Optional("") String state, ITestContext context) {
-		verifyPremiums(context);
+		return Constants.States.KY.equals(getState()) ? PremiumAndCoveragesQuoteTab.getPUPCoveragePremium() : PremiumAndCoveragesQuoteTab.getPolicyTermPremium();
 	}
 }
