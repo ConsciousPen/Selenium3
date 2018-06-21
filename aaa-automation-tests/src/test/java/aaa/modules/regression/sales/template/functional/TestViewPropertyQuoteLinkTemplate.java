d761454bcaa7f7f206ce1da5e946c0b20d3a6051
Kopparapu, Sreekanth <sreekanth.kopparapu@csaa.com>
2018-06-21 15:39:09 -0700

    PAS-14842 Created Tests for P&C View Property Quote Link

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestViewPropertyQuoteLinkTemplate.java
@@ -0,0 +1,48 @@
+package aaa.modules.regression.sales.template.functional;
+
+
+import aaa.main.modules.policy.home_ss.defaulttabs.PremiumsAndCoveragesQuoteTab;
+import aaa.modules.policy.PolicyBaseTest;
+import toolkit.datax.TestData;
+import toolkit.webdriver.BrowserController;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestViewPropertyQuoteLinkTemplate extends PolicyBaseTest {
+
+    protected void pas14842_testViewPropertyQuoteLinkSS(TestData td) {
+
+        initiatePolicy();
+        getPolicyType().get().getDefaultView().fillUpTo(td, PremiumsAndCoveragesQuoteTab.class);
+
+        assertThat(PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.isPresent()).isFalse();
+
+        new PremiumsAndCoveragesQuoteTab().calculatePremium();
+        assertThat(PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.isEnabled()).isTrue();
+        PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.click();
+        assertThat(BrowserController.get().driver().getWindowHandles().size()).isEqualTo(2);
+
+    }
+
+    protected void pas14842_testViewPropertyQuoteLinkCA(TestData td) {
+
+        initiatePolicy();
+        getPolicyType().get().getDefaultView().fillUpTo(td, aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab.class);
+
+        assertThat(aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.isPresent()).isFalse();
+
+        new aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab().calculatePremium();
+        assertThat(aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.isEnabled()).isTrue();
+        aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab.linkViewPropertyQuote.click();
+        assertThat(BrowserController.get().driver().getWindowHandles().size()).isEqualTo(2);
+
+    }
+
+    private void initiatePolicy(){
+
+        // Create customer and policy
+        mainApp().open();
+        createCustomerIndividual();
+        getPolicyType().get().initiate();
+    }
+}
