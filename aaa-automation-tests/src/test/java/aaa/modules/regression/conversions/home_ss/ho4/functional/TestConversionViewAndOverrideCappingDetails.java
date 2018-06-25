d18cb6c8c8459c9001048c8af571a1a33d81a185
Kazlauskiene, Reda (C) <Reda.Kazlauskiene@csaa.com>
2018-06-25 14:38:38 -0700

    Revert "PAS-14035: Capping capability: UT"
    This reverts commit 5b4438aaa064d787e3dfced94e372a47aacaa078.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ss/ho4/functional/TestConversionViewAndOverrideCappingDetails.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ss/ho4/functional/TestConversionViewAndOverrideCappingDetails.java
@@ -45,7 +45,7 @@ import static toolkit.verification.CustomAssertions.assertThat;
 public class TestConversionViewAndOverrideCappingDetails extends HomeSSHO4BaseTest {
 
     @Parameters({"state"})
-    @StateList(states = {Constants.States.AZ, Constants.States.DE, Constants.States.MD, Constants.States.NJ, Constants.States.PA, Constants.States.VA, Constants.States.UT})
+    @StateList(states = {Constants.States.AZ, Constants.States.DE, Constants.States.MD, Constants.States.NJ, Constants.States.PA, Constants.States.VA, Constants.States.NJ, Constants.States.UT})
     @Test(groups = {Groups.REGRESSION, Groups.HIGH})
     @TestInfo(component = ComponentConstant.Conversions.HOME_SS_HO4, testCaseId = "PAS-3002")
     public void testPolicyViewCappingDetails(@Optional("VA") String state) {
