6f5391d0d10442f62c3916c5f773b141cc9f60f6
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-06-27 13:10:06 -0700

    Added CA product

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestUpdateWildfireScore.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestUpdateWildfireScore.java
@@ -2,10 +2,10 @@ package aaa.modules.regression.sales.template.functional;
 
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
+import aaa.main.metadata.policy.HomeCaMetaData;
 import aaa.main.metadata.policy.HomeSSMetaData;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.modules.policy.home_ss.defaulttabs.GeneralTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.PremiumsAndCoveragesQuoteTab;
 import aaa.main.modules.policy.home_ss.defaulttabs.PropertyInfoTab;
 import aaa.main.modules.policy.home_ss.defaulttabs.ReportsTab;
 import aaa.modules.policy.PolicyBaseTest;
@@ -17,9 +17,13 @@ import static toolkit.verification.CustomAssertions.assertThat;
 public class TestUpdateWildfireScore extends PolicyBaseTest {
 
     private PropertyInfoTab propertyInfoTab = new PropertyInfoTab();
-    private PremiumsAndCoveragesQuoteTab premiumsAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
+    private aaa.main.modules.policy.home_ca.defaulttabs.PropertyInfoTab propertyInfoTabCA = new aaa.main.modules.policy.home_ca.defaulttabs.PropertyInfoTab();
+
     private ReportsTab reportsTab = new ReportsTab();
+    private aaa.main.modules.policy.home_ca.defaulttabs.ReportsTab reportsTabCA = new aaa.main.modules.policy.home_ca.defaulttabs.ReportsTab();
+
     private TextBox wildfireScore = propertyInfoTab.getFireReportAssetList().getAsset(HomeSSMetaData.PropertyInfoTab.FireReport.WILDFIRE_SCORE);
+    private TextBox wildfireScoreCA = propertyInfoTabCA.getFireReportAssetList().getAsset(HomeCaMetaData.PropertyInfoTab.FireReport.WILDFIRE_SCORE);
 
 
     public void pas12922_UpdateWildfireScoreNoPrivilegeNB(PolicyType policyType) {
@@ -105,7 +109,7 @@ public class TestUpdateWildfireScore extends PolicyBaseTest {
         // Update Wildfire Score .
         wildfireScore.setValue("99");
 
-        // Check taht Reports Tab Wildfire Score did not change
+        // Check that Reports Tab Wildfire Score did not change
         NavigationPage.toViewTab(NavigationEnum.HomeSSTab.REPORTS.get());
         assertThat(reportsTab.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
         mainApp().close();
@@ -133,7 +137,7 @@ public class TestUpdateWildfireScore extends PolicyBaseTest {
         // Update Wildfire Score .
         wildfireScore.setValue("99");
 
-        // Check taht Reports Tab Wildfire Score did not change
+        // Check that Reports Tab Wildfire Score did not change
         NavigationPage.toViewTab(NavigationEnum.HomeSSTab.REPORTS.get());
         assertThat(reportsTab.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
         mainApp().close();
@@ -167,6 +171,152 @@ public class TestUpdateWildfireScore extends PolicyBaseTest {
         // Check that report value is the same
         NavigationPage.toViewTab(NavigationEnum.HomeSSTab.REPORTS.get());
         assertThat(reportsTab.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreNoPrivilegeNB(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type. Mask is there because field is disabled for unprivileged user
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData");
+
+        // Open App with unprivileged user. Create Customer. get Policy Type and initiate policy. Fill policy to PropertyInfo Tab.
+        loginA30();
+        createCustomerIndividual();
+        policyType.get().initiate();
+        policyType.get().getDefaultView().fillUpTo(testData, aaa.main.modules.policy.home_ca.defaulttabs.PropertyInfoTab.class, true);
+
+        // Assert that Wildfire Score is disabled.
+        assertThat(wildfireScoreCA).isDisabled();
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreNoPrivilegeEndorsement(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type. Mask is there because field is disabled for unprivileged user
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData");
+
+        // Open App with unprivileged user. Create Customer. get Policy Type and initiate policy. Fill policy to PropertyInfo Tab.
+        loginA30();
+        createCustomerIndividual();
+        policyType.get().createPolicy(testData);
+
+        // Endorse Policy. Assert that Wildfire Score is disabled. Save and Exit.
+        policyType.get().endorse().perform(testDataManager.policy.get(policyType).getTestData("Endorsement", "TestData"));
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PROPERTY_INFO.get());
+        assertThat(wildfireScoreCA).isDisabled();
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreNB(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData");
+
+        // Open App with user. Create Customer. get Policy Type and initiate policy. Fill policy to Reports Tab and save wildfire score
+        mainApp().open();
+        createCustomerIndividual();
+        policyType.get().initiate();
+        policyType.get().getDefaultView().fillUpTo(testData, ReportsTab.class, true);
+        String wildfireScoreValue = reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue();
+        reportsTabCA.submitTab();
+
+        // Assert that Wildfire Score is enabled.
+        propertyInfoTabCA.fillTab(testData);
+        assertThat(wildfireScoreCA).isEnabled();
+
+        // Update Wildfire Score and check if it is used.
+        wildfireScoreCA.setValue("99");
+
+        // Check that report value is the same
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        assertThat(reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreEndorsement(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData");
+
+        // Open App with user. Create Customer. get Policy Type and create policy.
+        mainApp().open();
+        createCustomerIndividual();
+        policyType.get().createPolicy(testData);
+
+        // Endorse Policy. Open reports tab Save Wildfire Score. Submit Tab.
+        policyType.get().endorse().perform(testDataManager.policy.get(policyType).getTestData("Endorsement", "TestData"));
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        String wildfireScoreValue = reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue();
+        reportsTabCA.submitTab();
+
+        // Make sure Wildfire Score is enabled
+        assertThat(wildfireScoreCA).isEnabled();
+
+        // Update Wildfire Score .
+        wildfireScoreCA.setValue("99");
+
+        // Check that Reports Tab Wildfire Score did not change
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        assertThat(reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreRenewal(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData");
+
+        // Open App with user. Create Customer. get Policy Type and initiate policy. Fill policy to PropertyInfo Tab.
+        mainApp().open();
+        createCustomerIndividual();
+        policyType.get().createPolicy(testData);
+
+        // Renew Policy. Open reports tab Save Wildfire Score. Submit Tab.
+        policyType.get().renew().perform();
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        String wildfireScoreValue = reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue();
+        reportsTabCA.submitTab();
+
+        // Make sure Wildfire Score is enabled
+        assertThat(wildfireScoreCA).isEnabled();
+
+        // Update Wildfire Score .
+        wildfireScoreCA.setValue("99");
+
+        // Check that Reports Tab Wildfire Score did not change
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        assertThat(reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
+        mainApp().close();
+    }
+
+    public void pas12922_UpdateCAWildfireScoreManualEntry(PolicyType policyType) {
+
+        // Create Test data for appropriate policy type Conversion
+        TestData testData = getStateTestData(testDataManager.policy.get(policyType).getTestData("Conversion"), "TestData");
+
+        // Create Test data for Initiate Renewal Entry
+        TestData initiateRenewal = getStateTestData(testDataManager.policy.get(policyType).getTestData("InitiateRenewalEntry"), "TestData");
+
+        // Open App with user. Create Customer. get Policy Type and initiate policy. Fill policy to PropertyInfo Tab.
+        mainApp().open();
+        createCustomerIndividual();
+
+        // Initiate manual entry. fill policy to reports Tab. Save Wildfire Score. Submit Tab.
+        customer.initiateRenewalEntry().perform(initiateRenewal);
+        policyType.get().getDefaultView().fillUpTo(testData, ReportsTab.class, true);
+        String wildfireScoreValue = reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue();
+        reportsTabCA.submitTab();
+
+        // Assert that Wildfire Score is enabled.
+        propertyInfoTabCA.fillTab(testData);
+        assertThat(wildfireScoreCA).isEnabled();
+
+        // Update Wildfire Score and check if it is used.
+        wildfireScoreCA.setValue("99");
+
+        // Check that report value is the same
+        NavigationPage.toViewTab(NavigationEnum.HomeCaTab.REPORTS.get());
+        assertThat(reportsTabCA.tblFirelineReport.getRow(1).getCell("Wildfire Score").getValue()).isEqualTo(wildfireScoreValue);
         mainApp().close();
     }
 
