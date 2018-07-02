429fc901ee15a54e9c6383c9b4d08f66e19f38e7
Carpenter, Josh (C) <Josh.Carpenter@csaa.com>
2018-07-02 16:30:12 -0700

    created testdata and added testing for endorsement scenarios

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMDOrderClueFNIExcluded.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMDOrderClueFNIExcluded.java
@@ -34,7 +34,7 @@ public class TestMDOrderClueFNIExcluded extends AutoSSBaseTest {
 	@Parameters({"state"})
 	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = {"PAS-5147"})
-	public void pas5147_testOrderClueFNIExcludedStandard(@Optional("MD") String state) {
+	public void pas5147_testOrderClueFNIExcluded_StandardNB(@Optional("MD") String state) {
 
 		TestData td = getStateTestData(testDataManager.getDefault(TestPolicyCreationBig.class), "TestData")
 				.adjust(TestData.makeKeyPath(DriverTab.class.getSimpleName() + "[0]", AutoSSMetaData.DriverTab.DRIVER_TYPE.getLabel()), "Excluded");
@@ -45,7 +45,7 @@ public class TestMDOrderClueFNIExcluded extends AutoSSBaseTest {
 
 	/**
 	 * @author Josh Carpenter
-	 * @name Test ability to order CLUE in MD when the FNI is excluded for nano policies
+	 * @name Test ability to order CLUE in MD when the FNI is excluded for nano policies at NB
 	 * @scenario
 	 * 1. Create new customer
 	 * 2. Initiate Nano MD SS Auto quote
@@ -58,7 +58,7 @@ public class TestMDOrderClueFNIExcluded extends AutoSSBaseTest {
 	@Parameters({"state"})
 	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = {"PAS-5147"})
-	public void pas5147_testOrderClueFNIExcludedNano(@Optional("MD") String state) {
+	public void pas5147_testOrderClueFNIExcluded_NanoNB(@Optional("MD") String state) {
 
 		TestData td = getStateTestData(testDataManager.getDefault(TestPolicyCreationBig.class), "TestData")
 				.adjust(TestData.makeKeyPath(DriverTab.class.getSimpleName() + "[0]", AutoSSMetaData.DriverTab.DRIVER_TYPE.getLabel()), "Excluded")
@@ -70,6 +70,59 @@ public class TestMDOrderClueFNIExcluded extends AutoSSBaseTest {
 
 	}
 
+	/**
+	 * @author Josh Carpenter
+	 * @name Test ability to order CLUE in MD when the FNI is changed to excluded and a driver is added during same mid-term endorsements
+	 * @scenario
+	 * 1. Create new customer
+	 * 2. Create MD Auto SS policy with 1 driver (FNI)
+	 * 3. Initiate mid-term endorsement
+	 * 4. Add a second driver as 'Included in Rating' and switch the first driver (FNI) to 'Excluded'
+	 * 5. Navigate to Rating Details and order reports
+	 * 6. Navigate to P & C tab and recalculate premium
+	 * 7. Navigate to DAR page and validate
+	 * @details
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+	@TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-5147"})
+	public void pas5147_testOrderClueExcludeFNIEndorsementStandard(@Optional("MD") String state) {
+
+		mainApp().open();
+		createCustomerIndividual();
+		createPolicy();
+
+		initiateEndorsementAndValidateCLUE();
+	}
+
+	/**
+	 * @author Josh Carpenter
+	 * @name Test ability to order CLUE in MD when the FNI is changed to excluded and a driver is added during same mid-term endorsements
+	 * @scenario
+	 * 1. Create new customer
+	 * 2. Create MD Auto SS Nano policy with 1 driver (FNI)
+	 * 3. Initiate mid-term endorsement
+	 * 4. Add a second driver as 'Included in Rating' and switch the first driver (FNI) to 'Excluded'
+	 * 5. Navigate to Rating Details and order reports
+	 * 6. Navigate to P & C tab and recalculate premium
+	 * 7. Navigate to DAR page and validate
+	 * @details
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+	@TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-5147"})
+	public void pas5147_testOrderClueExcludeFNIEndorsementNano(@Optional("MD") String state) {
+
+		mainApp().open();
+		createCustomerIndividual();
+		createPolicy(getPolicyTD().adjust(TestData.makeKeyPath(GeneralTab.class.getSimpleName(), AutoSSMetaData.GeneralTab.POLICY_INFORMATION.getLabel(),
+				AutoSSMetaData.GeneralTab.PolicyInformation.POLICY_TYPE.getLabel()), "Named Non Owner"));
+
+		initiateEndorsementAndValidateCLUE();
+
+	}
+
+
 	private void fillQuoteAndValidateCLUE(TestData td) {
 		mainApp().open();
 		createCustomerIndividual();
@@ -80,5 +133,29 @@ public class TestMDOrderClueFNIExcluded extends AutoSSBaseTest {
 		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(1).getCell("Select").controls.radioGroups.getFirst()).isEnabled();
 		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(1).getCell("Response").getValue()).isNotEmpty();
 	}
+
+	private void initiateEndorsementAndValidateCLUE() {
+		policy.endorse().perform(getPolicyTD("Endorsement", "TestData"));
+		policy.getDefaultView().fillFromTo(getTestSpecificTD("TestData"), GeneralTab.class, DriverActivityReportsTab.class, true);
+		assertThat(DriverActivityReportsTab.tableCLUEReports.getRows().size()).isEqualTo(2);
+		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(1).getCell("Select").controls.radioGroups.getFirst()).isDisabled();
+		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(2).getCell("Select").controls.radioGroups.getFirst()).isEnabled();
+		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(1).getCell("Response").getValue()).isNotEmpty();
+		assertThat(DriverActivityReportsTab.tableCLUEReports.getRow(2).getCell("Response").getValue()).isNotEmpty();
+	}
+
+	//		TestData td = getStateTestData(testDataManager.getDefault(TestPolicyCreationBig.class), "TestData");
+	//		TestData tdGeneralTab = td.getTestData(GeneralTab.class.getSimpleName()).getTestDataList(AutoSSMetaData.GeneralTab.NamedInsuredInformation.class.getSimpleName()).get(1)
+	//				.adjust(AutoSSMetaData.GeneralTab.NamedInsuredInformation.ADD_INSURED.getLabel(), "Click");
+	//		TestData tdDriverTab = td.getTestDataList(DriverTab.class.getSimpleName()).get(1)
+	//				.adjust(AutoSSMetaData.DriverTab.ADD_DRIVER.getLabel(), "Click");
+
+	//		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.GENERAL.get());
+	//		new GeneralTab().fillTab(tdGeneralTab);
+	//		new GeneralTab().submitTab();
+	//		new DriverTab().fillTab(tdDriverTab);
+	//		new DriverTab().submitTab();
+	//		new RatingDetailReportsTab().fillTab(td.getTestData(RatingDetailReportsTab.class.getSimpleName()));
+	//		new PremiumAndCoveragesTab().calculatePremium();
 
 }
