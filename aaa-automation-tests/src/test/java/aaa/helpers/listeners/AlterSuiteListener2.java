c7063403b368ab3a3ea0e5580333dc7f865fa2d5
Perapecha, Anton (C) <aperapecha@eisgroup.com>
2018-07-12 12:58:40 +0300

    AlterSuite Refactoring

---
+++ aaa-automation-tests/src/test/java/aaa/helpers/listeners/AlterSuiteListener2.java
@@ -0,0 +1,88 @@
+package aaa.helpers.listeners;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.testng.IAlterSuiteListener;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+import aaa.common.enums.Constants;
+import aaa.helpers.testng.CsaaXmlClass;
+import aaa.helpers.testng.CsaaXmlTest;
+import toolkit.config.PropertyProvider;
+import toolkit.utils.logging.CustomLogger;
+
+public class AlterSuiteListener2 implements IAlterSuiteListener {
+	@Override
+	public void alter(List<XmlSuite> suites) {
+
+		for (XmlSuite suite : suites) {
+			List<XmlSuite> childSuites = suite.getChildSuites();
+			if (childSuites.isEmpty()) {
+				alterSuite(suite);
+			} else {
+				alter(childSuites);
+			}
+		}
+	}
+
+	private List<String> getStates(XmlTest test) {
+		String usState = PropertyProvider.getProperty("test.usstate");
+		String temp = test.getParameter("state");
+		List<String> states = new ArrayList<>();
+		if (StringUtils.isNotBlank(temp)) {
+			states = parseStates(temp);
+		} else if (StringUtils.isNotBlank(usState)) {
+			states = parseStates(usState);
+		} else {
+			states.add(Constants.States.UT);
+		}
+		return states;
+	}
+
+	private List<String> parseStates(String input) {
+		LinkedList<String> statesList = new LinkedList<>();
+		for (String state : input.split(",")) {
+			statesList.add(state.trim().toUpperCase());
+		}
+		return statesList;
+	}
+
+	private XmlSuite alterSuite(XmlSuite suite) {
+		String customSuiteName = PropertyProvider.getProperty("test.suitename");
+		if (!customSuiteName.isEmpty()) {
+			suite.setName(customSuiteName);
+		}
+		CustomLogger.getInstance().info(suite.toXml());
+		List<XmlTest> newTests = new LinkedList<>();
+
+		List<XmlTest> tests = suite.getTests();
+		for (XmlTest test : tests) {
+			List<XmlClass> classes = CsaaXmlTest.getClasses(test);
+			List<String> states = getStates(test);
+			for (String state : states) {
+				List<XmlClass> newClasses = new LinkedList<>();
+				for (XmlClass xmlClass : classes) {
+					CsaaXmlClass xmlClassNew = new CsaaXmlClass(xmlClass, state);
+					if (xmlClassNew.get() == null || xmlClassNew.get().getIncludedMethods() == null || xmlClassNew.get().getIncludedMethods().isEmpty()) {
+						break;
+					}
+					CsaaXmlTest xmlTestNew = new CsaaXmlTest(test, state, xmlClassNew.getPolicyType());
+					xmlTestNew.get().getXmlClasses().add(xmlClassNew.get());
+					xmlClassNew.get().setXmlTest(xmlTestNew.get());
+
+				}
+			}
+		}
+		suite.setTests(CsaaXmlTest.getAllTests());
+		for (XmlTest newTest : CsaaXmlTest.getAllTests()) {
+			newTest.setXmlSuite(suite);
+		}
+		CsaaXmlTest.clearTests();
+		CustomLogger.getInstance().info(suite.toXml());
+		return suite;
+	}
+
+}
