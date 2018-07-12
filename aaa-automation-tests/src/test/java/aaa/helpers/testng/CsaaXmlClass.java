c6f7a9fdce28cd4044818813467094b3310c5e3d
Perapecha, Anton (C) <aperapecha@eisgroup.com>
2018-07-12 12:47:43 +0300

    AlterSuite Refactoring

---
+++ aaa-automation-tests/src/test/java/aaa/helpers/testng/CsaaXmlClass.java
@@ -0,0 +1,121 @@
+package aaa.helpers.testng;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlInclude;
+import aaa.utils.StateList;
+import toolkit.exceptions.IstfException;
+
+public class CsaaXmlClass {
+
+	private XmlClass xmlClass;
+
+	public CsaaXmlClass(XmlClass sourceClass, String state) {
+		xmlClass = parseClass(sourceClass, state);
+		//xmlClass = sourceClass;
+	}
+
+	public String getPolicyType() {
+		String className = xmlClass.getName();
+		if (className.contains(".auto_ca.") || className.contains(".auto_ss.")) {
+			return "Auto";
+		} else if (className.contains(".home_ca.") || className.contains(".home_ss.")) {
+			return "Home";
+		} else if (className.contains(".pup.")) {
+			return "PUP";
+		} else {
+			return "Common";
+		}
+	}
+
+	public XmlClass get() {
+		return xmlClass;
+	}
+
+	private XmlClass createClass(XmlClass sourceClass) {
+		XmlClass resultClass = new XmlClass();
+		resultClass.setName(sourceClass.getName());
+		resultClass.setIndex(sourceClass.getIndex());
+		return resultClass;
+	}
+
+	private XmlClass parseClass(XmlClass xmlClass, String state) {
+		XmlClass resultXmlClass = createClass(xmlClass);
+		try {
+			Class clazz = Class.forName(xmlClass.getName());
+			List<XmlInclude> xmlInclude = xmlClass.getIncludedMethods();
+			List<String> xmlExclude = xmlClass.getExcludedMethods();
+			List<XmlInclude> resultInclude = new LinkedList<>();
+
+			if (xmlInclude != null && !xmlInclude.isEmpty()) {
+
+				for (XmlInclude include : xmlInclude) {
+					checkTestAnnotation(clazz, include.getName());
+					if (isMethodMatch(clazz, include.getName(), state)) {
+						resultInclude.add(include);
+					}
+				}
+				resultXmlClass.setIncludedMethods(resultInclude);
+			} else {
+				for (Method method : clazz.getDeclaredMethods()) {
+					if (method.isAnnotationPresent(Test.class) && !xmlExclude.contains(method.getName())) {
+						if (isMethodMatch(clazz, method.getName(), state)) {
+							resultInclude.add(new XmlInclude(method.getName()));
+						}
+					}
+				}
+			}
+			resultXmlClass.setIncludedMethods(resultInclude);
+			for (XmlInclude include : resultInclude) {
+				include.setXmlClass(resultXmlClass);
+			}
+			resultXmlClass.setExcludedMethods(new LinkedList<>());
+
+		} catch (ClassNotFoundException e) {
+			throw new IstfException("Malformed suite: ", e.getCause());
+		}
+		return resultXmlClass;
+	}
+
+	private StateList getAnnotation(Class clazz, String methodName) {
+		StateList statesAnn = null;
+		Method method = null;
+		try {
+			method = clazz.getDeclaredMethod(methodName, String.class);
+		} catch (NoSuchMethodException e) {
+			throw new IstfException(String.format("Malformed suite. Check suite xml %s:%s ", clazz.getName(), methodName), e.getCause());
+		}
+		if (method.isAnnotationPresent(StateList.class)) {
+			statesAnn = method.getAnnotation(StateList.class);
+		} else if (clazz.isAnnotationPresent(StateList.class)) {
+			statesAnn = (StateList) clazz.getDeclaringClass().getAnnotation(StateList.class);
+		}
+		return statesAnn;
+	}
+
+	private void checkTestAnnotation(Class clazz, String method) {
+		try {
+			if (!clazz.getDeclaredMethod(method, String.class).isAnnotationPresent(Test.class)) {
+				Assert.fail(String.format("Malformed Suite!!! Method %s:%s is doesn't have @Test annotation", clazz.getName(), method));
+			}
+		} catch (NoSuchMethodException e) {
+			Assert.fail(String.format("Malformed Suite!!! No such method in class -> %s:%s", clazz.getName(), method));
+		}
+	}
+
+	private Boolean isMethodMatch(Class clazz, String methodName, String state) {
+		List<XmlInclude> list = new LinkedList<>();
+		StateList statesAnn = getAnnotation(clazz, methodName);
+		if (statesAnn != null) {
+			return Arrays.asList(statesAnn.states()).contains(state) && !Arrays.asList(statesAnn.statesExcept()).contains(state);
+		} else {
+			return true;
+		}
+	}
+
+}
---
+++ aaa-automation-tests/src/test/java/aaa/helpers/testng/CsaaXmlTest.java
@@ -0,0 +1,67 @@
+package aaa.helpers.testng;
+
+import java.util.*;
+import org.testng.collections.Maps;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlPackage;
+import org.testng.xml.XmlTest;
+
+public class CsaaXmlTest {
+
+	private static Map<String, XmlTest> testMap = new LinkedHashMap<>();
+	private XmlTest xmlTest;
+	private String key;
+
+	public CsaaXmlTest(XmlTest xmlTest, String state, String policyType) {
+		key = state.concat("_").concat(policyType);
+		if (!testMap.containsKey(key)) {
+			this.xmlTest = createTest(xmlTest, state, policyType);
+		} else {
+			this.xmlTest = testMap.get(key);
+		}
+	}
+
+	public static List<XmlTest> getAllTests() {
+		return new ArrayList<>(testMap.values());
+	}
+
+	public static List<XmlClass> getClasses(XmlTest test) {
+		List<XmlClass> resultClasses = new LinkedList<>();
+		resultClasses = test.getClasses();
+		for (XmlPackage xmlPackage : test.getPackages()) {
+			resultClasses.addAll(xmlPackage.getXmlClasses());
+		}
+		return resultClasses;
+	}
+
+	public static void clearTests() {
+		testMap.clear();
+	}
+
+	public XmlTest get() {
+		return this.xmlTest;
+	}
+
+	private XmlTest createTest(XmlTest test, String state, String policyType) {
+		XmlTest xmlTest = new XmlTest();
+		/*String testName = test.getName();
+		if (!testName.startsWith(state) || !testName.contains(state.toUpperCase())) {
+			xmlTest.setName(policyType.concat(" ").concat(state).concat(" ").concat(test.getName()));
+		} else {
+			xmlTest.setName(test.getName());
+		}*/
+		xmlTest.setName(key);
+		xmlTest.setVerbose(test.getVerbose());
+		xmlTest.setPreserveOrder(test.getPreserveOrder());
+		xmlTest.setIncludedGroups(test.getIncludedGroups());
+		xmlTest.setExcludedGroups(test.getExcludedGroups());
+		xmlTest.setParallel(test.getParallel());
+		xmlTest.setThreadCount(test.getThreadCount());
+		Map<String, String> parameters = Maps.newHashMap();
+		parameters.put("state", state);
+		xmlTest.setParameters(parameters);
+		testMap.put(key, xmlTest);
+		return xmlTest;
+	}
+
+}
