a0210d46758d298801b92c53ffec17965fe99d6d
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-08-03 10:15:42 -0700

    Fixed override error for ho3 and added additional testdata

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestHurricaneDeductibleTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestHurricaneDeductibleTemplate.java
@@ -2,14 +2,10 @@ package aaa.modules.regression.sales.template.functional;
 
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
+import aaa.main.enums.ErrorEnum;
 import aaa.main.metadata.policy.HomeSSMetaData;
 import aaa.main.modules.policy.PolicyType;
-import aaa.main.modules.policy.home_ss.defaulttabs.ApplicantTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.BindTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.MortgageesTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.PremiumsAndCoveragesQuoteTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.PropertyInfoTab;
-import aaa.main.modules.policy.home_ss.defaulttabs.PurchaseTab;
+import aaa.main.modules.policy.home_ss.defaulttabs.*;
 import aaa.modules.policy.PolicyBaseTest;
 import toolkit.datax.TestData;
 import toolkit.webdriver.controls.ComboBox;
@@ -22,8 +18,10 @@ public class TestHurricaneDeductibleTemplate extends PolicyBaseTest {
 	private PremiumsAndCoveragesQuoteTab premiumsAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
 	private BindTab bindTab = new BindTab();
 	private PurchaseTab purchaseTab = new PurchaseTab();
+	private ErrorTab errorTab = new ErrorTab();
 
-	public void pas6907_testMDHurricaneDeductible(PolicyType policyType) {
+	public void
+	pas6907_testMDHurricaneDeductible(PolicyType policyType) {
 
 		// TestData for Home . Required Zip Code and Distance to coast for mandatory 1% Hurricane deductible
 		TestData tdHome = getStateTestData(testDataManager.policy.get(policyType).getTestData("DataGather"), "TestData")
@@ -32,7 +30,10 @@ public class TestHurricaneDeductibleTemplate extends PolicyBaseTest {
 				HomeSSMetaData.ApplicantTab.DwellingAddress.ZIP_CODE.getLabel()), "21056")
 				.adjust(TestData.makeKeyPath(PropertyInfoTab.class.getSimpleName(),
 						HomeSSMetaData.PropertyInfoTab.RISKMETER.getLabel(),
-						HomeSSMetaData.PropertyInfoTab.Riskmeter.DISTANCE_TO_COAST_MILES.getLabel()), "1");
+						HomeSSMetaData.PropertyInfoTab.Riskmeter.DISTANCE_TO_COAST_MILES.getLabel()), "1")
+				.adjust(TestData.makeKeyPath(PropertyInfoTab.class.getSimpleName(),
+						HomeSSMetaData.PropertyInfoTab.RISKMETER.getLabel(),
+						HomeSSMetaData.PropertyInfoTab.Riskmeter.ELEVATION_FEET.getLabel()), "50");
 
 		// Open App initiate policy
 		mainApp().open();
@@ -69,6 +70,11 @@ public class TestHurricaneDeductibleTemplate extends PolicyBaseTest {
 		premiumsAndCoveragesQuoteTab.calculatePremium();
 		NavigationPage.toViewTab(NavigationEnum.HomeSSTab.BIND.get());
 		bindTab.submitTab();
+		if(errorTab.isVisible()){
+			errorTab.overrideErrors(ErrorEnum.Errors.ERROR_AAA_HO_SS6260765);
+			errorTab.override();
+			bindTab.submitTab();
+		}
 
 		// Renew Policy
 		policyType.get().renew().perform();
