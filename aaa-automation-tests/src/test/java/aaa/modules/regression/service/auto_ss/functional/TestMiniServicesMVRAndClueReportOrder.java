5477ce6ca2d616b5cb14a547ef3a8a6664707b47
Gubbala, Megha <megha.gubbala@csaa.com>
2018-08-07 17:20:46 -0700

    Order report

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesMVRAndClueReportOrder.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesMVRAndClueReportOrder.java
@@ -71,15 +71,6 @@ public class TestMiniServicesMVRAndClueReportOrder extends TestMiniServicesMVRAn
 		pas16694_orderReports_not_Named_Insured_endorsementBody(getPolicyType());
 	}
 
-	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-	@TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-15374"})
-	public void pas15374_driverWithMajorViolation(@Optional("VA") String state) {
-
-		pas15374_driverWithMajorViolationErrorBody();
-
-	}
-
 	/**
 	 * @author Jovita Pukenaite
 	 * @name More than two minor violations, rule 200095
