5aeb7b4eb61e1ef469991487de1d61f1ffe56d84
Bond, Brian (C) <Brian.Bond@csaa.com>
2018-08-08 17:52:24 -0700

    SQL query is working now. Assert done on first SQL response. Removed last name from YAML.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestMemberSinceDate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestMemberSinceDate.java
@@ -1,8 +1,11 @@
 package aaa.modules.regression.sales.auto_ca.choice.functional;
 
+import aaa.common.Tab;
 import aaa.common.enums.Constants;
+import aaa.main.metadata.policy.AutoCaMetaData;
 import aaa.main.modules.policy.auto_ca.defaulttabs.MembershipTab;
 import aaa.utils.StateList;
+import org.assertj.core.api.Assertions;
 import org.mortbay.log.Log;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -24,7 +27,7 @@ public class TestMemberSinceDate extends AutoCaChoiceBaseTest {
      * Precondition: Have an active valid membership response from the Stub
      * 1. Create Customer.
      * 2. Create Auto CA Quote up to Membership tab.
-     * 3. Validate that the Member Since Date in the DB is null.
+     * 3. Validate that the Member Since Date in the DB and UI are null.
      * 4. Order report in the UI.
      * 5. Validate that the Member Since Date in the DB now matches the Stub response.
      * @details
@@ -34,10 +37,6 @@ public class TestMemberSinceDate extends AutoCaChoiceBaseTest {
     @TestInfo(component = ComponentConstant.Sales.AUTO_CA_CHOICE, testCaseId = "PAS-17193")
     public void pas17193_MemberSinceDate_DB_Matches_Stub_Response(@Optional("CA") String state) {
 
-        // BondTODO: Remove this query after getting the DB working.
-        String query = Get_RMSPolicyInfoSQLQuery("QCAAS952918572");
-        // BondTODO: Get Data from DB
-        //String deleteMe = DBService.get().getValue(query).get();
 
         /*--Step 1--*/
         Log.info("Step 1: Create Customer.");
@@ -52,16 +51,18 @@ public class TestMemberSinceDate extends AutoCaChoiceBaseTest {
         policy.getDefaultView().fillUpTo(testData, MembershipTab.class, false);
 
         /*--Step 3--*/
-        Log.info("Step 3: Validate that the Member Since Date in the DB is null.");
-
-        String quoteNumber = policy.getDefaultView().getTab(MembershipTab.class).labelPolicyNumber.getValue();
+        Log.info("Step 3: Validate that the Member Since Date in the DB and UI are null.");
+        String quoteNumber = policy.getDefaultView().getTab(MembershipTab.class).getPolicyNumber();
 
         // Click save to store the quote in the db so can be accessed.
-        policy.getDefaultView().getTab(MembershipTab.class).buttonTopSave.click();
+        Tab.buttonTopSave.click();
 
-        // BondTODO: Get Data from DB
-        String value = DBService.get().getValue(Get_RMSPolicyInfoSQLQuery(quoteNumber)).orElse("");
-        // BondTODO: Assert data is not set yet.
+        Boolean dbMemberSinceDateIsNull = !GetAAAMemberSinceDateFromSQL(quoteNumber).isPresent();
+
+        Assertions.assertThat(dbMemberSinceDateIsNull);
+
+        // BondTODO: Validate no control for this is present in the UI. May have to try/catch this one.
+        //policy.getDefaultView().getTab(MembershipTab.class).//verifyFieldIsNotDisplayed("Member Since");
 
         /*--Step 4--*/
         Log.info("Step 4: Order report in the UI.");
@@ -74,13 +75,24 @@ public class TestMemberSinceDate extends AutoCaChoiceBaseTest {
         // BondTODO: Assert data is not set yet.
     }
 
-    private String Get_RMSPolicyInfoSQLQuery(String quoteNumber){
+    /**
+     * Returns the AAA Membership Member Since Date from DB.
+     * @param quoteOrPolicyNumber
+     * @return
+     */
+    private java.util.Optional<String> GetAAAMemberSinceDateFromSQL(String quoteOrPolicyNumber){
+
+        String quoteOrPolicyColumn = quoteOrPolicyNumber.toUpperCase().startsWith("Q") ? "quotenumber" : "policynumber";
+
+        String columnToJoinOn = String.format("AND ps.%1s ='%2s' ", quoteOrPolicyColumn, quoteOrPolicyNumber);
+
         String query =
-                "SELECT ps.POLICYNUMBER, ps.CONVERSIONDT, OP.productcd, OP.id, OP.POLICYNUMBER AS MEMBERSHIPPOLICYEntered, MS.ORDERMEMBERSHIPNUMBER, MS.Membershipnumber AS RMSResponse, MS.MEMBERSINCEDATE AS MS_MEMBERSINCEDATE, MS.LASTNAME AS MEMBERSHIP_LASTNAME, OP.INSURERNAME AS OPLASTNAME, TRUNC(MS.ORDERDATE) AS MS_ORDERDATE, TRUNC(MS.RECEIPTDATE) AS MS_RECEIPTDATE, MS.MEMBERSHIPSTATUS, OP.insurercd, OP.OVERRIDETYPE AS OP_OVERRIDETYPE, TRUNC(OP.MEMBERSINCEDATE) AS OP_MEMBERSINCEDATE, PS.ProductCD, ps.POLICYSTATUSCD, ps.TIMEDPOLICYSTATUSCD, ps.TXSUBTYPE, ps.TXTYPE, ps.CURRENTREVISIONIND, MS.AAABESTMEMBERSHIPSTATUS, MS.ERRORCODE, MS.ERRORMESSAGETEXT, TRUNC(MS.EFFECTIVEDATE) AS MS_EFFECTIVEDATE, TRUNC(MS.ENDDATE) AS MS_ENDDATE, EMEM.EMEMBEROPT, EMEM.EVERENROLLED, EMEM.EVALUESTATUS, EMEM.EMEMBEROPTINDATE, MS.REPORTDATAENTITYID, TRUNC(ps.EFFECTIVE) AS EFFECTIVE, TRUNC(ps.EXPIRATION)AS EXPIRATION, ps.CURRENTREVISIONIND, ps.CREATEDON, TRUNC(ps.CREATEDON) AS CREATEDON, TRUNC(ps.TRANSACTIONDATE) AS TRANSACTIONDATE, TRUNC(ps.TRANSACTIONEFFECTIVEDATE) AS TRANSACTIONEFFDATE, MS.REsponsemessageService, TRUNC(MS.AAABESTMEMBERSHIPDATE)AS AAABESTMEMBERSHIPDATE , RDE.REPORTDATA, ps.POLICYDETAIL_ID, ps.id AS POLICYSUMMARYID, ms.id AS MEMSummaryId FROM policysummary ps JOIN OTHERORPRIORPOLICY OP ON OP.POLICYDETAIL_ID=ps.POLICYDETAIL_ID AND OP.PRODUCTCD ='membership' AND ps.quotenumber ='"
-        + quoteNumber + "' LEFT JOIN pasadm.AAAEMEMBERDETAILSENTITY EMEM ON EMEM.ID=PS.EMEMBERDETAIL_ID JOIN MEMBERSHIPSUMMARYENTITY MS ON MS.ID=ps.MEMBERSHIPSUMMARY_ID left JOIN AAAREPORTDATAENTITY RDE ON RDE.ID=MS.REPORTDATAENTITYID AND RDE.REPORTTYPE ='MEMBERSHIP' AND RDE.ENTITYREFNO=PS.POLICYNUMBER ORDER BY ps.EFFECTIVE DESC;\n";
+                "SELECT MS.MEMBERSINCEDATE AS MS_MEMBERSINCEDATE " +
+                        "FROM policysummary ps " +
+                        "JOIN OTHERORPRIORPOLICY OP ON OP.POLICYDETAIL_ID=ps.POLICYDETAIL_ID AND OP.PRODUCTCD ='membership' " +
+                         columnToJoinOn +
+                        "JOIN MEMBERSHIPSUMMARYENTITY MS ON MS.ID=ps.MEMBERSHIPSUMMARY_ID";
 
-
-
-        return query;
+        return DBService.get().getValue(query);
     }
 }
