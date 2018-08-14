2396539b158090a70364d42d017437ce2f377aa8
Kazlauskiene, Reda (C) <reda.kazlauskiene@csaa.com>
2018-08-14 00:41:09 -0700

    PAS-17792: Update precondition test

--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/UpdateRolePrecondition.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/UpdateRolePrecondition.java
@@ -1,14 +1,21 @@
 package aaa.modules.regression.finance.operational_reports;
 
 import aaa.admin.modules.security.role.RoleType;
+import aaa.helpers.product.OperationalReportsHelper;
+import aaa.main.enums.OperationalReportsConstants;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
 
 public class UpdateRolePrecondition extends OperationalReportsBaseTest {
 
     @Test
     public void updateRole() {
+        OperationalReportsHelper.prepareEuwOpReportsPrivileges();
         adminApp().open();
-        searchAndUpdateOrCreateRole(
-                testDataManager.securityRole.get(RoleType.CORPORATE).getTestData("DataGather", "TestData"), RoleType.CORPORATE);
+        verifyRole(testDataManager.securityRole.get(RoleType.CORPORATE).getTestData("DataGather", "TestData"), RoleType.CORPORATE,
+                Arrays.asList(OperationalReportsConstants.RolesPriviliges.REPORTS_OPERATIONAL_EUW_SCHEDULE,
+                OperationalReportsConstants.RolesPriviliges.REPORTS_OPERATIONAL_EUW_VIEW));
     }
 }
