5cfc128580772feae5674b96b34c1bc976624f4b
Maiko, Kanstantsin (C) <kmaiko@eisgroup.com>
2018-08-15 11:11:28 +0300

    Conversion Smoke tests update

--- aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ca/ho3/HdesImportSmokeTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/home_ca/ho3/HdesImportSmokeTest.java
@@ -5,6 +5,7 @@ import org.testng.ITestContext;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import aaa.common.enums.Constants;
 import aaa.common.pages.SearchPage;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
@@ -14,8 +15,10 @@ import aaa.helpers.conversion.HdesConversionData;
 import aaa.helpers.product.ProductRenewalsVerifier;
 import aaa.main.enums.ProductConstants;
 import aaa.modules.policy.HomeCaHO3BaseTest;
+import aaa.utils.StateList;
 import toolkit.utils.TestInfo;
 
+@StateList(states = {Constants.States.CA})
 public class HdesImportSmokeTest extends HomeCaHO3BaseTest {
 
 	@Parameters({"state"})
@@ -23,7 +26,7 @@ public class HdesImportSmokeTest extends HomeCaHO3BaseTest {
 	@TestInfo(component = ComponentConstant.Conversions.HOME_CA_HO3)
 	public void hdesCAHO3ImportTest(@Optional("CA") String state, ITestContext context) {
 		LocalDateTime effDate = getTimePoints().getConversionEffectiveDate();
-		ConversionPolicyData data = new HdesConversionData("HO3/1.xml", effDate);
+		ConversionPolicyData data = new HdesConversionData("HO3/20170808_045542_CNV_B_HDESPROP_EXGPAS_8435_D_20170908_30000506_XE65055.xml", effDate);
 		String policyNum = ConversionUtils.importPolicy(data, context, false);
 
 		mainApp().open();
