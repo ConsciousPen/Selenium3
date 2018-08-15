89aeaa2288a99d6e18ac477cb14cf83ec924fbd3
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-08-15 13:19:18 -0700

    Refactored InitiateHOQute tests to re use endorsement form template. Deleted InitiateHOQuoteTemplate

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestInitiateHOQuoteHO29Endorsement.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestInitiateHOQuoteHO29Endorsement.java
@@ -2,9 +2,10 @@
  * CONFIDENTIAL AND TRADE SECRET INFORMATION. No portion of this work may be copied, distributed, modified, or incorporated into any other media without EIS Group prior written consent. */
 package aaa.modules.regression.sales.home_ca.ho6.functional;
 
-
 import aaa.main.enums.EndorsementForms;
 import aaa.main.modules.policy.PolicyType;
+import aaa.main.modules.policy.home_ca.defaulttabs.EndorsementTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.PurchaseTab;
 import aaa.modules.regression.sales.template.functional.TestEndorsementsTabAbstract;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -44,8 +45,19 @@ public class TestInitiateHOQuoteHO29Endorsement extends TestEndorsementsTabAbstr
 	@TestInfo(component = ComponentConstant.Sales.HOME_CA_HO6, testCaseId = "PAS-13261")
 	public void pas13261_testInitiateHOQuoteHO29added(@Optional("CA") String state) {
 
-		initiateNewBusinessTx(false);
+		createQuoteAndFillUpTo(EndorsementTab.class);
+
+		// Assert that HO-29 is added by default. Purchase Policy.
 		checkEndorsementIsAvailableInIncludedEndorsements(EndorsementForms.HomeCAEndorsementForms.HO_29.getFormId());
 
+		policy.getDefaultView().fillFromTo(getPolicyTD(), EndorsementTab.class, PurchaseTab.class, true);
+		new PurchaseTab().submitTab();
+
+		// Initiate HO Quote from Policy Summary Page. Fill Policy up to Endorsement Tab.
+		policy.initiateHOQuote().start();
+		policy.getDefaultView().fillUpTo(getPolicyTD(), EndorsementTab.class);
+
+		// Assert that HO-29 is added by default. Purchase Policy.
+		checkEndorsementIsAvailableInIncludedEndorsements(EndorsementForms.HomeCAEndorsementForms.HO_29.getFormId());
 	}
 }
