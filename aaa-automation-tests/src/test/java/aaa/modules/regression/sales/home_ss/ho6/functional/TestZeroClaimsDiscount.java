a16713963b8a31fc24d90a019515c4639ddb0454
Carpenter, Josh <Josh.Carpenter@csaa.com>
2018-08-16 10:56:39 -0700

    added renewals testing and javaDoc comments

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ss/ho6/functional/TestZeroClaimsDiscount.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ss/ho6/functional/TestZeroClaimsDiscount.java
@@ -18,7 +18,7 @@ public class TestZeroClaimsDiscount extends TestZeroClaimsDiscountTemplate {
 
 	/**
 	 * @author Josh Carpenter
-	 * @name Test that the system is not applying the Zero Claims Discount when a renewal image has a claim that is included in rating
+	 * @name Test Zero Claims Discount for SS HO6 Quotes
 	 * @scenario
 	 * 1. Create customer
 	 * 2. Initiate SS HO6 quote
@@ -43,10 +43,43 @@ public class TestZeroClaimsDiscount extends TestZeroClaimsDiscountTemplate {
 	 * @details
 	 **/
 	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH}, description = "Test Zero Claims Discount for SS HO6")
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH}, description = "Test Zero Claims Discount for SS HO6 Quote")
 	@TestInfo(component = ComponentConstant.Sales.HOME_SS_HO6, testCaseId = "PAS-9088")
-	public void pas9088_testZeroClaimsDiscountHO4Quote(@Optional("") String state) {
+	public void pas9088_testZeroClaimsDiscountHO6Quote(@Optional("") String state) {
 		pas9088_testZeroClaimsDiscountQuote();
+	}
+
+	/**
+	 * @author Josh Carpenter
+	 * @name Test Zero Claims Discount for SS HO6 Renewals
+	 * @scenario
+	 * 1. Create customer
+	 * 2. Create SS HO6 Policy
+	 * 3. Create renewal image and navigate to P & C tab and calculate premium
+	 * 4. Validate the zero claims discount is present and capture the premium amount
+	 * 5. Navigate to Property Info tab and add a claim:
+	 * 		a. Date of Loss = 1 year ago today
+	 * 		b. Cause of Loss = Liability
+	 * 		c. Amount of Loss = $1,001
+	 * 		d. Claim Status = Closed
+	 * 		e. AAA Claim = Yes
+	 * 		f. Catastrophe = No
+	 * 6. Navigate to P & C tab, calculate premium
+	 * 7. Validate no zero claims discount is present and premium has increased
+	 * 8. Change claim to 'Open' and validate discount/premium is added/decreased
+	 * 9. Change claim to back to 'Closed' and validate discount/premium is removed/increased
+	 * 10. Change claim to 'Subrogation' and validate discount/premium is added/decreased
+	 * 11. Change claim to back to 'Closed' and validate discount/premium is removed/increased
+	 * 12. Change claim Date to more than 3 years ago and validate discount/premium is added/decreased
+	 * 13. Change date back to 1 year ago and validate discount/premium is removed/increased
+	 * 14. Change claim amount to less than $1000 and validate discount/premium is added/decreased
+	 * @details
+	 **/
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH}, description = "Test Zero Claims Discount for SS HO6 Renewals")
+	@TestInfo(component = ComponentConstant.Sales.HOME_SS_HO6, testCaseId = "PAS-9088")
+	public void pas9088_testZeroClaimsDiscountHO6Renewal(@Optional("") String state) {
+		pas9088_testZeroClaimsDiscountRenewal();
 	}
 }
 
