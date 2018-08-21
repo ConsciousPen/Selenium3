9971dbffb90a12d0ab62d5890bac3e501846d5ba
Kazlauskiene, Reda (C) <reda.kazlauskiene@csaa.com>
2018-08-21 05:46:48 -0700

    PAS-17792: Update GenerateReport test

--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsEuwDetailsGeneration.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsEuwDetailsGeneration.java
@@ -1,5 +1,7 @@
 package aaa.modules.regression.finance.operational_reports;
 
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
 import aaa.helpers.product.OperationalReportsHelper;
 import com.exigen.istf.exec.testng.TimeShiftTestUtil;
 import com.jayway.awaitility.Awaitility;
@@ -10,30 +12,31 @@ import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Test;
 import toolkit.config.PropertyProvider;
 import toolkit.utils.SSHController;
+import toolkit.utils.TestInfo;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import static aaa.helpers.cft.CFTHelper.downloadComplete;
-import static aaa.helpers.cft.CFTHelper.remoteDownloadComplete;
+import static aaa.helpers.cft.CFTHelper.*;
 
 
-public class GenerateReport extends OperationalReportsBaseTest {
+public class TestOperationalReportsEuwDetailsGeneration extends OperationalReportsBaseTest {
 
     private static final String REMOTE_DOWNLOAD_FOLDER_PROP = "test.remotefile.location"; // location /root/Downloads
     private static final String REMOTE_DOWNLOAD_FOLDER = "/home/autotest/Downloads";
-    private static final String DOWNLOAD_DIR = System.getProperty("user.dir") + "/src/test/resources/op_reports/";
+    private static final String DOWNLOAD_DIR = System.getProperty("user.dir") + PropertyProvider.getProperty("test.downloadfiles.location");
 
     private String remoteFileLocation = PropertyProvider.getProperty(REMOTE_DOWNLOAD_FOLDER_PROP);
 
-    @Test
-    public void generateReport() throws SftpException, JSchException, IOException {
+    @Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
+    @TestInfo(component = ComponentConstant.Finance.OPERATIONAL_REPORTS, testCaseId = "PAS-17631")
+    public void testOperationalReportsEuwDetailsGeneration() throws SftpException, JSchException, IOException {
         File downloadDir = new File(DOWNLOAD_DIR);
-        OperationalReportsHelper.checkDirectory(downloadDir);
-        OperationalReportsHelper.checkFile(DOWNLOAD_DIR, "Validations.xlsx");
+        checkDirectory(downloadDir);
 
         opReportApp().open();
+        // get map from OR reports
         if (StringUtils.isNotEmpty(remoteFileLocation)) {
             String monitorInfo = TimeShiftTestUtil.getContext().getBrowser().toString();
             String monitorAddress = monitorInfo.substring(monitorInfo.indexOf(" ") + 1, monitorInfo.indexOf(":", monitorInfo.indexOf(" ")));
@@ -46,6 +49,8 @@ public class GenerateReport extends OperationalReportsBaseTest {
             sshControllerRemote.deleteFile(new File(REMOTE_DOWNLOAD_FOLDER + "/*.*"));
             Awaitility.await().atMost(threeMinutes).until(() -> remoteDownloadComplete(sshControllerRemote, new File(REMOTE_DOWNLOAD_FOLDER)) == 0);
             operationalReport.create(getOperationalReportsTD("DataGather", "TestData_EUW_Detail"));
+            Awaitility.await().atMost(threeMinutes).until(() -> remoteDownloadComplete(sshControllerRemote, new File(REMOTE_DOWNLOAD_FOLDER)) == 1);
+            // moving Balances from monitor to download dir
             sshControllerRemote.downloadFolder(new File(REMOTE_DOWNLOAD_FOLDER), downloadDir);
             Awaitility.await().atMost(threeMinutes).until(() -> downloadComplete(downloadDir, OperationalReportsHelper.EXCEL_FILE_EXTENSION) == 1);
             sshControllerRemote.deleteFile(new File(REMOTE_DOWNLOAD_FOLDER + "/*.*"));
