4d6bebec1ce976f1de8f40b195a61574b386c662
Bond, Brian (C) <Brian.Bond@csaa.com>
2018-08-21 17:51:09 -0700

    Added Modularity to both classes. First successful manual execution of valid test Elastic Search Test

--- aaa-automation-tests/src/test/java/aaa/helpers/db/queries/AAAMembershipQueries.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/db/queries/AAAMembershipQueries.java
@@ -5,23 +5,58 @@ import toolkit.db.DBService;
 public class AAAMembershipQueries {
 
     /**
+     * Verified Membership states from PAS.
+     */
+    public enum AAAMembershipStatus {ACTIVE, LAPSED, CANCELED, FUTURE_DATED, PENDING, Error, No_Hit}
+
+    // BondTODO: Make query for AAABESTMEMBERSHIPSTATUS
+    // BondTODO: Make query for ORDERMEMBERSHIPNUMBER
+
+    /**
      * Returns the AAA Membership Member Since Date from DB.
      * @param quoteOrPolicyNumber
      * @return
      */
     public static java.util.Optional<String> GetAAAMemberSinceDateFromSQL(String quoteOrPolicyNumber){
-
-        String quoteOrPolicyColumn = quoteOrPolicyNumber.toUpperCase().startsWith("Q") ? "quotenumber" : "policynumber";
-
-        String columnToJoinOn = String.format("AND ps.%1s ='%2s' ", quoteOrPolicyColumn, quoteOrPolicyNumber);
-
         String query =
                 "SELECT MS.MEMBERSINCEDATE AS MS_MEMBERSINCEDATE " +
                         "FROM policysummary ps " +
                         "JOIN OTHERORPRIORPOLICY OP ON OP.POLICYDETAIL_ID=ps.POLICYDETAIL_ID AND OP.PRODUCTCD ='membership' " +
-                        columnToJoinOn +
+                        GetQuoteOrPolicyNumberJoinSQL(quoteOrPolicyNumber) +
                         "JOIN MEMBERSHIPSUMMARYENTITY MS ON MS.ID=ps.MEMBERSHIPSUMMARY_ID";
 
         return DBService.get().getValue(query);
+    }
+
+    /**
+     * Changes the Membership status *After* policy was created to specific status.
+     * @param policyNumber
+     * @param updatedStatus
+     */
+    public static void UpdateAAAMembershipStatusInSQL(String policyNumber, AAAMembershipStatus updatedStatus){
+        // BondToDo: Check with Rajesh if this works for Quotes. Update line 43 to use the GetQuoteOrPolicyNumberJoinSQL() if so.
+        String query =
+                String.format(
+                        "UPDATE membershipsummaryentity mse SET mse.membershipstatus = '%1s' " +
+                        "WHERE mse.id IN (" +
+                        "SELECT ms.id FROM policysummary ps JOIN membershipsummaryentity ms ON ms.id = ps.membershipsummary_id " +
+                                "AND PS.policynumber='%2s')",
+                        updatedStatus.name(),   //%1s
+                        policyNumber);          //%2s
+
+        DBService.get().executeUpdate(query);
+    }
+
+    /**
+     * Used to get an appropriate AND column statement using passed in quoteOrPolicyNumber for AAA Membership Queries.
+     * EX: quoteOrPolicyNumber("QAZSS952918540") returns "AND ps.quotenumber='QAZSS952918540' "
+     * EX: quoteOrPolicyNumber("AZSS952918540" ) returns "AND ps.policynumber='AZSS952918540' "
+     * @param quoteOrPolicyNumber
+     * @return
+     */
+    private static String GetQuoteOrPolicyNumberJoinSQL(String quoteOrPolicyNumber){
+        String quoteOrPolicyColumnName = quoteOrPolicyNumber.toUpperCase().startsWith("Q") ? "quotenumber" : "policynumber";
+        String SQL = String.format("AND ps.%1s ='%2s' ", quoteOrPolicyColumnName, quoteOrPolicyNumber);
+        return SQL;
     }
 }
--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestBestMembershipLogic.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestBestMembershipLogic.java
@@ -3,6 +3,7 @@ package aaa.modules.regression.sales.auto_ss.functional;
 import aaa.common.enums.Constants;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
+import aaa.helpers.db.queries.AAAMembershipQueries;
 import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.modules.policy.AutoSSBaseTest;
 import aaa.utils.StateList;
@@ -21,14 +22,51 @@ import java.time.LocalDateTime;
 @StateList(states = Constants.States.AZ)
 public class TestBestMembershipLogic extends AutoSSBaseTest {
 
+
+    @Parameters({"state"})
+    @Test()
+    public void GetAdminPage(@Optional("") String state){
+        AAAMembershipQueries.UpdateAAAMembershipStatusInSQL("AZSS952918539", AAAMembershipQueries.AAAMembershipStatus.Error);
+        adminApp().open();
+    }
+
     /**
      * This test requires manual intervention so is disabled until BML wiremock piece put in place.
      * @author Brian Bond
      * @name BML considers Expiration Date on Transfer In Status - PAS-15944
      * @scenario
      * 1. *Manual Intervention Required* Set the BML up for mocking.
-     * 2. *Manual Intervention Required* Mock the BML service for Inactive.
-     * 3. Create policy with no membership.
+     * 2. *Manual Intervention Required* Mock the BML service for Inactive as well as edit SQL Member Status to be Error.
+     * 3. Create policy with valid but inactive membership by overwriting status after policy created.
+     * 4. Move VDM forward to NB + 15.
+     * 5. *Manual Intervention Required* Mock BML service for an expiration date prior to effective date and transfer-in policy status and role status.
+     * 6. Verify the correct job handling occurs given response.
+     * @details
+     */
+    @Parameters({"state"})
+    @Test(/*enabled = false,*/ groups = {Groups.FUNCTIONAL, Groups.HIGH}, description = "17193: MemberSinceDate in database matches stub response")
+    @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-17193")
+    public void pas15944_BML_NB15_Valid_Expiration_Date_And_Active_Status(@Optional("") String state) {
+        // BondTODO: Update this to verify AAABestMemberShipStatus and confirm with Rajesh MembershipPolicyEntered or OrderMembershipNumber
+        // BondTODO: Also check on UpdateAAAMembershipStatusInSQL() works with PS.Quote as well.
+        String policyNumber = CreatePolicyAndMoveToNB15();
+
+        /*--Step 5--*/
+        log.info("Step 5: *Manual Intervention Required* Mock BML service for an expiration date prior to effective date and transfer-in status.");
+
+        /*--Step 6--*/
+        log.info("Step 6: Verify the correct job handling occurs given response.");
+        STG1STG2JobExecute();
+    }
+
+    /**
+     * This test requires manual intervention so is disabled until BML wiremock piece put in place.
+     * @author Brian Bond
+     * @name BML considers Expiration Date on Transfer In Status - PAS-15944
+     * @scenario
+     * 1. *Manual Intervention Required* Set the BML up for mocking.
+     * 2. *Manual Intervention Required* Mock the BML service for Inactive as well as edit SQL Member Status to be Error.
+     * 3. Create policy with valid but inactive membership by overwriting status after policy created.
      * 4. Move VDM forward to NB + 15.
      * 5. *Manual Intervention Required* Mock BML service for an expiration date prior to effective date and transfer-in policy status and role status.
      * 6. Verify the correct job handling occurs given response.
@@ -39,24 +77,7 @@ public class TestBestMembershipLogic extends AutoSSBaseTest {
     @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-17193")
     public void pas15944_BML_considers_Expiration_Date_On_Transfer_In_Status(@Optional("") String state) {
 
-        /*--Step 1--*/
-        log.info("Step 1: *Manual Intervention Required* Set the BML up for mocking.");
-        // Setup SOAPUI Mocking
-        // Modify the enterpriseSearchService.enterpriseCustomerDetailsSearchUri in admin to point at the mock.
-
-
-
-        /*--Step 2--*/
-        log.info("Step 2: *Manual Intervention Required* Mock the BML service for Inactive.");
-
-        /*--Step 3--*/
-        log.info("Step 3: Create policy with no membership.");
-        CreateAutoSSPolicy();
-        LocalDateTime policyEffectiveDate = PolicySummaryPage.getEffectiveDate();
-
-        /*--Step 4--*/
-        log.info("Move VDM forward to NB + 15.");
-        TimeSetterUtil.getInstance().nextPhase(policyEffectiveDate.plusDays(15));
+        String policyNumber = CreatePolicyAndMoveToNB15();
 
         /*--Step 5--*/
         log.info("Step 5: *Manual Intervention Required* Mock BML service for an expiration date prior to effective date and transfer-in status.");
@@ -64,26 +85,75 @@ public class TestBestMembershipLogic extends AutoSSBaseTest {
 
         /*--Step 6--*/
         log.info("Step 6: Verify the correct job handling occurs given response.");
-        JobUtils.executeJob(Jobs.membershipValidationJob);
+        STG1STG2JobExecute();
+    }
+
+    private String CreatePolicyAndMoveToNB15(){
+        /*--Step 1--*/
+        log.info("Step 1: *Manual Intervention Required* Set the BML up for mocking.");
+        // Setup SOAPUI Mocking
+        // Modify the enterpriseSearchService.enterpriseCustomerDetailsSearchUri in admin to point at the mock.
+
+
+        /*--Step 2--*/
+        log.info("Step 2: *Manual Intervention Required* Mock the BML service for Inactive as well as edit SQL Member Status to be Error.");
+
+
+        /*--Step 3--*/
+        log.info("Step 3: Create policy with valid but inactive membership by overwriting status after policy created.");
+        String policyNumber = CreateAutoSSPolicy();
+        SetErrorStatus(policyNumber);
+
+        /*--Step 4--*/
+        log.info("Move VDM forward to NB + 15.");
+        MoveToNB15();
+
+        return policyNumber;
     }
 
     private String CreateAutoSSPolicy(){
-
         // keypathTabSection Result: "GeneralTab|AAAProductOwned"
-        String keypathTabSection = TestData.makeKeyPath(aaa.main.modules.customer.defaulttabs.GeneralTab.class.getSimpleName(), AutoSSMetaData.GeneralTab.AAA_PRODUCT_OWNED.getLabel());
+        String keypathTabSection = TestData.makeKeyPath(aaa.main.modules.customer.defaulttabs.GeneralTab.class.getSimpleName(),
+                AutoSSMetaData.GeneralTab.AAA_PRODUCT_OWNED.getLabel());
 
         // keypathCurrentMember Result: "GeneralTab|AAAProductOwned|Current AAA Member"
-        String keypathCurrentMember = TestData.makeKeyPath(keypathTabSection, AutoSSMetaData.GeneralTab.AAAProductOwned.CURRENT_AAA_MEMBER.getLabel());
+        String keypathCurrentMember = TestData.makeKeyPath(keypathTabSection,
+                AutoSSMetaData.GeneralTab.AAAProductOwned.CURRENT_AAA_MEMBER.getLabel());
 
         // keypathMemberNum Result: "GeneralTab|AAAProductOwned|Membership Number"
-        String keypathMemberNum = TestData.makeKeyPath(keypathTabSection, AutoSSMetaData.GeneralTab.AAAProductOwned.MEMBERSHIP_NUMBER.getLabel());
+        String keypathMemberNum = TestData.makeKeyPath(keypathTabSection,
+                AutoSSMetaData.GeneralTab.AAAProductOwned.MEMBERSHIP_NUMBER.getLabel());
 
         TestData testData = getPolicyTD()
-                .adjust(keypathCurrentMember, "No")
-                .mask(keypathMemberNum);
+                .adjust(keypathCurrentMember, "Yes")
+                .adjust(keypathMemberNum, "9436258506738011");
 
         mainApp().open();
         createCustomerIndividual();
         return createPolicy(testData);
+    }
+
+    private void SetErrorStatus(String policyNumber){
+        AAAMembershipQueries.UpdateAAAMembershipStatusInSQL(policyNumber, AAAMembershipQueries.AAAMembershipStatus.Error);
+    }
+
+    private void MoveToNB15(){
+        LocalDateTime policyEffectiveDate = PolicySummaryPage.getEffectiveDate();
+        TimeSetterUtil.getInstance().nextPhase(policyEffectiveDate.plusDays(15));
+    }
+
+    private void MoveToNB30(){
+        LocalDateTime policyEffectiveDate = PolicySummaryPage.getEffectiveDate();
+        TimeSetterUtil.getInstance().nextPhase(policyEffectiveDate.plusDays(30));
+    }
+
+    private void STG1STG2JobExecute() {
+        JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+        JobUtils.executeJob(Jobs.aaaAutomatedProcessingInitiationJob);
+        JobUtils.executeJob(Jobs.automatedProcessingRatingJob);
+        JobUtils.executeJob(Jobs.automatedProcessingRunReportsServicesJob);
+        JobUtils.executeJob(Jobs.automatedProcessingIssuingOrProposingJob);
+        JobUtils.executeJob(Jobs.automatedProcessingStrategyStatusUpdateJob);
+        //JobUtils.executeJob(Jobs.automatedProcessingBypassingAndErrorsReportGenerationJob);
     }
 }
