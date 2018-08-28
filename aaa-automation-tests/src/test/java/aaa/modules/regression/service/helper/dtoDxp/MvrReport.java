9b30858c43f69e5bca558b4de922956384a2e54f
Gubbala, Megha <megha.gubbala@csaa.com>
2018-08-28 11:15:43 -0700

    coverage fix

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/helper/dtoDxp/MvrReport.java
@@ -0,0 +1,9 @@
+package aaa.modules.regression.service.helper.dtoDxp;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class MvrReport {
+
+	@ApiModelProperty(value = "License status", required = true)
+	public String choicePointLicenseStatus;
+}
