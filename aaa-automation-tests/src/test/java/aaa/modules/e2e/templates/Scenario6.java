6f610ff0132396f51f52719dc1cd89a1c262b11d
Belakova, Natalija (C) <nbelakova@eisgroup.com>
2018-08-30 14:57:34 +0300

    [E2E] fix installment for end of February for leap year 2020

--- aaa-automation-tests/src/test/java/aaa/modules/e2e/templates/Scenario6.java
+++ aaa-automation-tests/src/test/java/aaa/modules/e2e/templates/Scenario6.java
@@ -261,9 +261,10 @@ public class Scenario6 extends ScenarioBaseTest {
 		int leapDays = 0;
 		for (LocalDateTime date : installmentDueDates) {
 			if (date.plusYears(1).isEqual(TimeSetterUtil.getInstance().parse("02/28/2020", DateTimeUtils.MM_DD_YYYY))) {
-				leapDays = policyEffectiveDate.getDayOfMonth() - 28;
+				leapDays = (policyEffectiveDate.getDayOfMonth() - 28)>0 ? 1 : 0;
 			}
-			installmentsSum = installmentsSum.add(BillingHelper.getInstallmentDueByDueDate(date.plusYears(1).minusDays(leapDays)));
+			installmentsSum = installmentsSum.add(BillingHelper.getInstallmentDueByDueDate(date.plusYears(1).plusDays(leapDays)));
+			leapDays = 0;
 		}
 		totalDue = BillingSummaryPage.getTotalDue();
 	}
@@ -292,9 +293,14 @@ public class Scenario6 extends ScenarioBaseTest {
 		BillingSummaryPage.showPriorTerms();
 		new BillingAccountPoliciesVerifier().setPolicyStatus(PolicyStatus.POLICY_ACTIVE).verifyRowWithEffectiveDate(policyExpirationDate);
 
+		int leapDays = 0;
 		Dollar renewInstallmentsSum = new Dollar(0);
 		for (LocalDateTime date : installmentDueDates) {
-			renewInstallmentsSum = renewInstallmentsSum.add(BillingHelper.getInstallmentDueByDueDate(date.plusYears(1)));
+			if (date.plusYears(1).isEqual(TimeSetterUtil.getInstance().parse("02/28/2020", DateTimeUtils.MM_DD_YYYY))) {
+				leapDays = (policyEffectiveDate.getDayOfMonth() - 28)>0 ? 1 : 0;
+			}
+			renewInstallmentsSum = renewInstallmentsSum.add(BillingHelper.getInstallmentDueByDueDate(date.plusYears(1).plusDays(leapDays)));
+			leapDays = 0;
 		}
 
 		installmentsSum.subtract(overpayment).verify.equals(renewInstallmentsSum);
