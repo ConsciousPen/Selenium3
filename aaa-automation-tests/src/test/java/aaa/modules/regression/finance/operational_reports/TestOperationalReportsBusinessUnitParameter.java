fc3a722b339dd54fe63f570c745d4c69ffe8249f
Piatrouski, Maksim (C) <gxkepia@tent.trt.csaa.pri>
2018-09-10 06:20:21 -0700

    reformatted classes

--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsBusinessUnitParameter.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsBusinessUnitParameter.java
@@ -1,5 +1,9 @@
 package aaa.modules.regression.finance.operational_reports;
 
+import static toolkit.verification.CustomAssertions.assertThat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.testng.annotations.Test;
 import aaa.admin.metadata.reports.OperationalReportsMetaData;
 import aaa.admin.modules.reports.operationalreports.defaulttabs.OperationalReportsTab;
 import aaa.helpers.constants.ComponentConstant;
@@ -7,54 +11,48 @@ import aaa.helpers.constants.Groups;
 import aaa.main.enums.OperationalReportsConstants;
 import aaa.modules.BaseTest;
 import aaa.toolkit.webdriver.customcontrols.AdvancedSelectorOR;
-import org.testng.annotations.Test;
 import toolkit.utils.TestInfo;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static toolkit.verification.CustomAssertions.assertThat;
 
 public class TestOperationalReportsBusinessUnitParameter extends BaseTest {
 
-    private OperationalReportsTab orTab = new OperationalReportsTab();
+	private OperationalReportsTab orTab = new OperationalReportsTab();
 
-    /**
-     * @author Reda Kazlauskiene
-     * Objectives: check that “Business Unit” values are displayed and selectable
-     * TC Steps:
-     * 1. Navigate Reports -> Operational Reports;
-     * 2. Fill Category, Type
-     * 3. Click Button “Business Unit”
-     * 4. check that “Business Unit” values are displayed and selectable
-     */
+	/**
+	 * @author Reda Kazlauskiene
+	 * Objectives: check that “Business Unit” values are displayed and selectable
+	 * TC Steps:
+	 * 1. Navigate Reports -> Operational Reports;
+	 * 2. Fill Category, Type
+	 * 3. Click Button “Business Unit”
+	 * 4. check that “Business Unit” values are displayed and selectable
+	 */
 
-    @Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
-    @TestInfo(component = ComponentConstant.Finance.OPERATIONAL_REPORTS, testCaseId = "PAS-15535")
-    public void pas15535_testOperationalReportsBusinessUnitParameter() {
+	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
+	@TestInfo(component = ComponentConstant.Finance.OPERATIONAL_REPORTS, testCaseId = "PAS-15535")
+	public void pas15535_testOperationalReportsBusinessUnitParameter() {
 
-        opReportApp().open();
-        orTab.fillTab(getOperationalReportsTD("DataGather","TestData_EUW_Detail"));
+		opReportApp().open();
+		orTab.fillTab(getOperationalReportsTD("DataGather", "TestData_EUW_Detail"));
 
-        //Check "Business Unit” values;
-        AdvancedSelectorOR businessUnit = orTab.getAssetList().getAsset(OperationalReportsMetaData.OperationalReportsTab.BUSINESS_UNIT);
+		//Check "Business Unit” values;
+		AdvancedSelectorOR businessUnit = orTab.getAssetList().getAsset(OperationalReportsMetaData.OperationalReportsTab.BUSINESS_UNIT);
 
-        assertThat(businessUnit.getAdvancedSelectorText().getValue()).isEqualTo("All");
+		assertThat(businessUnit.getAdvancedSelectorText().getValue()).isEqualTo("All");
 
-        businessUnit.setValue(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
-                OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY));
+		businessUnit.setValue(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
+				OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY));
 
-        assertThat(businessUnit.getAdvancedSelectorText().getValue()).contains(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
-                OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY));
+		assertThat(businessUnit.getAdvancedSelectorText().getValue()).contains(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
+				OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY));
 
-        businessUnit.setValue(Arrays.asList("ALL"));
+		businessUnit.setValue(Arrays.asList("ALL"));
 
-        assertThat(businessUnit.getAdvancedSelectorText().getValue()).contains(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
-                OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY, OperationalReportsConstants.BusinessUnit.CSAA_GENERAL_INSURANCE_COMPANY,
-                OperationalReportsConstants.BusinessUnit.CSAA_INTERINSURANCE_BUREAU));
+		assertThat(businessUnit.getAdvancedSelectorText().getValue()).contains(Arrays.asList(OperationalReportsConstants.BusinessUnit.CSAA_AFFINITY_INSURANCE_COMPANY,
+				OperationalReportsConstants.BusinessUnit.CSAA_FIRE_CASUALTY_INSURANCE_COMPANY, OperationalReportsConstants.BusinessUnit.CSAA_GENERAL_INSURANCE_COMPANY,
+				OperationalReportsConstants.BusinessUnit.CSAA_INTERINSURANCE_BUREAU));
 
-        businessUnit.setValue(new ArrayList<>());
+		businessUnit.setValue(new ArrayList<>());
 
-        assertThat(businessUnit.getAdvancedSelectorText().getValue()).isEqualTo("All");
-    }
+		assertThat(businessUnit.getAdvancedSelectorText().getValue()).isEqualTo("All");
+	}
 }
--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsCheckEuwDetailDeliveryFormat.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/operational_reports/TestOperationalReportsCheckEuwDetailDeliveryFormat.java
@@ -1,36 +1,35 @@
 package aaa.modules.regression.finance.operational_reports;
 
+import static toolkit.verification.CustomAssertions.assertThat;
+import org.testng.annotations.Test;
 import aaa.admin.metadata.reports.OperationalReportsMetaData;
 import aaa.admin.modules.reports.operationalreports.defaulttabs.OperationalReportsTab;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.modules.BaseTest;
-import org.testng.annotations.Test;
 import toolkit.utils.TestInfo;
-
-import static toolkit.verification.CustomAssertions.assertThat;
 
 public class TestOperationalReportsCheckEuwDetailDeliveryFormat extends BaseTest {
 
-    private OperationalReportsTab orTab = new OperationalReportsTab();
+	private OperationalReportsTab orTab = new OperationalReportsTab();
 
-    /**
-     * @author Maksim Piatrouski
-     * Objectives: check following Report formats are presented and enabled: 'Excel', 'PDF', 'CSV'
-     * TC Steps:
-     * 1. Navigate Reports -> Operational Reports;
-     * 2. Fill Category, Type, Name;
-     * 3. Check "Delivery Format" options
-     */
+	/**
+	 * @author Maksim Piatrouski
+	 * Objectives: check following Report formats are presented and enabled: 'Excel', 'PDF', 'CSV'
+	 * TC Steps:
+	 * 1. Navigate Reports -> Operational Reports;
+	 * 2. Fill Category, Type, Name;
+	 * 3. Check "Delivery Format" options
+	 */
 
-    @Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
-    @TestInfo(component = ComponentConstant.Finance.OPERATIONAL_REPORTS, testCaseId = "PAS-15537")
-    public void pas15537_testOperationalReportsCheckEuwDetailDeliveryFormat() {
-        opReportApp().open();
+	@Test(groups = {Groups.FUNCTIONAL, Groups.MEDIUM})
+	@TestInfo(component = ComponentConstant.Finance.OPERATIONAL_REPORTS, testCaseId = "PAS-15537")
+	public void pas15537_testOperationalReportsCheckEuwDetailDeliveryFormat() {
+		opReportApp().open();
 
-        orTab.fillTab(getOperationalReportsTD("DataGather","TestData_EUW_Detail"));
+		orTab.fillTab(getOperationalReportsTD("DataGather", "TestData_EUW_Detail"));
 
-        assertThat(orTab.getAssetList().getAsset(OperationalReportsMetaData.OperationalReportsTab.REPORT_FORMAT))
-                .containsAllOptions(getTestSpecificTD("TestData_Check").getList("ReportFormats"));
-    }
+		assertThat(orTab.getAssetList().getAsset(OperationalReportsMetaData.OperationalReportsTab.REPORT_FORMAT))
+				.containsAllOptions(getTestSpecificTD("TestData_Check").getList("ReportFormats"));
+	}
 }
