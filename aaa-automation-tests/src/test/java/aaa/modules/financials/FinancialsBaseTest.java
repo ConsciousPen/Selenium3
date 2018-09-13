2689afa2b9de1bb26a56a3f10c33e2091d8e5271
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-09-13 16:04:35 -0700

    fixed test data for pup policies

--- aaa-automation-tests/src/test/java/aaa/modules/financials/FinancialsBaseTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/financials/FinancialsBaseTest.java
@@ -2,10 +2,18 @@ package aaa.modules.financials;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.testng.annotations.AfterSuite;
 import aaa.modules.policy.PolicyBaseTest;
 
 public class FinancialsBaseTest extends PolicyBaseTest {
 
 	protected static final List<String> POLICIES = new ArrayList<>();
+
+	@AfterSuite
+	public void testPolicyLogging() {
+		for (String policy : POLICIES) {
+			log.info(policy);
+		}
+	}
 
 }
--- aaa-automation-tests/src/test/java/aaa/modules/financials/template/TestNewBusinessTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/financials/template/TestNewBusinessTemplate.java
@@ -1,7 +1,10 @@
 package aaa.modules.financials.template;
 
+import aaa.main.modules.policy.PolicyType;
+import aaa.main.modules.policy.pup.defaulttabs.PrefillTab;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.financials.FinancialsBaseTest;
+import toolkit.datax.TestData;
 import toolkit.utils.datetime.DateTimeUtils;
 
 public class TestNewBusinessTemplate extends FinancialsBaseTest {
@@ -12,7 +15,11 @@ public class TestNewBusinessTemplate extends FinancialsBaseTest {
 	}
 
 	protected void testNBZ_03() {
-		openAppAndCreatePolicy(getBackDatedPolicyTD(getPolicyType(), DateTimeUtils.getCurrentDateTime().plusWeeks(1).format(DateTimeUtils.MM_DD_YYYY)));
+		TestData td = getBackDatedPolicyTD(getPolicyType(), DateTimeUtils.getCurrentDateTime().plusWeeks(1).format(DateTimeUtils.MM_DD_YYYY));
+		if (getPolicyType().equals(PolicyType.PUP)) {
+			td = new PrefillTab().adjustWithRealPolicies(td, getPrimaryPoliciesForPup());
+		}
+		openAppAndCreatePolicy(td);
 		POLICIES.add(PolicySummaryPage.getPolicyNumber());
 	}
 
