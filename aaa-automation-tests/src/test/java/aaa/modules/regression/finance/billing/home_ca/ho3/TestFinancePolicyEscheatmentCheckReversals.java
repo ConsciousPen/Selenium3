f437d7aee9e029d57d4e61f5e869f5996dd8cac6
Piatrouski, Maksim (C) <gxkepia@tent.trt.csaa.pri>
2018-09-17 02:30:57 -0700

    pas 18992 almost done

--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/billing/home_ca/ho3/TestFinancePolicyEscheatmentCheckReversals.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/billing/home_ca/ho3/TestFinancePolicyEscheatmentCheckReversals.java
@@ -1,10 +1,13 @@
 package aaa.modules.regression.finance.billing.home_ca.ho3;
 
+import static toolkit.verification.CustomAssertions.assertThat;
+import java.time.LocalDateTime;
 import org.testng.annotations.Test;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
+import aaa.common.pages.Page;
 import aaa.common.pages.SearchPage;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
@@ -12,26 +15,40 @@ import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.main.enums.SearchEnum;
 import aaa.main.modules.billing.account.BillingAccount;
-import aaa.main.modules.billing.account.actiontabs.OtherTransactionsActionTab;
 import aaa.main.modules.policy.PolicyType;
+import aaa.main.pages.summary.BillingSummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
+import toolkit.webdriver.controls.composite.table.Cell;
 
 public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 
 	/**
 	 * @author Maksim Piatrouski
-	 * Objectives : check "Escheatment" not displayed in Transaction Subtype drop down section
-	 * on the PAS UI billing page - the Other Transactions button under the payments and other transactions section
+	 * Objectives : check reverse rules of a systematically created escheatment transaction(s).
+	 * Preconditions:
+	 * 1. Create Annual Policy
+	 * 2. Pay $25 more than full with check
+	 * 3. Refund 25$ with check - run *aaaRefundGenerationAsyncJob*
+	 * 4. Run *aaaRefundDisbursementAsyncJob* to make refund status to issued
+	 * 7. Create Renewal
+	 * 8. Turn time for more than a year of Refund
+	 * 9. Run Esheatment async job at the beginning of the month:  *aaaEscheatmentProcessAsyncJob*
+	 * 10. Navigate to BA
 	 * TC Steps:
-	 * 1. Create Policy
-	 * 2. Navigate Billing - Other Transactions
-	 * 3. Set Transaction Type = "Adjustment"
-	 * 4. Check "Escheatment" not displayed in Transaction Subtype drop down
+	 * 1. Check new Escheatment transaction exist
+	 * 2. Click Reverse action
+	 * 3. Click Cancel button - check no new transaction in Payments & Other Transactions table
+	 * 4. Click Reverse action
+	 * 5. Click Ok button - check:
+	 * New transaction created with negative amount
+	 * New BAM message created
+	 * Balance due and Prepaid amounts recalculated
+	 * In original Escheatment transaction status changed from Applied to Reversed
+	 * Reversed action not exist
 	 */
 
-	OtherTransactionsActionTab otherTransactionsActionTab = new OtherTransactionsActionTab();
 	BillingAccount billingAccount = new BillingAccount();
 	TestData tdBilling = testDataManager.billingAccount;
 
@@ -52,8 +69,13 @@ public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 		NavigationPage.toMainTab(NavigationEnum.AppMainTabs.BILLING.get());
 		billingAccount.acceptPayment().perform(tdBilling.getTestData("AcceptPayment", "TestData_Check"), new Dollar(25));
 
+		LocalDateTime paymentDate = TimeSetterUtil.getInstance().getCurrentTime();
+		LocalDateTime refundDate = getTimePoints().getRefundDate(paymentDate);
+		TimeSetterUtil.getInstance().nextPhase(refundDate);
 		JobUtils.executeJob(Jobs.aaaRefundGenerationAsyncJob);
 		JobUtils.executeJob(Jobs.aaaRefundDisbursementAsyncJob);
+		mainApp().open();
+		SearchPage.openBilling(policyNumber);
 
 		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
 		policy.renew().perform();
@@ -64,5 +86,18 @@ public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 
 		mainApp().reopen();
 		SearchPage.openBilling(policyNumber);
+
+		Cell escheatmentActions = BillingSummaryPage.tablePaymentsOtherTransactions
+				.getRowContains("Subtype/Reason", "Escheatment").getCell("Action");
+		assertThat(escheatmentActions.getValue()).contains("Reverse");
+
+		escheatmentActions.controls.links.get("Reverse").click();
+		Page.dialogConfirmation.reject();
+		assertThat(BillingSummaryPage.tablePaymentsOtherTransactions.getRow(1)
+				.getCell("Subtype/Reason")).hasValue("Escheatment");
+
+		escheatmentActions.controls.links.get("Reverse").click();
+		Page.dialogConfirmation.confirm();
+		//check new transaction, etc
 	}
 }
