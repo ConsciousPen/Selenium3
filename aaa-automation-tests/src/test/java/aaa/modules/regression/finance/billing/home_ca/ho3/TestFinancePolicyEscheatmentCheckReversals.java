9e17bdaa96312a56690387b60c49e9a6bb8e3b2d
Piatrouski, Maksim (C) <gxkepia@tent.trt.csaa.pri>
2018-09-19 02:19:00 -0700

    replaced renewal to job, replaced table in assertions, created new suite for timeshift and moved test in it

--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/billing/home_ca/ho3/TestFinancePolicyEscheatmentCheckReversals.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/billing/home_ca/ho3/TestFinancePolicyEscheatmentCheckReversals.java
@@ -3,6 +3,7 @@ package aaa.modules.regression.finance.billing.home_ca.ho3;
 import static toolkit.verification.CustomAssertions.assertThat;
 import static toolkit.verification.CustomSoftAssertions.assertSoftly;
 import java.time.LocalDateTime;
+import org.mortbay.log.Log;
 import org.testng.annotations.Test;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
@@ -12,13 +13,14 @@ import aaa.common.pages.Page;
 import aaa.common.pages.SearchPage;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
+import aaa.helpers.http.HttpStub;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
-import aaa.main.enums.SearchEnum;
 import aaa.main.modules.billing.account.BillingAccount;
 import aaa.main.modules.policy.PolicyType;
 import aaa.main.pages.summary.BillingSummaryPage;
 import aaa.main.pages.summary.NotesAndAlertsSummaryPage;
+import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
 import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
@@ -74,11 +76,16 @@ public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 		TimeSetterUtil.getInstance().nextPhase(refundDate);
 		JobUtils.executeJob(Jobs.aaaRefundGenerationAsyncJob);
 		JobUtils.executeJob(Jobs.aaaRefundDisbursementAsyncJob);
-		mainApp().open();
-		SearchPage.openBilling(policyNumber);
 
-		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
-		policy.renew().perform();
+		mainApp().open();
+		SearchPage.openPolicy(policyNumber);
+		LocalDateTime policyExpirationDate = PolicySummaryPage.getExpirationDate();
+		LocalDateTime renewImageGenDate = getTimePoints().getRenewImageGenerationDate(policyExpirationDate);
+		Log.info("Policy Renewal Image Generation Date" + renewImageGenDate);
+		TimeSetterUtil.getInstance().nextPhase(renewImageGenDate);
+		JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
+		HttpStub.executeAllBatches();
+		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
 
 		TimeSetterUtil.getInstance().nextPhase(TimeSetterUtil.getInstance().getStartTime().plusMonths(13));
 
@@ -99,11 +106,13 @@ public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 		escheatmentActions.controls.links.get("Reverse").click();
 		Page.dialogConfirmation.confirm();
 
-		Dollar totalPaid = new Dollar(BillingSummaryPage.tableBillingGeneralInformation.getRow(1)
+		Dollar totalPaid = new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1)
 				.getCell("Total Paid").getValue());
-		Dollar totalDue = new Dollar(BillingSummaryPage.tableBillingGeneralInformation.getRow(1)
+		Dollar prepaid = new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1)
+				.getCell("Prepaid").getValue());
+		Dollar totalDue = new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1)
 				.getCell("Total Due").getValue());
-		Dollar billableAmount = new Dollar(BillingSummaryPage.tableBillingGeneralInformation.getRow(1)
+		Dollar billableAmount = new Dollar(BillingSummaryPage.tableBillingAccountPolicies.getRow(1)
 				.getCell("Billable Amount").getValue());
 
 		assertSoftly(softly -> {
@@ -114,7 +123,8 @@ public class TestFinancePolicyEscheatmentCheckReversals extends PolicyBaseTest {
 			softly.assertThat(BillingSummaryPage.tablePaymentsOtherTransactions.getRow(2)
 					.getCell("Action").getValue()).doesNotContain("Reverse");
 			softly.assertThat(totalDue.toString()).isEqualTo("($25.00)");
-			softly.assertThat(billableAmount).isEqualTo(totalPaid.add(totalDue));
+			softly.assertThat(prepaid.toString()).isEqualTo("($25.00)");
+			softly.assertThat(totalPaid).isEqualTo(billableAmount.subtract(totalDue));
 
 			NotesAndAlertsSummaryPage.activitiesAndUserNotes.expand();
 			softly.assertThat(NotesAndAlertsSummaryPage.activitiesAndUserNotes.getRow(1)
