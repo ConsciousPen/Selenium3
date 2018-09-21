f4df7fd305304f07ab936ae9047b286c3a485a16
Johns, Chris <christopher.johns@csaa.com>
2018-09-21 10:23:52 -0700

    Johns: Removed Shell E2E Claims Test

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestClaimsAssignment.java
@@ -0,0 +1,54 @@
+package aaa.modules.regression.sales.auto_ss.functional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.modules.policy.AutoSSBaseTest;
+import aaa.modules.regression.service.helper.HelperCommon;
+import aaa.modules.regression.service.helper.dtoClaim.ClaimsAssignmentResponse;
+import toolkit.utils.TestInfo;
+
+public class TestClaimsAssignment extends AutoSSBaseTest {
+	private String DEFAULT_JSON_PATH = "src/test/resources/claimsmatch/";
+	private String claimsUrl = "https://claims-assignment.apps.prod.pdc.digital.csaa-insurance.aaa.com/pas-claims/v1";
+ 	/**
+	 * * @author Chris Johns
+	 * @name Test Claims Matching Micro Service - Test 1 -3 Claims: No match, Exiting match, DL Match
+	 * @scenario
+	 * Test Steps:
+	 * 1. Send JSON Request with 3 claims to the Claims Matching Micro Service
+	 * 2. Verify the following claims match results:
+	 *      --Claim 1, 1TAZ1111OHS: No Match
+	 *      --Claim 2, 7TZ02222OHS: Existing Match
+	 *      --Claim 3, 3TAZ3333OHS: DL Match
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
+	@TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-12465"})
+	public void claimsMatching_test1(@Optional("AZ") String state) throws IOException {
+		//Define which JSON request to use
+		String claimsRequest = new String(Files.readAllBytes(Paths.get(DEFAULT_JSON_PATH + "NoMatch_ExistingMatch_DLMatch.json")));
+
+		//Use 'runJsonRequestPostClaims' to send the JSON request to the Claims Assignment Micro Service
+		ClaimsAssignmentResponse microServiceResponse = HelperCommon.runJsonRequestPostClaims(claimsUrl, claimsRequest);
+
+		//Throw the microServiceResponse to log - assists with debugging
+		log.info(microServiceResponse.toString());
+
+		//Verify the First claim is in the unmatched section
+		assertThat(microServiceResponse.getUnmatchedClaims().get(0).getClaimNumber()).isEqualTo("1TAZ1111OHS");
+
+		//Verify that the Second claim returned is an existing match and the Third claim is a DL match
+		assertThat(microServiceResponse.getMatchedClaims().get(0).getMatchCode()).isEqualTo("EXISTING_MATCH");
+		assertThat(microServiceResponse.getMatchedClaims().get(1).getMatchCode()).isEqualTo("DL");
+	}
+
+}
+
+
