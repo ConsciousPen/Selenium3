562fb20463c5b3a0f44fe66911803e3759178c2a
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2018-09-27 12:52:41 -0700

    Fixed HO4 scenario

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestPPCReportReOrderRuleTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestPPCReportReOrderRuleTemplate.java
@@ -8,7 +8,11 @@ import aaa.main.enums.ErrorEnum;
 import aaa.main.enums.ProductConstants;
 import aaa.main.metadata.policy.HomeCaMetaData;
 import aaa.main.modules.policy.PolicyType;
-import aaa.main.modules.policy.home_ca.defaulttabs.*;
+import aaa.main.modules.policy.home_ca.defaulttabs.ApplicantEndorsementTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.ApplicantTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.BindTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.ErrorTab;
+import aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
 import aaa.modules.regression.sales.home_ss.ho3.functional.TestDisableReorderReport;
@@ -34,40 +38,37 @@ public class TestPPCReportReOrderRuleTemplate extends PolicyBaseTest {
 
 		policy.createEndorsement(endorsementTd1.adjust(getPolicyTD("Endorsement", "TestData")));
 		NavigationPage.toViewTab(NavigationEnum.HomeCaTab.APPLICANT.get());
-
 		applicantEndorsementTabCA.getDwellingAddressAssetList().getAsset(HomeCaMetaData.ApplicantTab.DwellingAddress.COUNTY)
 				.setValue("Los Angeles");
 
-		policy.getDefaultView().fillFromTo(endorsementTd2, ApplicantTab.class, BindTab.class, true);
-		bindTabCA.submitTab();
-
-		String policyNumber = PolicySummaryPage.getPolicyNumber();
-		LocalDateTime renewalTime = PolicySummaryPage.getExpirationDate();
-
-		TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(83));
-		JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
-		JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
-		TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(73));
-		JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
-		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
-		TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(58));
-		JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
-		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
-		TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(48));
-		JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
-		JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
-
-		purchaseRenewal(renewalTime, policyNumber);
-		PolicySummaryPage.buttonRenewals.click();
-
-		policy.endorse().perform(getPolicyTD("Endorsement", "TestData"));
-		NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES.get());
-		NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
-		premiumsAndCoveragesQuoteTabCA.calculatePremium();
-
 		if (policyType.equals(PolicyType.HOME_CA_HO4)) {
+			policy.getDefaultView().fillFromTo(endorsementTd2, ApplicantTab.class, PremiumsAndCoveragesQuoteTab.class, true);
 			errorTabCA.verify.errorsPresent(ErrorEnum.Errors.ERROR_AAA_HO_CA7150360);
 		} else {
+			policy.getDefaultView().fillFromTo(endorsementTd2, ApplicantTab.class, BindTab.class, true);
+			bindTabCA.submitTab();
+			String policyNumber = PolicySummaryPage.getPolicyNumber();
+			LocalDateTime renewalTime = PolicySummaryPage.getExpirationDate();
+
+			TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(83));
+			JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+			JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
+			TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(73));
+			JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+			JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+			TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(58));
+			JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+			JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+			TimeSetterUtil.getInstance().nextPhase(renewalTime.minusDays(48));
+			JobUtils.executeJob(Jobs.aaaBatchMarkerJob);
+			JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+
+			purchaseRenewal(renewalTime, policyNumber);
+			PolicySummaryPage.buttonRenewals.click();
+			policy.endorse().perform(getPolicyTD("Endorsement", "TestData"));
+			NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES.get());
+			NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
+			premiumsAndCoveragesQuoteTabCA.calculatePremium();
 			NavigationPage.toViewTab(NavigationEnum.HomeCaTab.BIND.get());
 			bindTabCA.submitTab();
 			assertThat(PolicySummaryPage.labelPolicyStatus).hasValue(ProductConstants.PolicyStatus.POLICY_ACTIVE);
