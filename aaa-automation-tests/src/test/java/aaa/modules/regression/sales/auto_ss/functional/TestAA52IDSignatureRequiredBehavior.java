562d3600e99260d1109a3dcdeecd8adb477a799e
Kopparapu, Sreekanth <sreekanth.kopparapu@csaa.com>
2018-09-28 11:27:02 -0700

    Updated AA52ID Endorsement and Renewals

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestAA52IDSignatureRequiredBehavior.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestAA52IDSignatureRequiredBehavior.java
@@ -25,6 +25,8 @@ public class TestAA52IDSignatureRequiredBehavior extends AutoSSBaseTest {
 	private ComboBox umCoverage = premiumAndCoveragesTab.getAssetList().getAsset(AutoSSMetaData.PremiumAndCoveragesTab.UNINSURED_MOTORISTS_BODILY_INJURY);
 	private ComboBox uimCoverage = premiumAndCoveragesTab.getAssetList().getAsset(AutoSSMetaData.PremiumAndCoveragesTab.UNDERINSURED_MOTORISTS_BODILY_INJURY);
 	private RadioGroup disclosureStmtRadioBtn = documentsAndBindTab.getRequiredToBindAssetList().getAsset(AutoSSMetaData.DocumentsAndBindTab.RequiredToBind.UNINSURED_UNDERINSURED_DISCLOSURE_STATEMENT_AND_REJECTION_OF_COVERAGE);
+	public static final int ENDORSEMENT = 1;
+	public static final int RENEWAL = 2;
 
 	/**
 	 * @author Josh Carpenter
@@ -51,7 +53,20 @@ public class TestAA52IDSignatureRequiredBehavior extends AutoSSBaseTest {
 		policy.endorse().perform(getPolicyTD("Endorsement", "TestData"));
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 
-		testResetSignatureOption();
+		testResetSignatureAddUM();
+		initiateNavigateToBindTab(ENDORSEMENT);
+
+		testResetSignatureAddUIM();
+		initiateNavigateToBindTab(ENDORSEMENT);
+
+		testResetSignatureRejectUM();
+		initiateNavigateToBindTab(ENDORSEMENT);
+
+		testResetSignatureRejectUIM();
+		initiateNavigateToBindTab(ENDORSEMENT);
+
+		testResetSignatureRejectAddBothCoverages(ENDORSEMENT);
+		initiateNavigateToBindTab(ENDORSEMENT);
 
 	}
 
@@ -83,66 +98,24 @@ public class TestAA52IDSignatureRequiredBehavior extends AutoSSBaseTest {
 		policy.renew().perform();
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 
-		testResetSignatureOption();
+		testResetSignatureAddUM();
+		initiateNavigateToBindTab(RENEWAL);
+
+		testResetSignatureAddUIM();
+		initiateNavigateToBindTab(RENEWAL);
+
+		testResetSignatureRejectUM();
+		initiateNavigateToBindTab(RENEWAL);
+
+		testResetSignatureRejectUIM();
+		initiateNavigateToBindTab(RENEWAL);
+
+		testResetSignatureRejectAddBothCoverages(RENEWAL);
+		initiateNavigateToBindTab(RENEWAL);
 
 	}
 
-	/**
-	 * @author Josh Carpenter
-	 * @name Test that the signature option in the 'Required to Bind' section is reset after adding/removing UM/UIM coverage during Amended Renewals
-	 * @scenario
-	 * 1.  Create Customer
-	 * 2.  Create Auto SS ID Policy
-	 * 3.  Create renewal image and navigate to Documents & Bind Tab
-	 * 4.  Validate the signature option is set to anything except 'Not Signed'
-	 * 5.  Reject UM Coverage and validate signature option
-	 * 6.  Save renewal
-	 * 7.  Create a new renewal image version and navigate P & C tab
-	 * 8.  Reject UIM Coverage and validate signature option
-	 * 9.  Save renewal
-	 * 10. Create a new renewal image version and navigate P & C tab
-	 * 11. Add UM Coverage and validate signature option
-	 * 12. Save renewal
-	 * 13. Create a new renewal image version and navigate P & C tab
-	 * 14. Add UIM Coverage and validate signature option
-	 * @details
-	 */
-	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Renewal.AUTO_SS, testCaseId = "PAS-18892")
-	public void pas18892_testResetSignatureOptionAmendedRenewal(@Optional("ID") String state) {
-
-		openAppAndCreatePolicy();
-		policy.renew().perform();
-		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
-		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
-
-		// Reject UM Coverage, validate signature option, and save
-		setCoverage(umCoverage, Coverage.NONE);
-		validateSignatureStatusOnBindTab();
-		documentsAndBindTab.saveAndExit();
-
-		// Create new renewal image version, reject UIM coverage, validate signature option, and save
-		createRenewalVersion();
-		setCoverage(uimCoverage, Coverage.NONE);
-		validateSignatureStatusOnBindTab();
-		documentsAndBindTab.saveAndExit();
-
-		// Create new renewal image version, add UM coverage, validate signature option, and save
-		createRenewalVersion();
-		setCoverage(umCoverage, Coverage.COV_100_300);
-		validateSignatureStatusOnBindTab();
-		documentsAndBindTab.saveAndExit();
-
-		// Create new renewal image version, add UIM coverage, validate signature option
-		createRenewalVersion();
-		setCoverage(uimCoverage, Coverage.COV_100_300);
-		validateSignatureStatusOnBindTab();
-
-	}
-
-	private void testResetSignatureOption() {
-
+	private void testResetSignatureRejectUM() {
 		// Validate signature status is anything but 'Not Signed'
 		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
 
@@ -150,22 +123,50 @@ public class TestAA52IDSignatureRequiredBehavior extends AutoSSBaseTest {
 		setCoverage(umCoverage, Coverage.NONE);
 		validateSignatureStatusOnBindTab();
 
+	}
+
+	private void testResetSignatureRejectUIM() {
+		// Validate signature status is anything but 'Not Signed'
+		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
+
 		// Reject UIM Coverage and validate signature option
 		setCoverage(uimCoverage, Coverage.NONE);
 		validateSignatureStatusOnBindTab();
 
+	}
+
+	private void testResetSignatureAddUM() {
+		// Validate signature status is anything but 'Not Signed'
+		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
+
 		// Add UM Coverage and validate signature option
 		setCoverage(umCoverage, Coverage.COV_100_300);
 		validateSignatureStatusOnBindTab();
+	}
+
+	private void testResetSignatureAddUIM() {
+
+		// Validate signature status is anything but 'Not Signed'
+		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
 
 		// Add UIM Coverage and validate signature option
 		setCoverage(uimCoverage, Coverage.COV_100_300);
 		validateSignatureStatusOnBindTab();
 
+	}
+
+
+	private void testResetSignatureRejectAddBothCoverages(int txType) {
+
+		// Validate signature status is anything but 'Not Signed'
+		assertThat(disclosureStmtRadioBtn.getValue()).isNotEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
+
 		// Reject both UM & UIM and validate signature option
 		setCoverage(umCoverage, Coverage.NONE);
 		setCoverage(uimCoverage, Coverage.NONE);
 		validateSignatureStatusOnBindTab();
+
+		initiateNavigateToBindTab(txType);
 
 		// Add both UM & UIM and validate signature option
 		setCoverage(umCoverage, Coverage.COV_100_300);
@@ -184,13 +185,22 @@ public class TestAA52IDSignatureRequiredBehavior extends AutoSSBaseTest {
 		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 		assertThat(disclosureStmtRadioBtn.getValue()).isEqualTo(PolicyConstants.SignatureStatus.NOT_SIGNED);
 		disclosureStmtRadioBtn.setValue(PolicyConstants.SignatureStatus.PHYSICALLY_SIGNED);
+		documentsAndBindTab.submitTab();
 	}
 
-	private void createRenewalVersion() {
-		PolicySummaryPage.buttonRenewals.click();
-		policy.dataGather().start();
+	private void initiateNavigateToBindTab(int txType) {
+
+		if (txType == 1) {
+			policy.endorse().perform(getPolicyTD("Endorsement", "TestData"));
+		} else if (txType == 2) {
+			PolicySummaryPage.buttonRenewals.click();
+			policy.dataGather().start();
+		}
+
+		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DOCUMENTS_AND_BIND.get());
 	}
-	
+
+
 	private final class Coverage {
 		static final String NONE = "No Coverage";
 		static final String COV_100_300 = "$100,000/$300,000";
