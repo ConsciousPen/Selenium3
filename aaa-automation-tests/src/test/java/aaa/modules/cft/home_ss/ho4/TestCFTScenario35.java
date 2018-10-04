18257b87e2ee8efcbc404ea1f4fbae139dbff124
Laptsionak, Mikhail (C) <Mikhail.Laptsionak@csaa.com>
2018-10-04 17:43:48 +0300

    added AS400 CFT auto tests

--- aaa-automation-tests/src/test/java/aaa/modules/cft/home_ss/ho4/TestCFTScenario35.java
+++ aaa-automation-tests/src/test/java/aaa/modules/cft/home_ss/ho4/TestCFTScenario35.java
@@ -1,14 +1,14 @@
-package aaa.modules.cft.home_ss.ho3;
+package aaa.modules.cft.home_ss.ho4;
 
-import org.apache.commons.lang3.StringUtils;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
 import aaa.common.enums.Constants.States;
 import aaa.helpers.constants.Groups;
 import aaa.main.modules.policy.PolicyType;
 import aaa.modules.cft.ControlledFinancialBaseTest;
 import aaa.utils.StateList;
+import org.apache.commons.lang3.StringUtils;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 import toolkit.utils.TestInfo;
 
 public class TestCFTScenario35 extends ControlledFinancialBaseTest {
@@ -16,7 +16,7 @@ public class TestCFTScenario35 extends ControlledFinancialBaseTest {
 	@Test(groups = {Groups.CFT, Groups.TIMEPOINT})
 	@TestInfo(component = Groups.CFT)
 	@Parameters({STATE_PARAM})
-	@StateList(states = {States.NJ, States.DE, States.VA, States.MD, States.PA, States.AZ, States.KY, States.OK, States.CT})
+	@StateList(states = {States.AZ, States.KY, States.OK, States.CT})
 	public void cftTestScenario35(@Optional(StringUtils.EMPTY) String state) {
 		manualRenewalEntryOnStartDate();
 		generateRenewalOffer();
@@ -27,6 +27,6 @@ public class TestCFTScenario35 extends ControlledFinancialBaseTest {
 
 	@Override
 	protected PolicyType getPolicyType() {
-		return PolicyType.HOME_SS_HO3;
+		return PolicyType.HOME_SS_HO4;
 	}
 }
