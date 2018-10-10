b27c8c0fa61ddaba888d2511b77211a94babb57b
Kazlauskiene, Reda (C) <reda.kazlauskiene@csaa.com>
2018-10-10 05:39:36 -0700

    Create ER calculation time setter structure

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/ledger/home_ca/ho3/TestFinanceEPCalculationOOSEndorsement.java
@@ -0,0 +1,71 @@
+package aaa.modules.regression.finance.ledger.home_ca.ho3;
+
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.jobs.JobUtils;
+import aaa.helpers.jobs.Jobs;
+import aaa.main.modules.policy.PolicyType;
+import aaa.main.pages.summary.PolicySummaryPage;
+import aaa.modules.regression.finance.template.FinanceOperations;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import toolkit.utils.TestInfo;
+
+import java.time.LocalDateTime;
+
+public class TestFinanceEPCalculationOOSEndorsement extends FinanceOperations {
+    /**
+     * @author
+     * Objectives : check reverse rules of a systematically created escheatment transaction(s).
+     * Preconditions:
+     * 1. Create Annual Policy
+
+     */
+
+    @Override
+    protected PolicyType getPolicyType() {
+        return PolicyType.HOME_CA_HO3;
+    }
+
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Finance.LEDGER, testCaseId = "PAS-20277")
+    public void pas20277_testFinanceEPCalculationOOSEndorsement(@Optional("CA") String state) {
+
+        mainApp().open();
+        createCustomerIndividual();
+        createPolicy();
+        LocalDateTime today = TimeSetterUtil.getInstance().getCurrentTime();
+        LocalDateTime e1date = today.plusDays(62);
+        LocalDateTime e2date = e1date.plusDays(61);
+        LocalDateTime e3date = e2date.plusDays(124);
+
+        LocalDateTime jobEndDate  = PolicySummaryPage.getExpirationDate().plusMonths(1);
+        LocalDateTime jobDate = today.plusMonths(1).withDayOfMonth(1);
+
+        jobDate = runEPJobUntil(jobDate, e1date);
+
+        //endorse();
+
+        jobDate = runEPJobUntil(jobDate, e2date);
+
+        //endorse();
+
+        jobDate = runEPJobUntil(jobDate, e3date);
+
+//        endorse();
+//        rollOn();
+
+        runEPJobUntil(jobDate, jobEndDate);
+    }
+
+    private LocalDateTime runEPJobUntil(LocalDateTime jobDate, LocalDateTime until) {
+        while (until.isAfter(jobDate)) {
+            JobUtils.executeJob(Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+            jobDate = jobDate.plusMonths(1).withDayOfMonth(1);
+        }
+        return jobDate;
+    }
+}
