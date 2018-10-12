78101445f9a5c5037afe55090d0148c66d874eaa
Petrenko, Viktor (C) <vpetrenko@eisgroup.com>
2018-10-12 09:52:51 -0700

    builder

--- aaa-automation-tests/src/test/java/aaa/helpers/rest/RestRequestInfo.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/rest/RestRequestInfo.java
@@ -7,6 +7,65 @@ public class RestRequestInfo<T> {
 	public String url;
 	public String sessionId;
 	public Object bodyRequest;
-	public Class<T> responseType;
+	public Class <T> responseType;
 	public int status = Response.Status.OK.getStatusCode();
+
+
+	public RestRequestInfo<T> build()
+	{
+		RestRequestInfo<T> result = new RestRequestInfo<>();
+		result.setUrl(url);
+		result.setSessionId(sessionId);
+		result.setBodyRequest(bodyRequest);
+		result.setResponseType(responseType);
+		result.setStatus(status);
+		return result;
+	}
+
+	public RestRequestInfo<T> setUrl(String url) {
+		this.url = url;
+		return this;
+	}
+
+	public RestRequestInfo<T> setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+		return this;
+	}
+
+	public RestRequestInfo<T> setStatus(int status) {
+		this.status = status;
+		return this;
+	}
+
+	public RestRequestInfo<T> setResponseType(Class<T> responseType) {
+		this.responseType = responseType;
+		return this;
+	}
+
+	public RestRequestInfo<T> setBodyRequest(Object bodyRequest) {
+		this.bodyRequest = bodyRequest;
+		return this;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public Object getBodyRequest() {
+		return bodyRequest;
+	}
+
+
+	public Class<T> getResponseType() {
+		return responseType;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
 }
