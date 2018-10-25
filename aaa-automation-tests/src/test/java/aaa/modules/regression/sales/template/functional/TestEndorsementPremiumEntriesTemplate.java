f765155147996618367d680a79f904e3e7434e5f
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-10-25 09:08:26 -0700

    refactored method to get endorsements and made fixes to validation method in database

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestEndorsementPremiumEntriesTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestEndorsementPremiumEntriesTemplate.java
@@ -1,6 +1,8 @@
 package aaa.modules.regression.sales.template.functional;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
@@ -34,7 +36,7 @@ public class TestEndorsementPremiumEntriesTemplate extends PolicyBaseTest {
 
     }
 
-    private void validatePremiumEntriesInDB(String policyNumber, Map<String, String> endorsements) {
+    private void validatePremiumEntriesInDB(String policyNumber, Map<String, Dollar> endorsements) {
         String query =  "SELECT pe.premiumamt, c.coveragecd " +
                         "FROM pasadm.policysummary ps " +
                         "JOIN pasadm.coverage c ON c.policydetail_id = ps.policydetail_id " +
@@ -44,29 +46,35 @@ public class TestEndorsementPremiumEntriesTemplate extends PolicyBaseTest {
                         "AND ps.policynumber like '" + policyNumber + "'";
 
         List<Map<String, String>> dbEntries = DBService.get().getRows(query);
+        assertThat(dbEntries.size()).isEqualTo(endorsements.size());
 
         for (Map<String, String> row : dbEntries) {
-            for (String form : endorsements.keySet()) {
-                if (form.equals(row.get("COVERAGECD").substring(0, 5))) {
-                    assertThat(endorsements.get(form)).isEqualTo(row.get("PREMIUMAMT"));
-                }
-            }
-            endorsements.get(row.get("COVERAGECD").substring(0, 5));
+            assertThat(endorsements.get(row.get("COVERAGECD").substring(0, 5))).isEqualTo(new Dollar(row.get("PREMIUMAMT")));
         }
 
     }
 
-    private Map<String, String> getIncludedEndorsementsWithPremiumAmount() {
-        Map<String, String> endorsements = new LinkedHashMap<>();
+    private Map<String, Dollar> getIncludedEndorsementsWithPremiumAmount() {
+        // Get all included endorsement forms from Endorsements Tab
         policy.policyInquiry().start();
         NavigationPage.toViewTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES.get());
-        for (int i = 1; i <= endorsementTab.tblIncludedEndorsements.getRowsCount(); i++) {
-            String formId = new EndorsementTab().tblIncludedEndorsements.getColumn("Form ID").getCell(i).getValue();
-            NavigationPage.toViewSubTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
-            Dollar premAmt = new Dollar(PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowContains(PolicyConstants.PolicyEndorsementFormsTable.DESCRIPTION, formId)
-                    .getCell(PolicyConstants.PolicyEndorsementFormsTable.TERM_PREMIUM).getValue());
-            endorsements.put(formId, premAmt.toPlaingString());
-            NavigationPage.toViewSubTab(NavigationEnum.HomeCaTab.ENDORSEMENT.get());
+        List<String>  includedForms = IntStream.rangeClosed(1, endorsementTab.tblIncludedEndorsements.getRowsCount())
+                .mapToObj(i -> endorsementTab.tblIncludedEndorsements.getColumn(PolicyConstants.PolicyIncludedAndSelectedEndorsementsTable.FORM_ID).getCell(i).getValue()).collect(Collectors.toList());
+
+        // Get all premiums for included endorsements and merge into hashmap
+        Map<String, Dollar> endorsements = new LinkedHashMap<>();
+        NavigationPage.toViewSubTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
+        assertThat(PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowsCount()).isEqualTo(endorsements.size());
+        for (String form : includedForms) {
+            String sPremium = PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowContains(PolicyConstants.PolicyEndorsementFormsTable.DESCRIPTION, form)
+                    .getCell(PolicyConstants.PolicyEndorsementFormsTable.TERM_PREMIUM).getValue();
+            Dollar premium;
+            if (sPremium.equals(PolicyConstants.PolicyEndorsementFormsTable.INCLUDED)) {
+                premium = new Dollar(0);
+            } else {
+                premium = new Dollar(sPremium);
+            }
+            endorsements.put(form, premium);
         }
         return endorsements;
     }
