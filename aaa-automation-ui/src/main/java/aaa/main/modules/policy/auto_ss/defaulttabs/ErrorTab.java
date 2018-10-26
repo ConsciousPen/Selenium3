f62ffd80ee3ed18e62b9f5fe66e2e355208d51a0
Saltsevich, Tatsiana (C) <Tatsiana.Saltsevich@csaa.com>
2018-10-26 10:34:08 +0300

    D-T-AU-SS-CL-956-CNV new test added

--- aaa-automation-ui/src/main/java/aaa/main/modules/policy/auto_ss/defaulttabs/ErrorTab.java
+++ aaa-automation-ui/src/main/java/aaa/main/modules/policy/auto_ss/defaulttabs/ErrorTab.java
@@ -4,11 +4,11 @@
  */
 package aaa.main.modules.policy.auto_ss.defaulttabs;
 
+import org.openqa.selenium.By;
 import aaa.common.Tab;
 import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.main.modules.policy.abstract_tabs.CommonErrorTab;
 import aaa.toolkit.webdriver.customcontrols.FillableErrorTable;
-import org.openqa.selenium.By;
 import toolkit.webdriver.controls.composite.table.Table;
 
 /**
@@ -21,6 +21,7 @@ public class ErrorTab extends CommonErrorTab {
 
 	public Table tableErrors = new Table(By.xpath(".//form[@id='errorsForm']//table"));
 	public Table tableBaseErrors = new Table(By.xpath(".//form[@id='errorsForm']//table[2]"));
+	public Table tableTabFormErrors = new Table(By.xpath(".//div[@id='contents']//table//table"));
 
 	public ErrorTab() {
 		super(AutoSSMetaData.ErrorTab.class);
