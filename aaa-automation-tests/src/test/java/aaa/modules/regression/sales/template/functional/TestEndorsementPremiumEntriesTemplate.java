0b5b835bdaf5c1119251879d6e198d72bc363065
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-10-29 09:07:15 -0700

    worked on copy from policy/quote transactions

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestEndorsementPremiumEntriesTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestEndorsementPremiumEntriesTemplate.java
@@ -6,6 +6,7 @@ import java.util.stream.IntStream;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
+import aaa.common.pages.SearchPage;
 import aaa.main.enums.PolicyConstants;
 import aaa.main.modules.policy.home_ca.defaulttabs.EndorsementTab;
 import aaa.main.modules.policy.home_ca.defaulttabs.PremiumsAndCoveragesQuoteTab;
@@ -15,23 +16,47 @@ import static toolkit.verification.CustomAssertions.assertThat;
 
 public class TestEndorsementPremiumEntriesTemplate extends PolicyBaseTest {
 
-    EndorsementTab endorsementTab = new EndorsementTab();
+    private EndorsementTab endorsementTab = new EndorsementTab();
+    private PremiumsAndCoveragesQuoteTab premiumsAndCoveragesQuoteTab = new PremiumsAndCoveragesQuoteTab();
 
     protected void testEndorsementPremiumEntriesCopyFromPolicy() {
+
+        // Create policy and validate DB entries
         String policyNumber = openAppAndCreatePolicy();
         validatePremiumEntriesInDB(policyNumber, getIncludedEndorsementsWithPremiumAmount());
 
-        NavigationPage.toMainTab(NavigationEnum.AppMainTabs.POLICY.get());
+        // Copy policy and validate again
+        SearchPage.openPolicy(policyNumber);
         policy.policyCopy().perform(getStateTestData(testDataManager.policy.get(getPolicyType()), "CopyFromPolicy", "TestData"));
+        policy.getDefaultView().fillUpTo(getPolicyTD(), PremiumsAndCoveragesQuoteTab.class, true);
+        premiumsAndCoveragesQuoteTab.calculatePremium();
+        validatePremiumEntriesInDB(premiumsAndCoveragesQuoteTab.getPolicyNumber(), getIncludedEndorsementsWithPremiumAmount());
 
+    }
+
+    protected void testEndorsementPremiumEntriesCopyFromQuote() {
+
+        // Create quote and validate DB entries
+        createQuoteAndFillUpTo(PremiumsAndCoveragesQuoteTab.class);
+        String quoteNumber = premiumsAndCoveragesQuoteTab.getPolicyNumber();
+        validatePremiumEntriesInDB(quoteNumber, getIncludedEndorsementsWithPremiumAmount());
+
+        // Copy quote and validate again
+        SearchPage.openQuote(quoteNumber);
+        policy.copyQuote().perform(getStateTestData(testDataManager.policy.get(getPolicyType()), "CopyFromQuote", "TestData"));
+        premiumsAndCoveragesQuoteTab.calculatePremium();
+        validatePremiumEntriesInDB(premiumsAndCoveragesQuoteTab.getPolicyNumber(), getIncludedEndorsementsWithPremiumAmount());
 
     }
 
     protected void testEndorsementPremiumEntriesRewrite() {
 
+
+
     }
 
     protected void testEndorsementPremiumEntriesRenewal() {
+
 
 
     }
@@ -49,7 +74,8 @@ public class TestEndorsementPremiumEntriesTemplate extends PolicyBaseTest {
         assertThat(dbEntries.size()).isEqualTo(endorsements.size());
 
         for (Map<String, String> row : dbEntries) {
-            assertThat(endorsements.get(row.get("COVERAGECD").substring(0, 5))).isEqualTo(new Dollar(row.get("PREMIUMAMT")));
+            String coverageCdValue = row.get("COVERAGECD");
+            assertThat(endorsements.get(coverageCdValue.substring(coverageCdValue.indexOf("#")))).isEqualTo(new Dollar(row.get("PREMIUMAMT")));
         }
 
     }
@@ -64,7 +90,7 @@ public class TestEndorsementPremiumEntriesTemplate extends PolicyBaseTest {
         // Get all premiums for included endorsements and merge into hashmap
         Map<String, Dollar> endorsements = new LinkedHashMap<>();
         NavigationPage.toViewSubTab(NavigationEnum.HomeCaTab.PREMIUMS_AND_COVERAGES_QUOTE.get());
-        assertThat(PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowsCount()).isEqualTo(endorsements.size());
+        assertThat(PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowsCount()).isEqualTo(includedForms.size());
         for (String form : includedForms) {
             String sPremium = PremiumsAndCoveragesQuoteTab.tableEndorsementForms.getRowContains(PolicyConstants.PolicyEndorsementFormsTable.DESCRIPTION, form)
                     .getCell(PolicyConstants.PolicyEndorsementFormsTable.TERM_PREMIUM).getValue();
