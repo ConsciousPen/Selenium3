977d341f9ff491b9c00fd778c0b9c2fef336f0b3
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-10-29 13:22:47 -0700

    created testing for all products and added to test suite

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestEndorsementPremiumEntries.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestEndorsementPremiumEntries.java
@@ -1,13 +1,96 @@
 package aaa.modules.regression.sales.home_ca.ho6.functional;
 
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
 import aaa.main.modules.policy.PolicyType;
 import aaa.modules.regression.sales.template.functional.TestEndorsementPremiumEntriesTemplate;
+import toolkit.utils.TestInfo;
 
 public class TestEndorsementPremiumEntries extends TestEndorsementPremiumEntriesTemplate {
 
     @Override
     protected PolicyType getPolicyType() {
         return PolicyType.HOME_CA_HO6;
+    }
+
+    /**
+     * @author Josh Carpenter
+     * @name Test no duplicate endorsement premium entries exist for CA HO6 copied policies
+     * @scenario
+     * 1. Create policy
+     * 2. Validate Included Endorsements matches what is on P & C tab under Endorsements section (name and amount)
+     * 3. Validate DB entries match the included endorsements by name and amount and contain no duplicates
+     * 3. Initiate copy from policy transaction
+     * 4. Navigate to P & C tab and calculate premium
+     * 5. Validate Endorsements entries in DB do not contain duplicate values and the premium amounts are correct
+     * @details
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO6, testCaseId = {"PAS-19204"})
+    public void pas19204_testEndorsementPremiumEntriesCopyFromPolicy(@Optional("CA") String state) {
+        testEndorsementPremiumEntriesCopyFromPolicy();
+    }
+
+    /**
+     * @author Josh Carpenter
+     * @name Test no duplicate endorsement premium entries exist for CA HO6 copied quotes
+     * @scenario
+     * 1. Create quote, fill up to P & C tab
+     * 2. Calculate premium, save and exit
+     * 3. Validate Included Endorsements matches what is on P & C tab under Endorsements section (name and amount)
+     * 4. Validate DB entries match the included endorsements by name and amount and contain no duplicates
+     * 5. Initiate copy from quote transaction
+     * 6. Navigate to P & C tab and calculate premium
+     * 7. Validate Endorsements entries in DB do not contain duplicate values and the premium amounts are correct
+     * @details
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO6, testCaseId = {"PAS-19204"})
+    public void pas19204_testEndorsementPremiumEntriesCopyFromQuote(@Optional("CA") String state) {
+        testEndorsementPremiumEntriesCopyFromQuote();
+    }
+
+    /**
+     * @author Josh Carpenter
+     * @name Test no duplicate endorsement premium entries exist for CA HO6 renewals
+     * @scenario
+     * 1. Create policy
+     * 2. Validate Included Endorsements matches what is on P & C tab under Endorsements section (name and amount)
+     * 3. Validate DB entries match the included endorsements by name and amount and contain no duplicates
+     * 4. Create renewal image
+     * 5. Navigate to P & C tab and calculate premium
+     * 6. Validate Endorsements entries in DB do not contain duplicate values and the premium amounts are correct
+     * @details
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO6, testCaseId = {"PAS-19204"})
+    public void pas19204_testEndorsementPremiumEntriesRenewal(@Optional("CA") String state) {
+        testEndorsementPremiumEntriesRenewal();
+    }
+
+    /**
+     * @author Josh Carpenter
+     * @name Test no duplicate endorsement premium entries exist for CA HO6 rewrites
+     * @scenario
+     * 1. Create policy
+     * 2. Validate Included Endorsements matches what is on P & C tab under Endorsements section (name and amount)
+     * 3. Cancel policy
+     * 4. Rewrite policy
+     * 5. Navigate to P & C tab and calculate premium
+     * 6. Validate Endorsements entries in DB do not contain duplicate values and the premium amounts are correct
+     * @details
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO6, testCaseId = {"PAS-19204"})
+    public void pas19204_testEndorsementPremiumEntriesRewrite(@Optional("CA") String state) {
+        testEndorsementPremiumEntriesRewrite();
     }
 
 }
