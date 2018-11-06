8544d151e87032316d0decd38d8ea0c8c16892aa
Johns, Chris <christopher.johns@csaa.com>
2018-11-06 16:02:49 -0700

    --

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestOffLineClaims.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestOffLineClaims.java
@@ -117,7 +117,6 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
         });
     }
 
-	///////////////////////
     ///////////////////////
     @Parameters({"state"})
     @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
