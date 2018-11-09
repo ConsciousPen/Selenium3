1e4e14d8502f1411f1e4c2146b11b86f224ae3d1
Petrenko, Viktor (C) <vpetrenko@eisgroup.com>
2018-11-09 11:20:23 -0700

    mainApp().open(); moved to data provider

--- aaa-automation-tests/src/test/java/aaa/modules/bct/BackwardCompatibilityBaseTest.java
+++ aaa-automation-tests/src/test/java/aaa/modules/bct/BackwardCompatibilityBaseTest.java
@@ -127,7 +127,6 @@ public class BackwardCompatibilityBaseTest extends PolicyBaseTest {
 	}
 
 	protected Dollar getPreEndorsementPremium(IPolicy ipolicy, String policyNumber) {
-		mainApp().open();
 		SearchPage.openPolicy(policyNumber);
 		deletePendingTransaction(ipolicy);
 		Dollar policyPremium = PolicySummaryPage.TransactionHistory.getEndingPremium();
