422f176a84a84e58398c8fc89c22517771ca3417
Kazlauskiene, Reda (C) <reda.kazlauskiene@csaa.com>
2018-11-10 08:11:30 -0700

    PAS-21444: New tests added: TestFinanceEPCalculationRenewal

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/ledger/auto_ss/TestFinanceEPCalculationRenewal.java
@@ -0,0 +1,98 @@
+package aaa.modules.regression.finance.ledger.auto_ss;
+
+import aaa.common.enums.Constants;
+import aaa.common.pages.SearchPage;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.jobs.Jobs;
+import aaa.helpers.product.LedgerHelper;
+import aaa.main.enums.SearchEnum;
+import aaa.main.modules.policy.PolicyType;
+import aaa.main.pages.summary.PolicySummaryPage;
+import aaa.modules.regression.finance.template.FinanceOperations;
+import aaa.utils.StateList;
+import com.exigen.ipb.etcsa.utils.Dollar;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import toolkit.utils.TestInfo;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestFinanceEPCalculationRenewal extends FinanceOperations {
+
+	@Override
+	protected PolicyType getPolicyType() {
+		return PolicyType.AUTO_SS;
+	}
+
+	/**
+	 * @author Reda Kazlauskiene
+	 * Objectives : Renewal
+	 * Preconditions:
+	 * Every month earnedPremiumPostingAsyncTaskGenerationJob job is running
+	 * 1. Create Annual Auto SS Policy with Effective date today
+	 * 2. Create Renewal
+	 * 3. Verify Calculations
+	 */
+
+	@Parameters({"state"})
+	@StateList(states = {Constants.States.NV})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+	@TestInfo(component = ComponentConstant.Finance.LEDGER, testCaseId = "PAS-21444")
+	public void pas21444_testFinanceEPCalculationFlatCancellation(@Optional("NV") String state) {
+
+		mainApp().open();
+		createCustomerIndividual();
+		String policyNumber = createPolicy();
+		LocalDateTime today = TimeSetterUtil.getInstance().getCurrentTime();
+        LocalDateTime renewalJobEndDate = PolicySummaryPage.getExpirationDate().plusYears(1).plusMonths(1);
+		LocalDateTime jobDate = today.plusMonths(1).withDayOfMonth(1);
+
+		LocalDateTime expirationDate = PolicySummaryPage.getExpirationDate();
+		LocalDateTime renewalImageGenerationDate = getTimePoints().getRenewImageGenerationDate(expirationDate);
+        LocalDateTime renewalPreviewGenerationDate = getTimePoints().getRenewPreviewGenerationDate(expirationDate);
+        LocalDateTime renewalOfferGenerationDate = getTimePoints().getRenewOfferGenerationDate(expirationDate);
+        LocalDateTime billOfferGenerationDate = getTimePoints().getBillGenerationDate(expirationDate);
+        LocalDateTime billDueDate = getTimePoints().getBillDueDate(expirationDate);
+        LocalDateTime updateStatusDate = getTimePoints().getUpdatePolicyStatusDate(expirationDate);
+
+		jobDate = runEPJobUntil(jobDate, renewalImageGenerationDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+
+        mainApp().open();
+        SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
+		renewalImageGeneration(expirationDate, policyNumber);
+
+        jobDate = runEPJobUntil(jobDate, renewalPreviewGenerationDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+		renewalPreviewGeneration(expirationDate, policyNumber);
+
+        jobDate = runEPJobUntil(jobDate, renewalOfferGenerationDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+        renewalOfferGeneration(expirationDate, policyNumber);
+
+        jobDate = runEPJobUntil(jobDate, billOfferGenerationDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+		generateRenewalBill(expirationDate, today, policyNumber);
+
+        jobDate = runEPJobUntil(jobDate, billDueDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+        payRenewalBill(expirationDate, policyNumber);
+
+        jobDate = runEPJobUntil(jobDate, updateStatusDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+        updatePolicyStatus(expirationDate, today, policyNumber);
+
+        runEPJobUntil(jobDate, renewalJobEndDate, Jobs.earnedPremiumPostingAsyncTaskGenerationJob);
+
+		mainApp().open();
+		SearchPage.openPolicy(policyNumber, "Policy Active");
+		PolicySummaryPage.buttonTransactionHistory.click();
+
+		assertThat(LedgerHelper.getEndingActualPremium(policyNumber))
+				.isEqualTo(new Dollar(LedgerHelper.getEarnedMonthlyReportedPremiumTotal(policyNumber)));
+
+		List<TxType> txTypes = Arrays.asList(TxType.ISSUE, TxType.CANCEL);
+		validateEPCalculations(policyNumber, txTypes, today, expirationDate);
+	}
+}
--- aaa-automation-tests/src/test/java/aaa/modules/regression/finance/template/FinanceOperations.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/finance/template/FinanceOperations.java
@@ -15,7 +15,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import aaa.common.enums.Constants;
+import aaa.common.pages.SearchPage;
+import aaa.helpers.billing.BillingAccountPoliciesVerifier;
+import aaa.helpers.billing.BillingHelper;
+import aaa.helpers.billing.BillingPaymentsAndTransactionsVerifier;
+import aaa.helpers.http.HttpStub;
 import aaa.helpers.product.LedgerHelper;
+import aaa.helpers.product.PolicyHelper;
+import aaa.helpers.product.ProductRenewalsVerifier;
+import aaa.main.enums.BillingConstants;
+import aaa.main.enums.ProductConstants;
+import aaa.main.pages.summary.BillingSummaryPage;
+import aaa.main.pages.summary.PolicySummaryPage;
 import com.exigen.ipb.etcsa.utils.Dollar;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import aaa.common.enums.NavigationEnum;
@@ -25,8 +37,11 @@ import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.main.modules.billing.account.BillingAccount;
 import aaa.modules.policy.PolicyBaseTest;
+import net.sf.cglib.core.Local;
 import toolkit.datax.TestData;
 import toolkit.utils.datetime.DateTimeUtils;
+import toolkit.verification.CustomAssertions;
+import toolkit.verification.ETCSCoreSoftAssertions;
 
 public abstract class FinanceOperations extends PolicyBaseTest {
 
@@ -135,6 +150,84 @@ public abstract class FinanceOperations extends PolicyBaseTest {
 	protected void reinstatePolicy(int daysToEffective) {
 		reinstatePolicy(TimeSetterUtil.getInstance().getCurrentTime().plusDays(daysToEffective));
 	}
+
+    /**
+     * @author Reda Kazlauskiene
+     * @name Renew policy
+     */
+    protected void renewalImageGeneration(LocalDateTime policyExpirationDate, String policyNum) {
+        LocalDateTime renewDateImage = getTimePoints().getRenewImageGenerationDate(policyExpirationDate);
+        TimeSetterUtil.getInstance().nextPhase(renewDateImage);
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart1);
+        HttpStub.executeAllBatches();
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+
+        mainApp().open();
+        SearchPage.openPolicy(policyNum);
+        PolicyHelper.verifyAutomatedRenewalGenerated(renewDateImage);
+    }
+
+    protected void renewalPreviewGeneration(LocalDateTime policyExpirationDate, String policyNum) {
+        TimeSetterUtil.getInstance().nextPhase(getTimePoints().getRenewPreviewGenerationDate(policyExpirationDate));
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+
+        mainApp().open();
+        SearchPage.openPolicy(policyNum);
+        CustomAssertions.assertThat(PolicySummaryPage.buttonRenewals).isEnabled();
+
+        PolicySummaryPage.buttonRenewals.click();
+        new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
+    }
+
+    protected void renewalOfferGeneration(LocalDateTime policyExpirationDate, String policyNum) {
+        LocalDateTime renewDateOffer = getTimePoints().getRenewOfferGenerationDate(policyExpirationDate);
+        TimeSetterUtil.getInstance().nextPhase(renewDateOffer);
+        JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
+
+        mainApp().open();
+        SearchPage.openPolicy(policyNum);
+        //PolicySummaryPage.buttonRenewals.verify.enabled();
+        CustomAssertions.assertThat(PolicySummaryPage.buttonRenewals).isEnabled();
+        PolicySummaryPage.buttonRenewals.click();
+        new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PROPOSED).verify(1);
+    }
+
+    protected void generateRenewalBill(LocalDateTime policyExpirationDate, LocalDateTime policyEffectiveDate, String policyNum) {
+        LocalDateTime billGenDate = getTimePoints().getBillGenerationDate(policyExpirationDate);
+        TimeSetterUtil.getInstance().nextPhase(billGenDate);
+        JobUtils.executeJob(Jobs.aaaRenewalNoticeBillAsyncJob);
+        mainApp().open();
+        SearchPage.openBilling(policyNum);
+        BillingSummaryPage.showPriorTerms();
+        new BillingAccountPoliciesVerifier().setPolicyStatus(ProductConstants.PolicyStatus.POLICY_ACTIVE).verifyRowWithEffectiveDate(policyEffectiveDate);
+        new BillingAccountPoliciesVerifier().setPolicyStatus(ProductConstants.PolicyStatus.PROPOSED).verifyRowWithEffectiveDate(policyExpirationDate);
+    }
+
+    protected void payRenewalBill(LocalDateTime policyExpirationDate, String policyNum) {
+        LocalDateTime billDueDate = getTimePoints().getBillDueDate(policyExpirationDate);
+        if (getState().equals(Constants.States.CA)) {
+            billDueDate = policyExpirationDate; //avoid switch to Monday, Renewal bill should be payed before policyStatusUpdateJob
+        }
+        TimeSetterUtil.getInstance().nextPhase(billDueDate);
+
+        mainApp().open();
+        SearchPage.openBilling(policyNum);
+        Dollar minDue = new Dollar(BillingHelper.getBillCellValue(policyExpirationDate, BillingConstants.BillingBillsAndStatmentsTable.MINIMUM_DUE));
+        billingAccount.acceptPayment().perform(tdBilling.getTestData("AcceptPayment", "TestData_Cash"), minDue);
+        new BillingPaymentsAndTransactionsVerifier().verifyManualPaymentAccepted(DateTimeUtils.getCurrentDateTime(), minDue.negate());
+    }
+
+    protected void updatePolicyStatus(LocalDateTime policyExpirationDate, LocalDateTime policyEffectiveDate, String policyNum) {
+        LocalDateTime updateStatusDate = getTimePoints().getUpdatePolicyStatusDate(policyExpirationDate);
+        TimeSetterUtil.getInstance().nextPhase(updateStatusDate);
+        JobUtils.executeJob(Jobs.policyStatusUpdateJob);
+
+        mainApp().open();
+        SearchPage.openBilling(policyNum);
+        BillingSummaryPage.showPriorTerms();
+        new BillingAccountPoliciesVerifier().setPolicyStatus(ProductConstants.PolicyStatus.POLICY_EXPIRED).verifyRowWithEffectiveDate(policyEffectiveDate);
+        new BillingAccountPoliciesVerifier().setPolicyStatus(ProductConstants.PolicyStatus.POLICY_ACTIVE).verifyRowWithEffectiveDate(policyExpirationDate);
+    }
 
     public static void main(String[] args) {
         FinanceOperations financeOperations = new FinanceOperations() {};
