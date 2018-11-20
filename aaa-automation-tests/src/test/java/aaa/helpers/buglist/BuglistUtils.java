6d90e6681c18be4313c374a4a74c3cec927d0f25
Petrenko, Viktor (C) <vpetrenko@eisgroup.com>
2018-11-19 22:59:50 -0700

    guava conflict, https://bitbucket.org/atlassian/jira-rest-java-client/pull-requests/67/jrjc-233-fixed-deprecated-method/diff

--- aaa-automation-tests/src/test/java/aaa/helpers/buglist/BuglistUtils.java
+++ aaa-automation-tests/src/test/java/aaa/helpers/buglist/BuglistUtils.java
@@ -10,6 +10,7 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.atlassian.jira.rest.client.api.RestClientException;
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.fasterxml.jackson.dataformat.yaml.snakeyaml.Yaml;
 import com.google.common.base.Joiner;
@@ -18,8 +19,15 @@ import toolkit.utils.buglist.YAMLBugListReader;
 
 public class BuglistUtils {
 	private static final String PATH_BUGLIST = "src/test/resources/buglist.yaml";
-	protected static Logger log = LoggerFactory.getLogger(BuglistUtils.class);
+	private static Logger log = LoggerFactory.getLogger(BuglistUtils.class);
 
+	/**
+	 * broken until https://bitbucket.org/atlassian/jira-rest-java-client/pull-requests/67/jrjc-233-fixed-deprecated-method/diff
+	 * not merged
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
 	public static void removeClosed() throws ExecutionException, InterruptedException, IOException {
 		File bugListFile = new File(FilenameUtils.separatorsToSystem(PATH_BUGLIST));
 		List<Bug> bugsFromFile = new YAMLBugListReader().loadBugs(bugListFile);
@@ -31,7 +39,17 @@ public class BuglistUtils {
 		String jqlQuery = "key in (" + Joiner.on(",").join(bugListIds) + ")"
 				+ " and status in (Resolved, Closed, Delivered, Passed, Done, Ready)";
 
-		Iterable<Issue> foundIssues = JiraUtils.getClient().getSearchClient().searchJql(jqlQuery).get().getIssues();
+		Iterable<Issue> foundIssues = null;
+		try{
+			foundIssues = JiraUtils.getClient().getSearchClient().searchJql(jqlQuery).get().getIssues();
+		}catch (Exception e){
+			if(e.getCause() instanceof RestClientException){
+				RestClientException restClientException = (RestClientException) e.getCause();
+				throw new RestClientException("RestClientException: autorization settings, HTTP Status Code " + restClientException.getStatusCode().get(),e.getCause());
+			}else {
+				e.printStackTrace();
+			}
+		}
 		// delete and print(Closed, Delivered, Passed, Done, Ready) ids from bugListIds
 		for (Issue defect : foundIssues) {
 			String defectStatus = defect.getStatus().getName();
@@ -57,6 +75,8 @@ public class BuglistUtils {
 
 	static List<Map<String, Map<String, Object>>> getBuglistModel(List<Bug> buglist) {
 		List<Map<String, Map<String, Object>>> buglistContent = new ArrayList<>();
+
+		buglistContent.add(buglistItemExample());
 
 		for (Bug bug : buglist) {
 			Map<String, Map<String, Object>> buglistItem = new LinkedHashMap<>();
@@ -84,5 +104,29 @@ public class BuglistUtils {
 		}
 
 		return buglistContent;
+	}
+
+	private static Map<String, Map<String, Object>> buglistItemExample() {
+		Map<String, Map<String, Object>> buglistItem = new LinkedHashMap<>();
+
+		Map<String, Object> defectBody = new HashMap<>();
+
+		defectBody.put("summary", "[TO_BE_CREATED] Maximum allowable length of 'Underwriting Company' criteria warning is incorrect on Search page");
+		defectBody.put("reason", "PROD");
+
+		List<Map<String, String[]>> failuresList = new ArrayList<>();
+
+		Map<String, String[]> failures = new LinkedHashMap<>();
+		String[] tests = new String[] {"[aaa.modules.regression.common.TestSearchPageFillingAndWarnings.testSearchWarnings]"};
+		String[] messages = new String[] {"[\"Unexpeted warning message(s) is(are) present on search page: [Maximum criteria length for 'Underwriting Company #' is 255]\"]"};
+		failures.put("tests", tests);
+		failures.put("messages", messages);
+
+		failuresList.add(failures);
+		defectBody.put("failures", failuresList);
+
+		buglistItem.put("EISDEV-XXXXX1", defectBody);
+
+		return 	buglistItem;
 	}
 }
