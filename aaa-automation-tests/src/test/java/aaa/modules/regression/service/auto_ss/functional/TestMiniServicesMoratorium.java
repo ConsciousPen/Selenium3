4305c446f83f8f4858723743c588c37a297c5f42
Pukenaite, Jovita (C) <jovita.pukenaite@csaa.com>
2018-11-27 10:40:47 -0700

    part4

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesMoratorium.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesMoratorium.java
@@ -8,7 +8,11 @@ import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.helpers.rest.dtoDxp.*;
 import aaa.main.enums.ErrorDxpEnum;
+import aaa.main.metadata.CustomerMetaData;
+import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.main.modules.policy.PolicyType;
+import aaa.main.modules.policy.auto_ss.defaulttabs.ErrorTab;
+import aaa.main.modules.policy.auto_ss.defaulttabs.GeneralTab;
 import aaa.modules.regression.sales.template.functional.PolicyMoratorium;
 import aaa.modules.regression.service.helper.HelperCommon;
 import aaa.modules.regression.service.helper.HelperMiniServices;
@@ -18,13 +22,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import toolkit.datax.DataProviderFactory;
 import toolkit.datax.TestData;
 import toolkit.db.DBService;
 import toolkit.utils.TestInfo;
+import toolkit.utils.datetime.DateTimeUtils;
 
 import javax.ws.rs.core.Response;
 import java.time.format.DateTimeFormatter;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static toolkit.verification.CustomSoftAssertions.assertSoftly;
 
 public class TestMiniServicesMoratorium extends PolicyMoratorium {
@@ -48,16 +55,11 @@ public class TestMiniServicesMoratorium extends PolicyMoratorium {
 
     String endorsementDate = TimeSetterUtil.getInstance().getCurrentTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
     String endorsementDateBack = TimeSetterUtil.getInstance().getCurrentTime().minusDays(2).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-
-    TestData td_CA = getTestSpecificTD("TestData_Moratorium_DXP_Config_1");
-    MoratoriumRule moratoriumRule = getMoratoriumRule(td_CA);
-    TestData td_VA = getTestSpecificTD("TestData_Moratorium_DXP_Config_2");
-    MoratoriumRule moratoriumRule2 = getMoratoriumRule(td_VA);
-
+    String endorsementDateFuture = TimeSetterUtil.getInstance().getCurrentTime().plusDays(10).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
     VehicleUpdateDto updateVehReqUnderTheMoratorium_VA = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "Valhalla Square", "Ashburn", "20147", "VA");
     VehicleUpdateDto updateVehReqNotUnderTheMoratorium_VA = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "501 E Broad St", "Richmond", "23219", "VA");
-    VehicleUpdateDto updateVehReqNotUnderTheMoratorium_AZ = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "805 N 4rd Ave", "Phoenix", "85003", "AZ");
+    VehicleUpdateDto updateVehReqNotUnderTheMoratorium_AZ = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "24250 N 23rd Ave", "Phoenix", "85085", "AZ");
     VehicleUpdateDto updateVehReqUnderTheMoratorium_CA = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "992 Silverado Rd", "Hayward", "94541", "CA");
     VehicleUpdateDto updateVehReqNotUnderTheMoratorium_CA = DXPRequestFactory.createUpdateVehicleRequest("Pleasure", true, "2999 W 6th St", "Los Angeles", "90020", "CA");
 
@@ -89,6 +91,11 @@ public class TestMiniServicesMoratorium extends PolicyMoratorium {
     @TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-21466"})
     @StateList(states = {Constants.States.VA, Constants.States.AZ})
     public void pas21466_GarageAddressUnderTheMoratorium(@Optional("VA") String state) {
+        TestData td_CA = getTestSpecificTD("TestData_Moratorium_DXP_Config_1");
+        MoratoriumRule moratoriumRule = getMoratoriumRule(td_CA);
+        TestData td_VA = getTestSpecificTD("TestData_Moratorium_DXP_Config_2");
+        MoratoriumRule moratoriumRule2 = getMoratoriumRule(td_VA);
+
         mockMoratoriumRuleAndRunTest(td_CA, moratoriumRule);
         mockMoratoriumRuleAndRunTest(td_VA, moratoriumRule2);
 
@@ -152,31 +159,101 @@ public class TestMiniServicesMoratorium extends PolicyMoratorium {
     /**
      * @author Jovita Pukenaite
      * @name Moratoriums - Garaging Address on the Policy/Adding/Updating a Vehicle
-     * @scenario1 1. Create AZ backdate policy (-2d). Residential address should be under the moratorium.
-     * Policy should have one vehicle that is not under the moratorium.
-     * 2. Hit start endorsement info service. Check the response.
-     * 3. Create endorsement outside of PAS.
-     * 4. Add and update vehicle with different garage address, CA - under the moratorium.
-     * 5. Check the response. And try rate.
+     * @scenario1 1. Create AZ backdate policy (-5d). Residential address should be under the moratorium.
+     * 2. Hit start endorsement info service. Check the response. (-5d)
+     * and check for today date, when moratorium rule should exist, and future date.
+     * 3. Create endorsement outside of PAS (-5d).
+     * 4. Update existing vehicle garage address (not under moratorium)
+	 * 5. Bind and rate endorsement.
+	 * 6. Create new endorsement outside of PAS (today).
+     * 7. Add and update vehicle with different garage address, CA - under the moratorium.
+     * 8. Check the response. And try rate.
+	 * 9. Update new vehicle again, with CA address not under the moratorium.
+	 * 10. Check the response. Rate and Bind.
+	 * 11. Update new vehicle again, with AZ address not under the moratorium.
+	 * 12. Rate and Bind.
      */
-
     @Parameters({"state"})
     @Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
-    @TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-21466"})
     @StateList(states = {Constants.States.VA, Constants.States.AZ})
-    public void pas21466_GarageAddressUnderTheMoratoriumPart2(@Optional("VA") String state) {
+    @TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-21466"})
+    public void pas21466_GarageAddressUnderTheMoratoriumPart2(@Optional("AZ") String state) {
+
+        TestData td_CA = getTestSpecificTD("TestData_Moratorium_DXP_Config_1");
+        MoratoriumRule moratoriumRule = getMoratoriumRule(td_CA);
+        TestData td_AZ = getTestSpecificTD("TestData_Moratorium_DXP_Config_3");
+        MoratoriumRule moratoriumRule3 = getMoratoriumRule(td_AZ);
+		TestData tdError = DataProviderFactory.dataOf(ErrorTab.KEY_ERRORS, "All");
+
         mockMoratoriumRuleAndRunTest(td_CA, moratoriumRule);
-        mockMoratoriumRuleAndRunTest(td_VA, moratoriumRule2);
+        mockMoratoriumRuleAndRunTest(td_AZ, moratoriumRule3);
+
+		String generalTabSimpleName = CustomerMetaData.GeneralTab.class.getSimpleName();
+
+		TestData customerTd = (getCustomerIndividualTD("DataGather", "TestData")
+				.adjust(TestData.makeKeyPath(generalTabSimpleName, CustomerMetaData.GeneralTab.ZIP_CODE.getLabel()), "85003")
+				.adjust(TestData.makeKeyPath(generalTabSimpleName, CustomerMetaData.GeneralTab.ADDRESS_LINE_1.getLabel()), "805 N 4rd Avenue"))
+				.adjust(TestData.makeKeyPath(generalTabSimpleName, CustomerMetaData.GeneralTab.STATE.getLabel()), "AZ")
+				.adjust(TestData.makeKeyPath(generalTabSimpleName, CustomerMetaData.GeneralTab.CITY.getLabel()), "Phoenix");
+
+		TestData policyTd = getPolicyDefaultTD()
+				.adjust(TestData.makeKeyPath(GeneralTab.class.getSimpleName(),
+						AutoSSMetaData.GeneralTab.POLICY_INFORMATION.getLabel(), AutoSSMetaData.GeneralTab.PolicyInformation.EFFECTIVE_DATE.getLabel()),
+						DateTimeUtils.getCurrentDateTime().minusDays(5).format(DateTimeUtils.MM_DD_YYYY))
+				.adjust(AutoSSMetaData.ErrorTab.class.getSimpleName(), tdError);
 
         mainApp().open();
-        createCustomerIndividual();
-        String policyNumber = createPolicy(getBackDatedPolicyTD());
+        createCustomerIndividual(customerTd);
+        String policyNumber = createPolicy(policyTd);
 
+        checkMoratoriumInfoInStartEndorsementInfo(policyNumber, endorsementDateBack, false);
+        checkMoratoriumInfoInStartEndorsementInfo(policyNumber, endorsementDate, true);
+        checkMoratoriumInfoInStartEndorsementInfo(policyNumber, endorsementDateFuture, false);
 
+        HelperCommon.createEndorsement(policyNumber, endorsementDateBack);
 
+        //get vehicles oid's and update garage address to existing vehicle
+        ViewVehicleResponse viewEndorsementVehicleResponse = HelperCommon.viewEndorsementVehicles(policyNumber);
+        Vehicle vehicle1 = findVehicle(viewEndorsementVehicleResponse, policyTd.getTestDataList("VehicleTab").get(0).getValue("VIN"));
+        VehicleUpdateResponseDto updateResponseVehicle = HelperCommon.updateVehicle(policyNumber, vehicle1.oid, updateVehReqNotUnderTheMoratorium_AZ);
+        checkTheErrorAfterGarageAddressWasUpdated(updateResponseVehicle, false);
+
+        helperMiniServices.endorsementRateAndBind(policyNumber);
+
+        //error should not exist after update
+        checkMoratoriumInfoInStartEndorsementInfo(policyNumber, endorsementDate, false);
+
+        helperMiniServices.createEndorsementWithCheck(policyNumber);
+
+        //Add another vehicle, garage address different = false
+        VehicleUpdateResponseDto secondVehicleUpdateResponse = addVehicle(policyNumber, purchaseDate1, vin1);
+        checkTheErrorAfterGarageAddressWasUpdated(secondVehicleUpdateResponse, true);
+        checkRateServiceIfMoratoriumErrorExist(policyNumber, true, false);
+
+        //update second vehicle garage address (CA under the moratorium)
+        VehicleUpdateResponseDto updateResponseVehicle3 = HelperCommon.updateVehicle(policyNumber, secondVehicleUpdateResponse.oid, updateVehReqUnderTheMoratorium_CA);
+        checkTheErrorAfterGarageAddressWasUpdated(updateResponseVehicle3, true);
+        checkRateServiceIfMoratoriumErrorExist(policyNumber, true, false);
+
+		//update second vehicle garage address (CA not under the moratorium)
+		VehicleUpdateResponseDto updateResponseVehicle4 = HelperCommon.updateVehicle(policyNumber, secondVehicleUpdateResponse.oid, updateVehReqNotUnderTheMoratorium_CA);
+		checkTheErrorAfterGarageAddressWasUpdated(updateResponseVehicle4, false);
+		checkRateServiceIfMoratoriumErrorExist(policyNumber, false, true);
+
+		//update second vehicle garage address (CA not under the moratorium)
+		VehicleUpdateResponseDto updateResponseVehicle5 = HelperCommon.updateVehicle(policyNumber, secondVehicleUpdateResponse.oid, updateVehReqNotUnderTheMoratorium_AZ);
+		checkTheErrorAfterGarageAddressWasUpdated(updateResponseVehicle5, false);
+		checkRateServiceIfMoratoriumErrorExist(policyNumber, false, false);
+		helperMiniServices.endorsementRateAndBind(policyNumber);
 
         expireMoratorium(moratoriumRule.getName());
-        expireMoratorium(moratoriumRule2.getName());
+        expireMoratorium(moratoriumRule3.getName());
+    }
+
+    private Vehicle findVehicle(ViewVehicleResponse viewVehicleResponse, String vin) {
+        Vehicle vehicle = viewVehicleResponse.vehicleList.stream().filter(veh -> vin.equals(veh.vehIdentificationNo)).findFirst().orElse(null);
+        assertThat(vehicle).isNotNull();
+        return vehicle;
     }
 
     private void checkTheErrorAfterGarageAddressWasUpdated(VehicleUpdateResponseDto updateResponse, boolean isErrorShouldExist) {
@@ -194,7 +271,6 @@ public class TestMiniServicesMoratorium extends PolicyMoratorium {
         if (isErrorShouldExist) {
             ValidateEndorsementResponse endorsementInfoResponse = HelperCommon.startEndorsement(policyNumber, endorsementDate);
             assertSoftly(softly -> {
-                softly.assertThat(endorsementInfoResponse.allowedEndorsements).isEmpty();
                 softly.assertThat(endorsementInfoResponse.ruleSets.get(0).name).isEqualTo("PolicyRules");
                 softly.assertThat(endorsementInfoResponse.ruleSets.get(0).errors.stream().anyMatch(err -> err.message.equals(ErrorDxpEnum.Errors.MORATORIUM_EXIST.getMessage()))).isTrue();
             });
