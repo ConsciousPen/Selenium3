6d3ad5b9f24e00f2ead2e380e6f1050b4e337779
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-11-28 09:44:13 -0700

    added support for DP3 claims for other tests

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestClueSimplificationPropertyAbstract.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestClueSimplificationPropertyAbstract.java
@@ -368,7 +368,11 @@ public abstract class TestClueSimplificationPropertyAbstract extends TestClaimPo
     }
 
     protected void pas6695_testClueClaimsReconciliationClaimantOnly() {
-        createSpecificCustomerIndividual("Agustin", "Miras");
+        if (getPolicyType().equals(PolicyType.HOME_SS_DP3) || getPolicyType().equals(PolicyType.HOME_CA_DP3)) {
+            createSpecificCustomerIndividual("AgustinDP", "Miras");
+        } else {
+            createSpecificCustomerIndividual("Agustin", "Miras");
+        }
         policy.initiate();
         policy.getDefaultView().fillUpTo(getPolicyTD(), getPropertyInfoTab().getClass(), true);
         checkTblClaimRowCount(0);
@@ -388,7 +392,12 @@ public abstract class TestClueSimplificationPropertyAbstract extends TestClaimPo
     }
 
     protected void pas6703_testCatastropheIndicatorUnknownNB() {
-        createSpecificCustomerIndividual("Sachin", "Kohli");
+        if (getPolicyType().equals(PolicyType.HOME_SS_DP3) || getPolicyType().equals(PolicyType.HOME_CA_DP3)) {
+            createSpecificCustomerIndividual("SachinDP", "Kohli");
+        } else {
+            createSpecificCustomerIndividual("Sachin", "Kohli");
+        }
+
         policy.initiate();
         policy.getDefaultView().fillUpTo(getPolicyTD(), getPropertyInfoTab().getClass(), true);
         checkTblClaimRowCount(2);
@@ -438,7 +447,9 @@ public abstract class TestClueSimplificationPropertyAbstract extends TestClaimPo
 
         getReportsTab().submitTab();
         if (getPolicyType().equals(PolicyType.HOME_SS_DP3)) {
-            checkTblClaimRowCount(1);
+            // Can't use checkTblClaimRowCount with 1 claim (no table present)
+            assertThat(getClaimSourceAsset().getValue()).isEqualTo("CLUE");
+            assertThat(getClaimLossForAsset().getValue()).isEqualTo(Labels.APPLICANT_PROPERTY);
         } else {
             checkTblClaimRowCount(2);
         }
