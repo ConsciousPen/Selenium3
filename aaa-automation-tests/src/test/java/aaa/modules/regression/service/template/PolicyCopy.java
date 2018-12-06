9323eb271adf844b71ccf1292f1a96525ff9b6b9
Reva, Olga (C) <oreva@eisgroup.com>
2018-12-06 14:49:47 +0200

    Home SS: workaround added to privileges tests

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyCopy.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/template/PolicyCopy.java
@@ -3,6 +3,7 @@ package aaa.modules.regression.service.template;
 import static toolkit.verification.CustomAssertions.assertThat;
 
 import aaa.common.enums.Constants.UserGroups;
+import aaa.main.modules.policy.PolicyType;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.PolicyBaseTest;
 
@@ -13,14 +14,15 @@ public class PolicyCopy extends PolicyBaseTest {
 		createCustomerIndividual();
 		createPolicy();
 		String policyNumber = PolicySummaryPage.labelPolicyNumber.getValue();
-		 if (getUserGroup().equals(UserGroups.F35.get())||getUserGroup().equals(UserGroups.G36.get())) {
-			 policy.policyCopy().perform(getPolicyTD("CopyFromPolicy", "TestData_F35_G36"));
-				policy.calculatePremiumAndPurchase(getPolicyTD("CopyFromPolicy", "TestData_F35_G36"));
-	        }
-	        else {
-	        	policy.policyCopy().perform(getPolicyTD("CopyFromPolicy", "TestData"));
-	    		policy.calculatePremiumAndPurchase(getPolicyTD("CopyFromPolicy", "TestData"));
-	        }
+		if (getPolicyType().equals(PolicyType.HOME_SS_HO3) && 
+				(getUserGroup().equals(UserGroups.F35.get())||getUserGroup().equals(UserGroups.G36.get()))) {
+			policy.policyCopy().perform(getPolicyTD("CopyFromPolicy", "TestData_F35_G36"));
+			policy.calculatePremiumAndPurchase(getPolicyTD("CopyFromPolicy", "TestData_F35_G36"));
+		}
+		else {
+	        policy.policyCopy().perform(getPolicyTD("CopyFromPolicy", "TestData"));
+	    	policy.calculatePremiumAndPurchase(getPolicyTD("CopyFromPolicy", "TestData"));
+	    }
 
 		assertThat(PolicySummaryPage.labelPolicyNumber).as("Copied policy number is the same as initial policy number").doesNotHaveValue(policyNumber);
 	}
