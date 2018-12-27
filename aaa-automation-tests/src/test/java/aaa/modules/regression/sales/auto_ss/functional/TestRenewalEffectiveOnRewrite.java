c8eea8d5c2093dcd2f507ea9b10547bd6d45430f
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2018-12-27 15:38:36 -0700

    added error override for end of year issue

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestRenewalEffectiveOnRewrite.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestRenewalEffectiveOnRewrite.java
@@ -3,8 +3,9 @@ package aaa.modules.regression.sales.auto_ss.functional;
 import aaa.common.enums.Constants;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
+import aaa.main.enums.ErrorEnum;
 import aaa.main.metadata.policy.AutoSSMetaData;
-import aaa.main.modules.policy.auto_ss.defaulttabs.GeneralTab;
+import aaa.main.modules.policy.auto_ss.defaulttabs.*;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.AutoSSBaseTest;
 import aaa.utils.StateList;
@@ -39,9 +40,10 @@ public class TestRenewalEffectiveOnRewrite extends AutoSSBaseTest {
 	@Test(groups = {Groups.HIGH, Groups.FUNCTIONAL, Groups.TIMEPOINT}, description = "Test Renewal Effective On Rewrite")
 	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-22502")
 	public void pas22502_TestRenewalEffectiveOnRewrite(@Optional("AZ") String state) {
+        ErrorTab errorTab = new ErrorTab();
 
-		LocalDateTime policyEffective = TimeSetterUtil.getInstance().getCurrentTime();
 		String policyNumber = openAppAndCreatePolicy();
+        LocalDateTime policyEffective = PolicySummaryPage.getEffectiveDate();
 
 		policy.cancel().perform(getPolicyTD("Cancellation", "TestData"));
 		TimeSetterUtil.getInstance().nextPhase(policyEffective.plusDays(10));
@@ -50,8 +52,17 @@ public class TestRenewalEffectiveOnRewrite extends AutoSSBaseTest {
 		policy.rewrite().perform(getPolicyTD("Rewrite", "TestDataSameDate"));
 		String renewalEffective = PolicySummaryPage.getExpirationDate().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
 		policy.dataGather().start();
-		policy.getDefaultView().fill(getPolicyTD("Rewrite", "TestDataForBindRewrittenPolicy"));
+		policy.getDefaultView().fillFromTo(getPolicyTD("Rewrite", "TestDataForBindRewrittenPolicy"), PrefillTab.class, DocumentsAndBindTab.class, true);
+		new DocumentsAndBindTab().submitTab();
 
+		// Need to override error during the end of December when the cancellation date and base dates have different years
+		if (errorTab.tableErrors.isPresent()) {
+		    errorTab.overrideErrors(ErrorEnum.Errors.ERROR_AAA_10022001);
+		    errorTab.override();
+            new DocumentsAndBindTab().submitTab();
+        }
+
+        new PurchaseTab().fillTab(getPolicyTD("Rewrite", "TestDataForBindRewrittenPolicy")).submitTab();
 		policy.renew().perform();
 		assertThat(new GeneralTab().getPolicyInfoAssetList().getAsset(AutoSSMetaData.GeneralTab.PolicyInformation.EFFECTIVE_DATE)).hasValue(renewalEffective);
 	}
