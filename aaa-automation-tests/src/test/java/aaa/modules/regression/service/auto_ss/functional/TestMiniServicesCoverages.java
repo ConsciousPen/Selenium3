e1fd2496dbde0f75801d257e699b6e0fbd0000b2
Strazds, Maris (C) <maris.strazds@csaa.com>
2019-01-11 10:46:03 -0700

    Added order of coverages test for CO.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesCoverages.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/TestMiniServicesCoverages.java
@@ -480,7 +480,7 @@ public class TestMiniServicesCoverages extends TestMiniServicesCoveragesHelper {
 	@Parameters({"state"})
 	@StateList(states = {Constants.States.AZ, Constants.States.VA, Constants.States.DE, Constants.States.IN, Constants.States.KS,
 			Constants.States.MD, Constants.States.NV, Constants.States.OH, Constants.States.OR, Constants.States.CT, Constants.States.KY, Constants.States.SD,
-			Constants.States.WV, Constants.States.UT, Constants.States.DC})
+			Constants.States.WV, Constants.States.UT, Constants.States.DC, Constants.States.CO})
 	@Test(groups = {Groups.FUNCTIONAL, Groups.CRITICAL})
 	@TestInfo(component = ComponentConstant.Service.AUTO_SS, testCaseId = {"PAS-17646", "PAS-19013", "PAS-19042", "PAS-19016", "PAS-19024", "PAS-19044", "PAS-18202", "PAS-19055", "PAS-19052", "PAS-18350"})
 	public void pas17646_OrderOfCoverage(@Optional("VA") String state) {
