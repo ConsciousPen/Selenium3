6f1b791f52f0e9799a3d41981fa318dc8104d3d1
Vandersloot, Shayne (C) <shayne.vandersloot@csaa.com>
2019-02-01 12:49:50 -0700

    Updated validateCensusBlockGroupAndLatLong() to pass in parameters for testing between HO CA and HO SS. Added AVS mock values for the default HO AZ address used for automation (Note: the lat/long still needs to be updated for what is in the newer AVS mock sheet). Added a boolean for checkVRD since HO SS currently doesn't display census block group in the View Rating Details pop up.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/functional/TestHomeGranularity.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho3/functional/TestHomeGranularity.java
@@ -36,6 +36,9 @@ public class TestHomeGranularity extends TestHomeGranularityAbstract {
     private String censusBlock = HomeGranularityConstants.EADS_MOCK_CENSUS_BLOCK_CA;
     private String latitude = HomeGranularityConstants.EADS_MOCK_LATITUDE_CA;
     private String longitude = HomeGranularityConstants.EADS_MOCK_LONGITUDE_CA;
+    private String avsMockCensusBlock = HomeGranularityConstants.MOCK_CENSUS_BLOCK;
+    private String avsMockLatitude = HomeGranularityConstants.MOCK_LATITUDE;
+    private String avsMockLongitude = HomeGranularityConstants.MOCK_LONGITUDE;
 
     /**
      * @name test: Capture Census Block Group, Latitude and Longitude when address is validated
@@ -52,7 +55,7 @@ public class TestHomeGranularity extends TestHomeGranularityAbstract {
     @TestInfo(component = ComponentConstant.Sales.HOME_CA_HO3, testCaseId = "PAS-23235")
     //Note: If the coordinates become more precise then we need to refactor this test
     public void pas23235_validateCensusBlockGroupAndLatLong(@Optional("CA") String state) {
-        validateCensusBlockGroupAndLatLong();
+        validateCensusBlockGroupAndLatLong(avsMockCensusBlock, avsMockLatitude, avsMockLongitude,true);
     }
 
     /**
