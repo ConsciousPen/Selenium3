2491dbfe1a6dba6c8105d5d4bf929764defed55a
Johns, Chris <christopher.johns@csaa.com>
2019-02-11 15:51:44 -0700

    -Added Choice Test

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestOffLineClaims.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/choice/functional/TestOffLineClaims.java
@@ -4,39 +4,25 @@ import aaa.common.enums.Constants;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
 import aaa.common.pages.SearchPage;
-import aaa.helpers.claim.BatchClaimHelper;
 import aaa.helpers.constants.ComponentConstant;
 import aaa.helpers.constants.Groups;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
-import aaa.helpers.logs.PasAdminLogGrabber;
 import aaa.main.enums.SearchEnum;
-import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.main.modules.policy.PolicyType;
-import aaa.main.modules.policy.auto_ss.defaulttabs.DriverTab;
-import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.regression.sales.template.functional.TestOfflineClaimsCATemplate;
-import aaa.toolkit.webdriver.customcontrols.ActivityInformationMultiAssetList;
 import aaa.utils.StateList;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
 import com.google.common.collect.ImmutableMap;
-import org.assertj.core.api.Assertions;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import toolkit.datax.TestData;
 import toolkit.db.DBService;
 import toolkit.utils.TestInfo;
-import toolkit.utils.datetime.DateTimeUtils;
-import toolkit.verification.CustomSoftAssertions;
-
-import java.io.File;
-import java.util.List;
 import java.util.Map;
 
-import static aaa.common.pages.SearchPage.tableSearchResults;
 import static aaa.main.pages.summary.PolicySummaryPage.buttonRenewals;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @StateList(states = {Constants.States.CA})
 public class TestOffLineClaims extends TestOfflineClaimsCATemplate {
@@ -107,4 +93,77 @@ public class TestOffLineClaims extends TestOfflineClaimsCATemplate {
 	    // Check 2nd driver: Has DL match claim
 		compDLPuAssertions(CLAIM_NUMBER_1, CLAIM_NUMBER_2, CLAIM_NUMBER_3);
     }
+
+	/**
+	 * @author Chris Johns
+	 * PAS-22172 - END - CAS: reconcile permissive use claims when driver/named insured is added (avail for rating)
+	 * @name Test Offline STUB/Mock Data Claims
+	 * @scenario Test Steps:
+	 * 1. Create a Policy with 3 drivers; FNI will get X PU Claims
+	 * 2. Move time to R-63 and run Renewal Part1 + "renewalClaimOrderAsyncJob"
+	 * 3. Create CAS Response File Thru Automation Framework - This is the 'Offline Batch Job' step
+	 * 4. Move Time to R-46 and run Renewal Part2 + "claimsRenewBatchReceiveJob" - X PU claims are assigned
+	 * 5. Retrieve policy and enter renewal image
+	 * 6. Verify all PU claims are assigned to the FNI
+	 * 7. Accept a payment and renew the policy
+	 * 8. Initiate an endorsement
+	 * 9. Add an AFR driver who's CLUE report will return a claim that matches one of the PU claims on the FNI
+	 * 10. Calculate premium and order CLUE report
+	 * 11. Navigate to the drive tab, and verify the PU claim was moved from the FNI to the newly added driver
+	 * @details Clean Path. Expected Result is that PU claim will be move from the FNI to the newly added driver
+	 */
+	@Parameters({"state"})
+	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+	@TestInfo(component = ComponentConstant.Sales.AUTO_CA_CHOICE, testCaseId = "PAS-14679")
+	public void pas22172_ReconcilePUEndorsementAFRD(@Optional("CA") @SuppressWarnings("unused") String state) {
+
+		// Toggle ON PermissiveUse Logic
+		// Set DATEOFLOSS Parameter in DB: Equal to Claim3 dateOfLoss
+		// Set RISKSTATECD in DB to get policy DATEOFLOSS working
+		DBService.get().executeUpdate(SQL_UPDATE_PERMISSIVEUSE_DISPLAYVALUE);
+		DBService.get().executeUpdate(String.format(SQL_UPDATE_PERMISSIVEUSE_DATEOFLOSS, "11-NOV-18"));
+
+		createPolicyMultiDrivers();    // Create Customer and Policy with 4 drivers
+		runRenewalClaimOrderJob();     // Move to R-63, run batch job part 1 and offline claims batch job
+		generateClaimRequest();        // Download claim request and assert it
+
+		// Create the claim response
+		createCasClaimResponseAndUploadWithUpdatedDL(policyNumber, COMP_DL_PU_CLAIMS_DATA_MODEL, CLAIM_TO_DRIVER_LICENSE);
+		runRenewalClaimReceiveJob();   // Move to R-46 and run batch job part 2 and offline claims receive batch job
+
+		// Retrieve policy
+		mainApp().open();
+		SearchPage.search(SearchEnum.SearchFor.POLICY, SearchEnum.SearchBy.POLICY_QUOTE, policyNumber);
+
+		// Enter renewal image and verify claim presence
+		buttonRenewals.click();
+		policy.dataGather().start();
+		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.DRIVER.get());
+
+		// Check 1st driver: FNI, has the COMP match claim & PU Match Claim. Also Making sure that Claim4: 1002-10-8704-INVALID-dateOfLoss from data model is not displayed
+		// Check 2nd driver: Has DL match claim
+		compDLPuAssertions(CLAIM_NUMBER_1, CLAIM_NUMBER_2, CLAIM_NUMBER_3);
+		mainApp().close();
+
+		//Move time to R-35 and run batch jobs:
+		moveTimeAndRunRenewJobs(policyExpirationDate.minusDays(35));
+
+		//Accept Payment and renew the policy
+		payTotalAmtDue(policyNumber);
+		TimeSetterUtil.getInstance().nextPhase(policyExpirationDate);
+		JobUtils.executeJob(Jobs.policyStatusUpdateJob);
+
+		//Set test date for endorsement
+		TestData addDriverTd = getTestSpecificTD("Add_PU_Claim_Driver_Endorsement_CA");
+		//Initiate an endorsement: Add AFR Driver, calculate premium and order clue
+		initiateAddDriverEndorsement(policyNumber, addDriverTd);
+
+		//Navigate to Driver page and verify PU claim moved from FNI to newly added driver
+		NavigationPage.toViewTab(NavigationEnum.AutoCaTab.DRIVER.get());
+		puDropAssertions(CLAIM_NUMBER_1, CLAIM_NUMBER_3);
+
+		//Bind Endorsement
+		bindEndorsement();
+	}
+
 }
