d05513834a9ebf94db2dfe3bb599d5b4d7d4538d
Jemison, Tyrone <tyrone.jemison@csaa.com>
2019-02-20 10:05:13 -0700

    Restored what I accidentally removed.

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMultiPolicyDiscount.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMultiPolicyDiscount.java
@@ -50,6 +50,7 @@ public class TestMultiPolicyDiscount extends AutoSSBaseTest {
     private ErrorTab _errorTab = new ErrorTab();
     private PremiumAndCoveragesTab _pncTab = new PremiumAndCoveragesTab();
     private DocumentsAndBindTab _documentsAndBindTab = new DocumentsAndBindTab();
+    private PurchaseTab _purchaseTab = new PurchaseTab();
 
     /**
      * Make sure various combos of Unquoted Other AAA Products rate properly and are listed in the UI
