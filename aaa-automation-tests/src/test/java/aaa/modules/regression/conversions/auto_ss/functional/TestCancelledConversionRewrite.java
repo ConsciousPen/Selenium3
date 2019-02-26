c751526d4430157e825d3a21da245f5fdfef79ff
Razgunas, Dominykas (C) <Dominykas.Razgunas@csaa.com>
2019-02-26 03:22:39 -0700

    Created test for defect

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/auto_ss/functional/TestCancelledConversionRewrite.java
@@ -0,0 +1,58 @@
+package aaa.modules.regression.conversions.auto_ss.functional;
+
+import static toolkit.verification.CustomAssertions.assertThat;
+import java.time.LocalDateTime;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+import aaa.common.enums.Constants;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.main.modules.policy.PolicyType;
+import aaa.main.modules.policy.auto_ss.defaulttabs.PremiumAndCoveragesTab;
+import aaa.modules.regression.conversions.template.ManualConversionTemplate;
+import aaa.utils.StateList;
+import toolkit.datax.TestData;
+import toolkit.utils.TestInfo;
+import toolkit.utils.datetime.DateTimeUtils;
+
+@StateList(states = {Constants.States.KY, Constants.States.WV})
+
+public class TestCancelledConversionRewrite extends ManualConversionTemplate {
+
+	@Override
+	protected PolicyType getPolicyType() {
+		return PolicyType.AUTO_SS;
+	}
+
+    /**
+     * * @author Dominykas Razgunas
+     *
+     *@name Test Paperless Preferences properties and Inquiry mode
+     *@scenario
+     *1. Create customer.
+     *2. Create Auto SS NY Conversion Policy.
+     *3. Propose and Issue Policy.
+     *4. Cancel Policy on its effective date.
+     *5. Rewrite Policy.
+     *6. Initiate Data gather on rewritten quote.
+     *7. Navigate to P&C.
+     *8. Calculate premium.
+	 * 9. Check that no Error is fired
+     *@details
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH, Groups.TIMEPOINT}, description = "Test Premium Calculation for a rewritten conversion policy KY/WV")
+    @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-25298")
+    public void pas25298_calculatePremiumForRewrittenConversionPolicy(@Optional("WV") String state) {
+
+		LocalDateTime effDate = TimeSetterUtil.getInstance().getPhaseStartTime().plusDays(45);
+		manualRenewalEntryToActivePolicy();
+		policy.cancel().perform(getPolicyTD("Cancellation", "TestData").adjust(TestData.makeKeyPath("CancellationActionTab", "Cancel Date"), effDate.format(DateTimeUtils.MM_DD_YYYY)));
+		policy.rewrite().perform(getPolicyTD("Rewrite", "TestDataSameDate"));
+		policy.dataGather().start();
+		new PremiumAndCoveragesTab().calculatePremium();
+		assertThat(new PremiumAndCoveragesTab().btnCalculatePremium()).isPresent();
+    }
+}
