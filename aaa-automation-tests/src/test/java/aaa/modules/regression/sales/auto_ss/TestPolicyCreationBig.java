b6778f2eae451338921be6dda54c40a61a17e15e
Rajendran, Kiruthika (C) <kiruthika.rajendran@csaa.com>
2019-03-21 13:21:09 -0700

    final commit

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/TestPolicyCreationBig.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/TestPolicyCreationBig.java
@@ -29,7 +29,7 @@ public class TestPolicyCreationBig extends AutoSSBaseTest {
     @StateList(statesExcept = { States.CA })
 	@Test(groups = { Groups.REGRESSION, Groups.CRITICAL})
     @TestInfo(component = ComponentConstant.Sales.AUTO_SS)
-    public void testPolicyCreationBig(@Optional("MD") String state) {
+    public void testPolicyCreationBig(@Optional("") String state) {
 
     	mainApp().open();
         
