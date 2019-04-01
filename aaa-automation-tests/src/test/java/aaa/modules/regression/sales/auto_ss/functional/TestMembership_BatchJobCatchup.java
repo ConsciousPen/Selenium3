8ea6140d37df70db124d0f8fc49bec4e907e5f97
Jemison, Tyrone <tyrone.jemison@csaa.com>
2019-04-01 12:40:42 -0700

    Updated script

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMembership_BatchJobCatchup.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMembership_BatchJobCatchup.java
@@ -1,5 +1,6 @@
 package aaa.modules.regression.sales.auto_ss.functional;
 
+import aaa.helpers.db.queries.AAAMembershipQueries;
 import aaa.helpers.jobs.JobUtils;
 import aaa.helpers.jobs.Jobs;
 import aaa.modules.policy.AutoSSBaseTest;
@@ -17,17 +18,22 @@ import java.time.LocalDateTime;
 public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
     enum eThresholdTests {BEFORE, ON, AFTER}
 
-    @DataProvider(name = "ThresholdTestData")
-    public static Object[][] data() {
-        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 4}, {"AZ", eThresholdTests.ON, 5}, {"AZ", eThresholdTests.AFTER, 6}};
+    @DataProvider(name = "ThresholdTestData_STG1")
+    public static Object[][] data1() {
+        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 15, 4}, {"AZ", eThresholdTests.ON, 15, 5}, {"AZ", eThresholdTests.AFTER, 15, 6}};
+    }
+
+    @DataProvider(name = "ThresholdTestData_STG2")
+    public static Object[][] data2() {
+        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 30, 4}, {"AZ", eThresholdTests.ON, 30, 5}, {"AZ", eThresholdTests.AFTER, 30, 6}};
     }
 
     Integer _thresholdValue = 5;
     LocalDateTime thresholdMaxDate = null;
 
     @Parameters({"state"})
-    @Test(dataProvider = "ThresholdTestData")
-    public void STG1_TestThreshold(@Optional String state, eThresholdTests typeOfThresholdTest, Integer daysAfterNB) {
+    @Test(dataProvider = "ThresholdTestData_STG1")
+    public void STG1orSTG2_TestThreshold(@Optional String state, eThresholdTests typeOfThresholdTest, Integer nb15or30, Integer daysAfterNB) {
         // Creating Policy using Default Test Data
         mainApp().open();
         createCustomerIndividual();
@@ -36,23 +42,25 @@ public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
         // Saving policy data from summary page and closing App.
         String policyNumber = PolicySummaryPage.getPolicyNumber();
         LocalDateTime policyEffectiveDate = PolicySummaryPage.getEffectiveDate();
-        thresholdMaxDate = policyEffectiveDate.plusDays(_thresholdValue);
+        LocalDateTime nbDate = policyEffectiveDate.plusDays(nb15or30);
+        thresholdMaxDate = nbDate.plusDays(_thresholdValue);
         mainApp().close();
 
         // Moving JVM to NB+15+testedDay and assert system is inside of Catch-Up window.
-        TimeSetterUtil.getInstance().nextPhase(policyEffectiveDate.plusDays(15 + daysAfterNB)); //moving jvm to NB+15+4.
+        TimeSetterUtil.getInstance().nextPhase(nbDate.plusDays(daysAfterNB)); //moving jvm to NB+15+n.
         LocalDateTime rightNow = TimeSetterUtil.getInstance().getCurrentTime();
         switch(typeOfThresholdTest){
             case BEFORE:
+                CustomAssertions.assertThat(rightNow).isAfter(nbDate);
                 CustomAssertions.assertThat(rightNow).isBefore(thresholdMaxDate);
-                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s. FIND IN LOGS.", policyNumber, rightNow));
+                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s (NB+%s+%s). FIND IN LOGS.", policyNumber, rightNow, nb15or30.toString(), daysAfterNB.toString()));
                 break;
             case ON:
-                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s. FIND IN LOGS.", policyNumber, rightNow));
+                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s (NB+%s+%s). FIND IN LOGS.", policyNumber, rightNow, nb15or30.toString(), daysAfterNB.toString()));
                 CustomAssertions.assertThat(rightNow).isEqualToIgnoringHours(thresholdMaxDate);
                 break;
             case AFTER:
-                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s. NOT IN LOGS", policyNumber, rightNow));
+                System.out.println(String.format("QALOGS -> Completed batch execution for %s on %s (NB+%s+%s). SHOULD NOT BE IN LOGS", policyNumber, rightNow, nb15or30.toString(), daysAfterNB.toString()));
                 CustomAssertions.assertThat(rightNow).isAfter(thresholdMaxDate);
                 break;
             default:
@@ -63,6 +71,7 @@ public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
         JobUtils.executeJob(Jobs.membershipValidationJob);
 
         // Validate policyNumber is picked up by batch job at STG1.
+        CustomAssertions.assertThat(AAAMembershipQueries.getAAABestMembershipStatusFromSQL(policyNumber).toString()).isEqualToIgnoringCase("FOUND");
         // Manually open job logs and search for policyNumber in newly generated log.
     }
 
