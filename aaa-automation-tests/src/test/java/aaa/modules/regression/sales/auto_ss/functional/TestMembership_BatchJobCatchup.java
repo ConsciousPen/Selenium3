abde239ec7621a9378400babbe594bcd3970f9bb
Jemison, Tyrone <tyrone.jemison@csaa.com>
2019-04-02 10:26:32 -0700

    Test now forces membershipStatus= FOUND_STG1 before NB+30 job execution

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMembership_BatchJobCatchup.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestMembership_BatchJobCatchup.java
@@ -6,6 +6,7 @@ import aaa.helpers.jobs.Jobs;
 import aaa.modules.policy.AutoSSBaseTest;
 import aaa.main.pages.summary.PolicySummaryPage;
 import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
+import com.google.common.collect.Lists;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -14,25 +15,46 @@ import toolkit.utils.screenshots.ScreenshotManager;
 import toolkit.verification.CustomAssertions;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
     enum eThresholdTests {BEFORE, ON, AFTER}
 
+    static final Integer THRESHOLD_VALUE = 4;
+    static LocalDateTime thresholdMaxDate = null;
+
     @DataProvider(name = "ThresholdTestData_STG1")
-    public static Object[][] data1() {
-        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 15, 4}, {"AZ", eThresholdTests.ON, 15, 5}, {"AZ", eThresholdTests.AFTER, 15, 6}};
+    public static Object[][] ThresholdTestData_STG1() {
+        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 15, THRESHOLD_VALUE - 1}, {"AZ", eThresholdTests.ON, 15, THRESHOLD_VALUE}, {"AZ", eThresholdTests.AFTER, 15, THRESHOLD_VALUE + 1}};
+    }
+
+    @DataProvider(name = "ThresholdTestData_STG2")
+    public static Object[][] ThresholdTestData_STG2() {
+        return new Object[][] {{"AZ", eThresholdTests.BEFORE, 30, THRESHOLD_VALUE - 1}, {"AZ", eThresholdTests.ON, 30, THRESHOLD_VALUE}, {"AZ", eThresholdTests.AFTER, 30, THRESHOLD_VALUE + 1}};
     }
 
     @DataProvider(name = "ThresholdTestData_STG1_NoCatchup")
-    public static Object[][] data2() {
+    public static Object[][] ThresholdTestData_STG1_NoCatchup() {
         return new Object[][] {{"AZ", eThresholdTests.ON, 15, 0}};
     }
 
-    Integer _thresholdValue = 0;
-    LocalDateTime thresholdMaxDate = null;
+    @DataProvider(name = "ThresholdTestData_STG2_NoCatchup")
+    public static Object[][] ThresholdTestData_STG2_NoCatchup() {
+        return new Object[][] {{"AZ", eThresholdTests.ON, 30, 0}};
+    }
+
+    // Combines STG1 and STG2 test data for ability to test everything using this one method.
+    @DataProvider(name = "ThresholdTestData_STG1_STG2")
+    public static Object[][] ThresholdTestData_STG1_STG2() {
+        List<Object[]> all = Lists.newArrayList();
+        all.addAll(Arrays.asList(ThresholdTestData_STG1()));
+        all.addAll(Arrays.asList(ThresholdTestData_STG2()));
+        return all.toArray(new Object[all.size()][]);
+    }
 
     @Parameters({"state"})
-    @Test(dataProvider = "ThresholdTestData_STG1")
+    @Test(dataProvider = "ThresholdTestData_STG1_STG2")
     public void STG1orSTG2_TestThreshold(@Optional String state, eThresholdTests typeOfThresholdTest, Integer nb15or30, Integer daysAfterNB) {
         // Creating Policy using Default Test Data
         mainApp().open();
@@ -43,7 +65,7 @@ public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
         String policyNumber = PolicySummaryPage.getPolicyNumber();
         LocalDateTime policyEffectiveDate = PolicySummaryPage.getEffectiveDate();
         LocalDateTime nbDate = policyEffectiveDate.plusDays(nb15or30);
-        thresholdMaxDate = nbDate.plusDays(_thresholdValue);
+        thresholdMaxDate = nbDate.plusDays(THRESHOLD_VALUE);
         mainApp().close();
 
         // Moving JVM to NB+15+testedDay and assert system is inside of Catch-Up window.
@@ -68,11 +90,16 @@ public class TestMembership_BatchJobCatchup extends AutoSSBaseTest {
                 break;
         }
 
+        // If doing NB+30, force aaaBestMembershipStatus to FOUND
+        if(nb15or30==30){
+            AAAMembershipQueries.updateAAABestMembershipStatusInSQL(policyNumber, AAAMembershipQueries.AAABestMembershipStatus.FOUND_STG1);
+        }
+
         // Execute NB+15 / NB+30 Membership Validation Jobs
         JobUtils.executeJob(Jobs.membershipValidationJob);
 
         // Validate policyNumber is picked up by batch job at STG1.
-        CustomAssertions.assertThat(AAAMembershipQueries.getAAABestMembershipStatusFromSQL(policyNumber).toString()).isEqualToIgnoringCase("FOUND");
+        CustomAssertions.assertThat(AAAMembershipQueries.getAAABestMembershipStatusFromSQL(policyNumber).toString()).isEqualToIgnoringCase(AAAMembershipQueries.AAABestMembershipStatus.FOUND_STG1.toString());
     }
 
     // Use this to capture a screenshot in the event of an error, or at will.
