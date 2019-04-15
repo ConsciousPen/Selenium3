fa40afc65b6c374ae308f67135157c30ba87779f
Rajendran, Kiruthika (C) <kiruthika.rajendran@csaa.com>
2019-04-15 10:11:34 -0700

    changes committed

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestOffLineClaims.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ss/functional/TestOffLineClaims.java
@@ -1,58 +1,28 @@
 package aaa.modules.regression.sales.auto_ss.functional;
 
-import static aaa.common.pages.SearchPage.tableSearchResults;
-import static aaa.main.pages.summary.PolicySummaryPage.buttonRenewals;
-import static org.assertj.core.api.Assertions.assertThat;
-import java.io.File;
-import java.util.Map;
+import aaa.common.enums.Constants;
+import aaa.helpers.claim.BatchClaimHelper;
+import aaa.helpers.constants.ComponentConstant;
+import aaa.helpers.constants.Groups;
+import aaa.modules.regression.sales.template.functional.TestOfflineClaimsTemplate;
+import aaa.utils.StateList;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import com.exigen.ipb.etcsa.utils.TimeSetterUtil;
-import com.google.common.collect.ImmutableMap;
-import aaa.common.enums.Constants;
-import aaa.common.enums.NavigationEnum;
-import aaa.common.pages.NavigationPage;
-import aaa.common.pages.SearchPage;
-import aaa.helpers.claim.BatchClaimHelper;
-import aaa.helpers.constants.ComponentConstant;
-import aaa.helpers.constants.Groups;
-import aaa.helpers.jobs.JobUtils;
-import aaa.helpers.jobs.Jobs;
-import aaa.main.enums.SearchEnum;
-import aaa.main.metadata.policy.AutoSSMetaData;
-import aaa.main.modules.policy.auto_ss.defaulttabs.DriverTab;
-import aaa.main.pages.summary.PolicySummaryPage;
-import aaa.modules.regression.sales.template.functional.TestOfflineClaimsTemplate;
-import aaa.toolkit.webdriver.customcontrols.ActivityInformationMultiAssetList;
-import aaa.utils.StateList;
-import toolkit.datax.TestData;
 import toolkit.utils.TestInfo;
-import toolkit.utils.datetime.DateTimeUtils;
-import toolkit.verification.CustomSoftAssertions;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @StateList(states = {Constants.States.AZ})
 public class TestOffLineClaims extends TestOfflineClaimsTemplate {
 
-	// NOTE: Claims Matching Logic: e2e tests should use HTTP instead of HTTPS in DB (value of Microservice propertyname ='aaaClaimsMicroService.microServiceUrl')
-	// Example: http://claims-assignment.apps.prod.pdc.digital.csaa-insurance.aaa.com/pas-claims/v1
+    // NOTE: Claims Matching Logic: e2e tests should use HTTP instead of HTTPS in DB (value of Microservice propertyname ='aaaClaimsMicroService.microServiceUrl')
+    // Example: http://claims-assignment.apps.prod.pdc.digital.csaa-insurance.aaa.com/pas-claims/v1
 
-//    private static final String CLAIM_NUMBER_1 = "1002-10-8702";
-//    private static final String CLAIM_NUMBER_2 = "1002-10-8703";
-//    private static final String CLAIM_NUMBER_3 = "1002.10>8704";
-//	private static final String CLAIM_NUMBER_4 = "1FAZ1111OHS";
-//	private static final String CLAIM_NUMBER_5 = "4FAZ44444OHS";
-//    private static final String CLAIM_NUMBER_6 = "1002-10-8705";
-//    private static final String COMP_DL_PU_CLAIMS_DATA_MODEL = "comp_dl_pu_claims_data_model.yaml";
-	private static final String NAME_DOB_CLAIMS_DATA_MODEL = "name_dob_claims_data_model.yaml";
-//    private static final String INC_IN_RATING_3RD_RENEWAL_DATA_MODEL = "inc_in_rating_3rd_renewal_data_model.yaml";
-//    private static final String INC_RATING_CLAIM_1 = "IIRatingClaim1";
-//    private static final String INC_RATING_CLAIM_2 = "IIRatingClaim2";
-//    private static final String INC_RATING_CLAIM_3 = "IIRatingClaim3";
-//	private static final String INC_RATING_CLAIM_4 = "IIRatingClaim4";
-//	private static final Map<String, String> CLAIM_TO_DRIVER_LICENSE = ImmutableMap.of(CLAIM_NUMBER_1, "A12345222", CLAIM_NUMBER_2, "A12345222");
-//    private static final String CAS_CLUE_CLAIM = "1002-10-8704";
+    private static final String NAME_DOB_CLAIMS_DATA_MODEL = "name_dob_claims_data_model.yaml";
 
     /**
      * @author Andrii Syniagin
@@ -77,7 +47,7 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
      * PAS-14679 - DL # matching logic
      * PAS-14058 - COMP Claims match to FNI
      * PAS-18341 - Added PermissiveUse tag to Claims Service Contract
-	 * PAS-18300 - PERMISSIVE_USE match to FNI when dateOfLoss param = claim dateOfLoss
+     * PAS-18300 - PERMISSIVE_USE match to FNI when dateOfLoss param = claim dateOfLoss
      * @name Test Offline STUB/Mock Data Claims
      * @scenario Test Steps:
      * 1. Create a Policy with 3 drivers; 1 with no STUB data match, 2, and 3 with STUB data match
@@ -94,32 +64,32 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
     @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
     @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-14679")
     public void pas14679_CompDLPUMatchMore(@Optional("AZ") @SuppressWarnings("unused") String state) {
-         pas14679_CompDLPUMatchMore();
+        pas14679_CompDLPUMatchMore();
     }
 
-	/**
-	 * @author Kiruthika Rajendran
-	 * @author Chris Johns
-	 * PAS-14679 - DL # matching logic
-	 * PAS-14058 - COMP Claims match to FNI
-	 * PAS-18341 - Added PermissiveUse tag to Claims Service Contract
-	 * PAS-18300 - PERMISSIVE_USE match to FNI when dateOfLoss param = claim dateOfLoss
-	 * @name Test NAME and DOB Match logic Via Manual Renewal To Support Security Token Validation
-	 * @scenario Test Steps:
-	 * 1. Create a Policy with 4 drivers
-	 * 2. Initiate Manual Renewal
-	 * 3. Place the CAS Claim Response for PAS consumption
-	 * 4. Run Claims "claimsRenewBatchReceiveJob" Batch Job
-	 * 5. Retrieve policy and enter renewal image
-	 * 6. Verify Claim Data is applied to the correct driver.
-	 * @details Clean Path. Expected Result is that claims data is applied to the correct driver
-	 */
-	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-14679")
-	public void pas14679_CompDLPUMatchMoreManual(@Optional("AZ") @SuppressWarnings("unused") String state) {
+    /**
+     * @author Kiruthika Rajendran
+     * @author Chris Johns
+     * PAS-14679 - DL # matching logic
+     * PAS-14058 - COMP Claims match to FNI
+     * PAS-18341 - Added PermissiveUse tag to Claims Service Contract
+     * PAS-18300 - PERMISSIVE_USE match to FNI when dateOfLoss param = claim dateOfLoss
+     * @name Test NAME and DOB Match logic Via Manual Renewal To Support Security Token Validation
+     * @scenario Test Steps:
+     * 1. Create a Policy with 4 drivers
+     * 2. Initiate Manual Renewal
+     * 3. Place the CAS Claim Response for PAS consumption
+     * 4. Run Claims "claimsRenewBatchReceiveJob" Batch Job
+     * 5. Retrieve policy and enter renewal image
+     * 6. Verify Claim Data is applied to the correct driver.
+     * @details Clean Path. Expected Result is that claims data is applied to the correct driver
+     */
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-14679")
+    public void pas14679_CompDLPUMatchMoreManual(@Optional("AZ") @SuppressWarnings("unused") String state) {
         pas14679_CompDLPUMatchMoreManual();
-	}
+    }
 
     /**
      * @author Kiruthika Rajendran
@@ -187,8 +157,8 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
      * 5.1 Claim1: claimNumber1 - NOT INCLUDED IN RATING dateOfLoss = Two Policy Terms - 1 day (R1-1)
      * 5.1 Claim2: claimNumber2 - INCLUDED IN RATING dateOfLoss = Two Policy Terms (R1)
      * 5.1 Claim3: claimNumber3 - INCLUDED IN RATING dateOfLoss = Current Date (R3-46)
-	 * 5.1 Claim4: claimNumber4 - INCLUDED IN RATING dateOfLoss = Current Date (R3-46) PERMISSIVE_USE Match Assigned to FNI
-     *
+     * 5.1 Claim4: claimNumber4 - INCLUDED IN RATING dateOfLoss = Current Date (R3-46) PERMISSIVE_USE Match Assigned to FNI
+     * <p>
      * //TODO: Mantas Garsvinskas add one more claim on 1st Renewal for Existing match, will be implemented in other Story: PAS-22026
      * @details
      */
@@ -199,36 +169,37 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
         pas14552_includeClaimsInRatingDetermination();
     }
 
-	/**
-	 * @author Chris Johns
-	 * PAS-22172 - END - CAS: reconcile permissive use claims when driver/named insured is added (avail for rating)
-	 * @name Test Offline STUB/Mock Data Claims
-	 * @scenario Test Steps:
-	 * 1. Create a Policy with 3 drivers; FNI will get X PU Claims
-	 * 2. Move time to R-63 and run Renewal Part1 + "renewalClaimOrderAsyncJob"
-	 * 3. Create CAS Response File Thru Automation Framework - This is the 'Offline Batch Job' step
-	 * 4. Move Time to R-46 and run Renewal Part2 + "claimsRenewBatchReceiveJob" - X PU claims are assigned
-	 * 5. Retrieve policy and enter renewal image
-	 * 6. Verify all PU claims are assigned to the FNI
-	 * 7. Accept a payment and renew the policy
-	 * 8. Initiate an endorsement
-	 * 9. Add an AFR driver who's CLUE report will return a claim that matches one of the PU claims on the FNI
-	 * 10. Calculate premium and order CLUE report
-	 * 11. Navigate to the drive tab, and verify the PU claim was moved from the FNI to the newly added driver
-	 * @details Clean Path. Expected Result is that PU claim will be move from the FNI to the newly added driver
-	 */
+    /**
+     * @author Chris Johns
+     * PAS-22172 - END - CAS: reconcile permissive use claims when driver/named insured is added (avail for rating)
+     * @name Test Offline STUB/Mock Data Claims
+     * @scenario Test Steps:
+     * 1. Create a Policy with 3 drivers; FNI will get X PU Claims
+     * 2. Move time to R-63 and run Renewal Part1 + "renewalClaimOrderAsyncJob"
+     * 3. Create CAS Response File Thru Automation Framework - This is the 'Offline Batch Job' step
+     * 4. Move Time to R-46 and run Renewal Part2 + "claimsRenewBatchReceiveJob" - X PU claims are assigned
+     * 5. Retrieve policy and enter renewal image
+     * 6. Verify all PU claims are assigned to the FNI
+     * 7. Accept a payment and renew the policy
+     * 8. Initiate an endorsement
+     * 9. Add an AFR driver who's CLUE report will return a claim that matches one of the PU claims on the FNI
+     * 10. Calculate premium and order CLUE report
+     * 11. Navigate to the drive tab, and verify the PU claim was moved from the FNI to the newly added driver
+     * @details Clean Path. Expected Result is that PU claim will be move from the FNI to the newly added driver
+     */
     /**
      * PAS-23977 - END: Reconcile Claim # Formats (CLUE and CAS)
+     *
      * @name Test Offline STUB/Mock: reconcile permissive use claims when driver/named insured is added and compare of CLUE claim from newly added driver to existing PU Yes claim on FNI .
      * @scenario Test Steps: See Template For Details
      * @details Clean Path. Expected Result is that PU claim will be move from the FNI to the newly added driver and only claim numbers will be compared ignoring the format differences.
      */
-	@Parameters({"state"})
-	@Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
-	@TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-14679")
-	public void pas22172_ReconcilePUEndorsementAFRD(@Optional("AZ") @SuppressWarnings("unused") String state) {
+    @Parameters({"state"})
+    @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
+    @TestInfo(component = ComponentConstant.Sales.AUTO_SS, testCaseId = "PAS-14679")
+    public void pas22172_ReconcilePUEndorsementAFRD(@Optional("AZ") @SuppressWarnings("unused") String state) {
         pas22172_ReconcilePUEndorsementAFRD();
-	}
+    }
 
     /**
      * @author Kiruthika Rajendran
@@ -258,6 +229,7 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
     }
 
     //TODO - Check if this test case is required or not
+
     /**
      * @author Kiruthika Rajendran
      * @author Saranya Hariharan
@@ -265,18 +237,18 @@ public class TestOffLineClaims extends TestOfflineClaimsTemplate {
      * @name Test Offline STUB/Mock: validate permissive use indicator when driver/named insure is added
      * @scenario Test Steps:
      * 1. Create a quote with 2 drivers and named insured driver1  has the following activies
-     *        - Company and Customer input (Type other than Accident) - PU indicator do not show up
-     *        - Company and Customer input (Type as Accident) - - PU indicator shows up
-     *        - MVR claims - PU indicator do not show up
+     * - Company and Customer input (Type other than Accident) - PU indicator do not show up
+     * - Company and Customer input (Type as Accident) - - PU indicator shows up
+     * - MVR claims - PU indicator do not show up
      * 2. Bind the policy
      * 3. Initiate the first endorsement
      * 4. Validate the driver 1 named insure has following activties
-     *        - Company and Customer input (Type other than Accident) - PU indicator do not show up
-     *        - Company and Customer input (Type as Accident) - - PU indicator shows up
-     *        - MVR claims - PU indicator do not show up
+     * - Company and Customer input (Type other than Accident) - PU indicator do not show up
+     * - Company and Customer input (Type as Accident) - - PU indicator shows up
+     * - MVR claims - PU indicator do not show up
      * 5. Bind the endorsement
      * @details Clean Path. Expected Result is that Permissive Use Indicator on Driver Tab will not show up for non "claim" activity
-     *  // TODO: Once PU Indicator is TOGGLED ON for SS Auto , Assertions needs to be changed to verify the presence of PU Indicator for Activity Type "Accident".
+     * // TODO: Once PU Indicator is TOGGLED ON for SS Auto , Assertions needs to be changed to verify the presence of PU Indicator for Activity Type "Accident".
      */
     @Parameters({"state"})
     @Test(groups = {Groups.FUNCTIONAL, Groups.HIGH})
