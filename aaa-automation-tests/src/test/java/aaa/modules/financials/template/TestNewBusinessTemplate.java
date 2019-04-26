ae480ba81238573faa3da28616773f72d888c960
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2019-04-26 15:31:06 -0700

    fixes for PMT-21

--- aaa-automation-tests/src/test/java/aaa/modules/financials/template/TestNewBusinessTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/financials/template/TestNewBusinessTemplate.java
@@ -386,7 +386,7 @@ public class TestNewBusinessTemplate extends FinancialsBaseTest {
         validateCancellationTx(getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.PREMIUM,
                 BillingConstants.PaymentsAndOtherTransactionSubtypeReason.CANCELLATION), policyNumber, totalTaxesNB.add(totalTaxesEnd));
 
-        Dollar adjustmentAmt = getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.ADJUSTMENT, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.REALLOCATED_PAYMENT);
+        // Get fees for PMT-21 validations
         Dollar fees = new Dollar(0.00);
         if (getPolicyType().equals(PolicyType.AUTO_CA_CHOICE)) {
             fees = getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.FEE, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.CANCELLATION_FEE)
@@ -396,13 +396,14 @@ public class TestNewBusinessTemplate extends FinancialsBaseTest {
             fees = getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.FEE, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.CANCELLATION_FEE)
                     .add(getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.FEE, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.CA_FRAUD_ASSESSMENT_FEE));
         }
-        if (!getPolicyType().isAutoPolicy() && getPolicyType().isCaProduct() && !getPolicyType().equals(PolicyType.HOME_CA_HO4)) {
+        if (getPolicyType().equals(PolicyType.HOME_CA_DP3) || getPolicyType().equals(PolicyType.HOME_CA_HO3) || getPolicyType().equals(PolicyType.HOME_CA_HO6)) {
             fees = getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.FEE, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.CANCELLATION_FEE)
                     .add(getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.FEE, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.SEISMIC_SAFETY_FEE));
         }
 
         // PMT-21 validations
         if (getPolicyType().isCaProduct() && !getPolicyType().equals(PolicyType.HOME_CA_HO4)) {
+            Dollar adjustmentAmt = getBillingAmountByType(BillingConstants.PaymentsAndOtherTransactionType.ADJUSTMENT, BillingConstants.PaymentsAndOtherTransactionSubtypeReason.REALLOCATED_PAYMENT);
             Dollar totalFees = fees;
             assertSoftly(softly -> {
                 softly.assertThat(adjustmentAmt).isEqualTo(FinancialsSQL.getDebitsForAccountByPolicy(policyNumber, FinancialsSQL.TxType.OVERPAYMENT_REALLOCATION_ADJUSTMENT, "1001"));
