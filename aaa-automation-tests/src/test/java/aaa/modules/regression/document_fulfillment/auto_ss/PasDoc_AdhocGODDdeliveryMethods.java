d9d24252d6d294c63b69495371ff3f1725596928
Siamionau, Denis (C) <Denis.Siamionau@csaa.com>
2019-04-30 15:53:48 +0300

    PasDoc scenarios delivery methods implementation Tests fixed

--- aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/auto_ss/PasDoc_AdhocGODDdeliveryMethods.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/document_fulfillment/auto_ss/PasDoc_AdhocGODDdeliveryMethods.java
@@ -4,12 +4,14 @@ import aaa.common.enums.Constants;
 import aaa.common.pages.SearchPage;
 import aaa.helpers.constants.Groups;
 import aaa.helpers.docgen.impl.PasDocImpl;
+import aaa.helpers.xml.model.pasdoc.*;
 import aaa.main.enums.DocGenEnum;
 import aaa.main.enums.ProductConstants;
 import aaa.main.metadata.policy.AutoSSMetaData;
 import aaa.main.modules.policy.auto_ss.actiontabs.GenerateOnDemandDocumentActionTab;
 import aaa.main.pages.summary.PolicySummaryPage;
 import aaa.modules.policy.AutoSSBaseTest;
+import aaa.toolkit.webdriver.WebDriverHelper;
 import aaa.utils.StateList;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
@@ -17,7 +19,9 @@ import org.testng.annotations.Test;
 import toolkit.datax.TestData;
 import toolkit.verification.CustomSoftAssertions;
 import toolkit.verification.ETCSCoreSoftAssertions;
+import toolkit.webdriver.controls.RadioGroup;
 
+import static aaa.main.enums.DocGenEnum.Documents.AA11AZ;
 import static toolkit.verification.CustomAssertions.assertThat;
 
 public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
@@ -39,17 +43,17 @@ public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
 
         CustomSoftAssertions.assertSoftly(softly -> {
 
-            verifyOptionIsEnabled(softly, "eSignature", false, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Email", true, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Fax", false, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Central Print", false, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Local Print", true, true, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.E_SIGNATURE, false, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.EMAIL, true, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.FAX, false, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.CENTRAL_PRINT, false, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.LOCAL_PRINT, true, true, DocGenEnum.Documents.ACPUBI);
 
-            verifyOptionIsEnabled(softly, "eSignature", true, DocGenEnum.Documents.AA11AZ);
-            verifyOptionIsEnabled(softly, "Email", true, DocGenEnum.Documents.AA11AZ);
-            verifyOptionIsEnabled(softly, "Fax", false, DocGenEnum.Documents.AA11AZ);
-            verifyOptionIsEnabled(softly, "Central Print", true, DocGenEnum.Documents.AA11AZ);
-            verifyOptionIsEnabled(softly, "Local Print", true, true, DocGenEnum.Documents.AA11AZ);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.E_SIGNATURE, true, DocGenEnum.Documents.AA11AZ);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.EMAIL, true, DocGenEnum.Documents.AA11AZ);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.FAX, false, DocGenEnum.Documents.AA11AZ);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.CENTRAL_PRINT, true, DocGenEnum.Documents.AA11AZ);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.LOCAL_PRINT, true, true, DocGenEnum.Documents.AA11AZ);
         });
 
         documentActionTab.saveAndExit();
@@ -66,14 +70,14 @@ public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
         //policy2
         policy.copyPolicy(getPolicyTD("CopyFromPolicy", "TestData"));
         policy.policyDocGen().start();
-        verifyOptionIsEnabled(null, "eSignature", true, true, DocGenEnum.Documents.AA11AZ);
+        verifyOptionIsEnabled(null, DocGenEnum.DeliveryMethod.E_SIGNATURE, true, true, DocGenEnum.Documents.AA11AZ);
         documentActionTab.buttonCancel.click();
         //cancel policy2
         policy.cancel().perform(getPolicyTD("Cancellation", "TestData"));
         assertThat(PolicySummaryPage.labelPolicyStatus).hasValue(ProductConstants.PolicyStatus.POLICY_CANCELLED);
 
         policy.policyDocGen().start();
-        verifyOptionIsEnabled(null, "eSignature", true, false, DocGenEnum.Documents.AA11AZ);
+        verifyOptionIsEnabled(null, DocGenEnum.DeliveryMethod.E_SIGNATURE, true, false, DocGenEnum.Documents.AA11AZ);
         documentActionTab.buttonCancel.click();
 
         SearchPage.openPolicy(policy_declined);
@@ -86,7 +90,7 @@ public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
         log.info("PAS DOC: Policy created with #" + policy_declined);
         policy.policyDocGen().start();
 
-        verifyOptionIsEnabled(null, "eSignature", false, DocGenEnum.Documents.AA11AZ);
+        verifyOptionIsEnabled(null, DocGenEnum.DeliveryMethod.E_SIGNATURE, false, DocGenEnum.Documents.AA11AZ);
         documentActionTab.saveAndExit();
         //without verification delivery method - eSignature in policy3 (policy status - expired) - NOT APPLICABLE
     }
@@ -105,11 +109,11 @@ public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
 
 
         CustomSoftAssertions.assertSoftly(softly -> {
-            verifyOptionIsEnabled(softly, "eSignature", false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Fax", false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Central Print", false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Email", true, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
-            verifyOptionIsEnabled(softly, "Local Print", true, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.E_SIGNATURE, false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.FAX, false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.CENTRAL_PRINT, false, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.EMAIL, true, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
+            verifyOptionIsEnabled(softly, DocGenEnum.DeliveryMethod.LOCAL_PRINT, true, DocGenEnum.Documents.AAUBI, DocGenEnum.Documents.ACPUBI);
         });
         documentActionTab.saveAndExit();
     }
@@ -118,39 +122,75 @@ public class PasDoc_AdhocGODDdeliveryMethods extends AutoSSBaseTest {
     @StateList(states = Constants.States.AZ)
     @Test(groups = {Groups.DOCGEN, Groups.REGRESSION, Groups.HIGH})
     public void testScenario4(@Optional("") String state) {
-//        String content = RemoteHelper.get().getFileContent("/home/DocGen/pasdoc/outbound/AZSS952028460_1556182520435_4998987910801489950.xml");
-//        XmlHelper.xmlToModel(content, DocumentGenerationRequest.class, false);
-        PasDocImpl.verifyDocumentsGenerated("AZSS952028460", DocGenEnum.Documents.AA11AZ).getDistributionChannel();
-        /*mainApp().open();*/
 
-
-        System.out.println(" ");
-//        ;
-//        log.info("PAS DOC: Policy created with #" + PolicySummaryPage.getPolicyNumber());
-//        policy.policyDocGen().start();
-//        verifyOptionIsEnabled(null,"Email",true,DocGenEnum.Documents.AA11AZ);
-//        documentActionTab.generateDocuments(DocGenEnum.DeliveryMethod.CENTRAL_PRINT,  DocGenEnum.Documents.AA11AZ);
-//
-        //check distribution channel methods
+        mainApp().open();
+        createCustomerIndividual();
+        createPolicy();
+        verifyDistributionChannel(null, DocGenEnum.Documents.AA11AZ);
+        verifyDistributionChannel(DocGenEnum.DeliveryMethod.EMAIL, DocGenEnum.Documents.AA11AZ);
+        verifyDistributionChannel(DocGenEnum.DeliveryMethod.LOCAL_PRINT, DocGenEnum.Documents.AA11AZ);
+        verifyDistributionChannel(DocGenEnum.DeliveryMethod.CENTRAL_PRINT, DocGenEnum.Documents.AA11AZ);
+        verifyDistributionChannel(DocGenEnum.DeliveryMethod.E_SIGNATURE, DocGenEnum.Documents.AA11AZ);
     }
 
-    private void verifyOptionIsEnabled(ETCSCoreSoftAssertions softly, String radioBtnLabel, boolean unselectDocuments, boolean expectedValue, DocGenEnum.Documents... documents) {
-        if (!unselectDocuments) documentActionTab.selectDocuments(documents);
+    private void verifyOptionIsEnabled(ETCSCoreSoftAssertions softly, DocGenEnum.DeliveryMethod id, boolean unselectDocuments, boolean expectedValue, DocGenEnum.Documents... documents) {
         documentActionTab.selectDocuments(documents);
-
         if (softly != null) {
             softly.assertThat(documentActionTab.getAssetList().getAsset(AutoSSMetaData.GenerateOnDemandDocumentActionTab.DELIVERY_METHOD).
-                    getRadioButton(radioBtnLabel).isEnabled()).isEqualTo(expectedValue);
+                    getRadioButton(id.get()).isEnabled()).isEqualTo(expectedValue);
         } else {
             assertThat(documentActionTab.getAssetList().getAsset(AutoSSMetaData.GenerateOnDemandDocumentActionTab.DELIVERY_METHOD).
-                    getRadioButton(radioBtnLabel).isEnabled()).isEqualTo(expectedValue);
+                    getRadioButton(id.get()).isEnabled()).isEqualTo(expectedValue);
         }
-
-
         if (unselectDocuments) documentActionTab.unselectDocuments(documents);
     }
 
-    private void verifyOptionIsEnabled(ETCSCoreSoftAssertions softly, String radioBtnLabel, boolean expectedValue, DocGenEnum.Documents... documents) {
-        verifyOptionIsEnabled(softly, radioBtnLabel, false, expectedValue, documents);
+    private void verifyOptionIsEnabled(ETCSCoreSoftAssertions softly, DocGenEnum.DeliveryMethod deliveryMethod, boolean expectedValue, DocGenEnum.Documents... documents) {
+        verifyOptionIsEnabled(softly, deliveryMethod, false, expectedValue, documents);
+    }
+
+    private void verifyDistributionChannel(DocGenEnum.DeliveryMethod deliveryMethod, DocGenEnum.Documents... documents) {
+        ETCSCoreSoftAssertions softly = new ETCSCoreSoftAssertions();
+        policy.policyDocGen().start();
+        if (deliveryMethod != null) {
+            verifyOptionIsEnabled(softly, deliveryMethod, true, documents);
+            documentActionTab.getAssetList().getAsset("Delivery Method", RadioGroup.class)
+                    .setValue(deliveryMethod.get());
+            if (documentActionTab.textboxEmailAddress.isPresent()) {
+                documentActionTab.textboxEmailAddress.setValue("test@gmail.com");
+            } else if (documentActionTab.eSignatureEmail.isPresent()) {
+                documentActionTab.eSignatureOkBtn.click();
+                softly.assertThat(documentActionTab.eSignatureEmailError).isPresent();
+                documentActionTab.eSignatureEmail.setValue("test@gmail.com");
+                documentActionTab.eSignatureOkBtn.click();
+            }
+            documentActionTab.buttonOk.click();
+        } else {
+            documentActionTab.previewDocuments(null, AA11AZ);
+            WebDriverHelper.switchToDefault();
+        }
+        if (documentActionTab.buttonOk.isPresent()) documentActionTab.saveAndExit();
+        String distributionChannel = PasDocImpl.verifyDocumentsGenerated(PolicySummaryPage.getPolicyNumber(), documents)
+                .getDistributionChannel().get(0).getClass().getSimpleName();
+        if (deliveryMethod == null) {
+            softly.assertThat(distributionChannel).isEqualTo(EmailChannel.class.getSimpleName());
+        } else
+            switch (deliveryMethod) {
+                case EMAIL:
+                    softly.assertThat(distributionChannel).isEqualTo(EmailChannel.class.getSimpleName());
+                    break;
+                case LOCAL_PRINT:
+                    softly.assertThat(distributionChannel).isEqualTo(LocalPrintChannel.class.getSimpleName());
+                    break;
+                case CENTRAL_PRINT:
+                    softly.assertThat(distributionChannel).isEqualTo(CentralPrintChannel.class.getSimpleName());
+                    break;
+                case FAX:
+                    softly.assertThat(distributionChannel).isEqualTo(FaxChannel.class.getSimpleName());
+                    break;
+                case E_SIGNATURE:
+                    softly.assertThat(distributionChannel).isEqualTo(ESignatureChannel.class.getSimpleName());
+                    break;
+            }
     }
 }
