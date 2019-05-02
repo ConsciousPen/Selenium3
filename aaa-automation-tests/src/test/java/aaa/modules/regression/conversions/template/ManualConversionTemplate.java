c62ca64d312488444dfcbb4fc6c9666c4209fca7
Saltsevich, Tatsiana (C) <Tatsiana.Saltsevich@csaa.com>
2019-05-02 11:49:36 +0300

    manual conversion test fix

--- aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/template/ManualConversionTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/conversions/template/ManualConversionTemplate.java
@@ -41,7 +41,10 @@ public class ManualConversionTemplate extends PolicyBaseTest {
 		String policyNum = PolicySummaryPage.getPolicyNumber();
 
 		SearchPage.openPolicy(policyNum);
-		if (!getState().equals(Constants.States.MT) && !(getState().equals(Constants.States.WY) && !getPolicyType().isAutoPolicy())) {
+		if (!getState().equals(Constants.States.MT)
+				&& !(getState().equals(Constants.States.WY) && !getPolicyType().isAutoPolicy())
+				&& !getState().equals(Constants.States.MD))
+		{
 			new ProductRenewalsVerifier().setStatus(ProductConstants.PolicyStatus.PREMIUM_CALCULATED).verify(1);
 			TimeSetterUtil.getInstance().nextPhase(getTimePoints().getRenewOfferGenerationDate(effDate));
 			JobUtils.executeJob(Jobs.renewalOfferGenerationPart2);
