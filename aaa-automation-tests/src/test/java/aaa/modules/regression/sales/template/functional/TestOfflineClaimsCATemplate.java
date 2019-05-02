6c2096bc8b1985ebfa34b50e81c79b8f57592ef1
Rajendran, Kiruthika (C) <kiruthika.rajendran@csaa.com>
2019-05-02 13:47:07 -0700

    changes committed

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestOfflineClaimsCATemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestOfflineClaimsCATemplate.java
@@ -76,7 +76,6 @@ public class TestOfflineClaimsCATemplate extends CommonTemplateMethods {
     @SuppressWarnings("SpellCheckingInspection")
     public static final String SQL_UPDATE_PERMISSIVEUSE_DISPLAYVALUE = "UPDATE LOOKUPVALUE SET DISPLAYVALUE = 'TRUE' WHERE LOOKUPLIST_ID in (SELECT ID FROM LOOKUPLIST WHERE LOOKUPNAME = 'AAARolloutEligibilityLookup') and code = 'PermissiveUse' and PRODUCTCD = 'AAA_CSA'";
     public static final String SQL_UPDATE_PERMISSIVEUSE_DATEOFLOSS = "UPDATE LOOKUPVALUE SET DATEOFLOSS = '%s' WHERE LOOKUPLIST_ID in (SELECT ID FROM LOOKUPLIST WHERE LOOKUPNAME = 'AAARolloutEligibilityLookup') and code = 'PermissiveUse' and PRODUCTCD = 'AAA_CSA'";
-    public static final String SQL_UPDATE_PERMISSIVEUSE = "UPDATE LOOKUPVALUE SET DISPLAYVALUE = 'TRUE', DATEOFLOSS = '%s' WHERE LOOKUPLIST_ID in (SELECT ID FROM LOOKUPLIST WHERE LOOKUPNAME = 'AAARolloutEligibilityLookup') and code = 'PermissiveUse' and PRODUCTCD = 'AAA_CSA'";
     private static final String CLAIMS_URL = "https://claims-assignment-master.apps.prod.pdc.digital.csaa-insurance.aaa.com/pas-claims/v1"; //Post-Permissive Use
     public static final String SQL_REMOVE_RENEWALCLAIMRECEIVEASYNCJOB_BATCH_JOB_CONTROL_ENTRY = "DELETE FROM BATCH_JOB_CONTROL_ENTRY WHERE jobname='renewalClaimReceiveAsyncJob'";
     public static final String CLAIMS_MICROSERVICE_ENDPOINT = "select * from PROPERTYCONFIGURERENTITY where propertyname = 'aaaClaimsMicroService.microServiceUrl'";
