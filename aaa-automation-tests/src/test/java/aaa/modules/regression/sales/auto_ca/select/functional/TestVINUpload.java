9cc502b0eb61d36ce023c2dbdcde139d63c51018
Johns, Chris <christopher.johns@csaa.com>
2019-05-15 13:19:54 -0700

    --Minor edits

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/select/functional/TestVINUpload.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/auto_ca/select/functional/TestVINUpload.java
@@ -77,7 +77,9 @@ public class TestVINUpload extends TestVINUploadTemplate {
 
     /**
      * @author Lev Kazarnovskiy
+     * @author Chris Johns
      * PAS-4253 Restrict VIN Refresh by Vehicle Type
+     * PAS-27309 "Restrict VIN Refresh" should not apply when Select Quote Changes to Choice
      * See detailed steps in template file
      * {@link TestVINUploadTemplate#pas4253_restrictVehicleRefreshNB(String, String)}
      */
