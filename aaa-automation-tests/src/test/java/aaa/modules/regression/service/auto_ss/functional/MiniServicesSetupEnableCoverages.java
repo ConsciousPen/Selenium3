f453e4e6a96d76019311bb987f8e297faa5e82b3
Strazds, Maris (C) <maris.strazds@csaa.com>
2019-06-13 15:23:43 -0700

    PAS-28573 PAS-28572 Added precondition

---
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/service/auto_ss/functional/MiniServicesSetupEnableCoverages.java
@@ -0,0 +1,21 @@
+package aaa.modules.regression.service.auto_ss.functional;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import aaa.common.enums.Constants;
+import aaa.helpers.constants.Groups;
+import aaa.helpers.listeners.AaaTestListener;
+
+@Listeners({AaaTestListener.class})
+public class MiniServicesSetupEnableCoverages extends MiniServicesSetup {
+
+	@Test(description = "Enabling 'canChange' for UM for AZ on test environment", groups = {Groups.FUNCTIONAL, Groups.PRECONDITION})
+	public static void enableUMForAZ() {
+		enableCoverageForState(Constants.States.AZ, "UM");
+	}
+
+	@Test(description = "Enabling 'canChange' for UIM for AZ on test environment", groups = {Groups.FUNCTIONAL, Groups.PRECONDITION})
+	public static void enableUIMForAZ() {
+		enableCoverageForState(Constants.States.AZ, "UIM");
+	}
+}
