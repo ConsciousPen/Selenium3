1eed723a5b7ceec1f6d1320d3dd7ae1a287b95ed
Johns, Chris <christopher.johns@csaa.com>
2019-06-25 13:11:24 -0700

    --Updated test flow of pas4253 --Corrected data yml for pas27908 - select

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestVINUploadTemplate.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/template/functional/TestVINUploadTemplate.java
@@ -436,8 +436,10 @@ public class TestVINUploadTemplate extends CommonTemplateMethods {
 		});
 
 		//Navigate to the P&C Page and grab the comp and coll symbols on the VRD:
-		NavigationPage.toViewTab(NavigationEnum.AutoSSTab.PREMIUM_AND_COVERAGES.get());
-		premiumAndCoveragesTab.calculatePremium();
+		NavigationPage.toViewTab(NavigationEnum.AutoCaTab.ASSIGNMENT.get());
+		policy.getDefaultView().fillFromTo(testData, AssignmentTab.class, PremiumAndCoveragesTab.class, true);
+//		NavigationPage.toViewTab(NavigationEnum.AutoCaTab.PREMIUM_AND_COVERAGES.get());
+//		premiumAndCoveragesTab.calculatePremium();
 		PremiumAndCoveragesTab.RatingDetailsView.open();
 
 		//PAS-27309: Grab CA Select Symbols
