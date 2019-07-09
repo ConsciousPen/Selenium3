3d81f5c78c53c4964c59ead3bc8aa7bda166a216
Carpenter, Joshua <Josh.Carpenter@csaa.com>
2019-07-09 08:41:50 -0700

    fixed import statements due to ISTF merge

--- aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestCovADoesNotRevertCovCD.java
+++ aaa-automation-tests/src/test/java/aaa/modules/regression/sales/home_ca/ho6/functional/TestCovADoesNotRevertCovCD.java
@@ -3,7 +3,7 @@ package aaa.modules.regression.sales.home_ca.ho6.functional;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import com.exigen.ipb.etcsa.utils.Dollar;
+import com.exigen.ipb.eisa.utils.Dollar;
 import aaa.common.enums.Constants;
 import aaa.common.enums.NavigationEnum;
 import aaa.common.pages.NavigationPage;
